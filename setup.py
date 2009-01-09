2251d0bd3766fc6fd43a6df14275d0bb23922902
Damien Elmes <git@ichi2.net>
2009-01-09 10:18:44 +0900

    add chinese package too

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='anki',
       author_email='anki@ichi2.net',
       url='http://ichi2.net/anki/index.html',
       license='GPLv3',
-      packages=["anki", "anki.features", "anki.importing"],
+      packages=["anki", "anki.features", "anki.features.chinese", "anki.importing"],
       package_data={'anki':
                     ['samples/*','locale/*/*/*']},
       include_package_data=True,
