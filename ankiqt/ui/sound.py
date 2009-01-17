7f8c18ce352075dc842d9c18f540108cd0a23160
Damien Elmes <git@ichi2.net>
2009-01-17 15:33:39 +0900

    mention audio requirements, save recorder location, tweaks

--- ankiqt/ui/sound.py
+++ ankiqt/ui/sound.py
@@ -6,21 +6,32 @@ from PyQt4.QtCore import *
 
 import time
 from anki.sound import Recorder, play
+from ankiqt.ui.utils import saveGeom, restoreGeom
 
 def getAudio(parent, string=""):
     "Record and return filename"
     # record first
     r = Recorder()
     mb = QMessageBox(parent)
+    restoreGeom(mb, "audioRecorder")
+    mb.setWindowTitle("Anki")
+    mb.setIconPixmap(QPixmap(":/icons/media-record.png"))
     but = QPushButton(_("  Stop"))
     but.setIcon(QIcon(":/icons/media-playback-stop.png"))
-    but.setIconSize(QSize(32, 32))
+    #but.setIconSize(QSize(32, 32))
     mb.addButton(but, QMessageBox.RejectRole)
     mb.show()
-    mb.setText("Recording..")
-    QApplication.instance().processEvents()
+    t = time.time()
     r.start()
-    mb.exec_()
+    QApplication.instance().processEvents()
+    while not mb.clickedButton():
+        txt =_("Recording...<br>Time: %0.1f")
+        mb.setText(txt % (time.time() - t))
+        QApplication.instance().processEvents()
+    # ensure at least a second captured
+    saveGeom(mb, "audioRecorder")
+    while time.time() - t < 1:
+        time.sleep(0.1)
     r.stop()
     # process
     r.postprocess()
