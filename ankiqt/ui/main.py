f8c80dc03db486bce25f9f9f7bbce2e6b51d8701
Damien Elmes <git@ichi2.net>
2009-01-22 04:10:25 +0900

    only compare deck paths if path available

--- ankiqt/ui/main.py
+++ ankiqt/ui/main.py
@@ -853,8 +853,9 @@ To upgrade an old deck, download Anki 0.9.8.7."""))
             return
         if not file.lower().endswith(".anki"):
             file += ".anki"
-        if os.path.abspath(file) == os.path.abspath(self.deck.path):
-            return self.onSave()
+        if self.deck.path:
+            if os.path.abspath(file) == os.path.abspath(self.deck.path):
+                return self.onSave()
         if os.path.exists(file):
             # check for existence after extension
             if not ui.utils.askUser(
