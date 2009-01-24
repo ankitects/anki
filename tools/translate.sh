bf77ed1db7c443a2b588fd193dcd198a1f298e20
Damien Elmes <git@ichi2.net>
2009-01-24 16:03:06 +0900

    update chinese translations, translate script

--- tools/translate.sh
+++ tools/translate.sh
@@ -3,12 +3,14 @@
 # update translation files
 #
 
-if [ ! -d "locale" ]
+if [ ! -d "anki" ]
 then
-    echo "Please run this from the anki module directory"
+    echo "Please run this from the anki project root directory"
     exit
 fi
 
+oldpwd=$(pwd)
+cd anki
 allPyFiles=libanki.files
 echo "Generating translations.."
 for i in *.py features/*.py
@@ -28,3 +30,4 @@ do
     msgfmt $file --output-file=$outfile
 done
 rm $allPyFiles
+cd $oldpwd
