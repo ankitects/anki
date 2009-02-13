1915fb053a96c7bd9f8338aef21f8d0f1c5dc62a
Damien Elmes <git@ichi2.net>
2009-02-14 00:25:18 +0900

    after save&edit, refresh

--- ankiqt/ui/main.py
+++ ankiqt/ui/main.py
@@ -284,7 +284,7 @@ Please do not file a bug report with Anki.<br><br>""")
         elif state == "saveEdit":
             self.editor.saveFieldsNow()
             self.mainWin.buttonStack.show()
-            self.deck.s.flush()
+            self.deck.refresh()
             return self.moveToState("auto")
         elif state == "studyScreen":
             self.currentCard = None
