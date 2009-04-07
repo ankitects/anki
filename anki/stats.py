8d9c1cf191eb4d7b871ff31373bd48bdce2f6abb
Susanna Björverud <Susanna.Bjorverud@telia.com>
2009-04-07 11:14:10 +0200

    Adding html comments to strings to allow them to be translated differently

--- anki/stats.py
+++ anki/stats.py
@@ -332,18 +332,18 @@ class DeckStats(object):
         html += _("Total number of facts:") + " <b>%d</b><br><br>" % d.factCount
 
         html += "<b>" + _("Card counts") + "</b><br>"
-        html += _("Mature cards:") + " <b>%(old)d</b> (%(oldP)s)<br>" % {
+        html += _("Mature cards: <!--card count-->") + " <b>%(old)d</b> (%(oldP)s)<br>" % {
                 'old': stats['old'], 'oldP' : fmtPerc(stats['oldP'])}
-        html += _("Young cards:") + " <b>%(young)d</b> (%(youngP)s)<br>" % {
+        html += _("Young cards: <!--card count-->") + " <b>%(young)d</b> (%(youngP)s)<br>" % {
                 'young': stats['young'], 'youngP' : fmtPerc(stats['youngP'])}
         html += _("Unseen cards:") + " <b>%(new)d</b> (%(newP)s)<br><br>" % {
                 'new': stats['new'], 'newP' : fmtPerc(stats['newP'])}
         html += "<b>" + _("Correct answers") + "</b><br>"
-        html += _("Mature cards:") + " <b>" + fmtPerc(stats['gMatureYes%']) + (
+        html += _("Mature cards: <!--correct answers-->") + " <b>" + fmtPerc(stats['gMatureYes%']) + (
                 "</b> " + _("%(partOf)d of %(totalSum)d") % {
                 'partOf' : stats['gMatureYes'],
                 'totalSum' : stats['gMatureTotal'] } + "<br>")
-        html += _("Young cards:")  + " <b>" + fmtPerc(stats['gYoungYes%']) + (
+        html += _("Young cards: <!--correct answers-->")  + " <b>" + fmtPerc(stats['gYoungYes%']) + (
                 "</b> " + _("%(partOf)d of %(totalSum)d") % {
                 'partOf' : stats['gYoungYes'],
                 'totalSum' : stats['gYoungTotal'] } + "<br>")
