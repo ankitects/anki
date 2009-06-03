91afe651b3170e1574b1d730173932a346863b45
Damien Elmes <git@ichi2.net>
2009-06-04 06:59:07 +0900

    randomize after .anki import

--- anki/importing/anki10.py
+++ anki/importing/anki10.py
@@ -13,6 +13,7 @@ from anki.importing import Importer
 from anki.sync import SyncClient, SyncServer, BulkMediaSyncer
 from anki.lang import _
 from anki.utils import ids2str
+from anki.deck import NEW_CARDS_RANDOM
 import time
 
 class Anki10Importer(Importer):
@@ -21,7 +22,11 @@ class Anki10Importer(Importer):
 
     def doImport(self):
         "Import."
-        self.deck.startProgress(4)
+        random = self.deck.newCardOrder == NEW_CARDS_RANDOM
+        num = 4
+        if random:
+            num += 1
+        self.deck.startProgress(num)
         self.deck.updateProgress(_("Importing..."))
         src = DeckStorage.Deck(self.file)
         client = SyncClient(self.deck)
@@ -75,6 +80,10 @@ class Anki10Importer(Importer):
         self.total = len(res['added-facts']['facts'])
         src.s.rollback()
         src.engine.dispose()
+        # randomize?
+        if random:
+            self.deck.updateProgress()
+            self.deck.randomizeNewCards([x[0] for x in res['added-cards']])
         self.deck.flushMod()
         self.deck.finishProgress()
 
