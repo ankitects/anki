b014b532566d09cc37c95dbd088a6f80a7ec4ef6
Damien Elmes <git@ichi2.net>
2009-06-13 04:13:35 +0900

    include platform on mac setup

--- mac/setup.py
+++ mac/setup.py
@@ -63,7 +63,7 @@ OPTIONS = {
 	'iconfile': 'ankiqt/mac/anki.icns',
     "includes": ["sip", "cgi", "encodings", "encodings.utf_8",
                  "encodings.shift_jis", "_multibytecodec",
-                 "PyQt4.QtNetwork"],
+                 "PyQt4.QtNetwork", "platform"],
     'packages': ["sqlalchemy", "pysqlite2", "simplejson"],
     'excludes': ['_gtkagg', '_tkagg', "_wxagg",
                  "wx", "_wx",
