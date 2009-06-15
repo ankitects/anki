5ab8e9fcf25dfa9433d384b8602ee6f60beba0e9
Damien Elmes <git@ichi2.net>
2009-06-16 03:22:46 +0900

    clarify leech wording

--- ankiqt/ui/main.py
+++ ankiqt/ui/main.py
@@ -450,7 +450,8 @@ Please do not file a bug report with Anki.<br>""")
             self.deck.updatePriority(card)
         self.deck.refresh()
         self.setNotice(_("""\
-%s... is a <a href="http://ichi2.net/anki/wiki/Leeches">leech</a>.""") %
+%s... is a <a href="http://ichi2.net/anki/wiki/Leeches">leech</a>
+and has been suspended.""") %
                        stripHTML(self.currentCard.question)[0:30])
 
     def startRefreshTimer(self):
