285bf5a9a09e0386cbd2098450eab1bcb958ec36
Damien Elmes <git@ichi2.net>
2009-06-18 03:14:22 +0900

    add search for inactive

--- anki/deck.py
+++ anki/deck.py
@@ -1871,6 +1871,9 @@ cardTags.tagId in %s""" % ids2str(ids)
                 elif token == "suspended":
                     qquery += ("select id from cards where "
                                "priority = -3")
+                elif token == "inactive":
+                    qquery += ("select id from cards where "
+                               "priority = 0")
                 else: # due
                     qquery += ("select id from cards where "
                                "type in (0,1) and isDue = 1")
