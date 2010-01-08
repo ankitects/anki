aa731470ec946e7c18852b6c22419d5a2968c0d1
Damien Elmes <git@ichi2.net>
2010-01-08 23:35:51 +0900

    fix segfault on qt4.6 by storing index in local var

--- ankiqt/ui/cardlist.py
+++ ankiqt/ui/cardlist.py
@@ -185,8 +185,10 @@ interval, factor, noCount, priority, (select tags from facts where
 facts.id = cards.factId), (select created from facts where
 facts.id = cards.factId) from cards where id = :id""",
                                                         id=self.cards[index.row()][0])
+            # storing this in a local var first avoids a segfault
+            end = self.index(idx, 1)
             self.emit(SIGNAL("dataChanged(QModelIndex,QModelIndex)"),
-                      index, self.index(index.row(), 1))
+                      index, end)
         except:
             # called after search changed
             pass
