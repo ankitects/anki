a3b3e7312a2f99ac98762c8d16a6dbf884bd2157
Damien Elmes <git@ichi2.net>
2010-01-21 03:02:43 +0900

    this time webkit breaks bold. sigh

--- ankiqt/ui/utils.py
+++ ankiqt/ui/utils.py
@@ -200,9 +200,9 @@ def restoreHeader(widget, key):
 def mungeQA(deck, txt):
     txt = renderLatex(deck, txt)
     txt = stripSounds(txt)
-    # webkit currently doesn't handle underline
-    txt = txt.replace("text-decoration: underline;",
-                      "border-bottom: 1px solid #000;")
+    # webkit currently doesn't handle bold properly
+    txt = txt.replace("font-weight: 600;",
+                      "font-weight: 900;")
     return txt
 
 def getBase(deck, card):
