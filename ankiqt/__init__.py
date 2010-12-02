43e5f088059ffd5ab1fe07c8190b569c2f5f4205
Damien Elmes <git@ichi2.net>
2010-12-02 09:54:49 +0900

    bump version

--- ankiqt/__init__.py
+++ ankiqt/__init__.py
@@ -6,7 +6,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 appName="Anki"
-appVersion="1.1.4"
+appVersion="1.1.5"
 appWebsite="http://ichi2.net/anki/"
 appWiki="http://ichi2.net/anki/wiki/"
 appHelpSite="http://ichi2.net/anki/wiki/AnkiWiki"
