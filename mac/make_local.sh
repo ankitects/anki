25a61dcebcbb685c6c95382de849d769b0ceeac1
Damien Elmes <git@ichi2.net>
2010-12-12 22:48:40 +0900

    grab mac plugins from new location

--- mac/make_local.sh
+++ mac/make_local.sh
@@ -13,6 +13,7 @@ find . -name '*.pyc' -exec rm {} \;
 echo "adding image formats..."
 rm -rf ankiqt/imageformats
 mkdir ankiqt/imageformats
-cp -Rvf /Developer/Applications/Qt/plugins/imageformats/libq{gif,jpeg,svg,tiff}* ankiqt/imageformats
+cp -Rvf /usr/local/Trolltech/Qt-4.7.1/plugins/imageformats/libq{gif,jpeg,svg,tiff}.dylib ankiqt/imageformats
+#cp -Rvf /Developer/Applications/Qt/plugins/imageformats/libq{gif,jpeg,svg,tiff}* ankiqt/imageformats
 echo "building..."
 PYTHONPATH=ankiqt:libanki $PYTHON ankiqt/mac/setup.py bdist_dmg
