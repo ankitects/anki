431fd2585e2310efdcde50929d7e45f29a5928cc
Damien Elmes <git@ichi2.net>
2011-04-21 10:59:32 +0900

    when adding cards, use rand(1, maxfid)
    this means that content added earlier has a higher chance of appearing, but it
    makes it consistent with sortCards(), and the user can sort manually if need
    be

--- anki/deck.py
+++ anki/deck.py
@@ -233,7 +233,7 @@ qconf=?, conf=?, data=?""",
         fact.flush()
         # randomize?
         if self.randomNew():
-            due = random.randrange(1, 1000000)
+            due = random.randrange(1, fact.id)
         else:
             due = fact.id
         # add cards
@@ -289,7 +289,7 @@ select id from facts where id not in (select distinct fid from cards)""")
             # if this fact has existing new cards, use their due time
             due = self.db.scalar(
                 "select due from cards where fid = ? and queue = 0", fact.id)
-            due = due or random.randrange(1, 1000000)
+            due = due or random.randrange(1, self.conf['nextFid'])
         else:
             due = fact.id
         for template in self.findTemplates(fact, checkActive=False):
