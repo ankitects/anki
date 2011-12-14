47a170898af970b1bb866e9092545636bc5818c0
Damien Elmes <git@ichi2.net>
2011-12-14 10:20:56 +0900

    set default deck on upgrade; append old deck name to model

--- anki/importing/anki2.py
+++ anki/importing/anki2.py
@@ -117,6 +117,11 @@ class Anki2Importer(Importer):
         # if it doesn't exist, we'll copy it over, preserving id
         if not self.dst.models.have(mid):
             self.dst.models.update(src)
+            # if we're importing with a prefix, make the model default to it
+            if self.deckPrefix:
+                src['did'] = self.dst.decks.current()['id']
+                # and give it a unique name
+                src['name'] += " (%s)" % self.deckPrefix
             # make sure to bump usn
             self.dst.models.save(src)
             self._modelMap[mid] = mid
