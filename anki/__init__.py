0317e63cfbb7e3aa88f02dd0b45cb59f4a956cb9
Damien Elmes <git@ichi2.net>
2012-03-28 06:55:23 +0900

    remove init docstring; documented in addons.html now

--- anki/__init__.py
+++ anki/__init__.py
@@ -2,41 +2,6 @@
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-"""\
-Open a collection:
-
-    col = anki.Collection(path)
-
-Prepare scheduler, or reset scheduler after changes:
-
-    col.reset()
-
-Get a due card:
-
-    card = col.sched.getCard()
-    if not card:
-        # current deck is finished
-
-Show the card:
-
-    print card.q(), card.a()
-
-Answer the card:
-
-    col.sched.answerCard(card, ease)
-
-Edit the card:
-
-    note = card.note()
-    for (name, value) in note.items():
-        note[name] = value + " new"
-    note.flush()
-
-Save & close:
-
-    col.close()
-"""
-
 import sys, simplejson as _simplejson, httplib2 as _httplib2
 if sys.version_info[0] > 2:
     raise Exception("Anki should be run with Python 2")
