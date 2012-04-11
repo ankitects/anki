be35f333018f8afd17728bdd1e9a51253f134974
Damien Elmes <git@ichi2.net>
2012-04-12 00:57:55 +0900

    bump version

--- aqt/__init__.py
+++ aqt/__init__.py
@@ -4,7 +4,7 @@
 import os, sys
 from aqt.qt import *
 
-appVersion="2.0-beta2"
+appVersion="2.0-beta3"
 appWebsite="http://ankisrs.net/"
 appHelpSite="http://ankisrs.net/docs/dev/manual.html"
 appChanges="http://ankisrs.net/docs/dev/changes.html"
