69355be953a8d3a0fc502509523e305f07f1fcce
Damien Elmes <git@ichi2.net>
2012-04-22 05:30:46 +0900

    remove plugin enabling/disabling

--- aqt/addons.py
+++ aqt/addons.py
@@ -20,84 +20,30 @@ class AddonManager(object):
         self.mw = mw
         f = self.mw.form; s = SIGNAL("triggered()")
         self.mw.connect(f.actionOpenPluginFolder, s, self.onOpenAddonFolder)
-        self.mw.connect(f.actionEnableAllPlugins, s, self.onEnableAllAddons)
-        self.mw.connect(f.actionDisableAllPlugins, s, self.onDisableAllAddons)
         self.mw.connect(f.actionDownloadSharedPlugin, s, self.onGetAddons)
         if isWin:
             self.clearAddonCache()
         sys.path.insert(0, self.addonsFolder())
         self.loadAddons()
 
+    def files(self):
+        return [f for f in os.listdir(self.addonsFolder())
+                if f.endswith(".py")]
+
     def loadAddons(self):
-        on, off = self.files()
-        for file in on:
+        for file in self.files():
             try:
                 __import__(file.replace(".py", ""))
             except:
                 traceback.print_exc()
-        self.rebuildAddonsMenu()
 
     # Menus
     ######################################################################
-
-    def rebuildAddonsMenu(self):
-        if getattr(self, "addonActions", None) is None:
-            self.addonActions = []
-        for action in self.addonActions:
-            self.mw.form.menuStartup.removeAction(action)
-        self.addonActions = []
-        on, off = self.files()
-        def addObjs(l, enabled):
-            l.sort()
-            for file in l:
-                p = re.sub("\.py(\.off)?", "", file)
-                a = QAction(p, self.mw)
-                a.setCheckable(True)
-                a.setChecked(enabled)
-                self.mw.connect(a, SIGNAL("triggered()"),
-                                lambda f=file: self.toggleAddon(f))
-                self.mw.form.menuStartup.addAction(a)
-                self.addonActions.append(a)
-        addObjs(on, True)
-        addObjs(off, False)
 
     def onOpenAddonFolder(self, path=None):
         if path is None:
             path = self.addonsFolder()
         openFolder(path)
-
-    # Enabled/disabled list
-    ######################################################################
-
-    def files(self):
-        on = []
-        off = []
-        for f in os.listdir(self.addonsFolder()):
-            if not f.endswith(".py"):
-                continue
-            if f in self.mw.pm.meta['disabledAddons']:
-                off.append(f)
-            else:
-                on.append(f)
-        return on, off
-
-    def onEnableAllAddons(self):
-        self.mw.pm.meta['disabledAddons'] = []
-        self.mw.pm.save()
-        self.rebuildAddonsMenu()
-
-    def onDisableAllAddons(self):
-        on, off = self.files()
-        self.mw.pm.meta['disabledAddons'] = on + off
-        self.rebuildAddonsMenu()
-
-    def toggleAddon(self, file):
-        if file in self.mw.pm.meta['disabledAddons']:
-            self.mw.pm.meta['disabledAddons'].remove(file)
-        else:
-            self.mw.pm.meta['disabledAddons'].append(file)
-        self.mw.pm.save()
-        self.rebuildAddonsMenu()
 
     # Tools
     ######################################################################
