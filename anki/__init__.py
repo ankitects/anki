581830840cdc0404fb900969dfcaed17c7fa0807
Damien Elmes <git@ichi2.net>
2012-04-28 17:47:41 +0900

    we require python2.6 now

--- anki/__init__.py
+++ anki/__init__.py
@@ -7,8 +7,8 @@ import sys as _sys, httplib2 as _httplib2, \
 
 if _sys.version_info[0] > 2:
     raise Exception("Anki should be run with Python 2")
-elif _sys.version_info[1] < 5:
-    raise Exception("Anki requires Python 2.5+")
+elif _sys.version_info[1] < 6:
+    raise Exception("Anki requires Python 2.6+")
 elif _sys.getfilesystemencoding().lower() in ("ascii", "ansi_x3.4-1968"):
     raise Exception("Anki requires a UTF-8 locale.")
 elif _httplib2.__version__ < "0.7.0":
