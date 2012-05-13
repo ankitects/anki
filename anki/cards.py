b291b9f2c005e81e37a9c7c32f329a92fb660c92
Damien Elmes <git@ichi2.net>
2012-05-14 06:22:04 +0900

    show original deck name if in cram deck

--- anki/cards.py
+++ anki/cards.py
@@ -134,8 +134,8 @@ lapses=?, left=?, odue=?, odid=?, did=? where id = ?""",
     def _getQA(self, reload=False):
         if not self._qa or reload:
             f = self.note(reload); m = self.model()
-            data = [self.id, f.id, m['id'], self.did, self.ord, f.stringTags(),
-                    f.joinedFields()]
+            data = [self.id, f.id, m['id'], self.did, self.odid or self.ord,
+                    f.stringTags(), f.joinedFields()]
             self._qa = self.col._renderQA(data)
         return self._qa
 
