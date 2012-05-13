21d61be4c04b665c3fb240c8be332a40789e2928
Damien Elmes <git@ichi2.net>
2012-05-14 07:12:25 +0900

    bump version

--- aqt/__init__.py
+++ aqt/__init__.py
@@ -6,7 +6,7 @@ from aqt.qt import *
 import locale, gettext
 import anki.lang
 
-appVersion="2.0-beta9"
+appVersion="2.0-beta10"
 appWebsite="http://ankisrs.net/"
 appHelpSite="http://ankisrs.net/docs/dev/manual.html"
 appChanges="http://ankisrs.net/docs/dev/changes.html"
