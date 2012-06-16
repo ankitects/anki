140cc82a4329137452f4cd886ae64f7853a89854
Damien Elmes <git@ichi2.net>
2012-06-17 01:00:56 +0900

    fix deck rename exception

--- anki/errors.py
+++ anki/errors.py
@@ -16,4 +16,4 @@ class DeckRenameError(Exception):
     def __init__(self, description):
         self.description = description
     def __str__(self):
-        return "Couldn't rename deck: " + description
+        return "Couldn't rename deck: " + self.description
