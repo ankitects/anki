129d27b9f3374962551681f7784dbd8ed7529b0d
Damien Elmes <git@ichi2.net>
2012-09-13 09:43:29 +0900

    update about

--- aqt/about.py
+++ aqt/about.py
@@ -17,8 +17,8 @@ system. It's free and open source.")
     abouttext += (_("<a href='%s'>Visit website</a>") % aqt.appWebsite) + \
 "</span>"
     abouttext += '<p>' + _("Written by Damien Elmes, with patches, translation,\
- testing and design from:<p>%(cont)s") % {'cont': u"""Aaron Harsh, Alex
-Fraser, Andreas Klauer, Andrew Wright, Bernhard Ibertsberger, Charlene Barina,
+ testing and design from:<p>%(cont)s") % {'cont': u"""Aaron Harsh, Ádám Szegi,
+Alex Fraser, Andreas Klauer, Andrew Wright, Bernhard Ibertsberger, Charlene Barina,
 Christian Krause, Christian Rusche, David Smith, Dave Druelinger, Dotan Cohen,
 Emilio Wuerges, Emmanuel Jarri, Frank Harper, Gregor Skumavc, H. Mijail,
 Ian Lewis, Immanuel Asmus, Iroiro, Jarvik7,
