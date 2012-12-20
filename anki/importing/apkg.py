6e52331699f819fca998547b41506bdb07fce48d
Damien Elmes <git@ichi2.net>
2012-12-20 20:53:00 +0900

    fix importing of latex cache files

--- anki/importing/apkg.py
+++ anki/importing/apkg.py
@@ -24,7 +24,7 @@ class AnkiPackageImporter(Anki2Importer):
         Anki2Importer.run(self)
         # import static media
         for file, c in self.nameToNum.items():
-            if not file.startswith("_"):
+            if not file.startswith("_") and not file.startswith("latex-"):
                 continue
             path = os.path.join(self.col.media.dir(), file)
             if not os.path.exists(path):
