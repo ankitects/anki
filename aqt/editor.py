1717e56831e0d09382ad64a1afcd116135a07c8f
Damien Elmes <git@ichi2.net>
2013-01-08 11:01:32 +0900

    disable editor buttons until field focused

--- aqt/editor.py
+++ aqt/editor.py
@@ -536,6 +536,7 @@ class Editor(object):
         "Make NOTE the current note."
         self.note = note
         self.currentField = 0
+        self.disableButtons()
         # change timer
         if self.note:
             self.web.setHtml(_html % (
