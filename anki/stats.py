9642ce933780701069e43098c515d2b63a66891b
Damien Elmes <git@ichi2.net>
2013-02-25 16:13:50 +0900

    fixed off by one err in collection start date calc

--- anki/stats.py
+++ anki/stats.py
@@ -313,7 +313,7 @@ group by day order by day""" % (self._limit(), lim),
                 period = 1
             else:
                 period = max(
-                    1, (self.col.sched.dayCutoff - (t/1000)) / 86400)
+                    1, 1+((self.col.sched.dayCutoff - (t/1000)) / 86400))
         i = []
         self._line(i, _("Days studied"),
                    _("<b>%(pct)d%%</b> (%(x)s of %(y)s)") % dict(
