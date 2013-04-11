ankitects/anki

commit 5e8f45f7e53ddeedd9c243cdfc6eac7568f2f45c

don't play audio twice when changing templates (#472)
- setting current tab index triggered the function, so we don't need to call
  it manually
- no need to set index after adding card either, as it defaults to the most
  recently added tab

aqt/clayout.py
@@ -52,7 +52,6 @@ class CardLayout(QDialog):
         self.redrawing = True
         self.updateTabs()
         self.redrawing = False
-        self.selectCard(self.ord)
 
     def setupTabs(self):
         c = self.connect
@@ -66,7 +65,7 @@ class CardLayout(QDialog):
             add.setToolTip(_("Add new card"))
             c(add, SIGNAL("clicked()"), self.onAddCard)
             self.tabs.setCornerWidget(add)
-        c(self.tabs, SIGNAL("currentChanged(int)"), self.selectCard)
+        c(self.tabs, SIGNAL("currentChanged(int)"), self.onCardSelected)
         c(self.tabs, SIGNAL("tabCloseRequested(int)"), self.onRemoveTab)
 
     def updateTabs(self):
@@ -176,6 +175,9 @@ Please create a new card type first."""))
     ##########################################################################
 
     def selectCard(self, idx):
+        self.tabs.setCurrentIndex(idx)
+
+    def onCardSelected(self, idx):
         if self.redrawing:
             return
         self.ord = idx

commit 8fc4dcd50d4c0da4cc0001db2b72b5572a7a595e

fix bugs introduced in previous #472 fix

aqt/clayout.py
@@ -52,6 +52,10 @@ class CardLayout(QDialog):
         self.redrawing = True
         self.updateTabs()
         self.redrawing = False
+        idx = self.ord
+        if idx >= len(self.cards):
+            idx = len(self.cards) - 1
+        self.selectCard(idx)
 
     def setupTabs(self):
         c = self.connect
@@ -175,14 +179,15 @@ Please create a new card type first."""))
     ##########################################################################
 
     def selectCard(self, idx):
-        self.tabs.setCurrentIndex(idx)
+        if self.tabs.currentIndex() == idx:
+            # trigger a re-read
+            self.onCardSelected(idx)
+        else:
+            self.tabs.setCurrentIndex(idx)
 
     def onCardSelected(self, idx):
         if self.redrawing:
             return
-        self.ord = idx
-        if idx >= len(self.cards):
-            idx = len(self.cards) - 1
         self.card = self.cards[idx]
         self.tab = self.forms[idx]
         self.tabs.setCurrentIndex(idx)
@@ -289,8 +294,8 @@ Please create a new card type first."""))
         t['qfmt'] = "%s<br>\n%s" % (_("Edit to customize"), old['qfmt'])
         t['afmt'] = old['afmt']
         self.mm.addTemplate(self.model, t)
+        self.ord = len(self.cards)
         self.redraw()
-        self.selectCard(t['ord'])
 
     def onFlip(self):
         old = self.card.template()