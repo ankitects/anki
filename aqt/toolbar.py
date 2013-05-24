4d2ed10c94b4e2ebd4a78db3f378bf323c0b926a
Damien Elmes <git@ichi2.net>
2013-05-24 12:40:41 +0900

    fix toolbar alignment issues (#191)

--- aqt/toolbar.py
+++ aqt/toolbar.py
@@ -115,10 +115,17 @@ margin-top: 4px;
 font-weight: bold;
 }
 
-body {
+html {
+height: 100%;
 background: -webkit-gradient(linear, left top, left bottom,
   from(#ddd), to(#fff));
-margin: 0; padding: 0;
+margin:0; padding:0;
+}
+
+body {
+margin:0; padding:0;
+position:absolute;
+top:0;left:0;right:0;bottom:0;
 -webkit-user-select: none;
 border-bottom: 1px solid #aaa;
 }
