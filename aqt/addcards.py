19a48fec6a6873dca8985a16bef01696d1597120
Damien Elmes <git@ichi2.net>
2013-05-30 10:55:39 +0900

    Revert "remember tags when add cards window opened (#632)"
    This reverts commit ddc85b3a39b7f46485e8c99de9c3ec9838c60c0c.

--- aqt/addcards.py
+++ aqt/addcards.py
@@ -82,7 +82,6 @@ class AddCards(QDialog):
 
     def setupNewNote(self, set=True):
         f = self.mw.col.newNote()
-        f.tags = f.model()['tags']
         if set:
             self.editor.setNote(f, focus=True)
         return f
