e5bc1650f9989cdda2cba894cb2c28a1e7d38027
steveaw <swaring@ozemail.com.au>
2013-07-16 17:42:50 +1000

    Add hooks for context menus

--- aqt/reviewer.py
+++ aqt/reviewer.py
@@ -693,6 +693,7 @@ function showAnswer(txt) {
             a = m.addAction(label)
             a.setShortcut(QKeySequence(scut))
             a.connect(a, SIGNAL("triggered()"), func)
+        runHook("Reviewer.contextMenuEvent",self,m)
         m.exec_(QCursor.pos())
 
     def onOptions(self):
