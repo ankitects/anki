5dfe95aa67a77aa2b68f94cb04d5b93ec4fbfe3e
Damien Elmes <git@ichi2.net>
2013-11-04 23:03:33 +0900

    move debug logging into libanki
    we want to be able to log the initial automatic sync, which
    happens before the debug logging was set up in ankiqt
    
    also skip the flush, as it should eventually get written

--- anki/collection.py
+++ anki/collection.py
@@ -2,12 +2,15 @@
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import pprint
+import re
 import time
 import os
 import random
 import stat
 import datetime
 import copy
+import traceback
 
 from anki.lang import _, ngettext
 from anki.utils import ids2str, fieldChecksum, stripHTML, \
@@ -48,9 +51,12 @@ defaultConf = {
 # this is initialized by storage.Collection
 class _Collection(object):
 
+    debugLog = False
+
     def __init__(self, db, server=False):
         self.db = db
         self.path = db._path
+        self._openLog()
         self.log(self.path, anki.version)
         self.server = server
         self._lastSave = time.time()
@@ -773,4 +779,25 @@ and queue = 0""", intTime(), self.usn())
     ##########################################################################
 
     def log(self, *args, **kwargs):
-        runHook("log", args, kwargs)
+        if not self.debugLog:
+            return
+        def customRepr(x):
+            if isinstance(x, basestring):
+                return x
+            return pprint.pformat(x)
+        path, num, fn, y = traceback.extract_stack(
+            limit=2+kwargs.get("stack", 0))[0]
+        buf = u"[%s] %s:%s(): %s" % (intTime(), os.path.basename(path), fn,
+                                     ", ".join([customRepr(x) for x in args]))
+        self._logHnd.write(buf.encode("utf8") + "\n")
+        if os.environ.get("ANKIDEV"):
+            print buf
+
+    def _openLog(self):
+        if not self.debugLog:
+            return
+        lpath = re.sub("\.anki2$", ".log", self.path)
+        self._logHnd = open(lpath, "ab")
+
+    def _closeLog(self):
+        self._logHnd = None
