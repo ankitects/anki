1e1e1f6f959e13a19f3fb176857039aafebfb24d
Damien Elmes <git@ichi2.net>
2014-01-24 03:10:04 +0900

    catch broken pipe error

--- aqt/sync.py
+++ aqt/sync.py
@@ -170,7 +170,7 @@ AnkiWeb is too busy at the moment. Please try again in a few minutes.""")
         elif "10061" in err or "10013" in err or "10053" in err:
             return _(
                 "Antivirus or firewall software is preventing Anki from connecting to the internet.")
-        elif "10054" in err:
+        elif "10054" in err or "Broken pipe" in err:
             return _("Connection timed out. Either your internet connection is experiencing problems, or you have a very large file in your media folder.")
         elif "Unable to find the server" in err:
             return _(
