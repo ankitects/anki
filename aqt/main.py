28b2f8fe03ce51596951280e26bb1776c17da706
Damien Elmes <git@ichi2.net>
2014-04-16 01:43:33 +0900

    allow main webview to focus, which may fix shortcut issues
    some users reported plugins that define reviewer shortcuts broke

--- aqt/main.py
+++ aqt/main.py
@@ -512,7 +512,7 @@ title="%s">%s</button>''' % (
         self.toolbar = aqt.toolbar.Toolbar(self, tweb)
         self.toolbar.draw()
         # main area
-        self.web = aqt.webview.AnkiWebView(canCopy=True)
+        self.web = aqt.webview.AnkiWebView(canFocus=True)
         self.web.setObjectName("mainText")
         self.web.setFocusPolicy(Qt.WheelFocus)
         self.web.setMinimumWidth(400)
