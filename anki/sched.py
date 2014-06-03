ankitects/anki

commit bbc8f75d70b0898b42fb44243b1c73a37f9b468d

make sure OR clause doesn't allow suspended cards in filtered deck
fixes http://help.ankisrs.net/discussions/ankidesktop/6673-filter-decks-can-unsuspend-cards

anki/sched.py
@@ -954,7 +954,7 @@ select id from cards where did in %s and queue = 2 and due <= ? limit ?)"""
     def _fillDyn(self, deck):
         search, limit, order = deck['terms'][0]
         orderlimit = self._dynOrder(order, limit)
-        search += " -is:suspended -is:buried -deck:filtered"
+        search = "(%s) -is:suspended -is:buried -deck:filtered" % search
         try:
             ids = self.col.findCards(search, order=orderlimit)
         except:

commit 24e83abe22aefd145987a382fc53364a8bedb8cc

fix empty search in filtered deck
was broken by bbc8f75d70b0898b42fb44243b1c73a37f9b468d

anki/sched.py
@@ -954,7 +954,9 @@ select id from cards where did in %s and queue = 2 and due <= ? limit ?)"""
     def _fillDyn(self, deck):
         search, limit, order = deck['terms'][0]
         orderlimit = self._dynOrder(order, limit)
-        search = "(%s) -is:suspended -is:buried -deck:filtered" % search
+        if search.strip():
+            search = "(%s)" % search
+        search = "%s -is:suspended -is:buried -deck:filtered" % search
         try:
             ids = self.col.findCards(search, order=orderlimit)
         except: