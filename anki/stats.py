ankitects/anki

commit 46a38f843efd5ac37f1779ce9a24d30acd1b7035

fix rounding of cards/minute; use 1 decimal place

anki/stats.py
@@ -353,7 +353,11 @@ group by day order by day""" % (self._limit(), lim),
                 tot, period, unit))
         if total and tot:
             perMin = total / float(tot)
-            perMin = ngettext("%d card/minute", "%d cards/minute", perMin) % round(perMin)
+            perMin = round(perMin, 1)
+            perMin = ngettext("%d card/minute", "%.01f cards/minute", perMin) % perMin
+            # don't round down to zero
+            if float(perMin.split(' ')[0]) < 0.1:
+                perMin = ''.join(["<", _("%.01f cards/minute")]) % 0.1
             self._line(
                 i, _("Average answer time"),
                 _("%(a)0.1fs (%(b)s)") % dict(a=(tot*60)/total, b=perMin))

commit 0071ddfb897244c70d7ce84c37304a22cc2e3ecf

fix ngettext mistake in cards/minute stat
introduced in 46a38f843efd5ac37f1779ce9a24d30acd1b7035

anki/stats.py
@@ -354,7 +354,7 @@ group by day order by day""" % (self._limit(), lim),
         if total and tot:
             perMin = total / float(tot)
             perMin = round(perMin, 1)
-            perMin = ngettext("%d card/minute", "%.01f cards/minute", perMin) % perMin
+            perMin = _("%.01f cards/minute") % perMin
             # don't round down to zero
             if float(perMin.split(' ')[0]) < 0.1:
                 perMin = ''.join(["<", _("%.01f cards/minute")]) % 0.1