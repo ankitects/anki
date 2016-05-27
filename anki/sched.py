2b922b3055ed4685d566d901df61d354299e15b2
Dmitry Mikheev <ankitest@users.noreply.github.com>
2016-05-27 18:47:48 +0500

    PEP8

--- anki/sched.py
+++ anki/sched.py
@@ -927,7 +927,8 @@ select id from cards where did in %s and queue = 2 and due <= ? limit ?)"""
 
     def _updateRevIvl(self, card, ease):
         idealIvl = self._nextRevIvl(card, ease)
-        card.ivl = min(max(self._adjRevIvl(card, idealIvl), card.ivl+1), self._revConf(card)['maxIvl'])
+        card.ivl = min(max(self._adjRevIvl(card, idealIvl), card.ivl+1),
+                       self._revConf(card)['maxIvl'])
 
     def _adjRevIvl(self, card, idealIvl):
         if self._spreadRev:
