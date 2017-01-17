a44dc7f1742a5d7054539104086a570ee4404292
Damien Elmes <git@ichi2.net>
2017-01-17 13:39:38 +1000

    prevent backspace key from going back a page

--- aqt/webview.py
+++ aqt/webview.py
@@ -169,6 +169,23 @@ body { zoom: %f; %s }
 %s</style>
 <script>
 %s
+
+// prevent backspace key from going back a page
+document.addEventListener("keydown", function(evt) {
+    if (evt.keyCode != 8) {
+        return;
+    }
+    var isText = 0;
+    var nn = evt.target.nodeName;
+    if (nn == "INPUT" || nn == "TEXTAREA") {
+        isText = 1;
+    } else if (nn == "DIV" && evt.target.contentEditable) {
+        isText = 1;
+    }
+    if (!isText) {
+        evt.preventDefault();
+    }
+});
 </script>
 %s
 
