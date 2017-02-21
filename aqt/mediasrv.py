0f04744f7e828c12a69a5b7a263fc0392e319875
Damien Elmes <git@ichi2.net>
2017-02-21 13:27:29 +1000

    fix embedded fonts

--- aqt/mediasrv.py
+++ aqt/mediasrv.py
@@ -59,6 +59,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
             fs = os.fstat(f.fileno())
             self.send_header("Content-Length", str(fs[6]))
             self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.send_header("Access-Control-Allow-Origin", "*")
             self.end_headers()
             return f
         except:
