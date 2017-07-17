9df1947e41a5cbcb4c92deb610fe39c42649f906
Damien Elmes <git@ichi2.net>
2017-07-17 13:18:56 +1000

    require decorator

--- anki/hooks.py
+++ anki/hooks.py
@@ -13,13 +13,7 @@ If you call wrap() with pos='around', the original function will not be called
 automatically but can be called with _old().
 """
 
-import functools
-
-try:
-    # optional: like functools.wraps, but signature-preserving
-    import decorator
-except ImportError:
-    decorator = None
+import decorator
 
 # Hooks
 ##############################################################################
@@ -67,9 +61,6 @@ def wrap(old, new, pos="after"):
             return old(*args, **kwargs)
         else:
             return new(_old=old, *args, **kwargs)
-
-    if decorator is None:
-        return functools.wraps(repl)
 
     def decorator_wrapper(f, *args, **kwargs):
         return repl(*args, **kwargs)
