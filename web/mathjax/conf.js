72868507c7646efe27174ce96ea2baf5360dadb0
Damien Elmes <git@ichi2.net>
2017-08-05 12:47:07 +1000

    bundle mhchem

--- web/mathjax/conf.js
+++ web/mathjax/conf.js
@@ -2,7 +2,7 @@ window.MathJax = {
   jax: ["input/TeX","output/CommonHTML"],
   extensions: ["tex2jax.js"],
   TeX: {
-    extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
+    extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js", "mhchem.js"]
   },
   messageStyle: "none",
   skipStartupTypeset: true,
