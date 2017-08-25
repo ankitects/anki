2c9f286026e60ca4301f2417ee0b5339026dd2bc
Damien Elmes <git@ichi2.net>
2017-08-25 16:39:21 +1000

    update python ver dep

--- anki/__init__.py
+++ anki/__init__.py
@@ -4,8 +4,8 @@
 
 import sys
 
-if sys.version_info[0] < 3 or sys.version_info[1] < 5:
-    raise Exception("Anki requires Python 3.5+")
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    raise Exception("Anki requires Python 3.6+")
 
 version="2.1.0beta12" # build scripts grep this line, so preserve formatting
 from anki.storage import Collection
