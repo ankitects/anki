25cb6d3abeaed244a3fc21c19eaf7cccb4c673ca
Damien Elmes <git@ichi2.net>
2017-08-28 22:30:05 +1000

    bump version

--- anki/__init__.py
+++ anki/__init__.py
@@ -7,6 +7,6 @@ import sys
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     raise Exception("Anki requires Python 3.6+")
 
-version="2.1.0beta13" # build scripts grep this line, so preserve formatting
+version="2.1.0beta14" # build scripts grep this line, so preserve formatting
 from anki.storage import Collection
 __all__ = ["Collection"]
