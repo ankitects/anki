6a61516622dfc37ea35971ce717585f64e3c3d55
Damien Elmes <git@ichi2.net>
2017-10-13 13:11:22 +1000

    don't capture media keys

--- anki/mpv.py
+++ anki/mpv.py
@@ -73,6 +73,7 @@ class MPVBase:
         "--force-window=no",
         "--ontop",
         "--audio-display=no",
+        "--input-media-keys=no",
     ]
 
     def __init__(self, window_id=None, debug=False):
