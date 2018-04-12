1ba8deae4e2fc610d63a772bcbec81c6859eddf6
Damien Elmes <git@ichi2.net>
2018-04-12 17:59:51 +1000

    allow for slow machines
    https://anki.tenderapp.com/discussions/ankidesktop/27674-possible-race-condition-in-testtest_cardstest_delete-works-in-isolation

--- tools/tests.sh
+++ tools/tests.sh
@@ -22,4 +22,4 @@ else
     args=""
     echo "Call with coverage=1 to run coverage tests"
 fi
-(cd $dir && nosetests -vs --processes=16 $lim $args --cover-package=anki)
+(cd $dir && nosetests -vs --processes=16 --process-timeout=300 $lim $args --cover-package=anki)
