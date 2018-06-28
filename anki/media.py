ankitects/anki

commit 61cc3cec868d5bb7cb2e86862d1d20aebad44237

fix detection of media refs in cloze
https://anki.tenderapp.com/discussions/beta-testing/796-another-check-media-issue

anki/media.py
@@ -189,15 +189,15 @@ create table meta (dirMod int, lastUsn int); insert into meta values (0, 0);
         strings = []
         from anki.template.template import clozeReg
         def qrepl(m):
-            if m.group(3):
-                return "[%s]" % m.group(3)
+            if m.group(4):
+                return "[%s]" % m.group(4)
             else:
                 return "[...]"
         def arepl(m):
-            return m.group(1)
+            return m.group(2)
         for ord in ords:
             s = re.sub(clozeReg%ord, qrepl, string)
-            s = re.sub(clozeReg%".+?", "\\2", s)
+            s = re.sub(clozeReg%".+?", "\\4", s)
             strings.append(s)
         strings.append(re.sub(clozeReg%".+?", arepl, string))
         return strings

commit 9f7464fac2340ee0e14c028999c11b927c5a2e42

fix media check with [$]{{c1::a}}{{c2::b}}[/$]
https://anki.tenderapp.com/discussions/ankidesktop/28625-cloze-deletion-in-latex-check-medias-regression-in-21

appears to have broken in 61cc3cec868d5bb7cb2e86862d1d20aebad44237

anki/media.py
@@ -224,7 +224,7 @@ create table meta (dirMod int, lastUsn int); insert into meta values (0, 0);
             return m.group(2)
         for ord in ords:
             s = re.sub(clozeReg%ord, qrepl, string)
-            s = re.sub(clozeReg%".+?", "\\4", s)
+            s = re.sub(clozeReg%".+?", "\\2", s)
             strings.append(s)
         strings.append(re.sub(clozeReg%".+?", arepl, string))
         return strings