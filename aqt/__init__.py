c15df0462290b37dd6300de987308369bb33ac5f
Damien Elmes <git@ichi2.net>
2018-07-28 16:44:23 +1000

    remove beta warning

--- aqt/__init__.py
+++ aqt/__init__.py
@@ -302,8 +302,6 @@ environment points to a valid, writable folder.""")
     # remaining pm init
     pm.ensureProfile()
 
-    print("This is an BETA build - please do not package it up for Linux distributions")
-
     # load the main window
     import aqt.main
     mw = aqt.main.AnkiQt(app, pm, opts, args)
