eef0b92374233f67621f56feda3147aa9ccab069
Damien Elmes <gpg@ankiweb.net>
2018-10-29 21:06:11 +1000

    correctly load prefs file modified in more recent qt

--- aqt/profiles.py
+++ aqt/profiles.py
@@ -133,6 +133,12 @@ a flash drive.""" % self.base)
     def _unpickle(self, data):
         class Unpickler(pickle.Unpickler):
             def find_class(self, module, name):
+                if module == "PyQt5.sip":
+                    try:
+                        import PyQt5.sip
+                    except:
+                        # use old sip location
+                        module = "sip"
                 fn = super().find_class(module, name)
                 if module == "sip" and name == "_unpickle_type":
                     def wrapper(mod, obj, args):
