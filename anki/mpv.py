727570418ccc5b8324a37b5ead229c1d293045a5
Damien Elmes <gpg@ankiweb.net>
2018-12-13 21:30:17 +1000

    restart mpv if it stops responding
    https://anki.tenderapp.com/discussions/ankidesktop/31433-error

--- anki/mpv.py
+++ anki/mpv.py
@@ -344,7 +344,7 @@ class MPVBase:
         except Empty:
             return None
 
-    def _send_request(self, message, timeout=None):
+    def _send_request(self, message, timeout=None, _retry=1):
         """Send a command to the mpv process and collect the result.
         """
         self.ensure_running()
@@ -353,6 +353,13 @@ class MPVBase:
             return self._get_response(timeout)
         except MPVCommandError as e:
             raise MPVCommandError("%r: %s" % (message["command"], e))
+        except MPVTimeoutError as e:
+            if _retry:
+                print("mpv timed out, restarting")
+                self._stop_process()
+                return self._send_request(message, timeout, _retry-1)
+            else:
+                raise
 
     #
     # Public API
