20ff61e59eae6c32e4671c035a0d5438d169ab2f
Arthur-Milchior <arthur@milchior.fr>
2019-02-16 12:25:22 +0100

    Show deck of the addCard window

--- aqt/clayout.py
+++ aqt/clayout.py
@@ -61,7 +61,8 @@ class CardLayout(QDialog):
         self.setFocus()
 
     def redraw(self):
-        self.cards = self.col.previewCards(self.note, 2)
+        did = self.parent.deckChooser.selectedId() if self.addMode else None
+        self.cards = self.col.previewCards(self.note, 2, did = did)
         idx = self.ord
         if idx >= len(self.cards):
             self.ord = len(self.cards) - 1
