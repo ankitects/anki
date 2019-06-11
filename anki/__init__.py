464ba4264bba83814da04bc8407155e42e6287a9
Damien Elmes <gpg@ankiweb.net>
2019-06-11 15:25:45 +1000

    bump version

--- anki/__init__.py
+++ anki/__init__.py
@@ -10,6 +10,6 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 5:
 if sys.getfilesystemencoding().lower() in ("ascii", "ansi_x3.4-1968"):
     raise Exception("Anki requires a UTF-8 locale.")
 
-version="2.1.13" # build scripts grep this line, so preserve formatting
+version="2.1.14" # build scripts grep this line, so preserve formatting
 from anki.storage import Collection
 __all__ = ["Collection"]
