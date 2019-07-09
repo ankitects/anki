00f171e6c3f04970cdb7b3f8c228c35d36544740
Damien Elmes <gpg@ankiweb.net>
2019-07-09 15:16:02 +1000

    fix flag changes in browser not syncing

--- anki/collection.py
+++ anki/collection.py
@@ -937,4 +937,4 @@ and type=0""", [intTime(), self.usn()])
     def setUserFlag(self, flag, cids):
         assert 0 <= flag <= 7
         self.db.execute("update cards set flags = (flags & ~?) | ?, usn=?, mod=? where id in %s" %
-                        ids2str(cids), 0b111, flag, self._usn, intTime())
+                        ids2str(cids), 0b111, flag, self.usn(), intTime())
