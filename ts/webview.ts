f080616eedfc346df934b3b842fb289e3213531a
Damien Elmes <gpg@ankiweb.net>
2019-12-18 13:02:11 +1000

    webview.ts

--- ts/webview.ts
+++ ts/webview.ts
@@ -2,15 +2,16 @@
  * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
 
 // prevent backspace key from going back a page
-document.addEventListener("keydown", function (evt) {
+document.addEventListener("keydown", function (evt: KeyboardEvent) {
     if (evt.keyCode !== 8) {
         return;
     }
-    var isText = 0;
-    var nn = evt.target.nodeName;
+    let isText = 0;
+    const node = evt.target as Element;
+    const nn = node.nodeName;
     if (nn === "INPUT" || nn === "TEXTAREA") {
         isText = 1;
-    } else if (nn === "DIV" && evt.target.contentEditable) {
+    } else if (nn === "DIV" && (node as HTMLDivElement).contentEditable) {
         isText = 1;
     }
     if (!isText) {
