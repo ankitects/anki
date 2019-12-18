c1cbab0d2334d083575c9bbe8cfb505cfdcd911b
Damien Elmes <gpg@ankiweb.net>
2019-12-18 11:12:17 +1000

    avoid running checks/builds when nothing has changed
    with some defaults taken from https://tech.davis-hansson.com/p/make/

--- tools/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-TOOLS="$(cd "`dirname "$0"`"; pwd)"
-pylint -j 0 --rcfile=$TOOLS/../.pylintrc -f colorized --extension-pkg-whitelist=PyQt5 $TOOLS/../anki $TOOLS/../aqt
--- tools/typecheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-TOOLS="$(cd "`dirname "$0"`"; pwd)"
-mypy $TOOLS/../anki $TOOLS/../aqt
-(cd $TOOLS/.. && pytype --config pytype.conf)
