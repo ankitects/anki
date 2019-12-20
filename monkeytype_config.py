94a81230f66d752e017cc95caf47a551014f763e
Damien Elmes <gpg@ankiweb.net>
2019-12-20 19:44:08 +1000

    Revert "add arg hints to schedulers"
    This reverts commit a02d203604bc052c2bcbfad1d5bd6bd4f216e6b1.
    
    no longer needed

--- monkeytype_config.py
+++
@@ -1,9 +0,0 @@
-from monkeytype.config import DefaultConfig
-from monkeytype.typing import *
-
-class MyConfig(DefaultConfig):
-    def type_rewriter(self):
-        rws = (RemoveEmptyContainers(),RewriteConfigDict(),RewriteLargeUnion(2))
-        return ChainedRewriter(rws)
-
-CONFIG = MyConfig()
