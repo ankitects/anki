9383a72704c3aef0adb25610681656edc50e50dc
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-27 18:06:06 +0100

    Remove unneeded utf-8 encoding declaration
    UTF-8 is the default as of Python 3.
    See: https://docs.python.org/3/reference/lexical_analysis.html#encoding-declarations

--- anki/errors.py
+++ anki/errors.py
@@ -1,8 +1,7 @@
-from typing import Any
-
-# -*- coding: utf-8 -*-
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from typing import Any
 
 
 class AnkiError(Exception):
