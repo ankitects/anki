3d9c454997f7a2d04fb8305bb71bc313859721b0
Damien Elmes <gpg@ankiweb.net>
2020-01-08 12:30:08 +1000

    move furigana/hint filters into template2

--- pylib/anki/template/__init__.py
+++ pylib/anki/template/__init__.py
@@ -1,11 +1,6 @@
 from typing import Any
 
-from . import furigana, hint
 from .template import Template
-
-furigana.install()
-
-hint.install()
 
 
 def render(template, context=None, **kwargs) -> Any:
