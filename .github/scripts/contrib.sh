c6d0425020379cb353eb6375ebc829b35ec35e24
Damien Elmes <gpg@ankiweb.net>
2020-01-15 20:34:10 +1000

    basic anti spam protection

--- .github/scripts/contrib.sh
+++ .github/scripts/contrib.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+antispam=", at the domain "
+
 echo "All contributors:"
-git log --pretty=format:' - %ae' CONTRIBUTORS |sort |uniq |sort -f
+git log --pretty=format:' - %ae' CONTRIBUTORS |sort |uniq |sort -f | sed "s/@/$antispam/"
 
 headAuthor=$(git log -1 --pretty=format:'%ae')
+authorAt=$(echo "$headAuthor" | sed "s/@/$antispam/")
 if git log --pretty=format:'%ae' CONTRIBUTORS | grep -q "$headAuthor"; then
-    echo "Author $headAuthor found in CONTRIBUTORS"
+    echo "Author $authorAt found in CONTRIBUTORS"
 else
-    echo "Author $headAuthor NOT found in list"
+    echo "Author $authorAt NOT found in list"
     exit 1
 fi
