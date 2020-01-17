70061e6bfea5eeb0fef2eef3ad8ebbd727aa455e
Damien Elmes <gpg@ankiweb.net>
2020-01-17 10:51:36 +1000

    comment out the unused legacy code

--- pylib/tests/test_template.py
+++ pylib/tests/test_template.py
@@ -1,20 +1,4 @@
-from anki.template_legacy import _removeFormattingFromMathjax
 from tests.shared import getEmptyCol
-
-
-def test_remove_formatting_from_mathjax():
-    assert _removeFormattingFromMathjax(r"\(2^{{c3::2}}\)", 3) == r"\(2^{{C3::2}}\)"
-
-    txt = (
-        r"{{c1::ok}} \(2^2\) {{c2::not ok}} \(2^{{c3::2}}\) \(x^3\) "
-        r"{{c4::blah}} {{c5::text with \(x^2\) jax}}"
-    )
-    # Cloze 2 is not in MathJax, so it should not get protected against
-    # formatting.
-    assert _removeFormattingFromMathjax(txt, 2) == txt
-
-    txt = r"\(a\) {{c1::b}} \[ {{c1::c}} \]"
-    assert _removeFormattingFromMathjax(txt, 1) == (r"\(a\) {{c1::b}} \[ {{C1::c}} \]")
 
 
 def test_deferred_frontside():
