0942ffbff6cd08dbc83e64c79b4027e6cff43adb
Damien Elmes <gpg@ankiweb.net>
2020-01-20 19:12:34 +1000

    add a simple TTS field filter
    This simply wraps the field in extra text that the frontend will
    deal with. Also added some helpers for extracting and stripping
    audio and TTS tags from the rendered text.

--- pylib/anki/sound.py
+++ pylib/anki/sound.py
@@ -1,18 +1,61 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import re
-from typing import List
+"""
+Sound/TTS references extracted from card text.
 
-# Shared utils
+Use collection.backend.strip_av_tags(string) to remove all tags,
+and collection.backend.get_av_tags(string) to get a list of AVTags.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Union
+
+
+@dataclass
+class TTSTag:
+    """Records information about a text to speech tag.
+
+    See tts.py for more information.
+    """
+
+    args: List[str]
+    text: str
+
+
+@dataclass
+class SoundOrVideoTag:
+    """Contains the filename inside a [sound:...] tag.
+
+    Video files also use [sound:...].
+    """
+
+    filename: str
+
+
+# note this does not include image tags, which are handled with HTML.
+AVTag = Union[SoundOrVideoTag, TTSTag]
+
+# Legacy utils
 ##########################################################################
+# these will be removed in the future
 
 _soundReg = r"\[sound:(.*?)\]"
 
 
 def allSounds(text) -> List:
-    return re.findall(_soundReg, text)
+    from aqt import mw
+
+    return [
+        x.filename
+        for x in mw.col.backend.get_av_tags(text)
+        if isinstance(x, SoundOrVideoTag)
+    ]
 
 
 def stripSounds(text) -> str:
-    return re.sub(_soundReg, "", text)
+    from aqt import mw
+
+    return mw.col.backend.strip_av_tags(text)
