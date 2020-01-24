4c97b3f8205e6fb97dbaa4e8108bd8414e0a6a42
Damien Elmes <gpg@ankiweb.net>
2020-01-24 17:27:43 +1000

    case insensitive contributor compare
    https://github.com/ankitects/anki/pull/428

--- .github/scripts/contrib.sh
+++ .github/scripts/contrib.sh
@@ -9,7 +9,7 @@ git log --pretty=format:' - %ae' CONTRIBUTORS |sort |uniq |sort -f | sed "s/@/$a
 
 headAuthor=$(git log -1 --pretty=format:'%ae')
 authorAt=$(echo "$headAuthor" | sed "s/@/$antispam/")
-if git log --pretty=format:'%ae' CONTRIBUTORS | grep -q "$headAuthor"; then
+if git log --pretty=format:'%ae' CONTRIBUTORS | grep -qi "$headAuthor"; then
     echo "Author $authorAt found in CONTRIBUTORS"
 else
     echo "Author $authorAt NOT found in list"
