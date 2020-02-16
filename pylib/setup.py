07318a20c6b9eb67da8e6d148844cfb5bdd2774e
Damien Elmes <gpg@ankiweb.net>
2020-02-16 21:40:22 +1000

    test out the Python Fluent implementation
    The parsing step is considerably slower in Python, but if parsing
    is moved out of the test function, Python wins at 45ms to Rust's 67ms
    on 10,000 rounds, presumably due to the overhead of serializing to
    Protobuf. Not enough of a difference to justify the inclusion of extra
    dependencies and duplicating the lookup code in any case.

--- pylib/setup.py
+++ pylib/setup.py
@@ -23,5 +23,6 @@ setuptools.setup(
         "protobuf",
         'psutil; sys_platform == "win32"',
         'distro; sys_platform != "darwin" and sys_platform != "win32"',
+        "fluent.runtime",
     ],
 )
