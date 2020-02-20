ddf1d6acbd0ae4a2d7f79b14f7f56367eb6e766b
Damien Elmes <gpg@ankiweb.net>
2020-02-20 21:23:00 +1000

    add plural support to extract-po-string

--- qt/i18n/extract-po-string.py
+++ qt/i18n/extract-po-string.py
@@ -1,4 +1,8 @@
 import os
+
+import json
+
+import re
 import sys
 
 import polib
@@ -34,6 +38,47 @@ for (lang, cat) in cats:
                 print(lang, "has", msg)
                 to_insert.append((lang, msg))
                 break
+            elif entry.msgstr_plural and entry.msgstr_plural[0]:
+                # convert the dict into a list in the correct order
+                plurals = list(entry.msgstr_plural.items())
+                plurals.sort()
+                # update variables and discard keys
+                adjusted = []
+                for _k, msg in plurals:
+                    if replacements:
+                        for (old,new) in replacements:
+                            msg = msg.replace(old, f"{{${new}}}")
+                    adjusted.append(msg)
+                print(lang, "has", adjusted)
+                if len(adjusted) > 1:
+                    to_insert.append((lang, adjusted))
+                else:
+                    # language doesn't have plurals
+                    to_insert.append((lang, adjusted[0]))
+
+plurals = json.load(open("plurals.json"))
+
+def plural_text(key, lang, translation):
+    lang = re.sub("(_|-).*", "", lang)
+
+    # extract the variable - there should be only one
+    var = re.findall(r"{(\$.*?)}", translation[0])
+    if not len(var) == 1:
+        return None
+    var = var[0]
+
+    buf = f"{key} = {{ {var} ->\n"
+
+    # for each of the plural forms except the last
+    for idx, msg in enumerate(translation[:-1]):
+        plural_form = plurals[lang][idx]
+        buf += f"    [{plural_form}] {msg}\n"
+
+    # add the catchall
+    msg = translation[-1]
+    buf += f"   *[other] {msg}\n"
+    buf += "  }\n"
+    return buf
 
 for lang, translation in to_insert:
     dir = os.path.join("ftl", "core", lang)
@@ -45,6 +90,13 @@ for lang, translation in to_insert:
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-    open(ftl_path, "a").write(f"\n{key} = {translation}\n")
+    # simple, non-plural form?
+    if isinstance(translation, str):
+        buf = f"{key} = {translation}\n"
+    else:
+        buf = plural_text(key, lang, translation)
+
+    if buf:
+        open(ftl_path, "a").write(buf)
 
 print("done")
