d1284cb3a6ee2e330ca166e820f1d3cfa385352c
Damien Elmes <gpg@ankiweb.net>
2020-02-25 17:49:06 +1000

    interrupt current audio when autoplay off

--- qt/aqt/browser.py
+++ qt/aqt/browser.py
@@ -1735,8 +1735,11 @@ where id in %s"""
                 else:
                     audio = c.answer_av_tags()
                 av_player.play_tags(audio)
+            else:
+                av_player.maybe_interrupt()
 
-            txt = self.mw.prepare_card_text_for_display(txt)
+
+                txt = self.mw.prepare_card_text_for_display(txt)
             txt = gui_hooks.card_will_show(
                 txt, c, "preview" + self._previewState.capitalize()
             )
