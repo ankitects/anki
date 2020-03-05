2e4fb4401f6c43117dc13877b7d7114287b535c8
Damien Elmes <gpg@ankiweb.net>
2020-03-05 14:55:03 +1000

    fix incorrect mark_collection_modified()
    - usn shouldn't be changed
    - mtime is in milliseconds

--- rslib/src/time.rs
+++ rslib/src/time.rs
@@ -3,9 +3,16 @@
 
 use std::time;
 
-pub(crate) fn i64_unix_timestamp() -> i64 {
+pub(crate) fn i64_unix_secs() -> i64 {
     time::SystemTime::now()
         .duration_since(time::SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs() as i64
+}
+
+pub(crate) fn i64_unix_millis() -> i64 {
+    time::SystemTime::now()
+        .duration_since(time::SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as i64
 }
