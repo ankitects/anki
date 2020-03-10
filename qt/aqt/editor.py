3d5867759b1ffa24a8e2827f9861a9d4bb969116
evandrocoan <evandrocoan@hotmail.com>
2020-03-10 13:56:12 -0300

    Revert qt/aqt/editor.py BeautifulSoup fix because it was on the
    BeautifulSoup repository.

--- qt/aqt/editor.py
+++ qt/aqt/editor.py
@@ -494,13 +494,11 @@ class Editor:
         form.textEdit.moveCursor(QTextCursor.End)
         d.exec_()
         html = form.textEdit.toPlainText()
-        # https://anki.tenderapp.com/discussions/ankidesktop/39543-anki-is-replacing-the-character-by-when-i-exit-the-html-edit-mode-ctrlshiftx
-        if html.find(">") > -1:
-            # filter html through beautifulsoup so we can strip out things like a
-            # leading </div>
-            with warnings.catch_warnings() as w:
-                warnings.simplefilter("ignore", UserWarning)
-                html = str(BeautifulSoup(html, "html.parser"))
+        # filter html through beautifulsoup so we can strip out things like a
+        # leading </div>
+        with warnings.catch_warnings() as w:
+            warnings.simplefilter("ignore", UserWarning)
+            html = str(BeautifulSoup(html, "html.parser"))
         self.note.fields[field] = html
         self.note.flush()
         self.loadNote(focusTo=field)
