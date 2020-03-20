c90670ec3a93fec9d95b87baee8d13b854c15bb2
Damien Elmes <gpg@ankiweb.net>
2020-03-20 12:08:22 +1000

    tolerate some string IDs

--- rslib/src/config.rs
+++ rslib/src/config.rs
@@ -2,12 +2,16 @@
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 use crate::types::ObjID;
+use serde_aux::field_attributes::deserialize_number_from_string;
 use serde_derive::Deserialize;
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
-    #[serde(rename = "curDeck")]
+    #[serde(
+        rename = "curDeck",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub(crate) current_deck_id: ObjID,
     pub(crate) rollover: Option<i8>,
     pub(crate) creation_offset: Option<i32>,
--- rslib/src/decks.rs
+++ rslib/src/decks.rs
@@ -2,10 +2,12 @@
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 use crate::types::ObjID;
+use serde_aux::field_attributes::deserialize_number_from_string;
 use serde_derive::Deserialize;
 
 #[derive(Deserialize)]
 pub struct Deck {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub(crate) id: ObjID,
     pub(crate) name: String,
 }
