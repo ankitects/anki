1f8a1126a4f0edaffe7a84f74b3468e56a51ac47
Damien Elmes <gpg@ankiweb.net>
2020-03-21 07:49:44 +1000

    don't require trailing whitespace in .sql files

--- .github/scripts/trailing-newlines.sh
+++ .github/scripts/trailing-newlines.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-files=$(rg -l '[^\n]\z' -g '!*.{svg,scss,json}' || true)
+files=$(rg -l '[^\n]\z' -g '!*.{svg,scss,json,sql}' || true)
 if [ "$files" != "" ]; then
     echo "the following files are missing a newline on the last line:"
     echo $files
