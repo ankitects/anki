069cc11a86cb1b8c9230a47bf187835fdc75ff8d
evandrocoan <evandrocoan@hotmail.com>
2020-04-01 23:01:48 -0300

    Created the GitHub Actions step Upload to PyPi to run only on
    tagged releases of anki:
    https://anki.tenderapp.com/discussions/ankidesktop/40025-anki-in-python-package-index

--- pylib/setup.py
+++ pylib/setup.py
@@ -1,11 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
 import setuptools
 
-with open("../meta/version") as fh:
-    version = fh.read().strip()
+minimum_python_version = (3, 7)
+
+if sys.version_info < minimum_python_version:
+    raise RuntimeError(
+        "The minimum Python interpreter version required for Anki is '%s' "
+        "and version '%s' was found!" % (minimum_python_version, sys.version_info)
+    )
+
+install_requires = [
+    "beautifulsoup4",
+    "requests",
+    "decorator",
+    "protobuf",
+    'orjson; platform_machine == "x86_64"',
+    'psutil; sys_platform == "win32"',
+    'distro; sys_platform != "darwin" and sys_platform != "win32"',
+]
+
 
 setuptools.setup(
     name="anki",
-    version=version,
+    version="2.1.24",  # automatically updated
     author="Ankitects Pty Ltd",
     description="Anki's library code",
     long_description="Anki's library code",
@@ -16,13 +36,5 @@ setuptools.setup(
     package_data={"anki": ["py.typed"]},
     classifiers=[],
     python_requires=">=3.7",
-    install_requires=[
-        "beautifulsoup4",
-        "requests",
-        "decorator",
-        "protobuf",
-        'orjson; platform_machine == "x86_64"',
-        'psutil; sys_platform == "win32"',
-        'distro; sys_platform != "darwin" and sys_platform != "win32"',
-    ],
+    install_requires=install_requires,
 )
