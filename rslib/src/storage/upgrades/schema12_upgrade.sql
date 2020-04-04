ea2a3d758600d94b8ef6240424334604db3f1558
Damien Elmes <gpg@ankiweb.net>
2020-04-04 10:14:51 +1000

    use separate schema version for tag changes
    Will prevent issues if user upgrades after an unclean shutdown

--- rslib/src/storage/upgrades/schema12_upgrade.sql
+++ rslib/src/storage/upgrades/schema12_upgrade.sql
@@ -5,10 +5,6 @@ create table deck_config (
   usn integer not null,
   config text not null
 );
-create table tags (
-  tag text not null primary key collate unicase,
-  usn integer not null
-) without rowid;
 update col
 set
   ver = 12;
