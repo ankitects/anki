b7696e6381eb2bd4a893d4897d53f6eca57f4564
Damien Elmes <gpg@ankiweb.net>
2020-04-10 10:52:31 +1000

    merge the 12-14 upgrade code

--- rslib/src/storage/upgrades/schema13_upgrade.sql
+++
@@ -1,7 +0,0 @@
-create table tags (
-  tag text not null primary key collate unicase,
-  usn integer not null
-) without rowid;
-update col
-set
-  ver = 13;
--- rslib/src/storage/upgrades/schema14_upgrade.sql
+++ rslib/src/storage/upgrades/schema14_upgrade.sql
@@ -1,8 +1,19 @@
+create table deck_config (
+  id integer primary key not null,
+  name text not null collate unicase,
+  mtime_secs integer not null,
+  usn integer not null,
+  config blob not null
+);
 create table config (
   key text not null primary key,
   usn integer not null,
   mtime_secs integer not null,
   val blob not null
+) without rowid;
+create table tags (
+  tag text not null primary key collate unicase,
+  usn integer not null
 ) without rowid;
 update col
 set
