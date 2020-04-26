635c4bd27b25aa40efd92280aa5ee6271ae567d2
Arthur Milchior <Arthur@Milchior.fr>
2020-04-26 14:42:54 +0200

    remove a try, for code clarity

--- qt/aqt/addcards.py
+++ qt/aqt/addcards.py
@@ -123,14 +123,11 @@ class AddCards(QDialog):
         if oldNote:
             if not keep:
                 self.removeTempNote(oldNote)
-            for n in range(len(note.fields)):
-                try:
-                    if not keep or flds[n]["sticky"]:
-                        note.fields[n] = oldNote.fields[n]
-                    else:
-                        note.fields[n] = ""
-                except IndexError:
-                    break
+            for n in range(min(len(note.fields), len(oldNote.fields))):
+                if not keep or flds[n]["sticky"]:
+                    note.fields[n] = oldNote.fields[n]
+                else:
+                    note.fields[n] = ""
         self.setAndFocusNote(note)
 
     def removeTempNote(self, note: Note) -> None:
