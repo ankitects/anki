5605591cba6755dc7f41fc37bef9fbd0475aff2f
BlueGreenMagick <50060875+BlueGreenMagick@users.noreply.github.com>
2020-04-28 11:02:53 +0900

    make addon text selectable and link accessible

--- qt/aqt/addons.py
+++ qt/aqt/addons.py
@@ -1282,6 +1282,9 @@ class ConfigEditor(QDialog):
         self.mgr = dlg.mgr
         self.form = aqt.forms.addonconf.Ui_Dialog()
         self.form.setupUi(self)
+        self.form.label.setTextInteractionFlags(
+            Qt.TextSelectableByMouse | Qt.LinksAccessibleByMouse
+        )
         restore = self.form.buttonBox.button(QDialogButtonBox.RestoreDefaults)
         restore.clicked.connect(self.onRestoreDefaults)
         self.setupFonts()
