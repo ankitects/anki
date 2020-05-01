b19b3a670643c3239c557c2b613327c5bea53d60
evandrocoan <evandrocoan@hotmail.com>
2020-04-30 23:07:47 -0300

    Stop requiring ankirspy while running on development because
    maturin develop hides the package from pip
    https://github.com/PyO3/maturin/issues 304

--- pylib/setup.py
+++ pylib/setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+
 import setuptools
 
 install_requires = [
@@ -7,12 +9,14 @@ install_requires = [
     "requests",
     "decorator",
     "protobuf",
-    "ankirspy==2.1.25",  # automatically updated 1
     'orjson; platform_machine == "x86_64"',
     'psutil; sys_platform == "win32"',
     'distro; sys_platform != "darwin" and sys_platform != "win32"',
 ]
 
+# maturin develop hides the package from pip - https://github.com/ankitects/anki/pull/600
+if not os.environ.get("SKIP_ANKI_RSPY", False):
+    install_requires.append("ankirspy==2.1.25")  # automatically updated 1
 
 setuptools.setup(
     name="anki",
