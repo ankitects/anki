08f1843b67fb0ae6341aced6f422df04f035129c
Damien Elmes <gpg@ankiweb.net>
2020-05-19 11:27:02 +1000

    automatically omit notes or cards table when possible

--- rslib/src/search/notes.rs
+++ rslib/src/search/notes.rs
@@ -1,38 +1,17 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-use super::{parser::Node, sqlwriter::node_to_sql};
+use super::{parser::Node, sqlwriter::SqlWriter};
 use crate::collection::Collection;
 use crate::err::Result;
 use crate::notes::NoteID;
 use crate::search::parser::parse;
 
 impl Collection {
-    /// This supports card queries as well, but is slower.
     pub fn search_notes(&mut self, search: &str) -> Result<Vec<NoteID>> {
-        self.search_notes_inner(search, |sql| {
-            format!(
-                "select distinct n.id from cards c, notes n where c.nid=n.id and {}",
-                sql
-            )
-        })
-    }
-
-    /// This only supports note-related search terms.
-    pub fn search_notes_only(&mut self, search: &str) -> Result<Vec<NoteID>> {
-        self.search_notes_inner(search, |sql| {
-            format!("select n.id from notes n where {}", sql)
-        })
-    }
-
-    fn search_notes_inner<F>(&mut self, search: &str, build_sql: F) -> Result<Vec<NoteID>>
-    where
-        F: FnOnce(String) -> String,
-    {
         let top_node = Node::Group(parse(search)?);
-        let (sql, args) = node_to_sql(self, &top_node, self.normalize_note_text())?;
-
-        let sql = build_sql(sql);
+        let writer = SqlWriter::new(self);
+        let (sql, args) = writer.build_notes_query(&top_node)?;
 
         let mut stmt = self.storage.db.prepare(&sql)?;
         let ids: Vec<_> = stmt
