5ac9ea0e0ddb9f30f972dfd6468dbbe748f74497
Arthur Milchior <Arthur@Milchior.fr>
2020-05-30 18:15:40 +0200

    TS: allow arguments in hooks

--- qt/ts/src/hooks.ts
+++ qt/ts/src/hooks.ts
@@ -1,8 +1,8 @@
 /* Copyright: Ankitects Pty Ltd and contributors
  * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
 
-function _runHook(hooks: (() => void)[]) {
+function _runHook(hooks: ((...args) => void)[], ...args) {
     for (var i = 0; i < hooks.length; i++) {
-        hooks[i]();
+        hooks[i](...args);
     }
 }
