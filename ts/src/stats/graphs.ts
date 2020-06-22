dcff5e28fae9076d1f5bd5714554e842429082cb
Damien Elmes <gpg@ankiweb.net>
2020-06-22 19:11:50 +1000

    add top level component and pass search/day limit back from frontend

--- ts/src/stats/graphs.ts
+++ ts/src/stats/graphs.ts
@@ -7,40 +7,35 @@
 @typescript-eslint/ban-ts-ignore: "off" */
 
 import pb from "../backend/proto";
-import style from "./graphs.css";
 
-async function fetchData(): Promise<Uint8Array> {
-    let t = performance.now();
+async function fetchData(search: string, days: number): Promise<Uint8Array> {
     const resp = await fetch("/_anki/graphData", {
         method: "POST",
+        body: JSON.stringify({
+            search,
+            days,
+        }),
     });
     if (!resp.ok) {
         throw Error(`unexpected reply: ${resp.statusText}`);
     }
-    console.log(`fetch in ${performance.now() - t}ms`);
-    t = performance.now();
     // get returned bytes
     const respBlob = await resp.blob();
     const respBuf = await new Response(respBlob).arrayBuffer();
     const bytes = new Uint8Array(respBuf);
-    console.log(`bytes in ${performance.now() - t}ms`);
-    t = performance.now();
     return bytes;
 }
 
-import IntervalGraph from "./IntervalsGraph.svelte";
+export async function getGraphData(
+    search: string,
+    days: number
+): Promise<pb.BackendProto.GraphsOut> {
+    const bytes = await fetchData(search, days);
+    return pb.BackendProto.GraphsOut.decode(bytes);
+}
 
-export async function renderGraphs(): Promise<void> {
-    const bytes = await fetchData();
-    let t = performance.now();
-    const data = pb.BackendProto.GraphsOut.decode(bytes);
-    console.log(`decode in ${performance.now() - t}ms`);
-    t = performance.now();
-
-    document.head.append(style);
-
-    new IntervalGraph({
-        target: document.getElementById("svelte")!,
-        props: { cards: data.cards2 },
-    });
+export enum GraphRange {
+    Month = 1,
+    Year = 2,
+    All = 3,
 }
