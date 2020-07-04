a5e823f3ecc93d4f92d16ff40bc55008744f3a43
Damien Elmes <gpg@ankiweb.net>
2020-07-04 13:38:46 +1000

    use graph for card counts

--- ts/src/stats/CardCounts.svelte
+++ ts/src/stats/CardCounts.svelte
@@ -1,37 +1,41 @@
 <script lang="typescript">
-    import { gatherData, CardCounts } from "./card-counts";
+    import { defaultGraphBounds } from "./graphs";
+    import { gatherData, GraphData, renderCards } from "./card-counts";
     import pb from "../backend/proto";
     import { I18n } from "../i18n";
 
     export let sourceData: pb.BackendProto.GraphsOut | null = null;
     export let i18n: I18n;
 
-    let cardCounts: CardCounts | null = null;
+    let svg = null as HTMLElement | SVGElement | null;
+
+    let bounds = defaultGraphBounds();
+    bounds.height = 20;
+    bounds.marginLeft = 20;
+    bounds.marginRight = 20;
+    bounds.marginTop = 0;
+
+    let graphData: GraphData | null = null;
     $: if (sourceData) {
-        cardCounts = gatherData(sourceData, i18n);
+        graphData = gatherData(sourceData, i18n);
     }
+
+    $: if (graphData) {
+        renderCards(svg as any, bounds, graphData);
+    }
+
+    const total = i18n.tr(i18n.TR.STATISTICS_COUNTS_TOTAL_CARDS);
 </script>
 
-<style>
-    .counts-outer {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
-</style>
-
-{#if cardCounts}
+{#if graphData}
     <div class="graph">
-        <h1>{cardCounts.title}</h1>
-        <div class="counts-outer">
-            {#each cardCounts.counts as count}
-                <div>
-                    <div>
-                        <b>{count[0]}</b>
-                    </div>
-                    <div>{count[1]}</div>
-                </div>
-            {/each}
-        </div>
+        <h1>{graphData.title}</h1>
+
+        <svg bind:this={svg} viewBox={`0 0 ${bounds.width} ${bounds.height}`}>
+            <g class="days" />
+        </svg>
+
+        <div class="centered">{total}: {graphData.totalCards}</div>
+
     </div>
 {/if}
