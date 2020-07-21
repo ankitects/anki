348da6174485081a67b39741304bf4fe597de5d1
Damien Elmes <gpg@ankiweb.net>
2020-07-21 14:10:32 +1000

    include 1+ day learning cards in future due graph

--- ts/src/stats/future-due.ts
+++ ts/src/stats/future-due.ts
@@ -22,7 +22,7 @@ export interface GraphData {
 
 export function gatherData(data: pb.BackendProto.GraphsOut): GraphData {
     const due = (data.cards as pb.BackendProto.Card[])
-        .filter((c) => c.queue == CardQueue.Review) // && c.due >= data.daysElapsed)
+        .filter((c) => [CardQueue.Review, CardQueue.DayLearn].includes(c.queue))
         .map((c) => c.due - data.daysElapsed);
     const dueCounts = rollup(
         due,
