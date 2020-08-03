b363aaf401be63a995dede0e3ae109aa18ad6171
Damien Elmes <gpg@ankiweb.net>
2020-08-03 17:47:15 +1000

    handle some instances of floats when an integer is expected during sync

--- rslib/src/serde.rs
+++ rslib/src/serde.rs
@@ -1,7 +1,8 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-use serde::Deserialize as DeTrait;
+use crate::timestamp::TimestampSecs;
+use serde::{Deserialize as DeTrait, Deserializer};
 pub(crate) use serde_aux::field_attributes::{
     deserialize_bool_from_anything, deserialize_number_from_string,
 };
@@ -14,4 +15,51 @@ where
 {
     let v: Value = DeTrait::deserialize(deserializer)?;
     Ok(T::deserialize(v).unwrap_or_default())
+}
+
+pub fn deserialize_int_from_number<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    D: Deserializer<'de>,
+    T: serde::Deserialize<'de> + FromF64,
+{
+    #[derive(DeTrait)]
+    #[serde(untagged)]
+    enum IntOrFloat<T> {
+        Int(T),
+        Float(f64),
+    }
+
+    match IntOrFloat::<T>::deserialize(deserializer)? {
+        IntOrFloat::Float(s) => Ok(T::from_f64(s)),
+        IntOrFloat::Int(i) => Ok(i),
+    }
+}
+
+// It may be possible to use the num_traits crate instead in the future.
+pub trait FromF64 {
+    fn from_f64(val: f64) -> Self;
+}
+
+impl FromF64 for i32 {
+    fn from_f64(val: f64) -> Self {
+        val as Self
+    }
+}
+
+impl FromF64 for u32 {
+    fn from_f64(val: f64) -> Self {
+        val as Self
+    }
+}
+
+impl FromF64 for i64 {
+    fn from_f64(val: f64) -> Self {
+        val as Self
+    }
+}
+
+impl FromF64 for TimestampSecs {
+    fn from_f64(val: f64) -> Self {
+        TimestampSecs(val as i64)
+    }
 }
