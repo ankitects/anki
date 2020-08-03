fa9aa82830347d777fe57d46ac3277ea2c788891
Damien Elmes <gpg@ankiweb.net>
2020-08-03 11:29:07 +1000

    previous fs encoding test didn't catch things like latin-1
    https://forums.ankiweb.net/t/unicodeencodeerror-when-syncing/1746

--- pylib/anki/__init__.py
+++ pylib/anki/__init__.py
@@ -9,7 +9,10 @@ from anki.collection import Collection
 if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     raise Exception("Anki requires Python 3.7+")
 
-if sys.getfilesystemencoding().lower() in ("ascii", "ansi_x3.4-1968"):
+# ensure unicode filenames are supported
+try:
+    "テスト".encode(sys.getfilesystemencoding())
+except UnicodeEncodeError:
     raise Exception("Anki requires a UTF-8 locale.")
 
 
