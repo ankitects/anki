c9f9ae532e63b0b072a6012f1d067e28d48bafeb
Damien Elmes <gpg@ankiweb.net>
2020-08-04 15:15:57 +1000

    textual data for forecast
    Also stick to exact xMax, so we match the old graphs.

--- ts/src/stats/FutureDue.svelte
+++ ts/src/stats/FutureDue.svelte
@@ -1,17 +1,19 @@
 <script lang="typescript">
     import { I18n } from "../i18n";
     import { HistogramData } from "./histogram-graph";
-    import { GraphRange, RevlogRange } from "./graphs";
+    import { GraphRange, RevlogRange, TableDatum } from "./graphs";
     import { gatherData, GraphData, buildHistogram } from "./future-due";
     import pb from "../backend/proto";
     import HistogramGraph from "./HistogramGraph.svelte";
     import GraphRangeRadios from "./GraphRangeRadios.svelte";
+    import TableData from "./TableData.svelte";
 
     export let sourceData: pb.BackendProto.GraphsOut | null = null;
     export let i18n: I18n;
 
     let graphData = null as GraphData | null;
     let histogramData = null as HistogramData | null;
+    let tableData: TableDatum[] = [] as any;
     let backlog: boolean = true;
     let graphRange: GraphRange = GraphRange.Month;
 
@@ -20,7 +22,12 @@
     }
 
     $: if (graphData) {
-        histogramData = buildHistogram(graphData, graphRange, backlog, i18n);
+        [histogramData, tableData] = buildHistogram(
+            graphData,
+            graphRange,
+            backlog,
+            i18n
+        );
     }
 
     const title = i18n.tr(i18n.TR.STATISTICS_FUTURE_DUE_TITLE);
@@ -44,4 +51,5 @@
 
     <HistogramGraph data={histogramData} {i18n} />
 
+    <TableData {i18n} {tableData} />
 </div>
