2b0ffee689bd1c373a26be8f7fa9899cf56a9876
Damien Elmes <gpg@ankiweb.net>
2020-08-04 14:37:21 +1000

    factor out data table into separate file

---
+++ ts/src/stats/TableData.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+    import { I18n } from "../i18n";
+    import { TableDatum } from "./graphs";
+
+    export let i18n: I18n;
+    export let tableData: TableDatum[];
+</script>
+
+<div class="centered">
+    <table dir={i18n.direction()}>
+        {#each tableData as { label, value }}
+            <tr>
+                <td class="align-end">{label}:</td>
+                <td class="align-start">{value}</td>
+            </tr>
+        {/each}
+    </table>
+</div>
