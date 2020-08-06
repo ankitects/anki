bab0a775ba0a325735f3adddadfaef243dfc92c5
Arthur Milchior <Arthur@Milchior.fr>
2020-08-06 06:11:48 +0200

    NF: getCard duplicate remove

--- pylib/anki/sched.py
+++ pylib/anki/sched.py
@@ -133,43 +133,6 @@ class Scheduler(V2):
     def _dayLearnFirst(self):
         return False
 
-    def _getCard(self) -> Optional[Card]:
-        "Return the next due card id, or None."
-        # learning card due?
-        c = self._getLrnCard()
-        if c:
-            return c
-        # new first, or time for one?
-        if self._timeForNewCard():
-            c = self._getNewCard()
-            if c:
-                return c
-
-        # day learning first and card due?
-        dayLearnFirst = self._dayLearnFirst()
-        if dayLearnFirst:
-            c = self._getLrnDayCard()
-            if c:
-                return c
-
-        # card due for review?
-        c = self._getRevCard()
-        if c:
-            return c
-
-        if not dayLearnFirst:
-            # day learning card due?
-            c = self._getLrnDayCard()
-            if c:
-                return c
-
-        # new cards left?
-        c = self._getNewCard()
-        if c:
-            return c
-        # collapse or finish
-        return self._getLrnCard(collapse=True)
-
     # Learning queues
     ##########################################################################
 
