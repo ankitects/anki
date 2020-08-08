31323719bcf3ef54c53bcc94b225ca7c6ac58c6b
Henrik Giesel <hengiesel@gmail.com>
2020-08-08 15:08:30 +0200

    Insert gui_hook main_window_will_require_reset
    * into AnkiQt.requireReset method

--- qt/aqt/main.py
+++ qt/aqt/main.py
@@ -688,7 +688,7 @@ from the profile screen."
         "Signal queue needs to be rebuilt when edits are finished or by user."
         self.autosave()
         self.resetModal = modal
-        if self.interactiveState():
+        if gui_hooks.main_window_will_require_reset(self.interactiveState()):
             self.moveToState("resetRequired")
 
     def interactiveState(self):
