a1b5acd46a3f93f141d95f3a39c80306d1f9a9ec
Henrik Giesel <hengiesel@gmail.com>
2020-08-23 18:18:11 +0200

    Fix type signature: models is passed because it's a method

--- qt/tools/genhooks_gui.py
+++ qt/tools/genhooks_gui.py
@@ -688,8 +688,11 @@ hooks = [
     Hook(name="models_advanced_will_show", args=["advanced: QDialog"],),
     Hook(
         name="models_did_init_buttons",
-        args=["buttons: List[Tuple[str, Callable[[aqt.models.Models], None]]]", "models: aqt.models.Models"],
-        return_type="List[Tuple[str, Callable[[aqt.models.Models], None]]]",
+        args=[
+            "buttons: List[Tuple[str, Callable[[], None]]]",
+            "models: aqt.models.Models",
+        ],
+        return_type="List[Tuple[str, Callable[[], None]]]",
         doc="""Allows adding buttons to the Model dialog""",
     ),
     # Stats
