c87d60ea4c213888f6a62c225eb084f9bac75499
Henrik Giesel <hengiesel@gmail.com>
2020-08-23 19:05:40 +0200

    Remove unnecessary import

--- qt/aqt/models.py
+++ qt/aqt/models.py
@@ -2,7 +2,7 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 from operator import itemgetter
-from typing import Any, List, Tuple, Callable, Optional, Sequence
+from typing import Any, List, Optional, Sequence
 
 import aqt.clayout
 from anki import stdmodels
