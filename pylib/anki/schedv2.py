864864f650222afc185c3bfa2d7b65012733895a
Damien Elmes <gpg@ankiweb.net>
2020-08-27 09:35:31 +1000

    fix next learn message overestimating delay
    https://forums.ankiweb.net/t/bug-with-short-intervals-in-anki-2-1-scheduler/2678

--- pylib/anki/schedv2.py
+++ pylib/anki/schedv2.py
@@ -1297,7 +1297,7 @@ from cards where did in {dids} and queue = {QUEUE_TYPE_LRN}
         next = next or 0
         remaining = remaining or 0
         if next and next < self.dayCutoff:
-            next -= intTime() - self.col.conf["collapseTime"]
+            next -= intTime() + self.col.conf["collapseTime"]
             return self.col.backend.congrats_learn_message(
                 next_due=abs(next), remaining=remaining
             )
