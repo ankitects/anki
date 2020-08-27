748745fe1ac215beed5fc2f795a89d52017a67d5
Damien Elmes <gpg@ankiweb.net>
2020-08-27 16:12:30 +1000

    drop the old sass vars and define the properties directly

--- qt/tools/extract_scss_colors.py
+++ qt/tools/extract_scss_colors.py
@@ -10,15 +10,16 @@ for line in open("../ts/src/scss/_vars.scss"):
     line = line.strip()
     if not line:
         continue
-    m = re.match(r"^\$(.+): (.+);$", line)
+    m = re.match(r"--(.+): (.+);$", line)
     if not m:
-        print("failed to match", line)
+        if line != "}" and not ":root" in line:
+            print("failed to match", line)
         continue
 
     var = m.group(1)
     val = m.group(2)
 
-    colors[var] = val
+    colors.setdefault(var, []).append(val)
 
 with open("aqt/colors.py", "w") as buf:
     buf.write("# this file is auto-generated from _vars.scss\n")
