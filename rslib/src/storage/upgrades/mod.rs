87c6686930999d01a43bcaea563d8a625ceb53cc
Damien Elmes <gpg@ankiweb.net>
2020-09-22 08:52:48 +1000

    update schema to fix default initial ease in deck configs
    Closes #766
    
    - changes the on-disk representation from % to a multiplier,
    eg 250 -> 2.5, as this is consistent with the other options
    - resets deck configs at or below 1.3 to 2.5
    - for any cards that were using a reset deck config, reset their
    current factor if it's at or below 2.0x. The cutoff is arbitrary,
    and just intended to make sure we catch cards the user has rated
    Easy on multiple times. The existing due dates are left alone.

--- rslib/src/storage/upgrades/mod.rs
+++ rslib/src/storage/upgrades/mod.rs
@@ -1,6 +1,13 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+/// The minimum schema version we can open.
+pub(super) const SCHEMA_MIN_VERSION: u8 = 11;
+/// The version new files are initially created with.
+pub(super) const SCHEMA_STARTING_VERSION: u8 = 11;
+/// The maximum schema version we can open.
+pub(super) const SCHEMA_MAX_VERSION: u8 = 16;
+
 use super::SqliteStorage;
 use crate::err::Result;
 
@@ -19,6 +26,10 @@ impl SqliteStorage {
             self.upgrade_notetypes_to_schema15()?;
             self.upgrade_decks_to_schema15(server)?;
             self.upgrade_deck_conf_to_schema15()?;
+        }
+        if ver < 16 {
+            self.upgrade_deck_conf_to_schema16(server)?;
+            self.db.execute_batch("update col set ver = 16")?;
         }
 
         Ok(())
