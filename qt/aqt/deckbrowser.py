1827063e4fb684aa1e54bc87269b8640014225a7
Damien Elmes <gpg@ankiweb.net>
2020-09-27 11:50:48 +1000

    fix import shortcut
    https://forums.ankiweb.net/t/anki-2-1-34-35-beta/3569/21

--- qt/aqt/deckbrowser.py
+++ qt/aqt/deckbrowser.py
@@ -307,7 +307,7 @@ class DeckBrowser:
     drawLinks = [
         ["", "shared", _("Get Shared")],
         ["", "create", _("Create Deck")],
-        ["Ctrl+I", "import", _("Import File")],  # Ctrl+I works from menu
+        ["Ctrl+Shift+I", "import", _("Import File")],
     ]
 
     def _drawButtons(self):
