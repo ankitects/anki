909367efc16aae1e5c125f2213c53caf3f4321f7
Damien Elmes <gpg@ankiweb.net>
2020-10-10 19:02:59 +1000

    fixes for latest mypy

--- pylib/anki/importing/pauker.py
+++ pylib/anki/importing/pauker.py
@@ -39,7 +39,7 @@ class PaukerImporter(NoteImporter):
 
         try:
             f = gzip.open(self.file)
-            tree = ET.parse(f)
+            tree = ET.parse(f)  # type: ignore
             lesson = tree.getroot()
             assert lesson.tag == "Lesson"
         finally:
