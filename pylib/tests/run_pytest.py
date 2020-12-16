1b98e7e48f22e97d4e81bc013f4babc8153a3b1f
Damien Elmes <gpg@ankiweb.net>
2020-12-16 10:40:29 +1000

    add ability to specify an individual Python test

--- pylib/tests/run_pytest.py
+++ pylib/tests/run_pytest.py
@@ -7,4 +7,9 @@ os.environ["SHIFT_CLOCK_HACK"] = "1"
 
 if __name__ == "__main__":
     folder = os.path.join(os.path.dirname(__file__), "..", "tests")
-    sys.exit(pytest.main(["--verbose", "-s", folder]))
+    args = ["--verbose", "-s", folder]
+    # allow specifying an individual test, eg
+    # bazel test //pylib:pytest --test_env=PYTEST=test_bury
+    if pytest_extra := os.environ.get("PYTEST", ""):
+        args.extend(["-k", pytest_extra])
+    sys.exit(pytest.main(args))
