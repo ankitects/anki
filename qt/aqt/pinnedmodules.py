c18927b7ff502cc8806d0a3175ebd3a8910c02f4
Damien Elmes <gpg@ankiweb.net>
2020-12-18 19:55:52 +1000

    pin pyaudio and qtmultimedia

--- qt/aqt/pinnedmodules.py
+++ qt/aqt/pinnedmodules.py
@@ -1,10 +1,14 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+# pylint: disable=import-error,unused-import
+
+"""
 # this file is imported as part of the bundling process to ensure certain
 # modules are included in the distribution
 
-# pylint: disable=import-error,unused-import
+isort:skip_file
+"""
 
 # included implicitly in the past, and relied upon by some add-ons
 import cgi
@@ -21,12 +25,17 @@ import queue
 import typing
 import uuid
 
+# other modules we require that may not be automatically included
 import PyQt5.QtSvg
+import PyQt5.QtMultimedia
 import socks
+import pyaudio
 
 # legacy compat
 import anki.storage
 import anki.sync
+
+# platform-specifics
 from anki.utils import isLin, isMac, isWin
 
 if isWin:
@@ -38,7 +47,3 @@ if isWin:
 if isLin:
     # file locking
     import fcntl
-
-if isMac:
-    # recording
-    import PyQt5.QtMultimedia
