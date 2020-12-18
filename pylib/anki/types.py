faf992643a4fdd9999a4caf2e6acbe670d4f1ee0
Damien Elmes <gpg@ankiweb.net>
2020-12-18 16:50:55 +1000

    exhaustiveness checks on literals are now possible with the move to py38

--- pylib/anki/types.py
+++ pylib/anki/types.py
@@ -1,16 +1,5 @@
-import enum
-from typing import Any, NoReturn
+from typing import NoReturn
 
 
-class _Impossible(enum.Enum):
-    pass
-
-
-def assert_impossible(arg: NoReturn) -> NoReturn:
-    raise Exception(f"unexpected arg received: {type(arg)} {arg}")
-
-
-# mypy is not yet smart enough to do exhaustiveness checking on literal types,
-# so this will fail at runtime instead of typecheck time :-(
-def assert_impossible_literal(arg: Any) -> NoReturn:
+def assert_exhaustive(arg: NoReturn) -> NoReturn:
     raise Exception(f"unexpected arg received: {type(arg)} {arg}")
