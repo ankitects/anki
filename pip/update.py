198639a09582727dc4116875512f220a7f99dfda
Damien Elmes <gpg@ankiweb.net>
2020-12-21 13:44:29 +1000

    update setuptools; remove header
    So users don't need to manually exclude the header from the git commit

--- pip/update.py
+++ pip/update.py
@@ -3,15 +3,16 @@ import os
 import click
 from piptools.scripts import compile
 
+
 @click.group()
 def cli():
     pass
+
 
 cli.add_command(compile.cli, "compile")
 
 print("Updating deps...")
 os.chdir("pip")
-sys.argv[1:] = ["compile", "--allow-unsafe", "--upgrade"]
+sys.argv[1:] = ["compile", "--allow-unsafe", "--upgrade", "--no-header"]
 
 cli()
-
