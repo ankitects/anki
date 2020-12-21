4e1d57e2d2f0fae4897a75ef053cabfda758fc79
Damien Elmes <gpg@ankiweb.net>
2020-12-21 16:09:23 +1000

    check formatting of .js files in aqt/data
    mathjax.js did not match //ts:format output

--- qt/aqt/data/web/js/mathjax.js
+++ qt/aqt/data/web/js/mathjax.js
@@ -1,27 +1,27 @@
 window.MathJax = {
-  tex: {
-    displayMath: [["\\[", "\\]"]],
-    processRefs: false,
-    processEnvironments: false,
-    packages: {
-      "[+]": ["noerrors", "mhchem"],
+    tex: {
+        displayMath: [["\\[", "\\]"]],
+        processRefs: false,
+        processEnvironments: false,
+        packages: {
+            "[+]": ["noerrors", "mhchem"],
+        },
     },
-  },
-  startup: {
-    typeset: false,
-    pageReady: () => {
-      return MathJax.startup.defaultPageReady();
+    startup: {
+        typeset: false,
+        pageReady: () => {
+            return MathJax.startup.defaultPageReady();
+        },
     },
-  },
-  options: {
-    renderActions: {
-      addMenu: [],
-      checkLoading: [],
+    options: {
+        renderActions: {
+            addMenu: [],
+            checkLoading: [],
+        },
+        ignoreHtmlClass: "tex2jax_ignore",
+        processHtmlClass: "tex2jax_process",
     },
-    ignoreHtmlClass: "tex2jax_ignore",
-    processHtmlClass: "tex2jax_process",
-  },
-  loader: {
-    load: ["[tex]/noerrors", "[tex]/mhchem"],
-  },
+    loader: {
+        load: ["[tex]/noerrors", "[tex]/mhchem"],
+    },
 };
