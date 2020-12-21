ebeae9a5a049b575c2be209992605a71e7fb51f6
Damien Elmes <gpg@ankiweb.net>
2020-12-21 15:59:16 +1000

    don't pass BUILDINFO into build script
    It was causing the build script to be recompiled each time a commit was
    made, even though buildinfo.txt was not changing.

--- rslib/build/main.rs
+++ rslib/build/main.rs
@@ -5,13 +5,14 @@ fn main() {
     mergeftl::write_ftl_files_and_fluent_rs();
     protobuf::write_backend_proto_rs();
 
-    // copy or mock buildinfo in out_dir
-    let buildinfo = if let Ok(buildinfo) = std::env::var("BUILDINFO") {
-        std::fs::read_to_string(&buildinfo).expect("buildinfo missing")
-    } else {
-        "".to_string()
-    };
-    let buildinfo_out =
-        std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("buildinfo.txt");
-    std::fs::write(&buildinfo_out, buildinfo).unwrap();
+    // when building with cargo (eg for rust analyzer), generate a dummy BUILDINFO
+    if std::env::var("BAZEL").is_err() {
+        let buildinfo_out =
+            std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("buildinfo.txt");
+        std::fs::write(&buildinfo_out, "").unwrap();
+        println!(
+            "cargo:rustc-env=BUILDINFO={}",
+            buildinfo_out.to_str().expect("buildinfo")
+        );
+    }
 }
