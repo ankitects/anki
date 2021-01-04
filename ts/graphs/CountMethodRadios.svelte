47551df2f95e892fad01abf40c42c4d9c181bec8
Henrik Giesel <hengiesel@gmail.com>
2021-01-04 12:21:30 +0100

    Add CountMethodRadios and CardCountMethod enum

---
+++ ts/graphs/CountMethodRadios.svelte
@@ -0,0 +1,19 @@
+<script lang="typescript">
+    import type { I18n } from "anki/i18n";
+    import { CardCountMethod } from "./graph-helpers";
+
+    export let i18n: I18n;
+    export let cardCountMethod: CardCountMethod;
+
+    const byType = "By card type";
+    const byQueue = "By scheduling Queue";
+</script>
+
+<label>
+    <input type="radio" bind:group={cardCountMethod} value={CardCountMethod.ByType} />
+    {byType}
+</label>
+<label>
+    <input type="radio" bind:group={cardCountMethod} value={CardCountMethod.ByQueue} />
+    {byQueue}
+</label>
