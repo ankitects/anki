b87b32f2ea91c3d84e502112372e82a572226b2b
Damien Elmes <gpg@ankiweb.net>
2021-01-04 13:17:26 +1000

    update piptools; automatically insert winrt footer

--- pip/update.py
+++ pip/update.py
@@ -15,4 +15,19 @@ print("Updating deps...")
 os.chdir("pip")
 sys.argv[1:] = ["compile", "--allow-unsafe", "--upgrade", "--no-header"]
 
+
+def after_run():
+    with open("requirements.txt", "a+") as file:
+        file.write(
+            """
+# manually added for now; ensure it and the earlier winrt are not removed on update
+winrt==1.0.20330.1; sys.platform == "win32" and platform_release == "10" and python_version >= "3.9"
+"""
+        )
+
+
+import atexit
+
+atexit.register(after_run)
+
 cli()
