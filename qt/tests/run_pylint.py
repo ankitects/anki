4b11854a2e38870f9f8459295eeb8ade00a29c07
Damien Elmes <gpg@ankiweb.net>
2021-01-07 15:47:25 +1000

    fix pylint in qt
    pylint fails to read Qt modules when invoked as a subprocess with
    -j 0, and it looks like I committed this in the middle of debugging
    the issue. Work around it by invoking pylint directly. It's still
    awfully slow, taking 30 seconds on a 10 core machine.

--- qt/tests/run_pylint.py
+++ qt/tests/run_pylint.py
@@ -1,25 +1,21 @@
 import os
 import subprocess
 import sys
+import PyQt5
 
-import PyQt5.QtCore
+from pylint.lint import Run
 
 if __name__ == "__main__":
     (module, ini) = sys.argv[1:]
     ini = os.path.abspath(ini)
 
-    sys.exit(
-        subprocess.run(
-            [
-                sys.executable,
-                "-m",
-                "pylint",
-                "qt/aqt/qt.py",
-                "--rcfile",
-                ini,
-                "--extension-pkg-whitelist=PyQt5",
-                "-v",
-            ],
-            check=False,
-        ).returncode
+    Run(
+        [
+            "qt/aqt",
+            "--rcfile",
+            ini,
+            "-j",
+            "0",
+            "-v",
+        ]
     )
