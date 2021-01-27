3988010aa2c8fcbffddc7f90d9150b0c31c906d9
Damien Elmes <gpg@ankiweb.net>
2021-01-28 08:11:35 +1000

    avoid bumping mtime when correcting card eases on upgrade
    The USN is still set, which should cause the cards to sync in the
    non-conflict case, but if changes have been made on other devices
    the ease fix will not take priority, as we could be overwriting the
    reviews of someone who has not synced in a while.

--- rslib/src/storage/card/fix_low_ease.sql
+++ rslib/src/storage/card/fix_low_ease.sql
@@ -1,10 +1,9 @@
-update cards
-set factor = 2500,
-    usn = ?,
-    mod = ?
-where factor != 0
-    and factor <= 2000
-    and (
-        did in DECK_IDS
-        or odid in DECK_IDS
-    )
+UPDATE cards
+SET factor = 2500,
+  usn = ?
+WHERE factor != 0
+  AND factor <= 2000
+  AND (
+    did IN DECK_IDS
+    OR odid IN DECK_IDS
+  )
