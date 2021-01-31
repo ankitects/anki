7325d0e0d4c5b105180ad3e308e4fa4e3a2026c6
Damien Elmes <gpg@ankiweb.net>
2021-01-31 16:25:23 +1000

    add back basic rsbackend.py for compatibility

---
+++ pylib/anki/rsbackend.py
@@ -0,0 +1,17 @@
+# Copyright: Ankitects Pty Ltd and contributors
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+#
+# The backend code has moved into _backend; this file exists only to avoid breaking
+# some add-ons. They should be updated to point to the correct location in the
+# future.
+#
+# pylint: disable=unused-import
+
+from typing import TYPE_CHECKING
+
+from anki.lang import FormatTimeSpanContext
+from anki.decks import DeckTreeNode
+from anki.errors import NotFoundError, InvalidInput
+
+if TYPE_CHECKING:
+    from anki.lang import FormatTimeSpanContextValue, TRValue
