0697e197cb5dcd58ac2796267420f1430529947f
Damien Elmes <gpg@ankiweb.net>
2021-02-01 15:47:25 +1000

    update mypy and other Python deps
    latest mypy_protobuf can no longer be run directly, so we need
    to run a wrapper instead

---
+++ pylib/tools/protoc-gen-mypy.py
@@ -0,0 +1,9 @@
+# copied from mypy_protobuf:bin - simple launch wrapper
+import re
+import sys
+
+from mypy_protobuf import main
+
+if __name__ == "__main__":
+    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    sys.exit(main())
