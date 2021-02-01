2e917796676f717bb6530f761ee0719f5f06540e
Henrik Giesel <hengiesel@gmail.com>
2021-02-01 17:33:30 +0100

    Design BrowserSelect

--- ts/previewer/BrowserSelect.svelte
+++ ts/previewer/BrowserSelect.svelte
@@ -1,5 +1,20 @@
 <script lang="typescript">
+    const previousCard = () => {
+        // select previous card in browser
+    }
+
+    const nextCard = () => {
+        // select next card in browser
+    }
 </script>
 
+<style>
+    .bi::before {
+        font-weight: bold !important;
+    }
+</style>
+
 <div class="btn-group mx-1">
+    <button type="button" class="btn btn-primary" on:click={previousCard}><i class="bi bi-chevron-up"></i></button>
+    <button type="button" class="btn btn-primary" on:click={nextCard}><i class="bi bi-chevron-down"></i></button>
 </div>
