678a049d0eff462f92c4c7fac5a569134e6e62c9
Damien Elmes <gpg@ankiweb.net>
2021-02-09 13:38:04 +1000

    add missing license headers

--- ts/editor/inputHandlers.ts
+++ ts/editor/inputHandlers.ts
@@ -1,3 +1,6 @@
+/* Copyright: Ankitects Pty Ltd and contributors
+ * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
+
 import { EditingArea } from ".";
 import { nodeIsElement } from "./helpers";
 import { triggerChangeTimer } from "./changeTimer";
