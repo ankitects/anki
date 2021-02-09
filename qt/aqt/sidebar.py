e61875699851f17b7c7bfb09982c3c138a6ca869
Damien Elmes <gpg@ankiweb.net>
2021-02-09 19:29:59 +1000

    add escape hatch for new clicking behaviour, in case it causes problems

--- qt/aqt/sidebar.py
+++ qt/aqt/sidebar.py
@@ -316,9 +316,10 @@ class SidebarTreeView(QTreeView):
         self.setHeaderHidden(True)
         self.setIndentation(15)
         self.setAutoExpandDelay(600)
-        # pylint: disable=no-member
-        mode = QAbstractItemView.SelectionMode.ExtendedSelection  # type: ignore
-        self.setSelectionMode(mode)
+        if not self.mw.pm.sidebar_left_click_modifiers():
+            # pylint: disable=no-member
+            mode = QAbstractItemView.SelectionMode.ExtendedSelection  # type: ignore
+            self.setSelectionMode(mode)
         self.setDragDropMode(QAbstractItemView.InternalMove)
         self.setDragDropOverwriteMode(False)
 
@@ -434,7 +435,10 @@ class SidebarTreeView(QTreeView):
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
         super().mouseReleaseEvent(event)
         if event.button() == Qt.LeftButton:
-            if not self._keyboard_modified_pressed():
+            if (
+                not self._keyboard_modified_pressed()
+                or self.mw.pm.sidebar_left_click_modifiers()
+            ):
                 self._on_click_current()
         elif event.button() == Qt.RightButton:
             if self._keyboard_modified_pressed():
