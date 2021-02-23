61606130517ad9394f235d0d3885f3577467abc9
Damien Elmes <gpg@ankiweb.net>
2021-02-23 15:11:39 +1000

    split review/learn/new/relearn

---
+++ rslib/src/scheduler/answering/relearn.rs
@@ -0,0 +1,43 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+use crate::{
+    card::{CardQueue, CardType},
+    prelude::*,
+    scheduler::states::{CardState, IntervalKind, RelearnState},
+};
+
+use super::{CardStateUpdater, RevlogEntryPartial};
+
+impl CardStateUpdater {
+    pub(super) fn apply_relearning_state(
+        &mut self,
+        current: CardState,
+        next: RelearnState,
+    ) -> Result<Option<RevlogEntryPartial>> {
+        self.card.interval = next.review.scheduled_days;
+        self.card.remaining_steps = next.learning.remaining_steps;
+        self.card.ctype = CardType::Relearn;
+
+        let interval = next
+            .interval_kind()
+            .maybe_as_days(self.secs_until_rollover());
+        match interval {
+            IntervalKind::InSecs(secs) => {
+                self.card.queue = CardQueue::Learn;
+                self.card.due = TimestampSecs::now().0 as i32 + secs as i32;
+            }
+            IntervalKind::InDays(days) => {
+                self.card.queue = CardQueue::DayLearn;
+                self.card.due = (self.timing.days_elapsed + days) as i32;
+            }
+        }
+
+        Ok(RevlogEntryPartial::maybe_new(
+            current,
+            next.into(),
+            next.review.ease_factor,
+            self.secs_until_rollover(),
+        ))
+    }
+}
---
+++ rslib/src/scheduler/answering/review.rs
@@ -0,0 +1,33 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+use crate::{
+    card::{CardQueue, CardType},
+    prelude::*,
+    scheduler::states::{CardState, ReviewState},
+};
+
+use super::{CardStateUpdater, RevlogEntryPartial};
+
+impl CardStateUpdater {
+    pub(super) fn apply_review_state(
+        &mut self,
+        current: CardState,
+        next: ReviewState,
+    ) -> Result<Option<RevlogEntryPartial>> {
+        self.card.remove_from_filtered_deck_before_reschedule();
+        self.card.queue = CardQueue::Review;
+        self.card.ctype = CardType::Review;
+        self.card.interval = next.scheduled_days;
+        self.card.due = (self.timing.days_elapsed + next.scheduled_days) as i32;
+        self.card.ease_factor = (next.ease_factor * 1000.0).round() as u16;
+        self.card.lapses = next.lapses;
+
+        Ok(RevlogEntryPartial::maybe_new(
+            current,
+            next.into(),
+            next.ease_factor,
+            self.secs_until_rollover(),
+        ))
+    }
+}
