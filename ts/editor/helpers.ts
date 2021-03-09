2905881dcf8d5ef58a4ae7e54780dd31c62f03ec
Henrik Giesel <hengiesel@gmail.com>
2021-03-09 14:02:41 +0100

    Fix another import

--- ts/editor/helpers.ts
+++ ts/editor/helpers.ts
@@ -1,7 +1,7 @@
 /* Copyright: Ankitects Pty Ltd and contributors
  * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
 
-import type { EditingArea } from ".";
+import type { EditingArea } from "./editingArea";
 
 export function nodeIsElement(node: Node): node is Element {
     return node.nodeType === Node.ELEMENT_NODE;
