801ba295e3bb261a70b2689a844394b6528c5133
Henrik Giesel <hengiesel@gmail.com>
2021-03-16 23:49:15 +0100

    Remove global no-domain-line

--- ts/graphs/AxisTicks.svelte
+++ ts/graphs/AxisTicks.svelte
@@ -1,12 +1,19 @@
 <script lang="typescript">
     import type { GraphBounds } from "./graph-helpers";
+
     export let bounds: GraphBounds;
 </script>
 
+<style lang="scss">
+    g :global(.domain) {
+        opacity: 0.05;
+    }
+</style>
+
 <g
-    class="x-ticks no-domain-line"
+    class="x-ticks"
     transform={`translate(0,${bounds.height - bounds.marginBottom})`} />
-<g class="y-ticks no-domain-line" transform={`translate(${bounds.marginLeft}, 0)`} />
+<g class="y-ticks" transform={`translate(${bounds.marginLeft}, 0)`} />
 <g
-    class="y2-ticks no-domain-line"
+    class="y2-ticks"
     transform={`translate(${bounds.width - bounds.marginRight}, 0)`} />
