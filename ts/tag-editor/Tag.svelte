4c81dcc4c7ef552e116f596f1ea8555f132b78ae
Henrik Giesel <hengiesel@gmail.com>
2021-03-18 17:37:08 +0100

    Make tags look nice; add delete-icon

--- ts/tag-editor/Tag.svelte
+++ ts/tag-editor/Tag.svelte
@@ -3,9 +3,19 @@
 </script>
 
 <style lang="scss">
+    .tag {
+        /* important for tags with non-latin characters */
+        line-height: 2ch;
+    }
+
+    $white-translucent: rgba(255, 255, 255, 0.5);
+
+    .delete-icon:hover {
+        background-color: $white-translucent;
+    }
 </style>
 
-<span>
+<span class="tag bg-secondary rounded p-1 me-2">
     <span>{name}</span>
-    <span></span>
+    <span class="delete-icon bi bi-x rounded"></span>
 </span>
