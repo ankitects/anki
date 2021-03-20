0cc06ad03bb599dc7f489d02fe4f196c0621aafd
Damien Elmes <gpg@ankiweb.net>
2021-03-20 20:42:29 +1000

    switch Svelte compilation to worker model
    based on changes from upstream rules_svelte
    
    Their code was using run_node() instead of ctx.actions.run(), which
    seems to create a new worker for every CPU core, instead of respecting
    the standard limit of 4.

--- ts/svelte/svelte.js
+++ ts/svelte/svelte.js
@@ -1,55 +1,120 @@
 const fs = require("fs");
 const process = require("process");
 const path = require("path");
-
-const input = process.argv[2];
-const outputJs = process.argv[3];
-const temp = process.argv[4];
-
+const worker = require("@bazel/worker");
 const svelte = require("svelte/compiler.js");
-
-const source = fs.readFileSync(input, "utf8");
-
-const preprocessOptions = require("svelte-preprocess")({});
-preprocessOptions.filename = input;
-
 const svelte2tsx = require("svelte2tsx");
+const preprocess = require("svelte-preprocess");
+const ts = require("typescript");
 
-let tsoutput = svelte2tsx(source, {
-  filename: input,
-  strictMode: true,
-  isTsFile: true,
-});
-let codeLines = tsoutput.code.split("\n");
-// replace the "///<reference types="svelte" />" with a line
-// turning off checking, as we'll use svelte-check for that
-codeLines[0] = "// @ts-nocheck";
-const outputBase = path.basename(outputJs.replace(".mjs", ".tsx"));
-const outputTs = path.join(temp, outputBase);
-fs.writeFileSync(outputTs, codeLines.join("\n"));
+const tsOptions = {
+    jsx: "preserve",
+    declaration: true,
+    emitDeclarationOnly: true,
+    skipLibCheck: true,
+};
+const tsHost = ts.createCompilerHost(tsOptions);
 
-svelte.preprocess(source, preprocessOptions).then(
-  (processed) => {
-    let result;
+function writeFile(file, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+}
+
+function buildDeclarations(svelteTsFile, shims) {
+    const createdFiles = {}; //test2
+    tsHost.writeFile = (fileName, contents) => (createdFiles[fileName] = contents);
+    const program = ts.createProgram([svelteTsFile, ...shims], tsOptions, tsHost);
+    program.emit();
+    const dtsSource = createdFiles[svelteTsFile.replace(".ts", ".d.ts")];
+    return dtsSource;
+}
+
+async function writeDts(tsPath, dtsPath, shims) {
+    const dtsSource = buildDeclarations(tsPath, shims);
+    await writeFile(dtsPath, dtsSource);
+}
+
+async function writeTs(svelteSource, sveltePath, tsPath) {
+    let tsSource = svelte2tsx(svelteSource, {
+        filename: sveltePath,
+        strictMode: true,
+        isTsFile: true,
+    });
+    let codeLines = tsSource.code.split("\n");
+    // replace the "///<reference types="svelte" />" with a line
+    // turning off checking, as we'll use svelte-check for that
+    codeLines[0] = "// @ts-nocheck";
+    await writeFile(tsPath, codeLines.join("\n"));
+}
+
+//test
+async function writeJs(source, inputFilename, outputPath) {
+    const preprocessOptions = preprocess({});
+    preprocessOptions.filename = inputFilename;
+
     try {
-      result = svelte.compile(processed.toString(), {
-        format: "esm",
-        generate: "dom",
-        filename: outputJs,
-      });
+        const processed = await svelte.preprocess(source, preprocessOptions);
+        const result = svelte.compile(processed.toString(), {
+            format: "esm",
+            generate: "dom",
+            filename: outputPath,
+        });
+        // warnings are an error
+        if (result.warnings.length > 0) {
+            console.log(`warnings during compile: ${result.warnings}`);
+            return;
+        }
+        const outputSource = result.js.code;
+        await writeFile(outputPath, outputSource);
     } catch (err) {
-      console.log(`compile failed: ${err}`);
-      return;
+        console.log(`compile failed: ${err}`);
+        return;
     }
-    if (result.warnings.length > 0) {
-      console.log(`warnings during compile: ${result.warnings}`);
-      return;
-    }
+}
 
-    let code = result.js.code;
-    fs.writeFileSync(outputJs, code);
-  },
-  (error) => {
-    console.log(`preprocess failed: ${error}`);
-  }
-);
+async function compileSvelte(args) {
+    const [sveltePath, mjsPath, dtsPath, tempTsPath, ...shims] = args;
+    const svelteSource = await readFile(sveltePath);
+
+    await writeTs(svelteSource, sveltePath, tempTsPath);
+    await writeDts(tempTsPath, dtsPath, shims);
+    await writeJs(svelteSource, sveltePath, mjsPath);
+
+    return true;
+}
+
+function main(args) {
+    if (worker.runAsWorker(process.argv)) {
+        worker.log("Svelte running as a Bazel worker");
+        worker.runWorkerLoop(compileSvelte);
+    } else {
+        const paramFile = process.argv[2].replace(/^@/, "");
+        const commandLineArgs = fs.readFileSync(paramFile, "utf-8").trim().split("\n");
+        console.log("Svelte running as a standalone process");
+        compileSvelte(commandLineArgs);
+    }
+}
+
+if (require.main === module) {
+    main(process.argv.slice(2));
+    process.exitCode = 0;
+}
