4baa87348ec380d678c27c32ae5965dcca552f18
Henrik Giesel <hengiesel@gmail.com>
2021-03-24 23:35:51 +0100

    Create htmlFilter{Node,Styling} for better separation of concerns

---
+++ ts/editor/htmlFilterNode.ts
@@ -0,0 +1,32 @@
+export function removeNode(element: Node): void {
+    element.parentNode?.removeChild(element);
+}
+
+function iterateElement(
+    filter: (node: Node) => void,
+    fragment: DocumentFragment | Element
+): void {
+    for (const child of [...fragment.childNodes]) {
+        filter(child);
+    }
+}
+
+export const filterNode = (elementFilter: (element: Element) => void) => (
+    node: Node
+): void => {
+    switch (node.nodeType) {
+        case Node.COMMENT_NODE:
+            removeNode(node);
+            break;
+
+        case Node.DOCUMENT_FRAGMENT_NODE:
+            iterateElement(filterNode(elementFilter), node as DocumentFragment);
+
+        case Node.ELEMENT_NODE:
+            iterateElement(filterNode(elementFilter), node as Element);
+            elementFilter(node as Element);
+
+        default:
+        // do nothing
+    }
+};
