ab55a01869322908bc5c5b2bcb0576287e6d46f0
Henrik Giesel <hengiesel@gmail.com>
2021-03-24 23:35:51 +0100

    Create htmlFilter{Node,Styling} for better separation of concerns

--- ts/editor/htmlFilter.ts
+++ ts/editor/htmlFilter.ts
@@ -1,68 +1,56 @@
 /* Copyright: Ankitects Pty Ltd and contributors
  * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
 
-import { nodeIsElement } from "./helpers";
-import { filterElement } from "./htmlFilterElement";
+import {
+    filterElementBasic,
+    filterElementExtended,
+    filterElementInternal,
+} from "./htmlFilterElement";
+import { filterNode } from "./htmlFilterNode";
 
-////////////////////// //////////////////// ////////////////////
-
-function isHTMLElement(elem: Element): elem is HTMLElement {
-    return elem instanceof HTMLElement;
+enum FilterMode {
+    Basic,
+    Extended,
+    Internal,
 }
 
-// filtering from another field
-function filterInternalNode(elem: Element): void {
-    if (isHTMLElement(elem)) {
-        elem.style.removeProperty("background-color");
-        elem.style.removeProperty("font-size");
-        elem.style.removeProperty("font-family");
-    }
-    // recurse
-    for (let i = 0; i < elem.children.length; i++) {
-        const child = elem.children[i];
-        filterInternalNode(child);
-    }
+const filters: Record<FilterMode, (element: Element) => void> = {
+    [FilterMode.Basic]: filterElementBasic,
+    [FilterMode.Extended]: filterElementExtended,
+    [FilterMode.Internal]: filterElementInternal,
+};
+
+const whitespace = /[\n\t ]+/g;
+
+function collapseWhitespace(value: string): string {
+    return value.replace(whitespace, " ");
 }
 
-// filtering from external sources
-function filterNode(node: Node, extendedMode: boolean): void {
-    if (node.nodeType === Node.COMMENT_NODE) {
-        node.parentNode.removeChild(node);
-        return;
-    }
-    if (!nodeIsElement(node)) {
-        return;
-    }
-
-    // descend first, and take a copy of the child nodes as the loop will skip
-    // elements due to node modifications otherwise
-    for (const child of [...node.childNodes]) {
-        filterNode(child, extendedMode);
-    }
-
-    filterElement(node, extendedMode);
+function trim(value: string): string {
+    return value.trim();
 }
 
+const outputHTMLProcessors: Record<FilterMode, (outputHTML: string) => string> = {
+    [FilterMode.Basic]: (outputHTML: string): string =>
+        trim(collapseWhitespace(outputHTML)),
+    [FilterMode.Extended]: trim,
+    [FilterMode.Internal]: trim,
+};
 
-export function filterHTML(
-    html: string,
-    internal: boolean,
-    extendedMode: boolean
-): string {
-    // wrap it in <top> as we aren't allowed to change top level elements
-    const top = document.createElement("ankitop");
-    top.innerHTML = html;
+export function filterHTML(html: string, internal: boolean, extended: boolean): string {
+    const template = document.createElement("template");
+    template.innerHTML = html;
 
-    if (internal) {
-        filterInternalNode(top);
-    } else {
-        filterNode(top, extendedMode);
-    }
-    let outHtml = top.innerHTML;
-    if (!extendedMode && !internal) {
-        // collapse whitespace
-        outHtml = outHtml.replace(/[\n\t ]+/g, " ");
-    }
-    outHtml = outHtml.trim();
-    return outHtml;
+    const mode = internal
+        ? FilterMode.Internal
+        : extended
+        ? FilterMode.Extended
+        : FilterMode.Basic;
+
+    const content = template.content;
+    const filter = filterNode(filters[mode]);
+
+    filter(content);
+
+    return outputHTMLProcessors[mode](template.innerHTML);
 }
