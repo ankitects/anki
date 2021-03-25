7787a7827fe9cffe5ed08478bbc775e2aebd6c65
Henrik Giesel <hengiesel@gmail.com>
2021-03-25 23:32:23 +0100

    First implementation

--- ts/editor-toolbar/EditorToolbar.svelte
+++ ts/editor-toolbar/EditorToolbar.svelte
@@ -1,10 +1,27 @@
-<script>
+<script lang="typescript">
+    import ButtonBarOuter from "./ButtonBarOuter.svelte";
+    import ButtonBar from "./ButtonBar.svelte";
+
+    import LeftButton from "./LeftButton.svelte";
+    import RightButton from "./RightButton.svelte";
+
+    export let leftButtons = ["Fields...", "Cards..."];
+    export let rightButtons = ["bi bi-type-bold", "bi bi-pin-angle"];
+    export let nightMode: boolean;
+
+    console.log(nightMode);
 </script>
 
-<style lang="scss">
-    p {
-        color: red;
-        }
-</style>
+<ButtonBarOuter>
+    <ButtonBar>
+        {#each leftButtons as leftButton}
+            <LeftButton>{leftButton}</LeftButton>
+        {/each}
+    </ButtonBar>
 
-<p>Test</p>
+    <ButtonBar>
+        {#each rightButtons as rightButton}
+            <RightButton className={rightButton} />
+        {/each}
+    </ButtonBar>
+</ButtonBarOuter>
---
+++ ts/editor-toolbar/LeftButton.svelte
@@ -0,0 +1,20 @@
+<script lang="typescript">
+    import ButtonItem from "./ButtonItem.svelte";
+</script>
+<style lang="scss">
+    button {
+        padding: 3px 10px;
+        /* fit RightButton */
+        vertical-align: -.425em;
+    }
+
+    button:focus {
+        0 0 0 0.05rem rgba(255, 255, 255, 0.5);
+    }
+</style>
+
+<ButtonItem>
+    <button type="button" class="btn btn-secondary" on:click>
+        <slot />
+    </button>
+</ButtonItem>
