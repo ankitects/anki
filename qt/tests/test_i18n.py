9be367d5ff2696cbd8a5f3e14e8518c2fa8a174c
Damien Elmes <gpg@ankiweb.net>
2021-03-26 14:49:55 +1000

    update remaining TR references in .py files

--- qt/tests/test_i18n.py
+++ qt/tests/test_i18n.py
@@ -1,13 +1,11 @@
 import anki.lang
-from anki.lang import TR
 
 
 def test_no_collection_i18n():
     anki.lang.set_lang("zz")
-    tr2 = anki.lang.current_i18n.translate
+    tr = anki.lang.tr_legacyglobal
     no_uni = anki.lang.without_unicode_isolation
-    assert no_uni(tr2(TR.STATISTICS_REVIEWS, reviews=2)) == "2 reviews"
+    assert no_uni(tr.statistics_reviews(reviews=2)) == "2 reviews"
 
     anki.lang.set_lang("ja")
-    tr2 = anki.lang.current_i18n.translate
-    assert no_uni(tr2(TR.STATISTICS_REVIEWS, reviews=2)) == "2 枚の復習カード"
+    assert no_uni(tr.statistics_reviews(reviews=2)) == "2 枚の復習カード"
