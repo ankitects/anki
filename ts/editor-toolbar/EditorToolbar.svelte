fee78d25706cd22e0c055f8be35a4f83a99a15a8
Henrik Giesel <hengiesel@gmail.com>
2021-03-29 15:27:42 +0200

    Add remaining icons for toolbar

--- ts/editor-toolbar/EditorToolbar.svelte
+++ ts/editor-toolbar/EditorToolbar.svelte
@@ -9,6 +9,16 @@
     import italicIcon from "./type-italic.svg";
     import underlineIcon from "./type-underline.svg";
 
+    import superscriptIcon from "./format-superscript.svg";
+    import subscriptIcon from "./format-subscript.svg";
+    import bracketsIcon from "./code-brackets.svg";
+
+    import eraserIcon from "./eraser.svg";
+    import squareFillIcon from "./square-fill.svg";
+    import paperclipIcon from "./paperclip.svg";
+    import micIcon from "./mic.svg";
+    import threeDotsIcon from "./three-dots.svg";
+
     export let leftButtons = [
         { component: LabelButton, label: "Fields..." },
         { component: LabelButton, label: "Cards..." },
@@ -18,6 +28,18 @@
         { component: IconButton, icon: boldIcon },
         { component: IconButton, icon: italicIcon },
         { component: IconButton, icon: underlineIcon },
+
+        { component: IconButton, icon: superscriptIcon },
+        { component: IconButton, icon: subscriptIcon },
+
+        { component: IconButton, icon: eraserIcon },
+        { component: IconButton, icon: squareFillIcon },
+        { component: IconButton, icon: squareFillIcon },
+
+        { component: IconButton, icon: bracketsIcon },
+        { component: IconButton, icon: paperclipIcon },
+        { component: IconButton, icon: micIcon },
+        { component: IconButton, icon: threeDotsIcon },
     ];
     export let nightMode: boolean;
 
