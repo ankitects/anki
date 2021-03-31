f03367204569e054dc4e31d6fa706902696958cb
Henrik Giesel <hengiesel@gmail.com>
2021-03-31 15:40:11 +0200

    Add SelectButton

---
+++ ts/editor-toolbar/SelectButton.svelte
@@ -0,0 +1,52 @@
+<script lang="typescript">
+    import { onMount, createEventDispatcher, getContext } from "svelte";
+    import type { Readable } from "svelte/store";
+    import { disabledKey } from "./contextKeys";
+    import SelectOption from "./SelectOption.svelte";
+
+    interface Option {
+        label: string;
+        value: string;
+        selected: boolean;
+    }
+
+    export let id = "";
+    export let className = "";
+    export let props: Record<string, string> = {};
+
+    function extendClassName(classes: string) {
+        return `form-select ${classes}`;
+    }
+
+    export let disables;
+    export let options: Option[];
+
+    let buttonRef: HTMLSelectElement;
+
+    const dispatch = createEventDispatcher();
+    onMount(() => dispatch("mount", { button: buttonRef }));
+
+    const disabledStore = getContext(disabledKey);
+    $: disabled = disables && $disabledStore;
+</script>
+
+<style lang="scss">
+    select {
+        height: 30px;
+        width: auto;
+
+        font-size: smaller;
+        border-radius: 0;
+    }
+</style>
+
+<select
+    bind:this={buttonRef}
+    {disabled}
+    {id}
+    class={extendClassName(className)}
+    {...props}>
+    {#each options as option}
+        <SelectOption {...option} />
+    {/each}
+</select>
