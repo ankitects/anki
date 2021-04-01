10952de3c5fd356a9abe5b3dfb8f73d64379de31
Henrik Giesel <hengiesel@gmail.com>
2021-04-01 22:24:22 +0200

    Remove lazyProperties -> we use withLazyProperties instead

--- ts/lib/lazy.ts
+++ ts/lib/lazy.ts
@@ -1,29 +1,3 @@
-export function lazyProperties(
-    object: Record<string, unknown>,
-    properties: Record<string, () => unknown>
-): void {
-    const propertyDescriptorMap = Object.entries(properties)
-        .map(([name, getter]: [string, () => unknown]): [
-            string,
-            PropertyDescriptor
-        ] => [
-            name,
-            {
-                get: getter,
-                enumerable: true,
-            },
-        ])
-        .reduce(
-            (
-                accumulator: PropertyDescriptorMap,
-                [name, property]
-            ): PropertyDescriptorMap => ((accumulator[name] = property), accumulator),
-            {}
-        );
-
-    Object.defineProperties(object, propertyDescriptorMap);
-}
-
 export function withLazyProperties(
     object: Record<string, unknown>,
     properties: Record<string, () => unknown>
