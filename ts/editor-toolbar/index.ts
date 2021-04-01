cd69c7ffa37dd668d8deab599d5039fa59e03961
Henrik Giesel <hengiesel@gmail.com>
2021-04-01 02:26:56 +0200

    Add exports for add-ons

--- ts/editor-toolbar/index.ts
+++ ts/editor-toolbar/index.ts
@@ -1,18 +1,18 @@
 import type { SvelteComponent } from "svelte";
-
-import { checkNightMode } from "anki/nightmode";
-import { setupI18n, ModuleName } from "anki/i18n";
+import { writable } from "svelte/store";
 
 import EditorToolbarSvelte from "./EditorToolbar.svelte";
 
-// @ts-ignore
-export { updateActiveButtons, clearActiveButtons } from "./CommandIconButton.svelte";
-import { writable } from "svelte/store";
+import { checkNightMode } from "anki/nightmode";
+import { setupI18n, ModuleName } from "anki/i18n";
 
 import { notetypeButtons } from "./notetype";
 import { formatButtons } from "./format";
 import { colorButtons } from "./color";
 import { templateButtons, templateMenus } from "./template";
+
+// @ts-ignore
+export { updateActiveButtons, clearActiveButtons } from "./CommandIconButton.svelte";
 
 const defaultButtons = [notetypeButtons, formatButtons, colorButtons, templateButtons];
 const defaultMenus = [...templateMenus];
@@ -55,3 +55,13 @@ class EditorToolbar extends HTMLElement {
 }
 
 customElements.define("anki-editor-toolbar", EditorToolbar);
+
+/* Exports for add-ons */
+export { default as LabelButton } from "./LabelButton.svelte";
+export { default as IconButton } from "./IconButton.svelte";
+export { default as CommandIconButton } from "./CommandIconButton.svelte";
+export { default as SelectButton } from "./SelectButton.svelte";
+
+export { default as DropdownMenu } from "./DropdownMenu.svelte";
+export { default as DropdownItem } from "./DropdownItem.svelte";
+export { default as WithDropdownMenu } from "./WithDropdownMenu.svelte";
