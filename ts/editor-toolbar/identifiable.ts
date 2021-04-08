42e580430a4f6b98d990cde756453c6b67f6f847
Henrik Giesel <hengiesel@gmail.com>
2021-04-09 00:51:20 +0200

    Add a button / buttonGroup insertion API

---
+++ ts/editor-toolbar/identifiable.ts
@@ -0,0 +1,50 @@
+export interface Identifiable {
+    id?: string;
+}
+
+function normalize<T extends Identifiable>(
+    values: T[],
+    idOrIndex: string | number
+): number {
+    const normalizedIndex =
+        typeof idOrIndex === "string"
+            ? values.findIndex((value) => value.id === idOrIndex)
+            : idOrIndex >= 0
+            ? idOrIndex
+            : values.length + idOrIndex;
+
+    return normalizedIndex >= values.length ? -1 : normalizedIndex;
+}
+
+export function search<T extends Identifiable>(
+    values: T[],
+    idOrIndex: string | number
+): T | null {
+    const index = normalize(values, idOrIndex);
+
+    return index >= 0 ? values[index] : null;
+}
+
+export function insert<T extends Identifiable>(
+    values: T[],
+    value: T,
+    idOrIndex: string | number
+): T[] {
+    const index = normalize(values, idOrIndex);
+
+    return index >= 0
+        ? [...values.slice(0, index), value, ...values.slice(index)]
+        : values;
+}
+
+export function add<T extends Identifiable>(
+    values: T[],
+    value: T,
+    idOrIndex: string | number
+): T[] {
+    const index = normalize(values, idOrIndex);
+
+    return index >= 0
+        ? [...values.slice(0, index + 1), value, ...values.slice(index + 1)]
+        : values;
+}
