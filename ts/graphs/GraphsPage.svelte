8f0f8f9af8db2d9f85b499da828b9ab7ade5cf5d
Henrik Giesel <hengiesel@gmail.com>
2021-04-13 16:49:47 +0200

    Use graphs-base and congrats-base

--- ts/graphs/GraphsPage.svelte
+++ ts/graphs/GraphsPage.svelte
@@ -3,8 +3,6 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="typescript">
-    import "../sass/core.css";
-
     import type { SvelteComponent } from "svelte/internal";
     import { writable } from "svelte/store";
 
