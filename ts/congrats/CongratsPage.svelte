630ee9c6bb391f44fdeedb8fb6a267eae2575cb3
Henrik Giesel <hengiesel@gmail.com>
2021-04-13 16:49:47 +0200

    Use graphs-base and congrats-base

--- ts/congrats/CongratsPage.svelte
+++ ts/congrats/CongratsPage.svelte
@@ -3,8 +3,6 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
-    import "../sass/core.css";
-
     import type pb from "anki/backend_proto";
     import { buildNextLearnMsg } from "./lib";
     import { bridgeLink } from "anki/bridgecommand";
