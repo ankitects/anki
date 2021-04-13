e43f11339fa2fc85bb69e1cf774391a43de3764a
RumovZ <gp5glkw78@relay.firefox.com>
2021-04-13 11:30:32 +0200

    Declare all legacy pathnames in browser init

--- qt/aqt/browser/sidebar/__init__.py
+++ qt/aqt/browser/sidebar/__init__.py
@@ -1,8 +1,5 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-import sys
-
-import aqt
 from anki.utils import isMac
 from aqt.theme import theme_manager
 
@@ -16,7 +13,3 @@ from .model import SidebarModel
 from .searchbar import SidebarSearchBar
 from .toolbar import SidebarTool, SidebarToolbar
 from .tree import SidebarStage, SidebarTreeView
-
-# alias for the legacy pathname
-sys.modules["aqt.sidebar"] = sys.modules["aqt.browser.sidebar"]
-aqt.sidebar = sys.modules["aqt.browser.sidebar"]  # type: ignore
