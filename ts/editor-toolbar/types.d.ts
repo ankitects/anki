a6b102fadd583c72ef8d753dfa8abe418a203e4d
Henrik Giesel <hengiesel@gmail.com>
2021-04-15 15:59:52 +0200

    Add license headers

--- ts/editor-toolbar/types.d.ts
+++ ts/editor-toolbar/types.d.ts
@@ -1,3 +1,5 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import type { DynamicSvelteComponent } from "sveltelib/dynamicComponent";
 import type { SvelteComponentDev } from "svelte/internal";
 
