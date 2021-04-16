f2e313664c90471ae7cdefc8fc630a18a497b1ff
Henrik Giesel <hengiesel@gmail.com>
2021-04-16 17:14:47 +0200

    Adjust ButtonDropdown for night mode

--- ts/editor-toolbar/ButtonDropdown.svelte
+++ ts/editor-toolbar/ButtonDropdown.svelte
@@ -3,6 +3,8 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="typescript">
+    import { getContext } from "svelte";
+    import { nightModeKey } from "./contextKeys";
     import type { ToolbarItem } from "./types";
     import ButtonGroup from "./ButtonGroup.svelte";
 
@@ -10,10 +12,21 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export let className = "";
 
     function extendClassName(className: string): string {
-        return `dropdown-menu bg-transparent border-0 ${className}`;
+        return `dropdown-menu py-1 mb-0 ${
+            nightMode ? "btn-dropdown-menu--night-mode " : ""
+        }${className}`;
     }
 
     export let buttons: ToolbarItem[];
+
+    const nightMode = getContext(nightModeKey);
 </script>
+
+<style>
+    :global(.btn-dropdown-menu--night-mode) {
+        background-color: var(--window-bg);
+        border-color: var(--medium-border);
+    }
+</style>
 
 <ButtonGroup {id} className={extendClassName(className)} {buttons} />
