03f3dfa3d9b3d507740ed0346de9bda5ee055ac7
Damien Elmes <gpg@ankiweb.net>
2021-04-16 20:07:49 +1000

    switch to full lodash library
    Individual packages are going away in v5, and we can rely on tree
    shaking to only get what we need.

--- ts/graphs/tooltip.ts
+++ ts/graphs/tooltip.ts
@@ -1,7 +1,7 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import throttle from "lodash.throttle";
+import { throttle, DebouncedFunc } from "lodash-es";
 import Tooltip from "./Tooltip.svelte";
 
 let tooltip: Tooltip | null = null;
@@ -24,7 +24,9 @@ function showTooltipInner(msg: string, x: number, y: number): void {
     tooltip.$set({ html: msg, x, y, show: true });
 }
 
-export const showTooltip = throttle(showTooltipInner, 16);
+export const showTooltip: DebouncedFunc<
+    (msg: string, x: number, y: number) => void
+> = throttle(showTooltipInner, 16);
 
 export function hideTooltip(): void {
     const tooltip = getOrCreateTooltip();
