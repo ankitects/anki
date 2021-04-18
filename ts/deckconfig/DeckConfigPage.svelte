fc419e3b813401cfff8e2cd40ac26bb13dfa80fe
Damien Elmes <gpg@ankiweb.net>
2021-04-18 13:02:33 +1000

    tweak deckconfig top area formatting
    I suspect there's a better way to ensure selector and save button are
    shown on the same line on both large and small widths; PRs are welcome.

--- ts/deckconfig/DeckConfigPage.svelte
+++ ts/deckconfig/DeckConfigPage.svelte
@@ -19,20 +19,16 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     .inner {
         padding: 0.5em;
     }
-
-    :global(input, select) {
-        font-size: 16px;
-    }
 </style>
 
 <div id="modal">
     <!-- filled in later-->
 </div>
 
+<ConfigSelector {state} />
+
 <div class="outer">
     <div class="inner">
-        <ConfigSelector {state} />
-
         <ConfigEditor {state} />
     </div>
 </div>
