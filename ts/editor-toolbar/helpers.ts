7aaf7b7fe0996edf5d23ca926e1c65b48082dd43
Henrik Giesel <hengiesel@gmail.com>
2021-04-22 16:49:30 +0200

    Add license headers

--- ts/editor-toolbar/helpers.ts
+++ ts/editor-toolbar/helpers.ts
@@ -1,3 +1,5 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 export function mergeTooltipAndShortcut(
     tooltip: string | undefined,
     shortcutLabel: string | undefined
