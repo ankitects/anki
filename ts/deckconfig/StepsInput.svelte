0e9f36c416848bebded4189e5cef15fc9c4ebabd
Damien Elmes <gpg@ankiweb.net>
2021-04-22 17:28:14 +1000

    fill width when steps exceed 2; add bottom margin

--- ts/deckconfig/StepsInput.svelte
+++ ts/deckconfig/StepsInput.svelte
@@ -29,6 +29,13 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     }
 </script>
 
-<ConfigEntry {label} {subLabel} {value} {defaultValue} {warnings} on:revert={revert}>
+<ConfigEntry
+    {label}
+    {subLabel}
+    {value}
+    {defaultValue}
+    {warnings}
+    wholeLine={value.length > 2}
+    on:revert={revert}>
     <input type="text" value={stringValue} on:blur={update} class="form-control" />
 </ConfigEntry>
