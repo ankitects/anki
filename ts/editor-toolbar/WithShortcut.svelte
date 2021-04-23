00f170dddc96607487d555275cdb8677bf02467a
Damien Elmes <gpg@ankiweb.net>
2021-04-23 10:16:40 +1000

    update remaining anki/ references

--- ts/editor-toolbar/WithShortcut.svelte
+++ ts/editor-toolbar/WithShortcut.svelte
@@ -5,10 +5,10 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <script lang="typescript">
     import type { DynamicSvelteComponent } from "sveltelib/dynamicComponent";
     import type { ToolbarItem } from "./types";
-    import type { Modifier } from "anki/shortcuts";
+    import type { Modifier } from "lib/shortcuts";
 
     import { onDestroy } from "svelte";
-    import { registerShortcut, getPlatformString } from "anki/shortcuts";
+    import { registerShortcut, getPlatformString } from "lib/shortcuts";
 
     export let button: ToolbarItem;
     export let shortcut: string;
