5dfbbb9f9f40965e2dff1cef140f142990968f61
Henrik Giesel <hengiesel@gmail.com>
2021-04-26 21:45:28 +0200

    Remove unused import

--- ts/deckoptions/ConfigSelector.svelte
+++ ts/deckoptions/ConfigSelector.svelte
@@ -4,7 +4,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
     import * as tr from "lib/i18n";
-    import type { DeckOptionsState, ConfigListEntry } from "./lib";
+    import type { DeckOptionsState } from "./lib";
 
     import StickyBar from "sveltelib/StickyBar.svelte";
     import { getOptionsDropdown } from "./optionsDropdown";
