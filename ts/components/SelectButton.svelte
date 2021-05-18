94cdebe59c2de04ad09b0c95f9cc1818fbd91d69
Henrik Giesel <hengiesel@gmail.com>
2021-05-18 17:50:18 +0200

    Make styling on select button behave correctly

--- ts/components/SelectButton.svelte
+++ ts/components/SelectButton.svelte
@@ -5,7 +5,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <script lang="typescript">
     import type { Readable } from "svelte/store";
     import { onMount, createEventDispatcher, getContext } from "svelte";
-    import { disabledKey } from "./contextKeys";
+    import { disabledKey, nightModeKey } from "./contextKeys";
 
     export let id: string | undefined = undefined;
     let className = "";
@@ -15,16 +15,19 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
     export let disables = true;
 
+    const nightMode = getContext<boolean>(nightModeKey);
+    const disabled = getContext<Readable<boolean>>(disabledKey);
+    $: _disabled = disables && $disabled;
+
     let buttonRef: HTMLSelectElement;
 
     const dispatch = createEventDispatcher();
     onMount(() => dispatch("mount", { button: buttonRef }));
-
-    const disabled = getContext<Readable<boolean>>(disabledKey);
-    $: _disabled = disables && $disabled;
 </script>
 
 <style lang="scss">
+    @use "ts/sass/button_mixins" as button;
+
     select {
         display: inline-block;
         vertical-align: middle;
@@ -37,14 +40,13 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         box-shadow: none;
         border-radius: 0;
 
-        &:hover {
-            background-color: #eee;
-        }
-
         &:focus {
             outline: none;
         }
     }
+
+    @include button.btn-day($with-hover: false);
+    @include button.btn-night($with-hover: false);
 </style>
 
 <!-- svelte-ignore a11y-no-onchange -->
@@ -54,7 +56,9 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     bind:this={buttonRef}
     disabled={_disabled}
     {id}
-    class={` ${className}`}
+    class={className}
+    class:btn-day={!nightMode}
+    class:btn-night={nightMode}
     title={tooltip}
     on:change>
     <slot />
