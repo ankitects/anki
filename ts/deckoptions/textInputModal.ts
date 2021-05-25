412091ae60892b4635ed4aaaaae63444a19a6f24
Henrik Giesel <hengiesel@gmail.com>
2021-05-25 15:30:43 +0200

    Inline some bootstrap-dark styling

--- ts/deckoptions/textInputModal.ts
+++ ts/deckoptions/textInputModal.ts
@@ -7,17 +7,26 @@
 
 import TextInputModal from "./TextInputModal.svelte";
 
+import { checkNightMode } from "lib/nightmode";
+import { nightModeKey } from "components/contextKeys";
+
 export interface TextInputModalProps {
     title: string;
     prompt: string;
     startingValue?: string;
-    onOk: (string) => void;
+    onOk: (text: string) => void;
 }
 
 export function textInputModal(props: TextInputModalProps): TextInputModal {
     const target = document.getElementById("modal")!;
+
+    const nightMode = checkNightMode();
+    const context = new Map();
+    context.set(nightModeKey, nightMode);
+
     return new TextInputModal({
         target,
         props,
-    });
+        context,
+    } as any);
 }
