70c8b8940d4d52e95ad6143a6a73676ba8e0a610
Henrik Giesel <hengiesel@gmail.com>
2021-05-27 22:51:18 +0200

    Allow for gutters between rows

--- ts/deckoptions/ConfigEntry.svelte
+++ ts/deckoptions/ConfigEntry.svelte
@@ -21,7 +21,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     $: renderedTooltip = marked(tooltip);
 </script>
 
-<div {id} class="row gx-0">
+<div {id} class="row gx-0 gy-1 mt-0">
     <div class="col-8 d-flex align-items-center">
         {label}{#if renderedTooltip}<HelpPopup html={renderedTooltip} />{/if}
     </div>
@@ -37,7 +37,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 {#each warnings as warning}
     {#if warning}
-        <div class="row gx-0">
+        <div class="row gx-0 gy-1 mt-0">
             <div class="col-11 alert alert-warning mb-0" in:slide out:slide>
                 {warning}
             </div>
