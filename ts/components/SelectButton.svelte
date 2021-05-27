cb14011410b48a0856e93dfb116d97c1199a1fb1
Henrik Giesel <hengiesel@gmail.com>
2021-05-27 20:28:23 +0200

    Use Bootstrap containers instead of CSS in base for deckoptions

--- ts/components/SelectButton.svelte
+++ ts/components/SelectButton.svelte
@@ -50,6 +50,11 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         overflow-x: hidden;
     }
 
+    .btn-night {
+        /* override the default down arrow */
+        background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23FFFFFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
+    }
+
     @include button.btn-day($with-hover: false);
     @include button.btn-night($with-hover: false);
 </style>
