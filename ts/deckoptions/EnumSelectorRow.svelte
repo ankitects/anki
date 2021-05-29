5fa850703f6274acc221f75d430decdf6a970000
Henrik Giesel <hengiesel@gmail.com>
2021-05-29 13:06:17 +0200

    Implement EnumSelectorRow and StepsInputRow

--- ts/deckoptions/EnumSelectorRow.svelte
+++ ts/deckoptions/EnumSelectorRow.svelte
@@ -3,16 +3,19 @@
     License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
+    import type { Breakpoint } from "Col.svelte";
+
     import marked from "marked";
     import Row from "./Row.svelte";
     import Col from "./Col.svelte";
     import HelpPopup from "./HelpPopup.svelte";
-    import SpinBox from "./SpinBox.svelte";
+    import EnumSelector from "./EnumSelector.svelte";
     import RevertButton from "./RevertButton.svelte";
 
-    export let value: any;
-    export let defaultValue: any;
-    export let min = 0;
+    export let value: number;
+    export let defaultValue: number;
+    export let breakpoint: Breakpoint = "md";
+    export let choices: string[];
     export let markdownTooltip: string;
 </script>
 
@@ -20,8 +23,8 @@
     <Col size={7}>
         <slot /><HelpPopup html={marked(markdownTooltip)} />
     </Col>
-    <Col size={5}>
-        <SpinBox bind:value {min} />
+    <Col {breakpoint} size={5}>
+        <EnumSelector bind:value {choices} />
         <RevertButton bind:value {defaultValue} />
     </Col>
 </Row>
