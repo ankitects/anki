745f94089047b1e49ad904a15ee09525c6323fc1
Henrik Giesel <hengiesel@gmail.com>
2021-05-29 18:15:21 +0200

    Put Col types into col.ts

--- ts/deckoptions/EnumSelectorRow.svelte
+++ ts/deckoptions/EnumSelectorRow.svelte
@@ -3,7 +3,7 @@
     License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
-    import type { Breakpoint } from "Col.svelte";
+    import type { Breakpoint } from "./col";
 
     import marked from "marked";
     import Row from "./Row.svelte";
