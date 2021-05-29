8d32528e8d31baf812fc4a7b901e0c7b137253e5
Henrik Giesel <hengiesel@gmail.com>
2021-05-29 18:15:21 +0200

    Put Col types into col.ts

--- ts/deckoptions/Col.svelte
+++ ts/deckoptions/Col.svelte
@@ -2,12 +2,9 @@
 Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
-<script context="module" lang="ts">
-    export type Size = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-    export type Breakpoint = "xs" | "sm" | "md" | "lg" | "xl";
-</script>
-
 <script lang="ts">
+    import type { Breakpoint, Size } from "./col";
+
     export let breakpoint: Breakpoint | undefined = undefined;
     export let size: Size | undefined = undefined;
     export let grow = true;
