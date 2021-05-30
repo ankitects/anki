6d72e7bc49ed4ab44be4685e3b0a8c4d35c4aa78
Henrik Giesel <hengiesel@gmail.com>
2021-05-30 22:58:40 +0200

    Implement new ColorPicker

--- ts/editor/ColorButtons.svelte
+++ ts/editor/ColorButtons.svelte
@@ -8,66 +8,26 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import ButtonGroup from "components/ButtonGroup.svelte";
     import ButtonGroupItem from "components/ButtonGroupItem.svelte";
     import IconButton from "components/IconButton.svelte";
+    import ColorPicker from "components/ColorPicker.svelte";
     import WithShortcut from "components/WithShortcut.svelte";
     import WithColorHelper from "./WithColorHelper.svelte";
 
     import { textColorIcon, highlightColorIcon } from "./icons";
     import { appendInParentheses } from "./helpers";
 
-    import "./color.css";
-
     export let api = {};
-
-    const foregroundColorKeyword = "--foreground-color";
-    let color = "black";
-
-    $: {
-        document.documentElement.style.setProperty(foregroundColorKeyword, color);
-    }
 
     const wrapWithForecolor = (color: string) => () => {
         document.execCommand("forecolor", false, color);
-    }
+    };
 
     const wrapWithBackcolor = (color: string) => () => {
         document.execCommand("backcolor", false, color);
-    }
-
-    function setWithCurrentColor({ currentTarget }: Event): void {
-        color = (currentTarget as HTMLInputElement).value;
-    }
+    };
 </script>
 
 <ButtonGroup {api}>
-    <!--
-    <ButtonGroupItem>
-        <WithShortcut shortcut={"F7"} let:createShortcut let:shortcutLabel>
-            <IconButton
-                class="forecolor"
-                tooltip={appendInParentheses(
-                    tr.editingSetForegroundColor(),
-                    shortcutLabel
-                )}
-                on:click={wrapWithForecolor}
-                on:mount={createShortcut}
-            >
-                {@html squareFillIcon}
-            </IconButton>
-        </WithShortcut>
-    </ButtonGroupItem>
-
-    <ButtonGroupItem>
-        <WithShortcut shortcut={"F8"} let:createShortcut let:shortcutLabel>
-            <ColorPicker
-                tooltip={appendInParentheses(tr.editingChangeColor(), shortcutLabel)}
-                on:change={setWithCurrentColor}
-                on:mount={createShortcut}
-            />
-        </WithShortcut>
-    </ButtonGroupItem>
-    -->
-
-    <WithColorHelper let:colorHelperIcon let:color>
+    <WithColorHelper let:colorHelperIcon let:color let:setColor>
         <ButtonGroupItem>
             <WithShortcut shortcut={"F7"} let:createShortcut let:shortcutLabel>
                 <IconButton
@@ -83,9 +43,23 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
                 </IconButton>
             </WithShortcut>
         </ButtonGroupItem>
+
+        <ButtonGroupItem>
+            <WithShortcut shortcut={"F8"} let:createShortcut let:shortcutLabel>
+                <IconButton
+                    tooltip={appendInParentheses(
+                        tr.editingChangeColor(),
+                        shortcutLabel
+                    )}
+                    disables={false}
+                >
+                    <ColorPicker on:change={setColor} on:mount={createShortcut} />
+                </IconButton>
+            </WithShortcut>
+        </ButtonGroupItem>
     </WithColorHelper>
 
-    <WithColorHelper let:colorHelperIcon let:color>
+    <WithColorHelper let:colorHelperIcon let:color let:setColor>
         <ButtonGroupItem>
             <WithShortcut shortcut={"F7"} let:createShortcut let:shortcutLabel>
                 <IconButton
@@ -100,6 +74,12 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
                     {@html colorHelperIcon}
                 </IconButton>
             </WithShortcut>
+        </ButtonGroupItem>
+
+        <ButtonGroupItem>
+            <IconButton tooltip={tr.editingChangeColor()} disables={false}>
+                <ColorPicker on:change={setColor} />
+            </IconButton>
         </ButtonGroupItem>
     </WithColorHelper>
 </ButtonGroup>
