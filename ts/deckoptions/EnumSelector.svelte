65f0e7a4ea9404088e8ec39f5350c233c77e0e76
Henrik Giesel <hengiesel@gmail.com>
2021-05-30 19:46:39 +0200

    Use custom down arrow for both SelectButton and EnumSelector

--- ts/deckoptions/EnumSelector.svelte
+++ ts/deckoptions/EnumSelector.svelte
@@ -3,12 +3,26 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
+    import { getContext } from "svelte";
+    import { nightModeKey } from "components/contextKeys";
+
     export let choices: string[];
     export let value: number = 0;
+
+    const nightMode = getContext<boolean>(nightModeKey);
 </script>
 
-<select bind:value class="form-select">
+<select bind:value class:visible-down-arrow={nightMode} class="form-select">
     {#each choices as choice, idx}
         <option value={idx}>{choice}</option>
     {/each}
 </select>
+
+<style lang="scss">
+    @use "ts/sass/button_mixins" as button;
+
+    .visible-down-arrow {
+        /* override the default down arrow */
+        @include button.select-night-mode;
+    }
+</style>
