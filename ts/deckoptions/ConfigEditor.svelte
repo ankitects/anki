eb03d8ec74170b4b32f1f1d1d9def1c6d3aeb85e
Matthias M <62722460+kleinerpirat@users.noreply.github.com>
2021-05-31 11:58:05 +0200

    Apply grid layout to ConfigEditor
    using two classes for the grid items: small and large.

--- ts/deckoptions/ConfigEditor.svelte
+++ ts/deckoptions/ConfigEditor.svelte
@@ -9,7 +9,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import AdvancedOptions from "./AdvancedOptions.svelte";
     import BuryOptions from "./BuryOptions.svelte";
     import LapseOptions from "./LapseOptions.svelte";
-    import GeneralOptions from "./GeneralOptions.svelte";
+    import TimerOptions from "./TimerOptions.svelte";
+    import AudioOptions from "./AudioOptions.svelte";
     import Addons from "./Addons.svelte";
     import type { DeckOptionsState } from "./lib";
 
@@ -17,16 +18,43 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </script>
 
 <div class="outer">
-    <DailyLimits {state} />
-    <NewOptions {state} />
-    <LapseOptions {state} />
-    <BuryOptions {state} />
+    <div class="small">
+        <DailyLimits {state} />
+    </div>
+
+    <div class="large">
+        <NewOptions {state} />
+    </div>
+
+    <div class="large">
+        <LapseOptions {state} />
+    </div>
+
+    <div class="small">
+        <BuryOptions {state} />
+    </div>
+
     {#if state.v3Scheduler}
-        <DisplayOrder {state} />
+        <div class="small">
+            <DisplayOrder {state} />
+        </div>
     {/if}
-    <GeneralOptions {state} />
-    <Addons {state} />
-    <AdvancedOptions {state} />
+
+    <div class="small">
+        <TimerOptions {state} />
+    </div>
+
+    <div class="small">
+        <AudioOptions {state} />
+    </div>
+
+    <div class="small">
+        <Addons {state} />
+    </div>
+
+    <div class="large">
+        <AdvancedOptions {state} />
+    </div>
 </div>
 
 <style lang="scss">
@@ -35,14 +63,28 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         font-weight: bold;
         // adding a border decreases the default font size,
         // so increase it again
-        font-size: 2em;
+        font-size: 1.5em;
         border-bottom: 1px solid var(--medium-border);
         margin-right: 16px;
         margin-bottom: 0.5em;
     }
     .outer {
         // the right margin has an indent to allow for the undo
-        // buttons; add the same indent on the left for balance
-        padding-left: 16px;
+        // buttons; add some indentation on the left for balance
+        padding-left: 6px;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(24em, 1fr));
+        column-gap: 2em;
+        row-gap: 1.5em;
     }
+
+    .small {
+        grid-row: span 1;
+    }
+
+    // make grid items with more options span two rows
+    .large {
+        grid-row: span 2;
+    }
+
 </style>
