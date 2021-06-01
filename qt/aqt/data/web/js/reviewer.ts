daa696e703f0bbd2749e5a75c4c4f1e1888d3567
RumovZ <gp5glkw78@relay.firefox.com>
2021-06-01 18:14:54 +0200

    Tweak flag colors

--- qt/aqt/data/web/js/reviewer.ts
+++ qt/aqt/data/web/js/reviewer.ts
@@ -146,13 +146,13 @@ function _showAnswer(a: string, bodyclass: string): void {
 }
 
 const _flagColours = {
-    1: "#ff6666",
-    2: "#ff9900",
-    3: "#77ff77",
-    4: "#77aaff",
-    5: "#fc68e8",
-    6: "#00d3b7",
-    7: "#9e3bfa",
+    1: "#e25252",
+    2: "#ffb347",
+    3: "#54c414",
+    4: "#578cff",
+    5: "#ff82ee",
+    6: "#00d1b5",
+    7: "#9649dd",
 };
 
 function _drawFlag(flag: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7): void {
