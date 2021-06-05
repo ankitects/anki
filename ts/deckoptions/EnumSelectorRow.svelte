99caaf8d71542d5c28a2b11033aa91aca647d7ed
Henrik Giesel <hengiesel@gmail.com>
2021-06-05 14:35:02 +0200

    Switch Revert button and input element position

--- ts/deckoptions/EnumSelectorRow.svelte
+++ ts/deckoptions/EnumSelectorRow.svelte
@@ -29,7 +29,7 @@
         </WithTooltip>
     </Col>
     <Col {breakpoint} size={5}>
-        <EnumSelector bind:value {choices} />
         <RevertButton bind:value {defaultValue} />
+        <EnumSelector bind:value {choices} />
     </Col>
 </Row>
