6c9de074b645ab17c1939e2073021cbc693284d1
Henrik Giesel <hengiesel@gmail.com>
2021-06-05 18:47:16 +0200

    Fix tooltip regression

--- ts/deckoptions/BuryOptions.svelte
+++ ts/deckoptions/BuryOptions.svelte
@@ -21,7 +21,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         <CheckBoxRow
             bind:value={$config.buryNew}
             defaultValue={defaults.buryNew}
-            markdownTooltip={tr.deckConfigBuryTitle()}
+            markdownTooltip={tr.deckConfigBuryTooltip()}
         >
             {tr.deckConfigBuryNewSiblings()}
         </CheckBoxRow>
