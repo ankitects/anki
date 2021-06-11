9a00ef5a81cef9c3d5272772b47c901cdc96a2d8
Henrik Giesel <hengiesel@gmail.com>
2021-06-12 00:54:20 +0200

    Properly dispose of tooltip resources when unmounting

--- ts/deckoptions/WithTooltip.svelte
+++ ts/deckoptions/WithTooltip.svelte
@@ -3,19 +3,28 @@
     License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
+    import { onDestroy } from "svelte";
     import Tooltip from "bootstrap/js/dist/tooltip";
 
     export let tooltip: string;
 
+    let tooltipObject: Tooltip;
+
     function createTooltip(element: HTMLElement): void {
         element.title = tooltip;
-        new Tooltip(element, {
+        tooltipObject = new Tooltip(element, {
             placement: "bottom",
             html: true,
             offset: [0, 20],
             delay: { show: 250, hide: 0 },
         });
     }
+
+    onDestroy(() => {
+        if (tooltipObject) {
+            tooltipObject.dispose();
+        }
+    });
 </script>
 
 <slot {createTooltip} />
