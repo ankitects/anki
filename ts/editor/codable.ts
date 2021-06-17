8b15c81d347ec28cdfff8659d81900695e08aaae
Henrik Giesel <hengiesel@gmail.com>
2021-06-17 14:44:03 +0200

    Display CodeMirror in editingArea

--- ts/editor/codable.ts
+++ ts/editor/codable.ts
@@ -1,6 +1,10 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import CodeMirror from "codemirror/src/codemirror"
+
 export class Codable extends HTMLTextAreaElement {
-    connectedCallback(): void {}
+    connectedCallback(): void {
+        CodeMirror.fromTextArea(this);
+    }
 }
