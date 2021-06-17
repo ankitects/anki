df32f6002936ae65a2ce9279cceff37cef71cbd3
Henrik Giesel <hengiesel@gmail.com>
2021-06-17 18:36:12 +0200

    Successfully add monokai theming

--- ts/editor/codable.ts
+++ ts/editor/codable.ts
@@ -1,7 +1,8 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import CodeMirror from "codemirror/src/codemirror";
+import * as CodeMirror from "codemirror/lib/codemirror";
+import "codemirror/mode/htmlmixed/htmlmixed";
 
 const codeMirrorOptions = {
     lineNumbers: true,
