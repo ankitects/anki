05fa7daa97db9ffb14409cf6b385f447e38c16fe
Henrik Giesel <hengiesel@gmail.com>
2021-06-19 16:31:35 +0200

    Add type hint in WithColorHelper

--- ts/editor/WithColorHelper.svelte
+++ ts/editor/WithColorHelper.svelte
@@ -5,7 +5,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <script lang="typescript">
     import { colorHelperIcon } from "./icons";
 
-    export let color;
+    export let color: string;
 
     function setColor({ currentTarget }: Event): string {
         return (color = (currentTarget! as HTMLInputElement).value);
