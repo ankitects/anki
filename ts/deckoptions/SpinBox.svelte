b89fbe1aa54267ce6deec276aa7d8678b3b27e6a
Henrik Giesel <hengiesel@gmail.com>
2021-06-21 21:51:15 +0200

    Replace dash with underscore for night_mode_lib

--- ts/deckoptions/SpinBox.svelte
+++ ts/deckoptions/SpinBox.svelte
@@ -34,7 +34,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 />
 
 <style lang="scss">
-    @use "ts/sass/night-mode" as nightmode;
+    @use "ts/sass/night_mode" as nightmode;
 
     .nightMode {
         @include nightmode.input;
