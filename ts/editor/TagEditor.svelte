8b92ba7fa433beb378937f769afbd6eacac18496
Henrik Giesel <hengiesel@gmail.com>
2021-06-23 17:35:50 +0200

    Add Tag.svelte

--- ts/editor/TagEditor.svelte
+++ ts/editor/TagEditor.svelte
@@ -4,8 +4,11 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="typescript">
     import StickyBottom from "components/StickyBottom.svelte";
+    import Tag from "./Tag.svelte";
 </script>
 
 <StickyBottom>
-    <span>Tags</span>
+    <span>Tags:</span>
+    <Tag>en::foobar</Tag>
+    <Tag>zh::あちこ</Tag>
 </StickyBottom>
