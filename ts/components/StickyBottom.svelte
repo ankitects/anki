f9b320e105e70000fbe1a63ff467bf3689b7099a
Henrik Giesel <hengiesel@gmail.com>
2021-06-23 17:11:19 +0200

    Mount TagEditor

---
+++ ts/components/StickyBottom.svelte
@@ -0,0 +1,26 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="typescript">
+    export let id: string | undefined = undefined;
+    let className: string = "";
+    export { className as class };
+</script>
+
+<footer {id} class={`container-fluid py-1 ${className}`}>
+    <slot />
+</footer>
+
+<style lang="scss">
+    footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 10;
+
+        background: var(--window-bg);
+        border-top: 1px solid var(--medium-border);
+    }
+</style>
---
+++ ts/editor/TagEditor.svelte
@@ -0,0 +1,11 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="typescript">
+    import StickyBottom from "components/StickyBottom.svelte";
+</script>
+
+<StickyBottom>
+    <span>Tags</span>
+</StickyBottom>
