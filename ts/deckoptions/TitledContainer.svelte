4128d9fc5d6926bb7f31811556ab0b1112455181
Matthias M <62722460+kleinerpirat@users.noreply.github.com>
2021-06-27 19:57:34 +0200

    Move h1 CSS to TitledContainer
    Overlooked that one.

--- ts/deckoptions/TitledContainer.svelte
+++ ts/deckoptions/TitledContainer.svelte
@@ -32,6 +32,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <style lang="scss">
     h1 {
         border-bottom: 1px solid var(--medium-border);
+        font-size: clamp(1rem, calc(1.375rem + 1.5vw), 2rem);
     }
     div {
         grid-row: span var(--size);
