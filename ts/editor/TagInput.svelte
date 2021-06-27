3857ac07d7c78da548d480d1cd1576d98de61031
Henrik Giesel <hengiesel@gmail.com>
2021-06-27 03:46:42 +0200

    Implement tagsplit

--- ts/editor/TagInput.svelte
+++ ts/editor/TagInput.svelte
@@ -86,8 +86,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
     function onEnter(event: Event): void {
         event.preventDefault();
-        dispatch("tagenter");
-        input.blur();
+        dispatch("tagsplit", { start: input.selectionStart, end: input.selectionEnd });
     }
 
     function onKeydown(event: KeyboardEvent): void {
