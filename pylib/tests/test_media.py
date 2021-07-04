14110add551dbb4b8b6857336513a16bb129cb06
Damien Elmes <gpg@ankiweb.net>
2021-07-04 15:27:29 +1000

    stop (un)escaping media filenames
    Back in the WebKit days, images with Unicode filenames would fail to
    appear if they weren't percent-escaped. This no longer seems to be the
    case - with this patch, images appear correctly on the Mac and Windows
    platforms I tested with.
    
    Fixes https://forums.ankiweb.net/t/anki-2-1-45-beta/10664/96
    Fixes #1219

--- pylib/tests/test_media.py
+++ pylib/tests/test_media.py
@@ -49,10 +49,6 @@ def test_strings():
     assert sp("aoeu") == "aoeu"
     assert sp("aoeu[sound:foo.mp3]aoeu") == "aoeuaoeu"
     assert sp("a<img src=yo>oeu") == "aoeu"
-    es = col.media.escape_media_filenames
-    assert es("aoeu") == "aoeu"
-    assert es("<img src='http://foo.com'>") == "<img src='http://foo.com'>"
-    assert es('<img src="foo bar.jpg">') == '<img src="foo%20bar.jpg">'
 
 
 def test_deckIntegration():
