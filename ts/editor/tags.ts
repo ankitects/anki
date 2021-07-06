6ccf75a077c414a985c2d1ba2942338054aeccdf
Henrik Giesel <hengiesel@gmail.com>
2021-07-06 23:37:49 +0200

    Improve space behavior

--- ts/editor/tags.ts
+++ ts/editor/tags.ts
@@ -5,10 +5,10 @@ export function normalizeTagname(tagname: string): string {
     let trimmed = tagname.trim();
 
     while (true) {
-        if (trimmed.startsWith("::")) {
-            trimmed = trimmed.slice(2).trimStart();
-        } else if (trimmed.endsWith("::")) {
-            trimmed = trimmed.slice(0, -2).trimEnd();
+        if (trimmed.startsWith(":")) {
+            trimmed = trimmed.slice(1).trimStart();
+        } else if (trimmed.endsWith(":")) {
+            trimmed = trimmed.slice(0, -1).trimEnd();
         } else {
             return trimmed;
         }
