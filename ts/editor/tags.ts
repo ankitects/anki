3de1d6e604c110f95b64992cb1c2a56bc715c064
Henrik Giesel <hengiesel@gmail.com>
2021-07-08 21:25:01 +0200

    Use Unicode delimiter character

--- ts/editor/tags.ts
+++ ts/editor/tags.ts
@@ -1,13 +1,23 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+export const delimChar = "\u2237";
+
+export function replaceWithDelimChar(name: string): string {
+    return name.replace(/::/g, delimChar);
+}
+
+export function replaceWithColon(name: string): string {
+    return name.replace(/\u2237/gu, "::");
+}
+
 export function normalizeTagname(tagname: string): string {
     let trimmed = tagname.trim();
 
     while (true) {
-        if (trimmed.startsWith(":")) {
+        if (trimmed.startsWith(":") || trimmed.startsWith(delimChar)) {
             trimmed = trimmed.slice(1).trimStart();
-        } else if (trimmed.endsWith(":")) {
+        } else if (trimmed.endsWith(":") || trimmed.endsWith(delimChar)) {
             trimmed = trimmed.slice(0, -1).trimEnd();
         } else {
             return trimmed;
