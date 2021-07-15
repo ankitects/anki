ankitects/anki

commit dc61a6f54703085ed6dd39bb3bb9879994c5c18e

Remove jQuery from mark and flag in reviewer.ts

qt/aqt/data/web/js/reviewer.ts
@@ -6,7 +6,7 @@ declare var MathJax: any;
 type Callback = () => void | Promise<void>;
 
 var ankiPlatform = "desktop";
-var typeans;
+var typeans: HTMLElement | undefined;
 var _updatingQueue: Promise<void> = Promise.resolve();
 
 var onUpdateHook: Array<Callback>;
@@ -38,7 +38,10 @@ async function _updateQA(
     const renderError = (kind: string) => (error: Error): void => {
         const errorMessage = String(error).substring(0, 2000);
         const errorStack = String(error.stack).substring(0, 2000);
-        qa.innerHTML = `Invalid ${kind} on card: ${errorMessage}\n${errorStack}`.replace(/\n/g, "<br>");
+        qa.innerHTML = `Invalid ${kind} on card: ${errorMessage}\n${errorStack}`.replace(
+            /\n/g,
+            "<br>"
+        );
     };
 
     // hide current card
@@ -119,26 +122,26 @@ const _flagColours = {
 };
 
 function _drawFlag(flag: 0 | 1 | 2 | 3 | 4): void {
-    var elem = $("#_flag");
+    const elem = document.getElementById("_flag");
     if (flag === 0) {
-        elem.hide();
+        elem.setAttribute("hidden", "");
         return;
     }
-    elem.show();
-    elem.css("color", _flagColours[flag]);
+    elem.removeAttribute("hidden");
+    elem.style.color = _flagColours[flag];
 }
 
 function _drawMark(mark: boolean): void {
-    var elem = $("#_mark");
+    const elem = document.getElementById("_mark");
     if (!mark) {
-        elem.hide();
+        elem.setAttribute("hidden", "");
     } else {
-        elem.show();
+        elem.removeAttribute("hidden");
     }
 }
 
 function _typeAnsPress(): void {
-    if ((window.event as KeyboardEvent).keyCode === 13) {
+    if ((window.event as KeyboardEvent).code === "Enter") {
         pycmd("ans");
     }
 }

commit 090a54c6e6b8470c80755f4f7b9bce86ea1779aa

support numpad enter in type answer
https://forums.ankiweb.net/t/anki-2-1-45-beta/10664/149

regression introduced in dc61a6f547

qt/aqt/data/web/js/reviewer.ts
@@ -173,7 +173,8 @@ function _drawMark(mark: boolean): void {
 }
 
 function _typeAnsPress(): void {
-    if ((window.event as KeyboardEvent).code === "Enter") {
+    const code = (window.event as KeyboardEvent).code;
+    if (["Enter", "NumpadEnter"].includes(code)) {
         pycmd("ans");
     }
 }