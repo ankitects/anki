8429d0008134247a62e8a0f02bbeda9246d785ae
Henrik Giesel <hengiesel@gmail.com>
2021-07-21 03:18:33 +0200

    Add functionality to set float of image

---
+++ ts/editor/ImageHandleButtons.svelte
@@ -0,0 +1,38 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="typescript">
+    import { createEventDispatcher } from "svelte";
+
+    import ButtonToolbar from "components/ButtonToolbar.svelte";
+    import ButtonGroup from "components/ButtonGroup.svelte";
+    import ButtonGroupItem from "components/ButtonGroupItem.svelte";
+    import IconButton from "components/IconButton.svelte";
+
+    import { floatLeftIcon, floatRightIcon, resetIcon } from "./icons";
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<ButtonToolbar size={1.6} wrap={false}>
+    <ButtonGroup>
+        <ButtonGroupItem>
+            <IconButton on:click={() => dispatch("floatleft")}
+                >{@html floatLeftIcon}</IconButton
+            >
+        </ButtonGroupItem>
+
+        <ButtonGroupItem>
+            <IconButton on:click={() => dispatch("floatright")}
+                >{@html floatRightIcon}</IconButton
+            >
+        </ButtonGroupItem>
+
+        <ButtonGroupItem>
+            <IconButton on:click={() => dispatch("floatreset")}
+                >{@html resetIcon}</IconButton
+            >
+        </ButtonGroupItem>
+    </ButtonGroup>
+</ButtonToolbar>
