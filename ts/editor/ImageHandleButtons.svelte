8a8cd4ee382f506f74b6e1872e54c01f43ea21e0
Henrik Giesel <hengiesel@gmail.com>
2021-07-21 04:32:15 +0200

    Add float tooltips

--- ts/editor/ImageHandleButtons.svelte
+++ ts/editor/ImageHandleButtons.svelte
@@ -3,6 +3,8 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="typescript">
+    import * as tr from "lib/i18n";
+
     import ButtonToolbar from "components/ButtonToolbar.svelte";
     import ButtonGroup from "components/ButtonGroup.svelte";
     import ButtonGroupItem from "components/ButtonGroupItem.svelte";
@@ -16,19 +18,24 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <ButtonToolbar size={1.6} wrap={false}>
     <ButtonGroup>
         <ButtonGroupItem>
-            <IconButton active={float === "left"} on:click={() => (float = "left")}
-                >{@html floatLeftIcon}</IconButton
-            >
-        </ButtonGroupItem>
-
-        <ButtonGroupItem>
-            <IconButton active={float === "right"} on:click={() => (float = "right")}
-                >{@html floatRightIcon}</IconButton
+            <IconButton
+                tooltip={tr.editingFloatLeft()}
+                active={float === "left"}
+                on:click={() => (float = "left")}>{@html floatLeftIcon}</IconButton
             >
         </ButtonGroupItem>
 
         <ButtonGroupItem>
             <IconButton
+                tooltip={tr.editingFloatRight()}
+                active={float === "right"}
+                on:click={() => (float = "right")}>{@html floatRightIcon}</IconButton
+            >
+        </ButtonGroupItem>
+
+        <ButtonGroupItem>
+            <IconButton
+                tooltip={tr.editingFloatRemove()}
                 active={float === "" || float === "none"}
                 on:click={() => (float = "")}>{@html resetIcon}</IconButton
             >
