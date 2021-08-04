fce34cafd906d8ab28defe7166934a2ef32ba118
Henrik Giesel <hengiesel@gmail.com>
2021-08-04 22:25:05 +0200

    Introduce HandleSelection.svelte

---
+++ ts/editor/HandleSelection.svelte
@@ -0,0 +1,18 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<div>
+    <slot />
+</div>
+
+<style lang="scss">
+    div {
+        position: absolute;
+
+        left: var(--left, 0);
+        top: var(--top, 0);
+        width: var(--width);
+        height: var(--height);
+    }
+</style>
