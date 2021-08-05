f2f93ef67e2d14ea69917f74013078bf89ca2bc5
Henrik Giesel <hengiesel@gmail.com>
2021-08-05 22:48:36 +0200

    Move more logic into HandleSelection

--- ts/editor/HandleSelection.svelte
+++ ts/editor/HandleSelection.svelte
@@ -2,7 +2,47 @@
 Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
-<div>
+<script lang="ts">
+    export let container: HTMLElement;
+    export let activeImage: HTMLImageElement | null;
+
+    $: if (activeImage) {
+        updateSelection();
+    } else {
+        reset();
+    }
+
+    let left: number;
+    let top: number;
+    let width: number;
+    let height: number;
+
+    export function updateSelection() {
+        const containerRect = container.getBoundingClientRect();
+        const imageRect = activeImage!.getBoundingClientRect();
+
+        const containerLeft = containerRect.left;
+        const containerTop = containerRect.top;
+
+        left = imageRect!.left - containerLeft;
+        top = imageRect!.top - containerTop;
+        width = activeImage!.clientWidth;
+        height = activeImage!.clientHeight;
+    }
+
+    function reset() {
+        activeImage = null;
+
+        left = 0;
+        top = 0;
+        width = 0;
+        height = 0;
+    }
+</script>
+
+<div
+    style="--left: {left}px; --top: {top}px; --width: {width}px; --height: {height}px;"
+>
     <slot />
 </div>
 
