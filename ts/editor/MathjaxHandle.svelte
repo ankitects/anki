70e4fd5a3bb0d220a5e56a23f99dccf92969d8a3
Henrik Giesel <hengiesel@gmail.com>
2021-08-05 23:45:45 +0200

    Add HandleControl to MathjaxHandle

--- ts/editor/MathjaxHandle.svelte
+++ ts/editor/MathjaxHandle.svelte
@@ -3,8 +3,9 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
-    import HandleBackground from "./HandleBackground.svelte";
     import HandleSelection from "./HandleSelection.svelte";
+    import HandleBackground from "./HandleBackground.svelte";
+    import HandleControl from "./HandleControl.svelte";
 
     export let activeImage: HTMLImageElement | null = null;
     export let container: HTMLElement;
@@ -13,5 +14,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <HandleSelection {container} {activeImage}>
     {#if activeImage}
         <HandleBackground />
+
+        <HandleControl active={false} />
     {/if}
 </HandleSelection>
