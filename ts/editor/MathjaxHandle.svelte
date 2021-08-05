90b0000f7b700e2052122ef508385f0e832f86d6
Henrik Giesel <hengiesel@gmail.com>
2021-08-06 00:59:52 +0200

    Add InlineBlock buttons to mathjax components

--- ts/editor/MathjaxHandle.svelte
+++ ts/editor/MathjaxHandle.svelte
@@ -6,15 +6,43 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import HandleSelection from "./HandleSelection.svelte";
     import HandleBackground from "./HandleBackground.svelte";
     import HandleControl from "./HandleControl.svelte";
+    import MathjaxHandleInlineBlock from "./MathjaxHandleInlineBlock.svelte";
 
     export let activeImage: HTMLImageElement | null = null;
     export let container: HTMLElement;
+    export let isRtl: boolean;
+
+    let updateSelection: () => void;
 </script>
 
-<HandleSelection {container} {activeImage}>
+<HandleSelection {container} {activeImage} offsetX={2} offsetY={2} bind:updateSelection>
     {#if activeImage}
         <HandleBackground />
 
-        <HandleControl active={false} />
+        <div
+            class="mathjax-handle-inline-block"
+            class:is-rtl={isRtl}
+            on:click={updateSelection}
+        >
+            <MathjaxHandleInlineBlock {activeImage} {isRtl} />
+        </div>
+
+        <HandleControl />
     {/if}
 </HandleSelection>
+
+<style lang="scss">
+    div {
+        position: absolute;
+    }
+
+    .mathjax-handle-inline-block {
+        left: 3px;
+        top: 3px;
+
+        &.is-rtl {
+            left: auto;
+            right: 3px;
+        }
+    }
+</style>
