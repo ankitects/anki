b7d2791e92142b941e628f5df142ada4fd66ee1c
Henrik Giesel <hengiesel@gmail.com>
2021-08-05 04:07:04 +0200

    Remove unused import tick

--- ts/editor/ImageHandle.svelte
+++ ts/editor/ImageHandle.svelte
@@ -10,7 +10,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import ImageHandleFloat from "./ImageHandleFloat.svelte";
     import ImageHandleSizeSelect from "./ImageHandleSizeSelect.svelte";
 
-    import { onDestroy, tick } from "svelte";
+    import { onDestroy } from "svelte";
 
     export let container: HTMLElement;
     export let sheet: CSSStyleSheet;
