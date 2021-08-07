c6637852646c6722122d5b13a27039eba39473a2
Henrik Giesel <hengiesel@gmail.com>
2021-08-07 23:04:36 +0200

    Satisfy eslint

--- ts/editable/mathjax-component.ts
+++ ts/editable/mathjax-component.ts
@@ -1,3 +1,11 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+/* eslint
+@typescript-eslint/no-non-null-assertion: "off",
+@typescript-eslint/no-explicit-any: "off",
+ */
+
 import "mathjax/es5/tex-svg-full";
 
 import type { DecoratedElement, DecoratedElementConstructor } from "./decorated";
