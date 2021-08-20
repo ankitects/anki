87bd990ad9112638138ca3c51251ff2681515837
Henrik Giesel <hengiesel@gmail.com>
2021-08-20 17:09:50 +0200

    Force DeckOptionsPage compatibility for now

--- ts/deck-options/DeckOptionsPage.svelte
+++ ts/deck-options/DeckOptionsPage.svelte
@@ -39,7 +39,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
                 component: HtmlAddon,
                 html,
                 mounted,
-            },
+            } as unknown as DynamicSvelteComponent,
         ];
     }
 
