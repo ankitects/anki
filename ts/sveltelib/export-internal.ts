875fd622640d5897bece58b6f5446d65d9fd7d9d
Damien Elmes <gpg@ankiweb.net>
2021-08-24 10:19:26 +1000

    expose require() instead of the svelte global
    - Means add-on authors should not need to inject any code in their build
    - Should be more flexible - we can export multiple libraries if we wish,
    and don't have to worry about require() being clobbered by old add-ons.

--- ts/sveltelib/export-internal.ts
+++
@@ -1,6 +0,0 @@
-// Copyright: Ankitects Pty Ltd and contributors
-// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
-// allow Svelte add-ons
-import * as svelte_internal from "svelte/internal";
-window["svelte_internal"] = svelte_internal;
