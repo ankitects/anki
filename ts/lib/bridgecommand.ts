76ca1356ac2c0aaf91c9278f1bea4e8d535db2ca
Henrik Giesel <hengiesel@gmail.com>
2021-08-30 14:41:40 +0200

    Implement register-package and export some packages
    Exported packages are:
    * anki/packages
    * anki/shortcuts
    * anki/bridgecommand

--- ts/lib/bridgecommand.ts
+++ ts/lib/bridgecommand.ts
@@ -1,6 +1,8 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import { registerPackage } from "./register-package";
+
 declare global {
     interface Window {
         bridgeCommand<T>(command: string, callback?: (value: T) => void): void;
@@ -15,3 +17,7 @@ export function bridgeLink(command: string, label: string): string {
 export function bridgeCommand<T>(command: string, callback?: (value: T) => void): void {
     window.bridgeCommand<T>(command, callback);
 }
+
+registerPackage("anki/bridgecommand", {
+    bridgeCommand,
+});
