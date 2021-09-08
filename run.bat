fff26aa25802e01c8a74624dac12578f4a5c3e9d
evandrocoan <evandrocoan@hotmail.com>
2021-09-07 23:31:10 -0300

    Force bazel be more verbose

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 set PYTHONWARNINGS=default
-call .\bazel.bat run %BUILDARGS% //qt:runanki -k -- %*
+call .\bazel.bat run --explain=bazel.log --subcommands --show_progress --worker_verbose --verbose_failures --verbose_explanations=true --test_output=streamed %BUILDARGS% //qt:runanki -k -- %*
