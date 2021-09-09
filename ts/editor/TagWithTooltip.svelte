e8f487239191fbe55c39077a1400a06bbb68da7d
Henrik Giesel <hengiesel@gmail.com>
2021-09-09 20:16:52 +0200

    Only hide DeleteBadge when hovering on tag

--- ts/editor/TagWithTooltip.svelte
+++ ts/editor/TagWithTooltip.svelte
@@ -110,7 +110,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </div>
 
 <style lang="scss">
-    .select-mode :global(button) {
+    .select-mode :global(button:hover) {
         display: contents;
         cursor: crosshair;
 
