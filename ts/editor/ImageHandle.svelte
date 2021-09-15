b7d8ae8f22e0ef503df8ffad7ecc8437cd521752
Henrik Giesel <hengiesel@gmail.com>
2021-09-15 16:16:42 +0200

    Correctly pass down dropdownObject...

--- ts/editor/ImageHandle.svelte
+++ ts/editor/ImageHandle.svelte
@@ -135,7 +135,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 {#if sheet}
     <WithDropdown
-        placement="bottom"
+        drop="down"
         autoOpen={true}
         autoClose={false}
         let:createDropdown
