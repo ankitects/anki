e9d71bfbe4b5aaf18ce7c18c61859a7820e20924
Damien Elmes <gpg@ankiweb.net>
2021-09-30 19:18:10 +1000

    yarn upgrade --latest
    bootstrap still held back

--- ts/graphs/InputBox.svelte
+++ ts/graphs/InputBox.svelte
@@ -1,11 +1,14 @@
-<div>
-    <slot />
-</div>
-
 <!--
 Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
+<script lang="ts">
+</script>
+
+<div>
+    <slot />
+</div>
+
 <style lang="scss">
     div {
         display: flex;
