95605f90247e005cfd389105ca989ed6e4b0ee6c
Damien Elmes <gpg@ankiweb.net>
2021-10-02 23:37:12 +1000

    convert some range(len()) calls for pylint

--- pylib/anki/scheduler/v1.py
+++ pylib/anki/scheduler/v1.py
@@ -197,8 +197,7 @@ limit %d"""
             % (self._deckLimit(), self.reportLimit),
             self.dayCutoff,
         )
-        for i in range(len(self._lrnQueue)):
-            self._lrnQueue[i] = (self._lrnQueue[i][0], self._lrnQueue[i][1])
+        self._lrnQueue = [tuple(e) for e in self._lrnQueue]
         # as it arrives sorted by did first, we need to sort it
         self._lrnQueue.sort()
         return self._lrnQueue
