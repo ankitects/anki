c85ff90a678b1989964a0382c239826918dbf90c
Henrik Giesel <hengiesel@gmail.com>
2021-10-08 14:50:02 +0200

    Fix prettier after moving node_modules to repo dir

--- ts/format.sh
+++ ts/format.sh
@@ -1,3 +1,2 @@
 # this is a hack to invoke prettier directly from Bazel
-ts=${BUILD_WORKSPACE_DIRECTORY}/ts
-(cd $ts && ./node_modules/.bin/prettier --config .prettierrc --write . $ts/../qt/aqt/data/web/js)
+(cd "${BUILD_WORKSPACE_DIRECTORY}" && node_modules/.bin/prettier --config .prettierrc --write ts qt/aqt/data/web/js)
