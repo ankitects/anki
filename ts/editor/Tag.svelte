ac92155a8eeb8c238c96d4906d3869075ab86187
Henrik Giesel <hengiesel@gmail.com>
2021-10-09 02:25:03 +0200

    Put sass into repo directory (#1409)
    Fix Sass build

--- ts/editor/Tag.svelte
+++ ts/editor/Tag.svelte
@@ -44,7 +44,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </button>
 
 <style lang="scss">
-    @use "button-mixins" as button;
+    @use "sass/button-mixins" as button;
 
     @keyframes flash {
         0% {
