492e515c18db215e7f9bd9e3f8e84d09b9a67c11
Damien Elmes <gpg@ankiweb.net>
2021-10-28 20:16:20 +1000

    use aqt.qt to (partially) mock the old PyQt5 module
    We can't import the Qt5 libraries to do it - they are not usually
    available, and it will lead to crashes if both are imported at once.

--- qt/aqt/qt/qt5qt.py
+++ qt/aqt/qt/qt5qt.py
@@ -1,7 +1,6 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-# make sure not to optimize imports on this file
 # pylint: skip-file
 
 """
@@ -10,8 +9,8 @@ Compatibility shim for PyQt5.Qt
 
 from typing import Any
 
-from .qt5 import *
-
 
 def __getattr__(name: str) -> Any:
-    return getattr(Qt, name)  # type: ignore
+    import aqt.qt
+
+    return getattr(aqt.qt, name)
