cb3df5f1bed60936e0592d456447fe743e2c5fc4
Matthias M <62722460+kleinerpirat@users.noreply.github.com>
2021-11-15 11:38:10 +0100

    Add bootstrap icons to change-notetype

---
+++ ts/change-notetype/icons.ts
@@ -0,0 +1,9 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+/// <reference types="../lib/image-import" />
+
+export { default as arrowRightIcon } from "bootstrap-icons/icons/arrow-right.svg";
+export { default as exclamationIcon } from "bootstrap-icons/icons/exclamation-circle.svg";
+export { default as plusIcon } from "bootstrap-icons/icons/plus-lg.svg";
+export { default as minusIcon } from "bootstrap-icons/icons/dash-lg.svg";
