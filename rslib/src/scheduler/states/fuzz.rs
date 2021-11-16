196b46c658d1054d3862c9cd3da41e3a27b6f7a0
RumovZ <gp5glkw78@relay.firefox.com>
2021-11-16 13:40:06 +0100

    Switch to a smoother fuzz calculation

--- rslib/src/scheduler/states/fuzz.rs
+++ rslib/src/scheduler/states/fuzz.rs
@@ -3,6 +3,32 @@
 
 use super::StateContext;
 
+/// Describes a range of days for which a certain amount of fuzz is applied to
+/// the new interval.
+struct FuzzRange {
+    start: f32,
+    end: f32,
+    factor: f32,
+}
+
+static FUZZ_RANGES: [FuzzRange; 3] = [
+    FuzzRange {
+        start: 2.5,
+        end: 7.0,
+        factor: 0.15,
+    },
+    FuzzRange {
+        start: 7.0,
+        end: 20.0,
+        factor: 0.1,
+    },
+    FuzzRange {
+        start: 20.0,
+        end: f32::MAX,
+        factor: 0.05,
+    },
+];
+
 impl<'a> StateContext<'a> {
     /// Apply fuzz, respecting the passed bounds.
     /// Caller must ensure reasonable bounds.
@@ -45,24 +71,25 @@ fn constrained_fuzz_bounds(interval: f32, minimum: u32, maximum: u32) -> (u32, u
 }
 
 fn fuzz_bounds(interval: f32) -> (u32, u32) {
-    if interval < 2.5 {
-        let rounded = interval.round().max(1.0) as u32;
-        (rounded, rounded)
-    } else if interval < 7.0 {
-        fuzz_range(interval, 0.25, 0.0)
-    } else if interval < 30.0 {
-        fuzz_range(interval, 0.15, 2.0)
-    } else {
-        fuzz_range(interval, 0.05, 4.0)
-    }
-}
-
-fn fuzz_range(interval: f32, factor: f32, minimum: f32) -> (u32, u32) {
-    let delta = (interval * factor).max(minimum).max(1.0);
+    let delta = fuzz_delta(interval);
     (
         (interval - delta).round() as u32,
         (interval + delta).round() as u32,
     )
+}
+
+/// Return the amount of fuzz to apply to the interval in both directions.
+/// Short intervals do not get fuzzed. All other intervals get fuzzed by 1 day
+/// plus the number of its days in each defined fuzz range multiplied with the
+/// given factor.
+fn fuzz_delta(interval: f32) -> f32 {
+    if interval < 2.5 {
+        0.0
+    } else {
+        FUZZ_RANGES.iter().fold(1.0, |delta, range| {
+            delta + range.factor * (interval.min(range.end) - range.start).max(0.0)
+        })
+    }
 }
 
 #[cfg(test)]
@@ -92,27 +119,32 @@ mod test {
         // no fuzzing for an interval of 1-2.49
         assert_lower_middle_upper!(1.0, 1, 1000, 1, 1, 1);
         assert_lower_middle_upper!(2.49, 1, 1000, 2, 2, 2);
-        // 25%, 15%, 5% percent fuzz, but at least 1, 2, 4
-        assert_lower_middle_upper!(5.0, 1, 1000, 4, 5, 6);
-        assert_lower_middle_upper!(20.0, 1, 1000, 17, 20, 23);
-        assert_lower_middle_upper!(100.0, 1, 1000, 95, 100, 105);
+
+        // 1 day for intervals >= 2.5
+        assert_lower_middle_upper!(2.5, 1, 1000, 2, 3, 4);
+        // ... plus 0.15 for every day in the range 2.5-7
+        assert_lower_middle_upper!(7.0, 1, 1000, 5, 7, 9);
+        // ... plus 0.1 for every day in the range 7-20
+        assert_lower_middle_upper!(17.0, 1, 1000, 14, 17, 20);
+        // ... plus 0.05 for every day above 20
+        assert_lower_middle_upper!(37.0, 1, 1000, 33, 37, 41);
 
         // ensure fuzz range of at least 2, if allowed
         assert_lower_middle_upper!(2.0, 2, 1000, 2, 2, 2);
         assert_lower_middle_upper!(2.0, 3, 1000, 3, 4, 4);
         assert_lower_middle_upper!(2.0, 3, 3, 3, 3, 3);
 
-        // fuzz bracket transitions
+        // fuzz range transitions
         assert_lower_middle_upper!(6.9, 3, 1000, 5, 7, 9);
         assert_lower_middle_upper!(7.0, 3, 1000, 5, 7, 9);
         assert_lower_middle_upper!(7.1, 3, 1000, 5, 7, 9);
-        assert_lower_middle_upper!(29.9, 3, 1000, 25, 30, 34);
-        assert_lower_middle_upper!(30.0, 3, 1000, 26, 30, 34);
-        assert_lower_middle_upper!(30.1, 3, 1000, 26, 30, 34);
+        assert_lower_middle_upper!(19.9, 3, 1000, 17, 20, 23);
+        assert_lower_middle_upper!(20.0, 3, 1000, 17, 20, 23);
+        assert_lower_middle_upper!(20.1, 3, 1000, 17, 20, 23);
 
         // respect limits and preserve uniform distribution of valid intervals
-        assert_lower_middle_upper!(100.0, 101, 1000, 101, 103, 105);
-        assert_lower_middle_upper!(100.0, 1, 99, 95, 97, 99);
+        assert_lower_middle_upper!(100.0, 101, 1000, 101, 104, 107);
+        assert_lower_middle_upper!(100.0, 1, 99, 93, 96, 99);
         assert_lower_middle_upper!(100.0, 97, 103, 97, 100, 103);
     }
 }
