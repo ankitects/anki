e2a30666e44187891b7c36360a3bd60a869397db
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2021-11-24 03:09:55 +0100

    Change Notetype UI Rework (#1499)
    * Enable access to old notetype name
    
    * Set minimum height for ChangeNotetypeDialog
    
    * Add bootstrap icons to change-notetype
    
    * Move alert up and make it collapsible
    
    * Tweak some CSS
    
    - Add variables --sticky-bg and --sticky-border to StickyContainer
    - Tweak base.css
    
    * Add translatable string "(Nothing)"
    
    * Rework ChangeNotetype screen
    
    * Initially load option at newIndex and remaining options on focus
    
    Optimization for big notetypes:
    Should increase efficiency from O(n²) to O(n). Test on notetype with 500 templates shows significant improvement in load time (~10s down to ~1s).
    
    * Try to satisfy rust test
    
    * Change arrow direction depending on reading direction
    
    + add 0.5em top padding to main
    
    * Create Alert.svelte
    
    * Introduce CSS variable --pane-bg
    
    * Revert "Initially load option at newIndex and remaining options on focus"
    
    This reverts commit f42beee45c27dba9433d76217fb583b117fb5231.
    
    * Final cleanup
    
    * Refine padding/gutter

--- ts/change-notetype/Mapper.svelte
+++ ts/change-notetype/Mapper.svelte
@@ -4,43 +4,20 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
     import Container from "../components/Container.svelte";
-    import Row from "../components/Row.svelte";
-    import Col from "../components/Col.svelte";
     import MapperRow from "./MapperRow.svelte";
-    import * as tr from "../lib/ftl";
-    import { ChangeNotetypeState, MapContext } from "./lib";
-    import { slide } from "svelte/transition";
+    import Spacer from "../components/Spacer.svelte";
+    import type { ChangeNotetypeState, MapContext } from "./lib";
 
     export let state: ChangeNotetypeState;
     export let ctx: MapContext;
 
     let info = state.info;
-
-    $: unused = $info.unusedItems(ctx);
-    $: unusedMsg =
-        ctx === MapContext.Field
-            ? tr.changeNotetypeWillDiscardContent()
-            : tr.changeNotetypeWillDiscardCards();
 </script>
 
-<Container --gutter-inline="0.5rem" --gutter-block="0.1rem">
-    <Row --cols={2}>
-        <Col --col-size={1}><b>{tr.changeNotetypeCurrent()}</b></Col>
-        <Col --col-size={1}><b>{tr.changeNotetypeNew()}</b></Col>
-    </Row>
+<Spacer --height="0.5rem" />
 
+<Container --gutter-inline="0.5rem" --gutter-block="0.15rem">
     {#each $info.mapForContext(ctx) as _, newIndex}
         <MapperRow {state} {ctx} {newIndex} />
     {/each}
 </Container>
-
-{#if unused.length > 0}
-    <div class="alert alert-warning" in:slide out:slide>
-        {unusedMsg}
-        <ul>
-            {#each unused as entry}
-                <li>{entry}</li>
-            {/each}
-        </ul>
-    </div>
-{/if}
