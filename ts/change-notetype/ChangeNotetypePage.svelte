d12e88f8700eb60193071f01b3c59488733f20f3
Matthias M <62722460+kleinerpirat@users.noreply.github.com>
2021-12-02 15:59:21 +0100

    Adjust spacing to be more in line with rest of UI

--- ts/change-notetype/ChangeNotetypePage.svelte
+++ ts/change-notetype/ChangeNotetypePage.svelte
@@ -12,7 +12,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import NotetypeSelector from "./NotetypeSelector.svelte";
     import StickyNav from "./StickyNav.svelte";
     import Mapper from "./Mapper.svelte";
-    import Spacer from "../components/Spacer.svelte";
 
     export let state: ChangeNotetypeState;
     $: info = state.info;
@@ -21,7 +20,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 <div bind:offsetHeight={offset}>
     <NotetypeSelector {state} />
-    <Spacer --height="1em" />
 </div>
 
 <div id="scrollArea" style="--offset: {offset}px; --gutter-inline: 0.25rem;">
