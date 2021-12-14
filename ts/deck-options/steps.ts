5c5cc4c190b7c29e12ec68ec7d80b848834750b8
RumovZ <gp5glkw78@relay.firefox.com>
2021-12-14 11:05:21 +0100

    Lower steps cap in deck options
    Whereas large card intervals are converted to days, revlog intervals use
    i32s to store large numbers of seconds.

--- ts/deck-options/steps.ts
+++ ts/deck-options/steps.ts
@@ -49,8 +49,8 @@ function stringToMinutes(text: string): number {
         const [_, num, suffix] = match;
         const unit = suffixToUnit(suffix);
         const seconds = unitSeconds(unit) * parseInt(num, 10);
-        // should be representable as u32 seconds on the backend
-        const capped_seconds = Math.min(seconds, 2 ** 32 - 1);
+        // should be representable as negative i32 seconds in a revlog
+        const capped_seconds = Math.min(seconds, 2 ** 31);
         return capped_seconds / 60;
     } else {
         return 0;
