4b29ceee1f2bf42d6914a83042bb0f34889e68af
Damien Elmes <gpg@ankiweb.net>
2021-12-17 14:15:02 +1000

    add missing header

--- rslib/benches/benchmark.rs
+++ rslib/benches/benchmark.rs
@@ -1,3 +1,6 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 use anki::card_rendering::anki_tag_benchmark;
 use criterion::{criterion_group, criterion_main, Criterion};
 
