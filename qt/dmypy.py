ankitects/anki

commit 5efaf5a4be95e289210e964a5cd40b4f08829c8c

move Bazel convenience symlinks outside of repo folder
The default symlink location can cause slowdowns and wasted CPU cycles
in VS Code and PyCharm/IntelliJ, as they try to watch Bazel's (large)
build folder for changes. The issue can be mostly ameliorated in VS Code
by excluding the symlinks using globs in settings like watcherExclude,
but the Rust extension doesn't support globs, so each folder needs to be
listed out separately. And because the product name symlink depends on
the name of the directory you're building from, we can't just include
the excludes in .vscode - it will depend on the folder the user is storing
things.

PyCharm and IntelliJ behave even worse here - they continue to monitor
for changes in all folders of the repo, even if those folders have been
marked as excluded in the project settings. Placing the folders into the
IDE-global Editor>File Types>Ignored Files And Folders works around this,
but again we run into troubles making this work out of the box, especially
with the product name in the symlink.

One option would be to turn the symlinks off completely. They are not
required for building, and for scripting/debugging, we can get the folder
locations via 'bazel info'. But with that approach, we would no longer
be able to symlink build products into the source tree, as we do for
things like the generated backend methods and translations, so we'd lose
code completion for them that way.

Another option would be to place the symlinks in .bazel/ inside the repo.
That solves the VS Code case (in conjunction with a workspace config file),
but doesn't fully fix IntelliJ/PyCharm.

The only remaining option I can see is to place the symlinks outside the
repo. Bazel won't expand ~ in the symlink path, so we can't use something
like ~/.cache/bazel/anki to place the files near the other build files.
So we end up having to have the files written to ../bazel/anki, in the
repo's parent folder. Not very clean, but I don't see a better alternative

.bazelrc
@@ -1,12 +1,6 @@
 common --enable_platform_specific_config
 common --experimental_repository_cache_hardlinks
 
-# specify custom python path
-# build --action_env="PYO3_PYTHON=/usr/local/bin/python3.9"
-
-# only affects the ankihelper library
-#build:macos --macos_cpus=x86_64,arm64
-
 # runfiles are off by default on Windows, and we need them
 build --enable_runfiles
 
@@ -25,11 +19,21 @@ test --test_output=errors
 # don't add empty __init__.py files
 build --incompatible_default_to_explicit_init_py
 
+# custom output for CI
 build:ci --show_timestamps --isatty=0 --color=yes --show_progress_rate_limit=5
+
+# 'opt' config is an alias for building with optimizations
 build:opt -c opt
 
 # the TypeScript workers on Windows choke when deps are changed while they're
 # still running, so shut them down at the end of the build.
 build:windows --worker_quit_after_build
 
+# place convenience symlinks outside of the source folder, so IDEs don't try to
+# monitor Bazel's large symlink tree for changes
+build --symlink_prefix=../bazel/anki/
+build --experimental_no_product_name_out_symlink
+
+# allow extra user customizations in a separate file
+# (see .user.bazelrc for an example)
 try-import %workspace%/user.bazelrc

commit f842ab7c9d4246d729b832fc93852d3280728ad5

switch convenience symlinks to .bazel/
Unfortunately 5efaf5a4be95e289210e964a5cd40b4f08829c8c broke the Svelte
language tools - presumably having paths outside of the repo is confusing
them.

As a plan B, the symlinks have been shifted to a single subdir. Along
with some exclusions in the VS Code config, this should allow VS Code
to continue to work out of the box, but the docs will need updating
to reflect the extra work required for PyCharm/IntelliJ.

+ fix svelte-check execution on a system without node installed. It
still throws up some errors that are presumably caused by our multiple
rootDirs - not sure if there's an easy way to work around that.

.bazelrc
@@ -29,9 +29,8 @@ build:opt -c opt
 # still running, so shut them down at the end of the build.
 build:windows --worker_quit_after_build
 
-# place convenience symlinks outside of the source folder, so IDEs don't try to
-# monitor Bazel's large symlink tree for changes
-build --symlink_prefix=../bazel/anki/
+# place convenience symlinks inside a single folder for easier exclusion in IDEs
+build --symlink_prefix=.bazel/
 build --experimental_no_product_name_out_symlink
 
 # allow extra user customizations in a separate file

.gitignore
@@ -8,3 +8,4 @@ user.bazelrc
 rust-project.json
 node_modules
 .idea/
+.bazel

qt/dmypy.py
@@ -13,7 +13,7 @@ import time
 from pathlib import Path
 
 workspace = Path(os.environ["BUILD_WORKSPACE_DIRECTORY"])
-binroot = workspace / "../bazel/anki/bin"
+binroot = workspace / ".bazel/bin"
 dmypy_bin = binroot / "external/py_deps_mypy/rules_python_wheel_entry_point_dmypy"
 
 if sys.platform.startswith("win32"):
@@ -30,13 +30,13 @@ if subprocess.run(
         "--",
         "--config-file",
         "qt/mypy.ini",
-        "../bazel/anki/bin/qt/dmypy.runfiles/ankidesktop/pylib/anki",
-        "../bazel/anki/bin/qt/dmypy.runfiles/ankidesktop/qt/aqt",
+        ".bazel/bin/qt/dmypy.runfiles/ankidesktop/pylib/anki",
+        ".bazel/bin/qt/dmypy.runfiles/ankidesktop/qt/aqt",
         "--python-executable",
         os.path.abspath("python/stubs/extendsitepkgs"),
     ],
     env={
-        "MYPYPATH": "../bazel/anki/bin/qt/dmypy.runfiles/pyqt6",
+        "MYPYPATH": ".bazel/bin/qt/dmypy.runfiles/pyqt6",
         "EXTRA_SITE_PACKAGES": os.path.abspath(os.getenv("EXTRA_SITE_PACKAGES")),
     },
     cwd=workspace,

scripts/build
@@ -9,4 +9,4 @@ test -e WORKSPACE || (
 
 rm -rf bazel-dist
 bazel build --config opt dist
-tar xvf ../bazel/anki/bin/dist.tar
+tar xvf .bazel/bin/dist.tar

scripts/svelte-check
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-SASS_PATH=ts/sass:$(pwd)/../bazel/anki/bin/ts/sass \
+. scripts/node-env
+SASS_PATH=ts/sass:$(pwd)/.bazel/bin/ts/sass \
     node_modules/.bin/svelte-check \
     --workspace ts

scripts/ts-run
@@ -6,6 +6,6 @@
 # The pages can be accessed by, eg surfing to
 # http://localhost:40000/_anki/pages/deckconfig.html
 
-ANKI_DATA_FOLDER=$(pwd)/../bazel/anki/bin/qt/aqt \
+ANKI_DATA_FOLDER=$(pwd)/.bazel/bin/qt/aqt \
   QTWEBENGINE_REMOTE_DEBUGGING=8080 \
   ANKI_API_PORT=40000 ANKIDEV=1 ./run $*