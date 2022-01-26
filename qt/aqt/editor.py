298cf8f68fae6b325732ff8fac978c6c985235fa
Henrik Giesel <hengiesel@gmail.com>
2022-01-26 20:32:51 +0100

    Fix left over .svelte import from editor.py

--- qt/aqt/editor.py
+++ qt/aqt/editor.py
@@ -1362,7 +1362,7 @@ def set_cloze_button(editor: Editor) -> None:
     action = "show" if editor.note.note_type()["type"] == MODEL_CLOZE else "hide"
     editor.web.eval(
         'require("anki/ui").uiDidLoad.then(() =>'
-        f'require("anki/NoteEditor.svelte").instances[0].toolbar.templateButtons.{action}("cloze")'
+        f'require("anki/NoteEditor").instances[0].toolbar.templateButtons.{action}("cloze")'
         "); "
     )
 
