5807283e4dab0969960c93e882b1f02480997c1b
Henrik Giesel <hengiesel@gmail.com>
2022-02-08 04:38:17 +0100

    Flatten functions in merge-ranges

--- ts/domlib/surround/merge-ranges.ts
+++ ts/domlib/surround/merge-ranges.ts
@@ -31,76 +31,69 @@ function createInitialMergeMatch(childNodeRange: ChildNodeRange): MergeMatch {
  * <b><u>Hello</u></b><b><i>World</i></b> will be merged to
  * <b><u>Hello</u><i>World</i></b>
  */
-function tryMergingTillMismatch(base: Element) {
-    return function (
-        { mismatch, minimized /* must be nonempty */ }: MergeMatch,
-        childNodeRange: ChildNodeRange,
-    ): MergeMatch {
-        if (mismatch) {
+function tryMergingTillMismatch(
+    { mismatch, minimized /* must be nonempty */ }: MergeMatch,
+    childNodeRange: ChildNodeRange,
+    base: Element,
+): MergeMatch {
+    if (mismatch) {
+        return {
+            mismatch,
+            minimized: [childNodeRange, ...minimized],
+        };
+    } else {
+        const [nextChildNodeRange, ...restChildNodeRanges] = minimized;
+
+        if (
+            areSiblingChildNodeRanges(
+                childNodeRange,
+                nextChildNodeRange,
+            ) /* && !childNodeRange.parent === base */
+        ) {
+            const mergedChildNodeRange = mergeChildNodeRanges(
+                childNodeRange,
+                nextChildNodeRange,
+            );
+
+            const newChildNodeRange =
+                coversWholeParent(mergedChildNodeRange) &&
+                mergedChildNodeRange.parent !== base
+                    ? nodeToChildNodeRange(
+                          ascendWhileSingleInline(mergedChildNodeRange.parent, base),
+                      )
+                    : mergedChildNodeRange;
+
             return {
                 mismatch,
-                minimized: [childNodeRange, ...minimized],
+                minimized: [newChildNodeRange, ...restChildNodeRanges],
             };
         } else {
-            const [nextChildNodeRange, ...restChildNodeRanges] = minimized;
-
-            if (
-                areSiblingChildNodeRanges(
-                    childNodeRange,
-                    nextChildNodeRange,
-                ) /* && !childNodeRange.parent === base */
-            ) {
-                const mergedChildNodeRange = mergeChildNodeRanges(
-                    childNodeRange,
-                    nextChildNodeRange,
-                );
-
-                const newChildNodeRange =
-                    coversWholeParent(mergedChildNodeRange) &&
-                    mergedChildNodeRange.parent !== base
-                        ? nodeToChildNodeRange(
-                              ascendWhileSingleInline(
-                                  mergedChildNodeRange.parent,
-                                  base,
-                              ),
-                          )
-                        : mergedChildNodeRange;
-
-                return {
-                    mismatch,
-                    minimized: [newChildNodeRange, ...restChildNodeRanges],
-                };
-            } else {
-                return {
-                    mismatch: true,
-                    minimized: [childNodeRange, ...minimized],
-                };
-            }
+            return {
+                mismatch: true,
+                minimized: [childNodeRange, ...minimized],
+            };
         }
-    };
-}
-
-function getMergeMatcher(base: Element) {
-    return function mergeMatchInner(
-        accu: ChildNodeRange[],
-        childNodeRange: ChildNodeRange,
-    ): ChildNodeRange[] {
-        let accuInner = createInitialMergeMatch(childNodeRange);
-
-        for (let i = accu.length - 1; i >= 0; i--) {
-            accuInner = tryMergingTillMismatch(base)(accuInner, accu[i]);
-        }
-
-        return accuInner.minimized;
     }
 }
 
-export function mergeRanges(ranges: ChildNodeRange[], base: Element): ChildNodeRange[] {
-    const func = getMergeMatcher(base)
+function getMergeMatcher(
+    accu: ChildNodeRange[],
+    childNodeRange: ChildNodeRange,
+    base: Element,
+): ChildNodeRange[] {
+    let accuInner = createInitialMergeMatch(childNodeRange);
 
+    for (let i = accu.length - 1; i >= 0; i--) {
+        accuInner = tryMergingTillMismatch(accuInner, accu[i], base);
+    }
+
+    return accuInner.minimized;
+}
+
+export function mergeRanges(ranges: ChildNodeRange[], base: Element): ChildNodeRange[] {
     let result: ChildNodeRange[] = [];
     for (const range of ranges) {
-        result = func(result, range);
+        result = getMergeMatcher(result, range, base);
     }
 
     return result;
