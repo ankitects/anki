3077067a757c244043292fd59970136113f5e59b
Henrik Giesel <hengiesel@gmail.com>
2022-02-25 20:51:59 +0100

    Add outside slot for Portal

--- ts/components/Portal.svelte
+++ ts/components/Portal.svelte
@@ -2,11 +2,22 @@
 Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
-<script>
+<script lang="ts">
     import portal from "../sveltelib/portal";
+
     export let target = document.body;
+
+    let ref: HTMLElement;
 </script>
 
-<div use:portal={target} hidden>
+<div class="portal" bind:this={ref} use:portal={target} hidden>
     <slot />
 </div>
+
+<slot name="outside" {ref} />
+
+<style lang="scss">
+    .portal {
+        display: contents;
+    }
+</style>
