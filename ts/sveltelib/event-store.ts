66bba5af9530978732d4b9fade6c2463aff7c400
Henrik Giesel <hengiesel@gmail.com>
2022-03-02 05:21:19 +0100

    Replace (some) Bootstrap dropdowns with Floating UI (#1695)
    * Implement a first version of WithFloating and Portal
    
    * Add outside slot for Portal
    
    * Execute computePosition from WithFloating
    
    * Set up a first example of new WithFloating with the Latex menu
    
    * Use autoUpdate in WithFloating
    
    * Create sveltelib/position
    
    * Add event-store
    
    * Use event-store in close-on-click
    
    * Implement subscribeToUpdates
    
    * Introduce sass/elevation
    
    * Split close-on-click to closing-click and subscribe-trigger
    
    * Have closing-* stores return a symbol
    
    - This way they act more of an EventEmitter than a store
    
    * Allow passing show store
    
    * Remove styling on float on updatePosition removal
    
    * Implement a nice border for dropdowns
    
    * Apply different border and box-shadow to Popover in dark/light theme
    
    * Fix Ctrl+Shift+T not working
    
    * Satisfy formatters and tests
    
    * Add copyright header
    
    * move copyright header to top (dae)

---
+++ ts/sveltelib/event-store.ts
@@ -0,0 +1,42 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+import type { Readable, Subscriber } from "svelte/store";
+import { readable } from "svelte/store";
+
+import type { EventTargetToMap } from "../lib/events";
+import { on } from "../lib/events";
+import type { Callback } from "../lib/typing";
+
+type Init<T> = { new (type: string): T; prototype: T };
+
+/**
+ * A store wrapping an event. Automatically adds/removes event handler upon
+ * first/last subscriber.
+ *
+ * @remarks
+ * Should probably always be used in conjunction with `subscribeToUpdates`.
+ */
+function eventStore<T extends EventTarget, K extends keyof EventTargetToMap<T>>(
+    target: T,
+    eventType: Exclude<K, symbol | number>,
+    /**
+     * Store need an initial value. This should probably be a freshly
+     * constructed event, e.g. `new MouseEvent("click")`.
+     */
+    constructor: Init<EventTargetToMap<T>[K]>,
+): Readable<EventTargetToMap<T>[K]> {
+    const initEvent = new constructor(eventType);
+    return readable(
+        initEvent,
+        (set: Subscriber<EventTargetToMap<T>[K]>): Callback =>
+            on(target, eventType, set),
+    );
+}
+
+export default eventStore;
+
+const documentClick = eventStore(document, "click", MouseEvent);
+const documentKeyup = eventStore(document, "keyup", KeyboardEvent);
+
+export { documentClick, documentKeyup };
