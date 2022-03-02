903f960eacd790e4517ec778cbb39c1ebbea1545
Henrik Giesel <hengiesel@gmail.com>
2022-03-02 01:42:36 +0100

    Add copyright header

--- ts/sveltelib/portal.ts
+++ ts/sveltelib/portal.ts
@@ -1,3 +1,6 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 /**
  * @param element: The element to be moved.
  * @param target DOM Element where element is going to be appended
