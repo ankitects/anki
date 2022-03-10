d201f731986f253e6d763d79fb9e0f390cb085da
Henrik Giesel <hengiesel@gmail.com>
2022-03-10 02:47:12 +0100

    Remove console.log

--- ts/editor/tag-editor/TagEditor.svelte
+++ ts/editor/tag-editor/TagEditor.svelte
@@ -266,7 +266,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
             case "Tab":
                 if (!$show) {
-                    console.log($show);
                     break;
                 } else if (event.shiftKey) {
                     autocomplete.selectPrevious();
