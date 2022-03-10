2d6dd0630fd140590c6af7bd0eafc5078f603981
Damien Elmes <gpg@ankiweb.net>
2022-03-10 20:48:26 +1000

    Switch card.original_position to a proto3 optional

--- pylib/anki/cards.py
+++ pylib/anki/cards.py
@@ -11,7 +11,7 @@ import anki.collection
 import anki.decks
 import anki.notes
 import anki.template
-from anki import cards_pb2, generic_pb2, hooks
+from anki import cards_pb2, hooks
 from anki._legacy import DeprecatedNamesMixin, deprecated
 from anki.consts import *
 from anki.models import NotetypeDict, TemplateDict
@@ -90,7 +90,7 @@ class Card(DeprecatedNamesMixin):
         self.odid = anki.decks.DeckId(card.original_deck_id)
         self.flags = card.flags
         self.original_position = (
-            card.original_position.val if card.HasField("original_position") else None
+            card.original_position if card.HasField("original_position") else None
         )
 
     def _to_backend_card(self) -> cards_pb2.Card:
@@ -111,7 +111,7 @@ class Card(DeprecatedNamesMixin):
             original_due=self.odue,
             original_deck_id=self.odid,
             flags=self.flags,
-            original_position=generic_pb2.UInt32(val=self.original_position)
+            original_position=self.original_position
             if self.original_position is not None
             else None,
         )
