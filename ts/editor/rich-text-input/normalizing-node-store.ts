611d173b542beb634ec42e29d0871548fad71a95
Henrik Giesel <hengiesel@gmail.com>
2022-03-31 03:17:13 +0200

    Fix infinite update loop in editor with invalid input HTML (#1761)
    * Use async function in PlainTextInput
    
    * Clean up PlainTextInput
    
    * Refactor logic from {Rich,Plain}TextInput into own files
    
    * Remove prohibited tags on content.subscribe which also parses the html

---
+++ ts/editor/rich-text-input/normalizing-node-store.ts
@@ -0,0 +1,25 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+import type { DecoratedElement } from "../../editable/decorated";
+import type { NodeStore } from "../../sveltelib/node-store";
+import { nodeStore } from "../../sveltelib/node-store";
+import { decoratedElements } from "../DecoratedElements.svelte";
+
+function normalizeFragment(fragment: DocumentFragment): void {
+    fragment.normalize();
+
+    for (const decorated of decoratedElements) {
+        for (const element of fragment.querySelectorAll(
+            decorated.tagName,
+        ) as NodeListOf<DecoratedElement>) {
+            element.undecorate();
+        }
+    }
+}
+
+function getStore(): NodeStore<DocumentFragment> {
+    return nodeStore<DocumentFragment>(undefined, normalizeFragment);
+}
+
+export default getStore;
