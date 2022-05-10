4abc77aab53abd9bc8b2a2a55223b1535f600f61
Henrik Giesel <hengiesel@gmail.com>
2022-05-10 11:25:29 +0200

    Add ResizableImage.svelte in ts/editable

---
+++ ts/editable/ResizableImage.svelte
@@ -0,0 +1,9 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<style lang="scss">
+    :global(:not([data-editor-fullsize]), [data-editor-fullsize="false"]) {
+        width: var(--editor-width) !important;
+    }
+</style>
