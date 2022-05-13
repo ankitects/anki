3b8aa973966d11e124f3a0dcb93ed4083000b698
Henrik Giesel <hengiesel@gmail.com>
2022-05-13 05:23:35 +0200

    Use same config for editor and reviewer Mathjax (#1865)
    * Remove custom config and use tex-chtml-full for editor Mathjax
    
    * Add mathjax package in /ts
    
    * Share mathjax config between tex-svg and tex-chtml
    
    * Use "[+]" in Mathjax config again
    
    * Remove mention of MathJaxReady
    
    * Satisfy eslint

--- ts/editable/mathjax-element.ts
+++ ts/editable/mathjax-element.ts
@@ -1,8 +1,6 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import "mathjax/es5/tex-svg-full";
-
 import { placeCaretAfter, placeCaretBefore } from "../domlib/place-caret";
 import { on } from "../lib/events";
 import type { DecoratedElement, DecoratedElementConstructor } from "./decorated";
