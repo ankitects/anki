3c49b89065f5e392bea7de8871e23622416f88a8
Henrik Giesel <hengiesel@gmail.com>
2022-05-16 21:55:39 +0200

    Add NotetypeToolbar icons file

---
+++ ts/editor/notetype-toolbar/icons.ts
@@ -0,0 +1,9 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+/// <reference types="../../lib/image-import" />
+
+export { default as fieldIcon } from "@mdi/svg/svg/form-textbox.svg";
+export { default as deckIcon } from "@mdi/svg/svg/book-outline.svg";
+export { default as notetypeIcon } from "@mdi/svg/svg/newspaper.svg";
+export { default as cardtypeIcon } from "@mdi/svg/svg/application-braces-outline.svg";
