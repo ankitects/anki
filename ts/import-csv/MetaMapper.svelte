2ffbb70eee3eace77139a2687aeb39557adcbe2e
RumovZ <gp5glkw78@relay.firefox.com>
2022-05-17 17:59:17 +0200

    Fix meta column indices

--- ts/import-csv/MetaMapper.svelte
+++ ts/import-csv/MetaMapper.svelte
@@ -6,14 +6,22 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import * as tr from "../lib/ftl";
     import MapperRow from "./MapperRow.svelte";
 
+    let tagsIndex = 0;
+    let deckIndex = 0;
+    let notetypeIndex = 0;
+
     export let columnNames: string[];
-    export let tagsColumn: number = 0;
-    export let deckColumn: number = 0;
-    export let notetypeColumn: number = 0;
+    export let tagsColumn: number = tagsIndex - 1;
+    export let deckColumn: number = deckIndex - 1;
+    export let notetypeColumn: number = notetypeIndex - 1;
+
+    $: tagsColumn = tagsIndex - 1;
+    $: deckColumn = deckIndex - 1;
+    $: notetypeColumn = notetypeIndex - 1;
 
     $: options = [tr.changeNotetypeNothing()].concat(columnNames);
 </script>
 
-<MapperRow label={tr.editingTags()} {options} bind:index={tagsColumn} />
-<MapperRow label={tr.decksDeck()} {options} bind:index={deckColumn} />
-<MapperRow label={tr.notetypesNotetype()} {options} bind:index={notetypeColumn} />
+<MapperRow label={tr.editingTags()} {options} bind:index={tagsIndex} />
+<MapperRow label={tr.decksDeck()} {options} bind:index={deckIndex} />
+<MapperRow label={tr.notetypesNotetype()} {options} bind:index={notetypeIndex} />
