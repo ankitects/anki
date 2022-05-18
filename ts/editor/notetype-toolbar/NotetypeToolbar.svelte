f097bfb99a23fade8ae35f333e7b3cef524cd3ea
Henrik Giesel <hengiesel@gmail.com>
2022-05-18 17:42:52 +0200

    Fix prop name to DeckSelector

--- ts/editor/notetype-toolbar/NotetypeToolbar.svelte
+++ ts/editor/notetype-toolbar/NotetypeToolbar.svelte
@@ -25,7 +25,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         <CardtypeButton />
 
         {#if card}
-            <DeckSelector currentDeck={card.deckId} on:deckchange />
+            <DeckSelector currentDeckId={card.deckId} on:deckchange />
         {/if}
     </ButtonToolbar>
 </div>
