f7b57b1c4eee6c5430a4db1ccae7ae881957b796
Henrik Giesel <hengiesel@gmail.com>
2022-05-23 01:14:36 +0200

    Add getNoteId to exports (#1884)

--- ts/editor/NoteEditor.svelte
+++ ts/editor/NoteEditor.svelte
@@ -245,6 +245,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             setClozeHint,
             saveNow: saveFieldNow,
             focusIfField,
+            getNoteId,
             setNoteId,
             wrap,
             ...oldEditorAdapter,
