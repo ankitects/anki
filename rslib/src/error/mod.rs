0c2d6db345223d402a3e59c38b0420579cb496ef
dobefore <1432338032@qq.com>
2022-05-27 11:24:18 +0800

    impl stdError trait

--- rslib/src/error/mod.rs
+++ rslib/src/error/mod.rs
@@ -50,6 +50,8 @@ pub enum AnkiError {
     CustomStudyError(CustomStudyError),
     ImportError(ImportError),
 }
+//fix: doesn't satisfy `AnkiError: StdError` when using crate thiserror
+impl std::error::Error for AnkiError {}
 
 impl Display for AnkiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
