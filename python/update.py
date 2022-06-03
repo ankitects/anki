c4dbaa79705bd3982705f6600994107413f87d9f
Damien Elmes <gpg@ankiweb.net>
2022-06-03 12:57:31 +1000

    Support updating single Python dependency

--- python/update.py
+++ python/update.py
@@ -17,10 +17,16 @@ cli.add_command(compile.cli, "compile")
 
 print("Updating deps...")
 os.chdir(os.getenv("BUILD_WORKING_DIRECTORY"))
+
+if pkg := os.getenv("PACKAGE"):
+    upgrade_args = ["--upgrade-package", pkg]
+else:
+    upgrade_args = ["--upgrade"]
+
 sys.argv[1:] = [
     "compile",
     "--allow-unsafe",
-    "--upgrade",
+    *upgrade_args,
     "--no-header",
     "--strip-extras",
     "--generate-hashes",
