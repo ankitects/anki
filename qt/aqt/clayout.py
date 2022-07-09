ca69198097d24b51096cefa05876f73c67a24879
Damien Elmes <gpg@ankiweb.net>
2022-07-09 12:24:43 +1000

    Tweak type-answer example so users don't think the spelling is a mistake
    https://github.com/ankitects/anki/pull/1954

--- qt/aqt/clayout.py
+++ qt/aqt/clayout.py
@@ -571,7 +571,7 @@ class CardLayout(QDialog):
         hadHR = origLen != len(txt)
 
         def answerRepl(match: Match) -> str:
-            res = self.mw.reviewer.correct("exomple", "an example")
+            res = self.mw.reviewer.correct("example", "sample")
             if hadHR:
                 res = f"<hr id=answer>{res}"
             return res
@@ -579,7 +579,7 @@ class CardLayout(QDialog):
         repl: Union[str, Callable]
 
         if type == "q":
-            repl = "<input id='typeans' type=text value='exomple' readonly='readonly'>"
+            repl = "<input id='typeans' type=text value='example' readonly='readonly'>"
             repl = f"<center>{repl}</center>"
         else:
             repl = answerRepl
