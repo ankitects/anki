070c8ac735b0000dd33540113972ad342412f0ea
Aristotelis <glutanimate@users.noreply.github.com>
2022-07-22 04:45:47 +0200

    Add deck/collection export hooks (#1971)
    * Add ExportFormat enum and use it in Exporter classes
    
    * Add exporter hooks and call them from new exporters
    
    * Fix filter argument order and add example to docstring
    
    * Refactor: Avoid repeating ExportFormat
    
    * Rename Options to ExportOptions for better namespacing in add-ons
    
    * Add simplified legacy exporter hooks
    
    Allows add-ons to be notified of exports when legacy handlers are enabled, without the need for monkey-patches.
    
    * Switch away from ExportFormat, opting to pass exporter class/instance instead
    
    * Consistently use exporter instances rather than classes
    
    * Revert Exportdialog.exporters rename
    
    * Revert "Revert Exportdialog.exporters rename"
    
    This reverts commit 357a3aa85928cce702f06b88da5187d2b45cd22b.

--- qt/tools/genhooks_gui.py
+++ qt/tools/genhooks_gui.py
@@ -816,6 +816,46 @@ gui_hooks.webview_did_inject_style_into_page.append(mytest)
 
         `output` provides access to the unused/missing file lists and the text output that will be shown in the Check Media screen.""",
     ),
+    # Importing/exporting data
+    ###################
+    Hook(
+        name="exporter_will_export",
+        args=[
+            "export_options: aqt.import_export.exporting.ExportOptions",
+            "exporter: aqt.import_export.exporting.Exporter",
+        ],
+        return_type="aqt.import_export.exporting.ExportOptions",
+        doc="""Called before collection and deck exports.
+        
+        Allows add-ons to be notified of impending deck exports and potentially
+        modify the export options. To perform the export unaltered, please return
+        `export_options` as is, e.g.:
+        
+            def on_exporter_will_export(export_options: ExportOptions, exporter: Exporter):
+                if not isinstance(exporter, ApkgExporter):
+                    return export_options
+                export_options.limit = ...
+                return export_options
+        """,
+    ),
+    Hook(
+        name="exporter_did_export",
+        args=[
+            "export_options: aqt.import_export.exporting.ExportOptions",
+            "exporter: aqt.import_export.exporting.Exporter",
+        ],
+        doc="""Called after collection and deck exports.""",
+    ),
+    Hook(
+        name="legacy_exporter_will_export",
+        args=["legacy_exporter: anki.exporting.Exporter"],
+        doc="""Called before collection and deck exports performed by legacy exporters.""",
+    ),
+    Hook(
+        name="legacy_exporter_did_export",
+        args=["legacy_exporter: anki.exporting.Exporter"],
+        doc="""Called after collection and deck exports performed by legacy exporters.""",
+    ),
     # Dialog Manager
     ###################
     Hook(
