5189a125a647adfbcf0fc4ac7a482ff888046308
Aristotelis <glutanimate@users.noreply.github.com>
2022-07-22 11:33:07 +0200

    Introduce exporters_list_created hook to new export code (#1978)
    * Introduce exporters_list_created hook for new export code
    
    * Fix hook and argument name

--- qt/tools/genhooks_gui.py
+++ qt/tools/genhooks_gui.py
@@ -19,7 +19,7 @@ prefix = """\
 
 from __future__ import annotations
 
-from typing import Any, Callable, Sequence, Literal
+from typing import Any, Callable, Sequence, Literal, Type
 
 import anki
 import aqt
@@ -855,6 +855,15 @@ gui_hooks.webview_did_inject_style_into_page.append(mytest)
         name="legacy_exporter_did_export",
         args=["legacy_exporter: anki.exporting.Exporter"],
         doc="""Called after collection and deck exports performed by legacy exporters.""",
+    ),
+    Hook(
+        name="exporters_list_did_initialize",
+        args=["exporters: list[Type[aqt.import_export.exporting.Exporter]]"],
+        doc="""Called after the list of exporter classes is created.
+
+        Allows you to register custom exporters and/or replace existing ones by
+        modifying the exporter list.
+        """,
     ),
     # Dialog Manager
     ###################
