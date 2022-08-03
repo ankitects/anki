4cd47294e4d25ac0e8cd8ed9685e6c851165ca24
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-08-03 17:46:22 +0200

    Remove code icons, keep one pin icon version

--- ts/editor/icons.ts
+++ ts/editor/icons.ts
@@ -5,13 +5,10 @@
 
 export { default as incrementClozeIcon } from "../icons/contain-plus.svg";
 export { default as alertIcon } from "@mdi/svg/svg/alert.svg";
-export { default as htmlOn } from "@mdi/svg/svg/code-tags.svg";
 export { default as clozeIcon } from "@mdi/svg/svg/contain.svg";
 export { default as richTextOff } from "@mdi/svg/svg/eye-off-outline.svg";
 export { default as richTextOn } from "@mdi/svg/svg/eye-outline.svg";
-export { default as stickyOff } from "@mdi/svg/svg/pin-off-outline.svg";
-export { default as stickyOn } from "@mdi/svg/svg/pin-outline.svg";
-export { default as htmlOff } from "@mdi/svg/svg/xml.svg";
+export { default as stickyIcon } from "@mdi/svg/svg/pin-outline.svg";
 export { default as chevronUp } from "@mdi/svg/svg/chevron-up.svg";
 export { default as chevronRight } from "@mdi/svg/svg/chevron-right.svg";
 export { default as chevronDown } from "@mdi/svg/svg/chevron-down.svg";
