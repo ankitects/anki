bd5873b5327d0e8c9168842e1aad07368b2db4b5
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-08-13 12:19:14 +0200

    Create CollapseBadge component

--- ts/editor/LabelContainer.svelte
+++ ts/editor/LabelContainer.svelte
@@ -5,43 +5,44 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
 
-    import Badge from "../components/Badge.svelte";
     import * as tr from "../lib/ftl";
-    import { chevronDown } from "./icons";
+    import CollapseBadge from "./CollapseBadge.svelte";
 
     export let collapsed: boolean;
+    let hovered = false;
+
+    $: tooltip = collapsed ? tr.editingExpandField() : tr.editingCollapseField();
 
     const dispatch = createEventDispatcher();
 
     function toggle() {
         dispatch("toggle");
     }
-
-    $: tooltip = collapsed ? tr.editingExpandField() : tr.editingCollapseField();
 </script>
 
-<div
-    class="label-container"
-    on:mousedown|preventDefault
-    on:click|stopPropagation={toggle}
->
-    <span class="chevron" class:collapsed>
-        <Badge {tooltip} iconSize={80} --icon-align="text-bottom"
-            >{@html chevronDown}</Badge
-        >
+<div class="label-container" on:mousedown|preventDefault>
+    <span
+        class="clickable"
+        title={tooltip}
+        on:click|stopPropagation={toggle}
+        on:mouseenter={() => (hovered = true)}
+        on:mouseleave={() => (hovered = false)}
+    >
+        <CollapseBadge {collapsed} highlighted={hovered} />
+        <slot name="field-name" />
     </span>
-    <slot name="field-name" />
-
     <slot />
 </div>
 
 <style lang="scss">
     .label-container {
+        display: flex;
         position: sticky;
         top: 0;
         z-index: 3;
-        background: var(--window-bg);
-        cursor: pointer;
+        justify-content: space-between;
+        background-color: var(--label-color, transparent);
+        padding-bottom: 1px;
 
         /* pseudo element wider than container
            to cover up field borders on scroll
@@ -56,28 +57,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             right: -2px;
             background: var(--window-bg);
         }
-        .chevron {
-            opacity: 0.4;
-            transition: opacity 0.2s ease-in-out, transform 80ms ease-in;
-            &.collapsed {
-                transform: rotate(-90deg);
-            }
-        }
-        &:hover .chevron {
-            opacity: 1;
-        }
-
-        display: flex;
-        justify-content: space-between;
-
-        background-color: var(--label-color, transparent);
-
-        padding-bottom: 1px;
-    }
-
-    :global([dir="rtl"]) {
-        .chevron.collapsed {
-            transform: rotate(90deg);
+        .clickable {
+            cursor: pointer;
         }
     }
 </style>
