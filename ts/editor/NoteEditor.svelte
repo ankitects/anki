0bc2deba0831bc633c7377d59c5a3cc7bac4cb51
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-08-13 13:16:35 +0200

    Sort imports

--- ts/editor/NoteEditor.svelte
+++ ts/editor/NoteEditor.svelte
@@ -63,8 +63,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import MathjaxElement from "./MathjaxElement.svelte";
     import Notification from "./Notification.svelte";
     import { PlainTextInput } from "./plain-text-input";
-    import { editingInputIsRichText, RichTextInput } from "./rich-text-input";
     import PlainTextBadge from "./PlainTextBadge.svelte";
+    import { editingInputIsRichText, RichTextInput } from "./rich-text-input";
 
     function quoteFontFamily(fontFamily: string): string {
         // generic families (e.g. sans-serif) must not be quoted
