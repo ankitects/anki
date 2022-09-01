1ee8aa9642cbb253aabe00e89b41b3e33a1416cd
RumovZ <gp5glkw78@relay.firefox.com>
2022-09-01 18:05:30 +0200

    Remove redundant `distinct`s from sql query

--- rslib/src/storage/deck/all_decks_and_original_of_search_cards.sql
+++ rslib/src/storage/deck/all_decks_and_original_of_search_cards.sql
@@ -2,11 +2,11 @@ WITH cids AS (
   SELECT cid
   FROM search_cids
 )
-SELECT DISTINCT did
+SELECT did
 FROM cards
 WHERE id IN cids
 UNION
-SELECT DISTINCT odid
+SELECT odid
 FROM cards
 WHERE odid != 0
   AND id IN cids
