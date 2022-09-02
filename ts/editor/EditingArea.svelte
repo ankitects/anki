3d896b59498891ea82a35201fae4276c43d251a8
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-09-02 18:57:36 +0200

    Tweak inset shadow color

--- ts/editor/EditingArea.svelte
+++ ts/editor/EditingArea.svelte
@@ -202,7 +202,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             left: 0;
             pointer-events: none;
             border-radius: 5px;
-            border: 1px solid var(--border-subtle);
+            border: 1px solid var(--border-default);
             transition: box-shadow 80ms cubic-bezier(0.33, 1, 0.68, 1);
             box-shadow: inset 0 0 1px 0 var(--shadow-inset);
         }
