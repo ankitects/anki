e39a3edeab706739b0db303b41644f0877e0123f
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-09-02 18:25:37 +0200

    Experiment with inset look for fields again
    Is this too dark in night mode? It's the same color used for all other text inputs.

--- ts/editor/EditingArea.svelte
+++ ts/editor/EditingArea.svelte
@@ -204,7 +204,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             border-radius: 5px;
             border: 1px solid var(--border-subtle);
             transition: box-shadow 80ms cubic-bezier(0.33, 1, 0.68, 1);
-            box-shadow: inset 0 0 1px 1px var(--shadow-inset);
+            box-shadow: inset 0 0 1px 0 var(--shadow-inset);
         }
 
         &:focus-within {
