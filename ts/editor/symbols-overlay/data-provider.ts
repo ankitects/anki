7e031a4b78fa6b6f0532e2ac6bea2d112ab6837e
Henrik Giesel <hengiesel@gmail.com>
2022-09-08 00:31:31 +0200

    Use from npm packages rather than downloading from URL

--- ts/editor/symbols-overlay/data-provider.ts
+++ ts/editor/symbols-overlay/data-provider.ts
@@ -1,20 +1,52 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import symbolsTable from "./symbols";
+import { characterEntities } from "character-entities";
+import { gemoji } from "gemoji";
 
 interface SymbolsEntry {
-    names: string[];
     symbol: string;
+    names: string[];
+    tags: string[];
     containsHTML?: boolean;
     autoInsert?: boolean;
 }
 
 export type SymbolsTable = SymbolsEntry[];
 
+const symbolsTable: SymbolsTable = [];
+
+const characterTable: Record<string, string[]> = {};
+
+for (const [name, character] of Object.entries(characterEntities)) {
+    if (character in characterTable) {
+        characterTable[character].push(name);
+    } else {
+        characterTable[character] = [name];
+    }
+}
+
+for (const [character, names] of Object.entries(characterTable)) {
+    symbolsTable.push({
+        symbol: character,
+        names,
+        tags: [],
+    });
+}
+
+for (const { emoji, names, tags } of gemoji) {
+    symbolsTable.push({
+        symbol: emoji,
+        names,
+        tags,
+    });
+}
+
 export async function getSymbols(query: string): Promise<SymbolsTable> {
-    return symbolsTable.filter(({ names }) =>
-        names.some((name) => name.includes(query)),
+    return symbolsTable.filter(
+        ({ names, tags }) =>
+            names.some((name) => name.includes(query)) ||
+            tags.some((tag) => tag.includes(query)),
     );
 }
 
