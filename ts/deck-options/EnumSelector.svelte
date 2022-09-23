103b59face51f9db4a042824e6c4a3e56ce060db
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-09-23 03:17:59 +0200

    Pass disabled array to EnumSelector again

--- ts/deck-options/EnumSelector.svelte
+++ ts/deck-options/EnumSelector.svelte
@@ -6,11 +6,14 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import Select from "../components/Select.svelte";
     import SelectOption from "../components/SelectOption.svelte";
     export let options: string[] = [];
+    export let disabled: number[] = [];
     export let value = 0;
 </script>
 
 <Select current={options[value]}>
     {#each options as option, idx}
-        <SelectOption on:select={() => (value = idx)}>{option}</SelectOption>
+        <SelectOption disabled={disabled.includes(idx)} on:select={() => (value = idx)}
+            >{option}</SelectOption
+        >
     {/each}
 </Select>
