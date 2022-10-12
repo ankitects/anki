39b021f54e75a928e5dfe8a4b67a89f979a0b71e
Damien Elmes <gpg@ankiweb.net>
2022-10-12 14:56:38 +1000

    Switch back to clearing MathJax preview on blur
    This means two clicks are required again, but that's the lesser of two
    evils:
    
    https://forums.ankiweb.net/t/anki-2-1-55-beta-mathjax-preview/23529/8
    
    Better fixes welcome :-)

--- ts/editor/mathjax-overlay/MathjaxOverlay.svelte
+++ ts/editor/mathjax-overlay/MathjaxOverlay.svelte
@@ -192,10 +192,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
                             placeHandle(true);
                             await resetHandle();
                         }}
-                        on:tab={async () => {
-                            // Instead of resetting on blur, we reset on tab
-                            // Otherwise, when clicking from Mathjax element to another,
-                            // the user has to click twice (focus is called before blur?)
+                        on:blur={async () => {
                             await resetHandle();
                         }}
                         let:editor={mathjaxEditor}
