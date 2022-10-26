a0f7e106eceb3b3e47bc650d04aa800761190451
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-10-26 11:33:08 +0200

    Remove unused properties

--- ts/editor/NoteEditor.svelte
+++ ts/editor/NoteEditor.svelte
@@ -541,8 +541,6 @@ the AddCards dialog) should be implemented in the user of this component.
                             >
                                 <PlainTextInput
                                     {hidden}
-                                    isDefault={plainTextDefaults[index]}
-                                    richTextHidden={richTextsHidden[index]}
                                     on:focusout={() => {
                                         saveFieldNow();
                                         $focusedInput = null;
