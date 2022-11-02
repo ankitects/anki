3393e4ab6242d28b134669f4ae8f993ffcfcac6d
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-11-02 08:33:17 +0100

    Fix CSS selector

--- ts/components/Collapsible.svelte
+++ ts/components/Collapsible.svelte
@@ -93,7 +93,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             }
         }
 
-        .full-hide {
+        &.full-hide {
             &.collapsed {
                 display: none;
             }
