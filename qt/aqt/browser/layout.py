079d176cdff72899492419c19cda9ce35b56fee0
Glutanimate <glutanimate@users.noreply.github.com>
2022-11-03 02:27:38 +0100

    Equalize browser panes on splitter double-click

--- qt/aqt/browser/layout.py
+++ qt/aqt/browser/layout.py
@@ -3,8 +3,30 @@
 
 from enum import Enum
 
+from aqt.qt import QEvent, QObject, QSplitter, Qt
+
 
 class BrowserLayout(Enum):
     AUTO = "auto"
     VERTICAL = "vertical"
     HORIZONTAL = "horizontal"
+
+
+class QSplitterHandleEventFilter(QObject):
+    """Event filter that equalizes QSplitter panes on double-clicking the handle"""
+
+    def __init__(self, splitter: QSplitter):
+        super().__init__(splitter)
+        self._splitter = splitter
+
+    def eventFilter(self, object: QObject, event: QEvent) -> bool:
+        if event.type() == QEvent.Type.MouseButtonDblClick:
+            splitter_parent = self._splitter.parentWidget()
+            if self._splitter.orientation() == Qt.Orientation.Horizontal:
+                half_size = splitter_parent.width() // 2
+            else:
+                half_size = splitter_parent.height() // 2
+            self._splitter.setSizes([half_size, half_size])
+            return True
+
+        return super().eventFilter(object, event)
