e8fc43ed28d6718e2fadadc1b0ceb726d9fa5272
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-11-28 23:38:42 +0100

    Create Page.svelte

---
+++ ts/components/Page.svelte
@@ -0,0 +1,29 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="ts">
+    import ScrollArea from "./ScrollArea.svelte";
+
+    let className: string = "";
+    export { className as class };
+</script>
+
+<div class="page {className} d-flex flex-column">
+    <slot name="header" />
+
+    <ScrollArea scrollY>
+        <div class="page-content flex-grow-1">
+            <slot />
+        </div>
+    </ScrollArea>
+
+    <slot name="footer" />
+</div>
+
+<style lang="scss">
+    .page {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
