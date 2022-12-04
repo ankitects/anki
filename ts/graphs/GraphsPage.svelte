af44926fb61a7ecb270f7bbc1f6126d1216e5c5a
Damien Elmes <gpg@ankiweb.net>
2022-12-04 21:50:58 +1000

    Tweak graph margins again
    https://github.com/ankitects/anki/commit/76901b7436eb1265cb405eea5612f095d266a984#commitcomment-91940936

--- ts/graphs/GraphsPage.svelte
+++ ts/graphs/GraphsPage.svelte
@@ -59,13 +59,21 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         gap: 1em;
         grid-template-columns: repeat(3, minmax(0, 1fr));
         // required on Safari to stretch whole width
-        width: 100vw;
+        width: calc(100vw - 3em);
+        margin-left: 1em;
+        margin-right: 1em;
+
+        @media only screen and (max-width: 600px) {
+            width: calc(100vw - 1rem);
+            margin-left: 0.5rem;
+            margin-right: 0.5rem;
+        }
 
         @media only screen and (max-width: 1400px) {
-            grid-template-columns: 50% 50%;
+            grid-template-columns: 1fr 1fr;
         }
         @media only screen and (max-width: 1200px) {
-            grid-template-columns: 100%;
+            grid-template-columns: 1fr;
         }
         @media only screen and (max-width: 600px) {
             font-size: 12px;
