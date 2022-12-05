ankitects/anki

commit 23e6b2123e9ba59faf2c7174db178b6af701329b

Redesign deck options inputs (#2082)
* Create _input-mixins.scss

* Use button-mixins on more elements

* Replace <select> tag with custom Select component

* Fix RevertButton causing cursor: pointer when hidden

* Increase SaveButton chevron width

* Hide floating component box-shadow when inactive

* Rework SpinBox and move it into components

* Run eslint and prettier

* Remove leftover options prop

* Pass disabled array to EnumSelector again

* Update MapperRow.svelte

* Darken QHeaderView border color

Slipping this in without an extra PR.

* Adjust disabled color, border and cursor

* Remove redundant icon definition from stylesheets

* Fix deck options initial config

* Fix z-index issues in change notetype screen

It might be best to handle z-index locally in each user component instead of hard-coded component values.

* Give web SpinBox a horizontal design

* Give QRadioButton the same treatment as QCheckBox in #2079

* Fix unused CSS selector warning with base button-mixin

* Remove redundant import

* Fix deck options save button

* Delete input-mixins and remove unused down-arrow

* Run eslint on change-notetype

* Run eslint on components

ts/change-notetype/NotetypeSelector.svelte
@@ -7,9 +7,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import ButtonGroup from "../components/ButtonGroup.svelte";
     import ButtonToolbar from "../components/ButtonToolbar.svelte";
     import LabelButton from "../components/LabelButton.svelte";
-    import SelectButton from "../components/SelectButton.svelte";
+    import Select from "../components/Select.svelte";
     import SelectOption from "../components/SelectOption.svelte";
-    import StickyContainer from "../components/StickyContainer.svelte";
     import { arrowLeftIcon, arrowRightIcon } from "./icons";
     import type { ChangeNotetypeState } from "./lib";
     import SaveButton from "./SaveButton.svelte";
@@ -17,40 +16,30 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export let state: ChangeNotetypeState;
     const notetypes = state.notetypes;
     const info = state.info;
+    let value: number = 0;
 
-    async function blur(event: Event): Promise<void> {
-        await state.setTargetNotetypeIndex(
-            parseInt((event.target! as HTMLSelectElement).value),
-        );
-    }
+    $: state.setTargetNotetypeIndex(value);
+    $: options = Array.from($notetypes, (notetype) => notetype.name);
 </script>
 
-<StickyContainer
-    --gutter-block="0.1rem"
-    --gutter-inline="0.25rem"
-    --sticky-borders="0 0 1px"
->
-    <ButtonToolbar class="justify-content-between" size={2.3} wrap={false}>
-        <LabelButton disabled={true}>
-            {$info.oldNotetypeName}
-        </LabelButton>
-        <Badge iconSize={70}>
-            {#if window.getComputedStyle(document.body).direction == "rtl"}
-                {@html arrowLeftIcon}
-            {:else}
-                {@html arrowRightIcon}
-            {/if}
-        </Badge>
-        <ButtonGroup class="flex-grow-1">
-            <SelectButton class="flex-grow-1" on:change={blur}>
-                {#each $notetypes as entry}
-                    <SelectOption value={String(entry.idx)} selected={entry.current}>
-                        {entry.name}
-                    </SelectOption>
-                {/each}
-            </SelectButton>
-        </ButtonGroup>
+<ButtonToolbar class="justify-content-between" size={2.3} wrap={false}>
+    <LabelButton disabled={true}>
+        {$info.oldNotetypeName}
+    </LabelButton>
+    <Badge iconSize={70}>
+        {#if window.getComputedStyle(document.body).direction == "rtl"}
+            {@html arrowLeftIcon}
+        {:else}
+            {@html arrowRightIcon}
+        {/if}
+    </Badge>
+    <ButtonGroup class="flex-grow-1">
+        <Select class="flex-grow-1" current={options[value]}>
+            {#each options as option, idx}
+                <SelectOption on:select={() => (value = idx)}>{option}</SelectOption>
+            {/each}
+        </Select>
+    </ButtonGroup>
 
-        <SaveButton {state} />
-    </ButtonToolbar>
-</StickyContainer>
+    <SaveButton {state} />
+</ButtonToolbar>

qt/aqt/__init__.py
@@ -353,6 +353,7 @@ class AnkiApp(QApplication):
                         (
                             QPushButton,
                             QCheckBox,
+                            QRadioButton,
                             # classes with PyQt5 compatibility proxy
                             without_qt5_compat_wrapper(QToolButton),
                             without_qt5_compat_wrapper(QTabBar),

qt/aqt/forms/customstudy.ui
@@ -16,42 +16,42 @@
   <layout class="QVBoxLayout" name="verticalLayout">
    <item>
     <layout class="QGridLayout" name="gridLayout">
-     <item row="3" column="0">
+     <item row="3" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioAhead">
        <property name="text">
         <string>custom_study_review_ahead</string>
        </property>
       </widget>
      </item>
-     <item row="2" column="0">
+     <item row="2" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioForgot">
        <property name="text">
         <string>custom_study_review_forgotten_cards</string>
        </property>
       </widget>
      </item>
-     <item row="0" column="0">
+     <item row="0" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioNew">
        <property name="text">
         <string>custom_study_increase_todays_new_card_limit</string>
        </property>
       </widget>
      </item>
-     <item row="1" column="0">
+     <item row="1" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioRev">
        <property name="text">
         <string>custom_study_increase_todays_review_card_limit</string>
        </property>
       </widget>
      </item>
-     <item row="5" column="0">
+     <item row="5" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioCram">
        <property name="text">
         <string>custom_study_study_by_card_state_or_tag</string>
        </property>
       </widget>
      </item>
-     <item row="4" column="0">
+     <item row="4" column="0" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="radioPreview">
        <property name="text">
         <string>custom_study_preview_new_cards</string>

qt/aqt/forms/fields.ui
@@ -155,7 +155,7 @@
        </property>
       </widget>
      </item>
-     <item row="2" column="1">
+     <item row="2" column="1" alignment="Qt::AlignLeft">
       <widget class="QRadioButton" name="sortField">
        <property name="text">
         <string>fields_sort_by_this_field_in_the</string>

qt/aqt/forms/preview.ui
@@ -34,7 +34,7 @@
      <layout class="QVBoxLayout" name="verticalLayout">
       <item>
        <layout class="QHBoxLayout" name="horizontalLayout">
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="preview_front">
           <property name="text">
            <string notr="true">FRONT</string>
@@ -44,7 +44,7 @@
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="preview_back">
           <property name="text">
            <string notr="true">BACK</string>

qt/aqt/forms/stats.ui
@@ -74,7 +74,7 @@
         <string/>
        </property>
        <layout class="QHBoxLayout" name="horizontalLayout_2">
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="groups">
           <property name="text">
            <string notr="true">deck</string>
@@ -84,7 +84,7 @@
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="all">
           <property name="text">
            <string notr="true">collection</string>
@@ -100,7 +100,7 @@
         <string/>
        </property>
        <layout class="QHBoxLayout" name="horizontalLayout">
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="month">
           <property name="text">
            <string notr="true">1 month</string>
@@ -110,14 +110,14 @@
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="year">
           <property name="text">
            <string notr="true">1 year</string>
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="life">
           <property name="text">
            <string notr="true">deck life</string>

qt/aqt/forms/template.ui
@@ -52,7 +52,7 @@
       </property>
       <item>
        <layout class="QHBoxLayout" name="horizontalLayout">
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="front_button">
           <property name="toolTip">
            <string notr="true"/>
@@ -65,7 +65,7 @@
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="back_button">
           <property name="toolTip">
            <string notr="true"/>
@@ -75,7 +75,7 @@
           </property>
          </widget>
         </item>
-        <item>
+        <item alignment="Qt::AlignLeft">
          <widget class="QRadioButton" name="style_button">
           <property name="toolTip">
            <string notr="true"/>

qt/aqt/stylesheets.py
@@ -233,7 +233,7 @@ QHeaderView {{
     background: {tm.var(colors.CANVAS)};
 }}
 QHeaderView::section {{
-    border: 1px solid {tm.var(colors.BORDER_SUBTLE)};
+    border: 1px solid {tm.var(colors.BORDER)};
     background: {
         button_gradient(
             tm.var(colors.BUTTON_GRADIENT_START),
@@ -261,19 +261,19 @@ QHeaderView::section:hover {{
     };
 }}
 QHeaderView::section:first {{
-    border-left: 1px solid {tm.var(colors.BORDER_SUBTLE)}; 
+    border-left: 1px solid {tm.var(colors.BORDER)}; 
     border-top-left-radius: {tm.var(props.BORDER_RADIUS)};
 }}
 QHeaderView::section:!first {{
     border-left: none;
 }}
 QHeaderView::section:last {{
-    border-right: 1px solid {tm.var(colors.BORDER_SUBTLE)}; 
+    border-right: 1px solid {tm.var(colors.BORDER)}; 
     border-top-right-radius: {tm.var(props.BORDER_RADIUS)};
 }}
 QHeaderView::section:only-one {{
-    border-left: 1px solid {tm.var(colors.BORDER_SUBTLE)}; 
-    border-right: 1px solid {tm.var(colors.BORDER_SUBTLE)};
+    border-left: 1px solid {tm.var(colors.BORDER)}; 
+    border-right: 1px solid {tm.var(colors.BORDER)};
     border-top-left-radius: {tm.var(props.BORDER_RADIUS)};
     border-top-right-radius: {tm.var(props.BORDER_RADIUS)};
 }}
@@ -368,24 +368,34 @@ QSpinBox::down-arrow:off {{
 
 def checkbox_styles(tm: ThemeManager) -> str:
     return f"""
-QCheckBox {{
+QCheckBox,
+QRadioButton {{
     spacing: 8px;
     margin: 2px 0;
 }}
-QCheckBox::indicator {{
+QCheckBox::indicator,
+QRadioButton::indicator {{
     border: 1px solid {tm.var(colors.BUTTON_BORDER)};
-    border-radius: {tm.var(props.BORDER_RADIUS)};
     background: {tm.var(colors.CANVAS_INSET)};
     width: 16px;
     height: 16px;
 }}
+QCheckBox::indicator {{
+    border-radius: {tm.var(props.BORDER_RADIUS)};
+}}
+QRadioButton::indicator {{
+    border-radius: 8px;
+}}
 QCheckBox::indicator:hover,
-QCheckBox::indicator:checked:hover {{
+QCheckBox::indicator:checked:hover,
+QRadioButton::indicator:hover,
+QRadioButton::indicator:checked:hover {{
     border: 2px solid {tm.var(colors.BORDER_STRONG)};
     width: 14px;
     height: 14px;
 }}
-QCheckBox::indicator:checked {{
+QCheckBox::indicator:checked,
+QRadioButton::indicator:checked {{
     image: url({tm.themed_icon("mdi:check")});
 }}
 QCheckBox::indicator:indeterminate {{

commit 8165f95cdef8707260bfad20e0b9def154a9a437

Fix the wrong notetype being selected by default
Regressed in #2082

https://forums.ankiweb.net/t/anki-2-1-55-beta-7/25130/7

ts/change-notetype/NotetypeSelector.svelte
@@ -16,7 +16,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     const notetypes = state.notetypes;
     const info = state.info;
 
-    let value: number = 0;
+    let value = $notetypes.findIndex((e) => e.current);
     $: options = Array.from($notetypes, (notetype) => notetype.name);
     $: label = options[value];
 