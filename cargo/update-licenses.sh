2a6e716a3f6e68bf4562a769da1e505a1cdc08d4
RumovZ <gp5glkw78@relay.firefox.com>
2022-12-05 19:56:05 +0100

    Update licenses and fix script

--- cargo/update-licenses.sh
+++ cargo/update-licenses.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo-license --features rustls native-tls --json --manifest-path ../rslib/Cargo.toml > licenses.json
+cargo-license --features rustls --features native-tls --json --manifest-path ../rslib/Cargo.toml >licenses.json
