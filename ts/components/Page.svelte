19291fb71ef630137b5cb13171cb36726252c339
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-12-09 03:14:51 +0100

    Format again

--- ts/components/Page.svelte
+++ ts/components/Page.svelte
@@ -4,7 +4,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
     import { isDesktop } from "@tslib/platform";
-    
+
     import ScrollArea from "./ScrollArea.svelte";
 
     let className: string = "";
