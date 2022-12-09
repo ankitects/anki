d9716c08b8ee7a7d9879c8bee71e2a60fdc90829
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-12-09 22:57:42 +0100

    Move scrollbar from Fields to PaneContent
    and strip PaneContent of unused logic.

--- ts/components/PaneContent.svelte
+++ ts/components/PaneContent.svelte
@@ -11,23 +11,16 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export let scroll = true;
     const [element, elementResolve] = promiseWithResolver<HTMLElement>();
 
-    let clientWidth = 0;
     let clientHeight = 0;
-    let scrollWidth = 0;
     let scrollHeight = 0;
     let scrollTop = 0;
-    let scrollLeft = 0;
 
     $: overflowTop = scrollTop > 0;
     $: overflowBottom = scrollTop < scrollHeight - clientHeight;
-    $: overflowLeft = scrollLeft > 0;
-    $: overflowRight = scrollLeft < scrollWidth - clientWidth;
 
     $: shadows = {
         top: overflowTop ? "0 5px" : null,
         bottom: overflowBottom ? "0 -5px" : null,
-        left: overflowLeft ? "5px 0" : null,
-        right: overflowRight ? "-5px 0" : null,
     };
     const rest = "5px -5px var(--shadow)";
 
@@ -39,9 +32,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     async function updateScrollState(): Promise<void> {
         const el = await element;
         scrollHeight = el.scrollHeight;
-        scrollWidth = el.scrollWidth;
         scrollTop = el.scrollTop;
-        scrollLeft = el.scrollLeft;
     }
 </script>
 
@@ -52,7 +43,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     style:--client-height="{clientHeight}px"
     use:elementResolve
     bind:clientHeight
-    bind:clientWidth
     on:scroll={updateScrollState}
     on:resize={updateScrollState}
 >
@@ -66,19 +56,21 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         flex-grow: 1;
         overflow: hidden;
         &.scroll {
-            overflow: auto;
-        }
-        /* force box-shadow to be rendered above children */
-        &::before {
-            content: "";
-            position: fixed;
-            pointer-events: none;
-            left: 0;
-            right: 0;
-            z-index: 4;
-            height: var(--client-height);
-            box-shadow: var(--box-shadow);
-            transition: box-shadow 0.1s ease-in-out;
+            overflow-y: scroll;
+            overflow-x: auto;
+            overscroll-behavior: none;
+
+            /* force box-shadow to be rendered above children */
+            &::before {
+                content: "";
+                position: fixed;
+                pointer-events: none;
+                left: 0;
+                right: 0;
+                z-index: 100;
+                height: var(--client-height);
+                box-shadow: var(--box-shadow);
+            }
         }
     }
 </style>
