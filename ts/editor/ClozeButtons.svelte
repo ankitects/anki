ankitects/anki

commit 68fa661b532ef965e72357c2876636271f01fa67

Finish #2070: Single overlay instead of per field  (#2144)
* Move up MathjaxOverlay to be initialized only once

* Move ImageOverlay to NoteEditor root

* Move Symbols Overlay to NoteEditor root

* Refactor image overlay to not require second mutation observer

* Use elevation + overflow:hidden  in Editorfield

* Make it possible to show input next to each other again

* Set handle background color to code bg

* Make Collapsible unmount the component

* Simplify how decorated elements are mounted

* Set RichTextInput background to frame-bg again

* Strip out FocusTrap code

* Revert "Make Collapsible unmount the component"

This reverts commit 52722065ea199fa57ae750fa34bf47ee1c5aab3c.

* Allow clicking on label container to unfocus field

* Fix mathjax overlay resetting too its api too soon

* Allow scrolling on overlays

* Set focus-border border-color in focused field

* Fix background color of fields

* Add back grid-gap

removed it during merge to see if margin-top would behave any differently - which is not the case.

* Fix double border issue within Collapsible.svelte

* Format

* Edit appearance of focused fields a bit

* Remove unused properties

* Include elevation in button_mixins_lib

* Give label-container a background color

Co-authored-by: Henrik Giesel <hengiesel@gmail.com>

ts/editor/ClozeButtons.svelte
@@ -67,7 +67,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         });
     }
 
-    $: disabled = !editingInputIsRichText($focusedInput);
+    $: disabled = !$focusedInput || !editingInputIsRichText($focusedInput);
 
     const incrementKeyCombination = "Control+Shift+C";
     const sameKeyCombination = "Control+Alt+Shift+C";

sass/BUILD.bazel
@@ -83,6 +83,7 @@ sass_library(
     visibility = ["//visibility:public"],
     deps = [
         "vars_lib",
+        "elevation_lib",
     ],
 )
 

sass/elevation.scss
@@ -59,7 +59,7 @@ $umbra-opacity: 0.2;
 $penumbra-opacity: 0.14;
 $ambient-opacity: 0.12;
 
-@function box-shadow($level, $opacity-boost: 0, $color: black) {
+@function box-shadow($level, $opacity-boost, $color) {
     $umbra-z-value: map.get($umbra-map, $level);
     $penumbra-z-value: map.get($penumbra-map, $level);
     $ambient-z-value: map.get($ambient-map, $level);
@@ -75,6 +75,10 @@ $ambient-opacity: 0.12;
     );
 }
 
-@mixin elevation($level, $other: ()) {
-    box-shadow: list.join(box-shadow($level), $other);
+@mixin elevation($level, $opacity-boost: 0, $color: black) {
+    box-shadow: box-shadow($level, $opacity-boost, $color);
+}
+
+@mixin elevation-transition() {
+    transition: box-shadow 80ms cubic-bezier(0.33, 1, 0.68, 1);
 }

ts/components/Collapsible.svelte
@@ -8,6 +8,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import { tweened } from "svelte/motion";
 
     export let collapse = false;
+    export let toggleDisplay = false;
     export let animated = !document.body.classList.contains("reduced-motion");
 
     let collapsed = false;
@@ -62,6 +63,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     class="collapsible"
     class:animated
     class:expanded
+    class:full-hide={toggleDisplay}
+    class:collapsed={!expanded}
     class:measuring
     class:transitioning
     style:--height="{height}px"
@@ -75,8 +78,18 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 {/if}
 
 <style lang="scss">
+    .collapsible.full-hide {
+        &.collapsed {
+            display: none;
+        }
+        &.transitioning {
+            display: initial;
+        }
+    }
+
     .collapsible.animated {
         &.measuring {
+            display: unset;
             position: absolute;
             opacity: 0;
         }

ts/components/DropdownItem.svelte
@@ -15,12 +15,12 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export let disabled = false;
 
     $: if (buttonRef && active) {
-        /* buttonRef.scrollIntoView({ behavior: "smooth", block: "start" }); */
-        /* TODO will not work on Gecko */
-        (buttonRef as any).scrollIntoViewIfNeeded({
-            behavior: "smooth",
-            block: "start",
-        });
+        setTimeout(() =>
+            buttonRef.scrollIntoView({
+                behavior: "smooth",
+                block: "nearest",
+            }),
+        );
     }
 
     export let tabbable = false;

ts/editor/CodeMirror.svelte
@@ -98,6 +98,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 <style lang="scss">
     .code-mirror {
+        height: 100%;
+
         :global(.CodeMirror) {
             height: auto;
         }

ts/editor/DecoratedElements.svelte
@@ -1,13 +1,0 @@
-<!--
-Copyright: Ankitects Pty Ltd and contributors
-License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
--->
-<script context="module" lang="ts">
-    import { CustomElementArray } from "../editable/decorated";
-
-    const decoratedElements = new CustomElementArray();
-
-    export { decoratedElements };
-</script>
-
-<slot />

ts/editor/EditingArea.svelte
@@ -46,11 +46,10 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </script>
 
 <script lang="ts">
-    import { setContext as svelteSetContext, tick } from "svelte";
+    import { setContext as svelteSetContext } from "svelte";
     import { writable } from "svelte/store";
 
     import { fontFamilyKey, fontSizeKey } from "../lib/context-keys";
-    import FocusTrap from "./FocusTrap.svelte";
 
     export let fontFamily: string;
     const fontFamilyStore = writable(fontFamily);
@@ -65,7 +64,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export let content: Writable<string>;
 
     let editingArea: HTMLElement;
-    let focusTrap: FocusTrap;
 
     const inputsStore = writable<EditingInputAPI[]>([]);
     $: editingInputs = $inputsStore;
@@ -74,39 +72,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         return editingInputs.find((input) => input.focusable);
     }
 
-    function focusEditingInputIfAvailable(): boolean {
-        const availableInput = getAvailableInput();
-
-        if (availableInput) {
-            availableInput.focus();
-            return true;
-        }
-
-        return false;
-    }
-
-    function focusEditingInputIfFocusTrapFocused(): void {
-        if (focusTrap && focusTrap.isFocusTrap(document.activeElement!)) {
-            focusEditingInputIfAvailable();
-        }
-    }
-
-    $: {
-        $inputsStore;
-        /**
-         * Triggers when all editing inputs are hidden,
-         * the editor field has focus, and then some
-         * editing input is shown
-         */
-        focusEditingInputIfFocusTrapFocused();
-    }
-
     function focus(): void {
-        if (editingArea.contains(document.activeElement)) {
-            // do nothing
-        } else if (!focusEditingInputIfAvailable()) {
-            focusTrap.focus();
-        }
+        editingArea.contains(document.activeElement);
     }
 
     function refocus(): void {
@@ -114,51 +81,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
         if (availableInput) {
             availableInput.refocus();
-        } else {
-            focusTrap.blur();
-            focusTrap.focus();
-        }
-    }
-
-    function focusEditingInputInsteadIfAvailable(event: FocusEvent): void {
-        if (focusEditingInputIfAvailable()) {
-            event.preventDefault();
-        }
-    }
-
-    // Prevents editor field being entirely deselected when
-    // closing active field.
-    async function trapFocusOnBlurOut(event: FocusEvent): Promise<void> {
-        if (event.relatedTarget) {
-            return;
-        }
-
-        event.preventDefault();
-
-        const oldInputElement = event.target;
-
-        await tick();
-
-        let focusableInput: FocusableInputAPI | null = null;
-
-        const focusableInputs = editingInputs.filter(
-            (input: EditingInputAPI): boolean => input.focusable,
-        );
-
-        if (oldInputElement) {
-            for (const input of focusableInputs) {
-                focusableInput = await input.getInputAPI(oldInputElement);
-
-                if (focusableInput) {
-                    break;
-                }
-            }
-        }
-
-        if (focusableInput || (focusableInput = focusableInputs[0])) {
-            focusableInput.focus();
-        } else {
-            focusTrap.focus();
         }
     }
 
@@ -175,9 +97,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     setContextProperty(api);
 </script>
 
-<FocusTrap bind:this={focusTrap} on:focus={focusEditingInputInsteadIfAvailable} />
-
-<div bind:this={editingArea} class="editing-area" on:focusout={trapFocusOnBlurOut}>
+<div bind:this={editingArea} class="editing-area">
     <slot />
 </div>
 
@@ -187,29 +107,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         /* TODO allow configuration of grid #1503 */
         /* grid-template-columns: repeat(2, 1fr); */
 
-        position: relative;
-        background: var(--canvas-elevated);
-        border-radius: 5px;
-
-        /* Pseudo-element required to display
-           inset focus box-shadow above field contents */
-        &::after {
-            content: "";
-            position: absolute;
-            inset: 0;
-            pointer-events: none;
-            border-radius: 5px;
-            box-shadow: 0 0 0 3px var(--dupes-color), 0 0 2px 1px var(--shadow);
-            transition: box-shadow 80ms cubic-bezier(0.33, 1, 0.68, 1);
-        }
-
-        &:focus-within {
-            outline: none;
-            &::after {
-                border: none;
-                inset: 1px;
-                box-shadow: 0 0 0 3px var(--dupes-color), 0 0 0 2px var(--border-focus);
-            }
-        }
+        /* This defines the border between inputs */
+        grid-gap: 1px;
+        background-color: var(--border);
     }
 </style>

commit 697551c20b798da862d66b02fed0c1e451f23df3

Fix cloze buttons not working in MathJax editor
Regressed in #2144

ts/editor/ClozeButtons.svelte
@@ -16,6 +16,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import { context as noteEditorContext } from "./NoteEditor.svelte";
     import { editingInputIsRichText } from "./rich-text-input";
 
+    export let alwaysEnabled = false;
+
     const { focusedInput, fields } = noteEditorContext.get();
 
     // Workaround for Cmd+Option+Shift+C not working on macOS. The keyup approach works
@@ -67,7 +69,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         });
     }
 
-    $: disabled = !$focusedInput || !editingInputIsRichText($focusedInput);
+    $: disabled =
+        !alwaysEnabled && (!$focusedInput || !editingInputIsRichText($focusedInput));
 
     const incrementKeyCombination = "Control+Shift+C";
     const sameKeyCombination = "Control+Alt+Shift+C";

ts/editor/mathjax-overlay/MathjaxButtons.svelte
@@ -38,7 +38,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         </IconButton>
     </ButtonGroup>
 
-    <ClozeButtons on:surround />
+    <ClozeButtons on:surround alwaysEnabled={true} />
 
     <ButtonGroup>
         <IconButton