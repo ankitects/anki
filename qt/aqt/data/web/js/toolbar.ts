1bd076e8470a05e83cdef3bab2644a29c53c7c40
Glutanimate <glutanimate@users.noreply.github.com>
2023-01-06 04:19:29 +0100

    Move absolutely positioned add-on items to right toolbar tray
    Workaround that fixes breakages in add-ons like AMBOSS, Study Timer, and potentially others that currently still inject absolutely positioned elements into the toolbar using `top_toolbar_did_init_links`.

--- qt/aqt/data/web/js/toolbar.ts
+++ qt/aqt/data/web/js/toolbar.ts
@@ -27,3 +27,58 @@ function updateSyncColor(state: SyncState) {
             break;
     }
 }
+
+// Dealing with legacy add-ons that used CSS to absolutely position
+// themselves at toolbar edges
+
+function isAbsolutelyPositioned(node: Node): boolean {
+    if (!(node instanceof HTMLElement)) {
+        return false;
+    }
+    return getComputedStyle(node).position === "absolute";
+}
+
+function isLegacyAddonElement(node: Node): boolean {
+    if (isAbsolutelyPositioned(node)) {
+        return true;
+    }
+    for (const child of node.childNodes) {
+        if (isAbsolutelyPositioned(child)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getElementDimensions(element: HTMLElement): [number, number] {
+    const widths = [element.offsetWidth];
+    const heights = [element.offsetHeight];
+    for (const child of element.childNodes) {
+        if (!(child instanceof HTMLElement)) {
+            continue;
+        }
+        widths.push(child.offsetWidth);
+        heights.push(child.offsetHeight);
+    }
+    return [Math.max(...widths), Math.max(...heights)];
+}
+
+function moveLegacyAddonsToTray() {
+    const rightTray = document.getElementsByClassName("right-tray")[0];
+    const links = document.querySelectorAll<HTMLAnchorElement>(".toolbar > *");
+    const legacyAddonElements: HTMLAnchorElement[] = Array.from(links)
+        .reverse()
+        .filter(isLegacyAddonElement);
+
+    for (const element of legacyAddonElements) {
+        const wrapperElement = document.createElement("div");
+        const dimensions = getElementDimensions(element);
+        wrapperElement.append(element);
+        wrapperElement.style.cssText = `\
+width: ${dimensions[0]}px; height: ${dimensions[1]}}px;
+margin-left: 5px; margin-right: 5px; position: relative;`;
+        rightTray.append(wrapperElement);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", moveLegacyAddonsToTray);
