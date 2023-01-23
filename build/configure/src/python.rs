191b27e88a923a17942b70581a80402e4c11e4da
Mani <12841290+krmanik@users.noreply.github.com>
2023-01-23 18:44:47 +0800

    Use a ninja variable for Protoc binary (#2345)
    * Use a ninja variable for Protoc binary
    
    * fix whitespace

--- build/configure/src/python.rs
+++ build/configure/src/python.rs
@@ -178,7 +178,7 @@ impl BuildAction for GenPythonProto {
             })
             .collect();
         build.add_inputs("in", &self.proto_files);
-        build.add_inputs("protoc", inputs![":extract:protoc:bin"]);
+        build.add_inputs("protoc", inputs!["$protoc_binary"]);
         build.add_inputs("protoc-gen-mypy", inputs![":pyenv:protoc-gen-mypy"]);
         build.add_outputs("", python_outputs);
     }
