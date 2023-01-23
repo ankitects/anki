da7d4dd2fcb9dd4585398ee0ed9c82a56c30681c
Mani <12841290+krmanik@users.noreply.github.com>
2023-01-23 18:44:47 +0800

    Use a ninja variable for Protoc binary (#2345)
    * Use a ninja variable for Protoc binary
    
    * fix whitespace

--- build/configure/src/proto.rs
+++ build/configure/src/proto.rs
@@ -1,6 +1,8 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+use std::env;
+
 use ninja_gen::archives::download_and_extract;
 use ninja_gen::archives::with_exe;
 use ninja_gen::glob;
@@ -10,16 +12,31 @@ use ninja_gen::protobuf::protoc_archive;
 use ninja_gen::protobuf::ClangFormat;
 use ninja_gen::Build;
 use ninja_gen::Result;
+use ninja_gen::Utf8Path;
 
-pub fn download_protoc(build: &mut Build) -> Result<()> {
-    download_and_extract(
-        build,
-        "protoc",
-        protoc_archive(build.host_platform),
-        hashmap! {
-            "bin" => [with_exe("bin/protoc")]
-        },
-    )?;
+pub fn setup_protoc(build: &mut Build) -> Result<()> {
+    let protoc_binary = match env::var("PROTOC_BINARY") {
+        Ok(path) => {
+            assert!(
+                Utf8Path::new(&path).is_absolute(),
+                "PROTOC_BINARY must be absolute"
+            );
+            path.into()
+        }
+        Err(_) => {
+            download_and_extract(
+                build,
+                "protoc",
+                protoc_archive(build.host_platform),
+                hashmap! {
+                    "bin" => [with_exe("bin/protoc")]
+                },
+            )?;
+            inputs![":extract:protoc:bin"]
+        }
+    };
+    let protoc_binary = build.expand_inputs(protoc_binary);
+    build.variable("protoc_binary", &protoc_binary[0]);
     Ok(())
 }
 
