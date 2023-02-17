ankitects/anki

commit d20a7d291f96551a9f8fa53b1318057474ce8433

Validate and clamp deck config when loading
Fixes #2353

rslib/src/storage/deckconfig/mod.rs
@@ -16,7 +16,8 @@ use crate::deckconfig::DeckConfigInner;
 use crate::prelude::*;
 
 fn row_to_deckconf(row: &Row) -> Result<DeckConfig> {
-    let config = DeckConfigInner::decode(row.get_ref_unwrap(4).as_blob()?)?;
+    let mut config = DeckConfigInner::decode(row.get_ref_unwrap(4).as_blob()?)?;
+    config.ensure_values_valid();
     Ok(DeckConfig {
         id: row.get(0)?,
         name: row.get(1)?,

rslib/src/deckconfig/mod.rs
@@ -35,6 +35,40 @@ pub struct DeckConfig {
     pub inner: DeckConfigInner,
 }
 
+/// NOTE: this does not set the default steps
+const DEFAULT_DECK_CONFIG_INNER: DeckConfigInner = DeckConfigInner {
+    learn_steps: Vec::new(),
+    relearn_steps: Vec::new(),
+    new_per_day: 20,
+    reviews_per_day: 200,
+    new_per_day_minimum: 0,
+    initial_ease: 2.5,
+    easy_multiplier: 1.3,
+    hard_multiplier: 1.2,
+    lapse_multiplier: 0.0,
+    interval_multiplier: 1.0,
+    maximum_review_interval: 36_500,
+    minimum_lapse_interval: 1,
+    graduating_interval_good: 1,
+    graduating_interval_easy: 4,
+    new_card_insert_order: NewCardInsertOrder::Due as i32,
+    new_card_gather_priority: NewCardGatherPriority::Deck as i32,
+    new_card_sort_order: NewCardSortOrder::Template as i32,
+    review_order: ReviewCardOrder::Day as i32,
+    new_mix: ReviewMix::MixWithReviews as i32,
+    interday_learning_mix: ReviewMix::MixWithReviews as i32,
+    leech_action: LeechAction::TagOnly as i32,
+    leech_threshold: 8,
+    disable_autoplay: false,
+    cap_answer_time_to_secs: 60,
+    show_timer: false,
+    skip_question_when_replaying_answer: false,
+    bury_new: false,
+    bury_reviews: false,
+    bury_interday_learning: false,
+    other: Vec::new(),
+};
+
 impl Default for DeckConfig {
     fn default() -> Self {
         DeckConfig {
@@ -45,34 +79,7 @@ impl Default for DeckConfig {
             inner: DeckConfigInner {
                 learn_steps: vec![1.0, 10.0],
                 relearn_steps: vec![10.0],
-                new_per_day: 20,
-                reviews_per_day: 200,
-                new_per_day_minimum: 0,
-                initial_ease: 2.5,
-                easy_multiplier: 1.3,
-                hard_multiplier: 1.2,
-                lapse_multiplier: 0.0,
-                interval_multiplier: 1.0,
-                maximum_review_interval: 36_500,
-                minimum_lapse_interval: 1,
-                graduating_interval_good: 1,
-                graduating_interval_easy: 4,
-                new_card_insert_order: NewCardInsertOrder::Due as i32,
-                new_card_gather_priority: NewCardGatherPriority::Deck as i32,
-                new_card_sort_order: NewCardSortOrder::Template as i32,
-                review_order: ReviewCardOrder::Day as i32,
-                new_mix: ReviewMix::MixWithReviews as i32,
-                interday_learning_mix: ReviewMix::MixWithReviews as i32,
-                leech_action: LeechAction::TagOnly as i32,
-                leech_threshold: 8,
-                disable_autoplay: false,
-                cap_answer_time_to_secs: 60,
-                show_timer: false,
-                skip_question_when_replaying_answer: false,
-                bury_new: false,
-                bury_reviews: false,
-                bury_interday_learning: false,
-                other: vec![],
+                ..DEFAULT_DECK_CONFIG_INNER
             },
         }
     }
@@ -176,5 +183,80 @@ impl Collection {
         let conf = self.storage.get_deck_config(dcid)?.or_not_found(dcid)?;
         self.set_schema_modified()?;
         self.remove_deck_config_undoable(conf)
+    }
+}
+
+impl DeckConfigInner {
+    /// There was a period of time when the deck options screen was allowing
+    /// 0/NaN to be persisted, so we need to check the values are within
+    /// valid bounds when reading from the DB.
+    pub(crate) fn ensure_values_valid(&mut self) {
+        let default = DEFAULT_DECK_CONFIG_INNER;
+        ensure_u32_valid(&mut self.new_per_day, default.new_per_day, 0, 9999);
+        ensure_u32_valid(&mut self.reviews_per_day, default.reviews_per_day, 0, 9999);
+        ensure_u32_valid(
+            &mut self.new_per_day_minimum,
+            default.new_per_day_minimum,
+            0,
+            9999,
+        );
+        ensure_f32_valid(&mut self.initial_ease, default.initial_ease, 1.31, 5.0);
+        ensure_f32_valid(&mut self.easy_multiplier, default.easy_multiplier, 1.0, 5.0);
+        ensure_f32_valid(&mut self.hard_multiplier, default.hard_multiplier, 0.5, 1.3);
+        ensure_f32_valid(
+            &mut self.lapse_multiplier,
+            default.lapse_multiplier,
+            0.0,
+            1.0,
+        );
+        ensure_f32_valid(
+            &mut self.interval_multiplier,
+            default.interval_multiplier,
+            0.5,
+            2.0,
+        );
+        ensure_u32_valid(
+            &mut self.maximum_review_interval,
+            default.maximum_review_interval,
+            1,
+            36_500,
+        );
+        ensure_u32_valid(
+            &mut self.minimum_lapse_interval,
+            default.minimum_lapse_interval,
+            1,
+            36_500,
+        );
+        ensure_u32_valid(
+            &mut self.graduating_interval_good,
+            default.graduating_interval_good,
+            1,
+            36_500,
+        );
+        ensure_u32_valid(
+            &mut self.graduating_interval_easy,
+            default.graduating_interval_easy,
+            1,
+            36_500,
+        );
+        ensure_u32_valid(&mut self.leech_threshold, default.leech_threshold, 1, 9999);
+        ensure_u32_valid(
+            &mut self.cap_answer_time_to_secs,
+            default.cap_answer_time_to_secs,
+            1,
+            9999,
+        );
+    }
+}
+
+fn ensure_f32_valid(val: &mut f32, default: f32, min: f32, max: f32) {
+    if val.is_nan() || *val < min || *val > max {
+        *val = default;
+    }
+}
+
+fn ensure_u32_valid(val: &mut u32, default: u32, min: u32, max: u32) {
+    if *val < min || *val > max {
+        *val = default;
     }
 }

commit 9e0a6d5bf757787da20966d10914a845e936cfe2

Fix card ease being reset on schema upgrade
d20a7d291f96551a9f8fa53b1318057474ce8433 introduced a serious regression,
causing cards to be reset to the default ease when upgrading to the latest
schema version. This could also be triggered when exporting a colpkg with
legacy support.

rslib/src/storage/deckconfig/mod.rs
@@ -15,9 +15,11 @@ use crate::deckconfig::DeckConfigId;
 use crate::deckconfig::DeckConfigInner;
 use crate::prelude::*;
 
-fn row_to_deckconf(row: &Row) -> Result<DeckConfig> {
+fn row_to_deckconf(row: &Row, fix_invalid: bool) -> Result<DeckConfig> {
     let mut config = DeckConfigInner::decode(row.get_ref_unwrap(4).as_blob()?)?;
-    config.ensure_values_valid();
+    if fix_invalid {
+        config.ensure_values_valid();
+    }
     Ok(DeckConfig {
         id: row.get(0)?,
         name: row.get(1)?,
@@ -31,14 +33,22 @@ impl SqliteStorage {
     pub(crate) fn all_deck_config(&self) -> Result<Vec<DeckConfig>> {
         self.db
             .prepare_cached(include_str!("get.sql"))?
-            .query_and_then([], row_to_deckconf)?
+            .query_and_then([], |row| row_to_deckconf(row, true))?
+            .collect()
+    }
+
+    /// Does not cap values to those expected by the latest schema.
+    pub(crate) fn all_deck_config_for_schema16_upgrade(&self) -> Result<Vec<DeckConfig>> {
+        self.db
+            .prepare_cached(include_str!("get.sql"))?
+            .query_and_then([], |row| row_to_deckconf(row, false))?
             .collect()
     }
 
     pub(crate) fn get_deck_config_map(&self) -> Result<HashMap<DeckConfigId, DeckConfig>> {
         self.db
             .prepare_cached(include_str!("get.sql"))?
-            .query_and_then([], row_to_deckconf)?
+            .query_and_then([], |row| row_to_deckconf(row, true))?
             .map(|res| res.map(|d| (d.id, d)))
             .collect()
     }
@@ -46,7 +56,7 @@ impl SqliteStorage {
     pub(crate) fn get_deck_config(&self, dcid: DeckConfigId) -> Result<Option<DeckConfig>> {
         self.db
             .prepare_cached(concat!(include_str!("get.sql"), " where id = ?"))?
-            .query_and_then(params![dcid], row_to_deckconf)?
+            .query_and_then(params![dcid], |row| row_to_deckconf(row, true))?
             .next()
             .transpose()
     }
@@ -217,7 +227,7 @@ impl SqliteStorage {
 
     pub(super) fn upgrade_deck_conf_to_schema16(&self, server: bool) -> Result<()> {
         let mut invalid_configs = vec![];
-        for mut conf in self.all_deck_config()? {
+        for mut conf in self.all_deck_config_for_schema16_upgrade()? {
             // schema 16 changed starting ease of 250 to 2.5
             conf.inner.initial_ease /= 100.0;
             // new deck configs created with schema 15 had the wrong

rslib/src/storage/upgrades/mod.rs
@@ -78,6 +78,9 @@ impl SqliteStorage {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::collection::CollectionBuilder;
+    use crate::io::new_tempfile;
+    use crate::prelude::*;
 
     #[test]
     #[allow(clippy::assertions_on_constants)]
@@ -86,5 +89,26 @@ mod test {
             18, SCHEMA_MAX_VERSION,
             "must implement SqliteStorage::downgrade_to(SchemaVersion::V18)"
         );
+    }
+
+    #[test]
+    fn valid_ease_factor_survives_upgrade_roundtrip() -> Result<()> {
+        let tempfile = new_tempfile()?;
+        let mut col = CollectionBuilder::default()
+            .set_collection_path(tempfile.path())
+            .build()?;
+        let nt = col.get_notetype_by_name("Basic")?.unwrap();
+        let mut note = nt.new_note();
+        col.add_note(&mut note, DeckId(1))?;
+        col.storage
+            .db
+            .execute("update cards set factor = 1400", [])?;
+        col.close(Some(SchemaVersion::V11))?;
+        let col = CollectionBuilder::default()
+            .set_collection_path(tempfile.path())
+            .build()?;
+        let card = &col.storage.get_all_cards()[0];
+        assert_eq!(card.ease_factor, 1400);
+        Ok(())
     }
 }