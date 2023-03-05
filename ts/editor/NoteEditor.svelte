6889e6a1fbe15f89c48d6742d6ee71d2f6653b32
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2023-03-05 16:46:51 -0300

    Run ninja format

--- ts/editor/NoteEditor.svelte
+++ ts/editor/NoteEditor.svelte
@@ -154,7 +154,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     export function triggerChanges(): void {
         // I know this looks quite weird and doesn't seem to do anything
         // but if we don't call this after setPlainTexts() and setCollapsed()
-        // when switching notetypes, existing collapsibles won't react 
+        // when switching notetypes, existing collapsibles won't react
         // automatically to the updated props
         tick().then(() => {
             fieldsCollapsed = fieldsCollapsed;
