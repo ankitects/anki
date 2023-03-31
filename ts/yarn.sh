f05e5b448db04e1d1b9d2a47974f3fd5ec6e01e6
Damien Elmes <gpg@ankiweb.net>
2023-03-31 13:08:59 +1000

    Fix yarn.sh failing with linked out folder

--- ts/yarn.sh
+++ ts/yarn.sh
@@ -3,13 +3,12 @@
 
 set -e
 
-PATH=../out/extracted/node/bin
+PATH=$(realpath ../out/extracted/node/bin)
 
 ../out/extracted/node/bin/yarn $*
 
 cd ..
 
-PATH=out/extracted/node/bin
 ./node_modules/.bin/license-checker-rseidelsohn --production --json \
     --excludePackages anki --relativeLicensePath \
     --relativeModulePath > ts/licenses.json
