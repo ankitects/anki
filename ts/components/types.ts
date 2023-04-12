57d2e6bd5e7220d57785e353498aa9b5c7785152
Damien Elmes <gpg@ankiweb.net>
2023-04-12 16:36:15 +1000

    Remove some dead code and an invalid import from types.ts

--- ts/components/types.ts
+++ ts/components/types.ts
@@ -1,14 +1,5 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import type Pane from "./Pane.svelte";
-
 export type Size = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Breakpoint = "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
-
-export class ResizablePane {
-    resizable = {} as Pane;
-    height = 0;
-    minHeight = 0;
-    maxHeight = Infinity;
-}
