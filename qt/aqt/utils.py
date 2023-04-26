ankitects/anki

commit 30eef87b600ca0621635cce8c98ea287e8201331

Fix silent file save failures and bad default save path in Flatpak (#2427)
* add Jack Pearson to CONTRIBUTORS

* Skip file cleanup operations when impossible in sandbox

* Rename checkNeedPortalSupport -> running_in_sandbox

qt/aqt/utils.py
@@ -638,6 +638,23 @@ def getFile(
     return ret[0] if ret else None
 
 
+def running_in_sandbox():
+    """Check whether running in Flatpak or Snap. When in such a sandbox, Qt
+    will not report the true location of user-chosen files, but instead a
+    temporary location from which the sandboxing software will copy the file to
+    the user-chosen destination. Thus file renames are impossible and caching
+    the reported file location is unhelpful."""
+    in_flatpak = (
+        QStandardPaths.locate(
+            QStandardPaths.StandardLocation.RuntimeLocation,
+            "flatpak-info",
+        )
+        != ""
+    )
+    in_snap = os.environ.get("SNAP") != ""
+    return in_flatpak or in_snap
+
+
 def getSaveFile(
     parent: QDialog,
     title: str,
@@ -662,7 +679,7 @@ def getSaveFile(
         f"{key} (*{ext})",
         options=QFileDialog.Option.DontConfirmOverwrite,
     )[0]
-    if file:
+    if file and not running_in_sandbox():
         # add extension
         if not file.lower().endswith(ext):
             file += ext

CONTRIBUTORS
@@ -113,6 +113,7 @@ Mani <github.com/krmanik>
 Kaben Nanlohy <kaben.nanlohy@gmail.com>
 Tobias Predel <tobias.predel@gmail.com>
 Daniel Tang <danielzgtg.opensource@gmail.com>
+Jack Pearson <github.com/jrpear>
 
 ********************
 

commit 3d4459e395985368994807c14c9cf967d68df942

Fix file extension not being appended on export
Regressed in #2427

qt/aqt/utils.py
@@ -651,7 +651,7 @@ def running_in_sandbox():
         )
         != ""
     )
-    in_snap = os.environ.get("SNAP") != ""
+    in_snap = bool(os.environ.get("SNAP"))
     return in_flatpak or in_snap
 
 