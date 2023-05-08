ankitects/anki

commit 1e6686fcd8825f3b6e48cdcf222a3c36e775e176

Fix panic with invalid sync server URL with port (#2412)

CONTRIBUTORS
@@ -112,6 +112,7 @@ Fabricio Duarte <fabricio.duarte.jr@gmail.com>
 Mani <github.com/krmanik>
 Kaben Nanlohy <kaben.nanlohy@gmail.com>
 Tobias Predel <tobias.predel@gmail.com>
+Daniel Tang <danielzgtg.opensource@gmail.com>
 
 ********************
 

rslib/src/backend/sync/mod.rs
@@ -86,6 +86,11 @@ impl TryFrom<pb::sync::SyncAuth> for SyncAuth {
                 .endpoint
                 .map(|v| {
                     Url::try_from(v.as_str())
+                        // Without the next line, incomplete URLs like computer.local without the http://
+                        // are detected but URLs like computer.local:8000 are not.
+                        // By calling join() now, these URLs are detected too and later code that
+                        // uses and unwraps the result of join() doesn't panic
+                        .and_then(|x| x.join("/"))
                         .or_invalid("Invalid sync server specified. Please check the preferences.")
                 })
                 .transpose()?,

commit 912ad0d374924b5e8a4419efcdbbc4b54f5c0a55

Fix sync client ignoring directories (#2490)
* Fix sync client ignoring directories

Current implementation of the sync client does not properly send requests to a
self-hosted sync server if the URL has directories.

If the self-hosted sync URL is `https://example.org/foo/bar/`, Anki is expected
to send requests to `https://example.org/foo/bar/sync/hostKey`. Instead, it will
discard the directories and wrongly send requests to
`https://example.org/sync/hostKey`.

Fixes: 1e6686f ("Fix panic with invalid sync server URL with port")

* Add XeR to contributors

CONTRIBUTORS
@@ -119,6 +119,7 @@ Ingemar Berg <github.com/ingemarberg>
 Ben Kerman <ben@kermanic.org>
 Euan Kemp <euank@euank.com>
 Kieran Black <kieranlblack@gmail.com>
+XeR <github.com/XeR>
 
 ********************
 

rslib/src/backend/sync/mod.rs
@@ -90,7 +90,7 @@ impl TryFrom<pb::sync::SyncAuth> for SyncAuth {
                         // are detected but URLs like computer.local:8000 are not.
                         // By calling join() now, these URLs are detected too and later code that
                         // uses and unwraps the result of join() doesn't panic
-                        .and_then(|x| x.join("/"))
+                        .and_then(|x| x.join("./"))
                         .or_invalid("Invalid sync server specified. Please check the preferences.")
                 })
                 .transpose()?,