ankitects/anki

commit e5d5d1d4bdecfac326353d154c933e477c4e3eb8

Fix panic with invalid sync server URL with port (#2412)

rslib/src/backend/sync/mod.rs
@@ -86,6 +86,11 @@ impl TryFrom<pb::sync::SyncAuth> for SyncAuth {
                 .endpoint
                 .map(|v| {
                     Url::try_from(v.as_str())
+                        // Without the next line, incomplete URLs like computer.local without the http://
+                        // are detected but URLs like computer.local:8000 are not.
+                        // By calling join() now, these URLs are detected too and later code that
+                        // uses and unwraps the result of join() doesn't panic
+                        .and_then(|x| x.join("/"))
                         .or_invalid("Invalid sync server specified. Please check the preferences.")
                 })
                 .transpose()?,

CONTRIBUTORS
@@ -112,6 +112,7 @@ Fabricio Duarte <fabricio.duarte.jr@gmail.com>
 Mani <github.com/krmanik>
 Kaben Nanlohy <kaben.nanlohy@gmail.com>
 Tobias Predel <tobias.predel@gmail.com>
+Daniel Tang <danielzgtg.opensource@gmail.com>
 
 ********************
 

commit 70fca8880f125ecd1c150e8b070da5b5393e807c

Fix sync client ignoring directories
Current implementation of the sync client does not properly send requests to a
self-hosted sync server if the URL has directories.

If the self-hosted sync URL is `https://example.org/foo/bar/`, Anki is expected
to send requests to `https://example.org/foo/bar/sync/hostKey`. Instead, it will
discard the directories and wrongly send requests to
`https://example.org/sync/hostKey`.

Fixes: e5d5d1d ("Fix panic with invalid sync server URL with port")

rslib/src/backend/sync/mod.rs
@@ -90,7 +90,7 @@ impl TryFrom<pb::sync::SyncAuth> for SyncAuth {
                         // are detected but URLs like computer.local:8000 are not.
                         // By calling join() now, these URLs are detected too and later code that
                         // uses and unwraps the result of join() doesn't panic
-                        .and_then(|x| x.join("/"))
+                        .and_then(|x| x.join("./"))
                         .or_invalid("Invalid sync server specified. Please check the preferences.")
                 })
                 .transpose()?,