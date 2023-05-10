e6547bb9ab7d8ab15fd05c5b5cd5cc67d31217aa
Damien Elmes <gpg@ankiweb.net>
2023-05-10 11:24:27 +1000

    Automatically fetch win32 audio tools in .\run.bat

--- run.bat
+++ run.bat
@@ -6,9 +6,8 @@ set PYTHONPYCACHEPREFIX=out\pycache
 set ANKIDEV=1
 
 REM put mpv on the path
-REM run tools\ninja extract:win_amd64_audio to download this
 set PATH=%PATH%;out\extracted\win_amd64_audio
 
-call tools\ninja pylib/anki qt/aqt || exit /b 1
+call tools\ninja pylib/anki qt/aqt extract:win_amd64_audio || exit /b 1
 .\out\pyenv\scripts\python tools\run.py %* || exit /b 1
 popd
