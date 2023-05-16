c8d7615510966a67de1b31e2508519020aa6771d
Ren Tatsumoto <tatsu@autistici.org>
2023-05-16 12:16:18 +0300

    import optional

--- qt/aqt/profiles.py
+++ qt/aqt/profiles.py
@@ -10,7 +10,7 @@ import shutil
 import traceback
 from enum import Enum
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Optional
 
 import anki.lang
 import aqt.forms
