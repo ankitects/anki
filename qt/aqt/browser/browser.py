ankitects/anki

commit 98a161cec624bbb7e58f57c869741b349d96cc9f

add qtmajor qualification to version checks; update outdated min ver

qt/aqt/utils.py
@@ -515,7 +515,7 @@ def restoreGeom(
     if aqt.mw.pm.profile.get(key):
         widget.restoreGeometry(aqt.mw.pm.profile[key])
         if isMac and offset:
-            if qtminor > 6:
+            if qtmajor > 5 or qtminor > 6:
                 # bug in osx toolkit
                 s = widget.size()
                 widget.resize(s.width(), s.height() + offset * 2)
@@ -828,8 +828,6 @@ class MenuItem:
 
 
 def qtMenuShortcutWorkaround(qmenu: QMenu) -> None:
-    if qtminor < 10:
-        return
     for act in qmenu.actions():
         act.setShortcutVisibleInContextMenu(True)
 

qt/aqt/__init__.py
@@ -515,6 +515,7 @@ def _run(argv: Optional[list[str]] = None, exec: bool = True) -> Optional[AnkiAp
 
     if (
         isWin
+        and qtmajor == 5
         and (qtminor == 14 or (qtminor == 15 and qtpoint == 0))
         and "QT_QPA_PLATFORM" not in os.environ
     ):

qt/aqt/main.py
@@ -1204,8 +1204,6 @@ title="{}" {}>{}</button>""".format(
         qconnect(m.actionAbout.triggered, self.onAbout)
         qconnect(m.actionUndo.triggered, self.undo)
         qconnect(m.actionRedo.triggered, self.redo)
-        if qtminor < 11:
-            m.actionUndo.setShortcut(QKeySequence("Ctrl+Alt+Z"))
         qconnect(m.actionFullDatabaseCheck.triggered, self.onCheckDB)
         qconnect(m.actionCheckMediaDatabase.triggered, self.on_check_media_db)
         qconnect(m.actionDocumentation.triggered, self.onDocumentation)

qt/aqt/qt.py
@@ -53,7 +53,7 @@ qtmajor = (QT_VERSION & 0xFF0000) >> 16
 qtminor = (QT_VERSION & 0x00FF00) >> 8
 qtpoint = QT_VERSION & 0xFF
 
-if qtmajor != 5 or qtminor < 9 or qtminor == 10:
+if qtmajor < 5 or (qtmajor == 5 and qtminor < 14):
     raise Exception("Anki does not support your Qt version.")
 
 

qt/aqt/webview.py
@@ -346,13 +346,7 @@ class AnkiWebView(QWebEngineView):
         if isLin:
             factor = max(1, factor)
             return factor
-        # compensate for qt's integer scaling on windows?
-        if qtminor >= 14:
-            return 1
-        qtIntScale = self._getQtIntScale(screen)
-        desiredScale = factor * qtIntScale
-        newFactor = desiredScale / qtIntScale
-        return max(1, newFactor)
+        return 1
 
     def _getQtIntScale(self, screen: QWidget) -> int:
         # try to detect if Qt has scaled the screen

commit 35ea6b82826e27cebf1f4405a4b01e7906616ffc

Remove offset workaround in restoreGeom()
Way back in Qt4, there was an issue where (some?) windows would open
at a different location to where they were previously open. I've tested
the primary windows in Qt 5.14 on macOS, and the issue no longer seems
to exist, so this code is no longer useful.

The qtmajor > 5 check was a mistake introduced in 98a161cec624bbb7e58f57c869741b349d96cc9f;
it was intended to limit the code to Qt 5.

A quick grep of an add-on snapshot indicates there are no add-ons that
were using the offset param, so it has been removed.

qt/aqt/browser/browser.py
@@ -148,7 +148,7 @@ class Browser(QMainWindow):
             if self.layoutDirection() == Qt.LayoutDirection.RightToLeft
             else "editor"
         )
-        restoreGeom(self, self._editor_state_key, 0)
+        restoreGeom(self, self._editor_state_key)
         restoreSplitter(self.form.splitter, "editor3")
         restoreState(self, self._editor_state_key)
 

qt/aqt/utils.py
@@ -703,18 +703,12 @@ def saveGeom(widget: QWidget, key: str) -> None:
 def restoreGeom(
     widget: QWidget,
     key: str,
-    offset: int | None = None,
     adjustSize: bool = False,
     default_size: tuple[int, int] | None = None,
 ) -> None:
     key += "Geom"
     if existing_geom := aqt.mw.pm.profile.get(key):
         widget.restoreGeometry(existing_geom)
-        if is_mac and offset:
-            if qtmajor > 5 or qtminor > 6:
-                # bug in osx toolkit
-                s = widget.size()
-                widget.resize(s.width(), s.height() + offset * 2)
         ensureWidgetInScreenBoundaries(widget)
     elif adjustSize:
         widget.adjustSize()