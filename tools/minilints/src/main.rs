2c3947a8dd237a14235f09c30382b24b2549e0c2
Damien Elmes <gpg@ankiweb.net>
2023-05-26 12:47:11 +1000

    Move contributor check into minilints
    Will allow users to detect the issue locally with './ninja check'
    before pushing to CI.

--- tools/minilints/src/main.rs
+++ tools/minilints/src/main.rs
@@ -41,6 +41,7 @@ const IGNORED_FOLDERS: &[&str] = &[
 fn main() -> Result<()> {
     let want_fix = env::args().nth(1) == Some("fix".to_string());
     let mut ctx = LintContext::new(want_fix);
+    ctx.check_contributors()?;
     ctx.walk_folders(Path::new("."))?;
     if ctx.found_problems {
         std::process::exit(1);
@@ -121,6 +122,57 @@ impl LintContext {
             }
         }
         Ok(())
+    }
+
+    fn check_contributors(&self) -> Result<()> {
+        let antispam = ", at the domain ";
+
+        let last_author = String::from_utf8(
+            Command::new("git")
+                .args(["log", "-1", "--pretty=format:%ae"])
+                .output()?
+                .stdout,
+        )?;
+
+        let all_contributors = String::from_utf8(
+            Command::new("git")
+                .args(["log", "--pretty=format:%ae", "CONTRIBUTORS"])
+                .output()?
+                .stdout,
+        )?;
+        let all_contributors = all_contributors.lines().collect::<HashSet<&str>>();
+
+        if last_author == "49699333+dependabot[bot]@users.noreply.github.com" {
+            println!("Dependabot whitelisted.");
+            return Ok(());
+        } else if all_contributors.contains(last_author.as_str()) {
+            println!("Author found in CONTRIBUTORS");
+            return Ok(());
+        }
+
+        println!("All contributors:");
+        println!("{}", {
+            let mut contribs: Vec<_> = all_contributors
+                .iter()
+                .map(|s| s.replace('@', antispam))
+                .collect();
+            contribs.sort();
+            contribs.join("\n")
+        });
+
+        println!(
+            "Author {} NOT found in list",
+            last_author.replace('@', antispam)
+        );
+
+        println!(
+            "\nPlease make sure you modify the CONTRIBUTORS file using the email address you \
+                are committing from. If you have GitHub configured to hide your email address, \
+                you may need to make a change to the CONTRIBUTORS file using the GitHub UI, \
+                then try again."
+        );
+
+        std::process::exit(1);
     }
 }
 
