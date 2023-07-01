1d79f684bd3d1caa65de98ab57d5a3391a435203
Damien Elmes <gpg@ankiweb.net>
2023-07-01 22:10:29 +1000

    Fix newly-added image occlusion notetype not syncing
    https://forums.ankiweb.net/t/synchronisation-issue/31505/7

--- rslib/src/image_occlusion/notetype.rs
+++ rslib/src/image_occlusion/notetype.rs
@@ -20,8 +20,10 @@ impl Collection {
     pub fn add_image_occlusion_notetype_inner(&mut self) -> Result<()> {
         if self.get_first_io_notetype()?.is_none() {
             let mut nt = image_occlusion_notetype(&self.tr);
+            let usn = self.usn()?;
+            nt.set_modified(usn);
             let current_id = self.get_current_notetype_id();
-            self.add_notetype_inner(&mut nt, self.usn()?, false)?;
+            self.add_notetype_inner(&mut nt, usn, false)?;
             if let Some(current_id) = current_id {
                 // preserve previous default
                 self.set_current_notetype_id(current_id)?;
