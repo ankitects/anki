8c4eeecfcd90247e3c97cfe784eaf4601901207a
Hikaru Y <hkrysg@gmail.com>
2023-07-10 09:35:43 +0900

    Fix run-qt5.15.bat (#2569)

--- tools/run-qt5.15.bat
+++ tools/run-qt5.15.bat
@@ -5,6 +5,6 @@ set PYTHONWARNINGS=default
 set PYTHONPYCACHEPREFIX=out\pycache
 set ANKIDEV=1
 
-call tools\ninja pylib/anki qt/aqt pyenv-qt5.15 || exit /b 1
+call tools\ninja pylib qt pyenv-qt5.15 || exit /b 1
 .\out\pyenv-qt5.15\scripts\python tools\run.py %* || exit /b 1
 popd
