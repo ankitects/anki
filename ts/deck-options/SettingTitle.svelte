19d4f849ae57d4f4e84cb367491e4598cdc59cf4
Derek Dang <21960193+derekdang@users.noreply.github.com>
2023-07-18 05:49:02 -0700

    Address a11y warning for TabbedValue (#2582)
    * Address a11y warning for TabbedValue
    
    * Make the tab underline straight (dae)

--- ts/deck-options/SettingTitle.svelte
+++ ts/deck-options/SettingTitle.svelte
@@ -2,6 +2,8 @@
     Copyright: Ankitects Pty Ltd and contributors
     License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="setting-title" on:click>
     <slot />
 </div>
