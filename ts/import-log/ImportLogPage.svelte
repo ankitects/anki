637c92905a1eb15aae89e5406fcf8d63a25e4a75
Abdo <abdo@abdnh.net>
2023-07-19 19:38:29 +0300

    Remove unused import

--- ts/import-log/ImportLogPage.svelte
+++ ts/import-log/ImportLogPage.svelte
@@ -3,10 +3,7 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
-    import type {
-        ImportResponse,
-        ImportResponse_Log,
-    } from "@tslib/anki/import_export_pb";
+    import type { ImportResponse } from "@tslib/anki/import_export_pb";
     import {
         importAnkiPackage,
         importDone,
