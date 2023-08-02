3059193f2d86632011dfd1b3635f4821b80ef80e
Damien Elmes <gpg@ankiweb.net>
2023-08-02 20:17:01 +1000

    Give credit to loading.io

--- ts/components/BackendProgressIndicator.svelte
+++ ts/components/BackendProgressIndicator.svelte
@@ -26,6 +26,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     })();
 </script>
 
+<!-- spinner taken from https://loading.io/css/; CC0 -->
 {#if !result}
     <div class="progress">
         <div class="spinner" class:nightMode={$pageTheme.isDark}>
