ce9bc7909b073036f6d8807751011f1fb02ec17f
Michael Winkworth <mwinkwo@btinternet.com>
2023-09-08 23:34:25 +0100

    Fix formatting after attribute removal

--- ts/editor/DuplicateLink.svelte
+++ ts/editor/DuplicateLink.svelte
@@ -8,11 +8,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </script>
 
 <span class="duplicate-link-container">
-    <a
-        class="duplicate-link"
-        href="/#"
-        on:click={() => bridgeCommand("dupes")}
-    >
+    <a class="duplicate-link" href="/#" on:click={() => bridgeCommand("dupes")}>
         {tr.editingShowDuplicates()}
     </a>
 </span>
