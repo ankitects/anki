2254a1df7d40fb4f7fed688071dd057e249bc477
Damien Elmes <gpg@ankiweb.net>
2023-09-15 11:43:22 +1000

    Fix broken colpkg imports
    Introduced by "Update FSRS card data when preset or weights change"

--- qt/aqt/taskman.py
+++ qt/aqt/taskman.py
@@ -122,7 +122,9 @@ class TaskManager(QObject):
             self.mw.progress.finish()
             # allow the event loop to close the window before we proceed
             if on_done:
-                self.mw.progress.single_shot(100, lambda: on_done(fut))
+                self.mw.progress.single_shot(
+                    100, lambda: on_done(fut), requires_collection=False
+                )
 
         self.run_in_background(task, wrapped_done, uses_collection=uses_collection)
 
