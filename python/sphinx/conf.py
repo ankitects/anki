abb0046c92d7cb5f712344577428a6bed53a1194
Damien Elmes <gpg@ankiweb.net>
2023-10-12 13:19:01 +1000

    Don't write build outputs into source; use autoapi

--- python/sphinx/conf.py
+++ python/sphinx/conf.py
@@ -6,29 +6,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
-import sys
 
 project = 'Anki'
 copyright = '2023, Ankitects Pty Ltd and contributors'
 author = 'Ankitects Pty Ltd and contributors'
 
-REPO_ROOT = os.path.abspath('../../')
+REPO_ROOT = os.environ["REPO_ROOT"]
 
-# Autodoc requires us to add the modules to the Python path, see
-# https://stackoverflow.com/a/44980548/803801
-paths = [
-    'pylib',
-    'pylib/anki',
-    'pylib/anki/importing',
-    'qt',
-    'qt/aqt',
-    'out/pylib',
-    'out/qt',
-    'out/qt/_aqt',
-]
-
-for path in paths:
-    sys.path.insert(0, os.path.join(REPO_ROOT, path))
-
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme', 'autoapi.extension']
 html_theme = 'sphinx_rtd_theme'
+autoapi_python_use_implicit_namespaces = True
+autoapi_dirs = [os.path.join(REPO_ROOT, x) for x in ["pylib/anki", "out/pylib/anki", "qt/aqt", "out/qt/aqt"]]
