2ad77ec2403fca75f7b7fcfbd5b69eb4a4ddac6c
Damien Elmes <gpg@ankiweb.net>
2023-10-14 10:20:14 +1000

    Keep badge out of natural tab order
    If I've understood things correctly, this should allow regular
    users to skip over the badge elements with Tab, while still
    allowing screen reader apps to read/focus the badge elements.
    
    https://github.com/ankitects/anki/pull/2721#issuecomment-1761740985
    https://forums.ankiweb.net/t/anki-23-10-beta-5/35677/18

--- ts/components/Badge.svelte
+++ ts/components/Badge.svelte
@@ -31,6 +31,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     on:click
     on:mouseenter
     on:mouseleave
+    tabindex="-1"
 >
     <IconConstrain {iconSize} {widthMultiplier} {flipX}>
         <slot />
