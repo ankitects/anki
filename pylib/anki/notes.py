94d51390b75ceeecc3a859715224acc901ffc6ca
Abdo <abdo@abdnh.net>
2023-10-16 23:04:25 +0300

    Deprecate .flush()

--- pylib/anki/notes.py
+++ pylib/anki/notes.py
@@ -12,7 +12,7 @@ import anki.collection
 import anki.decks
 import anki.template
 from anki import hooks, notes_pb2
-from anki._legacy import DeprecatedNamesMixin
+from anki._legacy import DeprecatedNamesMixin, deprecated
 from anki.consts import MODEL_STD
 from anki.models import NotetypeDict, NotetypeId, TemplateDict
 from anki.utils import join_fields
@@ -78,6 +78,7 @@ class Note(DeprecatedNamesMixin):
             fields=self.fields,
         )
 
+    @deprecated(info="please use col.update_note()")
     def flush(self) -> None:
         """For an undo entry, use col.update_note() instead."""
         if self.id == 0:
