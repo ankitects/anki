072ab9983091dfb9c046637be10f99bafb04c5fe
Julius <juliusniemeyer1995@gmail.com>
2023-10-26 23:03:53 +0200

    fixed previous mistake

--- ts/editor/image-overlay/icons.ts
+++ ts/editor/image-overlay/icons.ts
@@ -1,10 +1,9 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-// currently floatRightButton and floatLeftButton are swapped because they are apparently reversed in the source as well
-export { default as floatRightIcon } from "@mdi/svg/svg/format-float-left.svg";
+export { default as floatLeftIcon } from "@mdi/svg/svg/format-float-left.svg";
 export { default as floatNoneIcon } from "@mdi/svg/svg/format-float-none.svg";
-export { default as floatLeftIcon } from "@mdi/svg/svg/format-float-right.svg";
+export { default as floatRightIcon } from "@mdi/svg/svg/format-float-right.svg";
 export { default as sizeClear } from "@mdi/svg/svg/image-remove.svg";
 export { default as sizeActual } from "@mdi/svg/svg/image-size-select-actual.svg";
 export { default as sizeMinimized } from "@mdi/svg/svg/image-size-select-large.svg";
