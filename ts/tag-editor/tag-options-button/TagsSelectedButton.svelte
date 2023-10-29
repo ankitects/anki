643bb03d09f4e48bc84534944ad93888727fe4c1
blob1234567890 <ben.k.olson@gmail.com>
2023-10-29 12:23:04 -0700

    quick fix syntax

--- ts/tag-editor/tag-options-button/TagsSelectedButton.svelte
+++ ts/tag-editor/tag-options-button/TagsSelectedButton.svelte
@@ -26,6 +26,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             case "Enter":
             case "Space":
                 show = !show;
+                break;
         }
     }
 </script>
