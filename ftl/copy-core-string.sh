5fa64e8f5573b5d0cf7055314ded364fa7892212
Damien Elmes <gpg@ankiweb.net>
2023-11-04 08:45:02 +1000

    Move 'review' out of 'no need to translate' section

--- ftl/copy-core-string.sh
+++ ftl/copy-core-string.sh
@@ -2,5 +2,7 @@
 # - sync ftl
 # - ./copy-core-string.sh scheduling-review browsing-sidebar-card-state-review
 # - confirm changes in core-repo/ correct
-# - commit and push changes, then update submodule in main repo
+# - commit and push changes
+# - ensure string in template isn't in the 'no need to translate' section
+# - update submodule in main repo
 ./ftl string copy ftl/core-repo/core ftl/core-repo/core $1 $2
