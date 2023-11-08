ae6a03942f651790c40f8d8479f90eb7715bf2af
Damien Elmes <gpg@ankiweb.net>
2023-11-08 14:00:41 +1000

    Check video tags in media check
    https://forums.ankiweb.net/t/video-tag-does-not-recognize-the-source-file-in-check-media/36850/2

--- rslib/src/text.rs
+++ rslib/src/text.rs
@@ -104,7 +104,7 @@ lazy_static! {
     pub static ref HTML_MEDIA_TAGS: Regex = Regex::new(
         r#"(?xsi)
             # the start of the image, audio, or object tag
-            <\b(?:img|audio|object)\b[^>]+\b(?:src|data)\b=
+            <\b(?:img|audio|video|object)\b[^>]+\b(?:src|data)\b=
             (?:
                     # 1: double-quoted filename
                     "
