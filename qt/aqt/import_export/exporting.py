cf28ec14375010537ec74e585859167aee885e26
RumovZ <gp5glkw78@relay.firefox.com>
2023-11-13 04:54:41 +0100

    Allow im-/exporting with or without deck configs (#2804)
    * Allow im-/exporting with or without deck configs
    
    Closes #2777.
    
    * Enable webengine remote debugging in launch.json
    
    * Reset deck limits and counts based on scheduling
    
    Also:
    - Fix `deck.common` not being reset.
    - Apply all logic only depending on the source collection in the
    gathering stage.
    - Skip checking for scheduling and only act based on whether the call
    wants scheduling. Preservation of filtered decks also depends on all
    original decks being included.
    - Fix check_ids() not covering revlog.
    
    * Fix importing legacy filtered decks w/o scheduling
    
    * Disable 'include deck options' by default, and fix tab order (dae)
    
    * deck options > deck presets (dae)

--- qt/aqt/import_export/exporting.py
+++ qt/aqt/import_export/exporting.py
@@ -12,7 +12,13 @@ from typing import Optional, Sequence, Type
 
 import aqt.forms
 import aqt.main
-from anki.collection import DeckIdLimit, ExportLimit, NoteIdsLimit, Progress
+from anki.collection import (
+    DeckIdLimit,
+    ExportAnkiPackageOptions,
+    ExportLimit,
+    NoteIdsLimit,
+    Progress,
+)
 from anki.decks import DeckId, DeckNameId
 from anki.notes import NoteId
 from aqt import gui_hooks
@@ -90,6 +96,9 @@ class ExportDialog(QDialog):
     def exporter_changed(self, idx: int) -> None:
         self.exporter = self.exporter_classes[idx]()
         self.frm.includeSched.setVisible(self.exporter.show_include_scheduling)
+        self.frm.include_deck_configs.setVisible(
+            self.exporter.show_include_deck_configs
+        )
         self.frm.includeMedia.setVisible(self.exporter.show_include_media)
         self.frm.includeTags.setVisible(self.exporter.show_include_tags)
         self.frm.includeHTML.setVisible(self.exporter.show_include_html)
@@ -137,6 +146,7 @@ class ExportDialog(QDialog):
         return ExportOptions(
             out_path=out_path,
             include_scheduling=self.frm.includeSched.isChecked(),
+            include_deck_configs=self.frm.include_deck_configs.isChecked(),
             include_media=self.frm.includeMedia.isChecked(),
             include_tags=self.frm.includeTags.isChecked(),
             include_html=self.frm.includeHTML.isChecked(),
@@ -170,6 +180,7 @@ class ExportDialog(QDialog):
 class ExportOptions:
     out_path: str
     include_scheduling: bool
+    include_deck_configs: bool
     include_media: bool
     include_tags: bool
     include_html: bool
@@ -184,6 +195,7 @@ class Exporter(ABC):
     extension: str
     show_deck_list = False
     show_include_scheduling = False
+    show_include_deck_configs = False
     show_include_media = False
     show_include_tags = False
     show_include_html = False
@@ -241,6 +253,7 @@ class ApkgExporter(Exporter):
     extension = "apkg"
     show_deck_list = True
     show_include_scheduling = True
+    show_include_deck_configs = True
     show_include_media = True
     show_legacy_support = True
 
@@ -260,9 +273,12 @@ class ApkgExporter(Exporter):
             op=lambda col: col.export_anki_package(
                 out_path=options.out_path,
                 limit=options.limit,
-                with_scheduling=options.include_scheduling,
-                with_media=options.include_media,
-                legacy_support=options.legacy_support,
+                options=ExportAnkiPackageOptions(
+                    with_scheduling=options.include_scheduling,
+                    with_deck_configs=options.include_deck_configs,
+                    with_media=options.include_media,
+                    legacy=options.legacy_support,
+                ),
             ),
             success=on_success,
         ).with_backend_progress(export_progress_update).run_in_background()
