7bc41303077f18a3beffd5e03ce5162c7b3ccdd0
blob1234567890 <ben.k.olson@gmail.com>
2023-11-13 20:38:57 -0800

    fixed scrolling

--- ts/components/Select.svelte
+++ ts/components/Select.svelte
@@ -153,6 +153,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             buttons[selected].classList.remove("focus");
         }
         buttons[num].classList.add("focus");
+        buttons[num].scrollIntoView(false);
         selected = num;
     }
 </script>
