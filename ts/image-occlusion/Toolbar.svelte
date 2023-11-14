2c59d23f978b2dfaee7d4f828cc02aa6dca43949
Mani <12841290+krmanik@users.noreply.github.com>
2023-11-14 19:12:07 +0800

    allow io shortcuts in mask editor only

--- ts/image-occlusion/Toolbar.svelte
+++ ts/image-occlusion/Toolbar.svelte
@@ -15,7 +15,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
     import { mdiEye, mdiFormatAlignCenter, mdiSquare, mdiViewDashboard } from "./icons";
     import { emitChangeSignal } from "./MaskEditor.svelte";
-    import { hideAllGuessOne } from "./store";
+    import { hideAllGuessOne, ioMaskEditorVisible } from "./store";
     import { drawEllipse, drawPolygon, drawRectangle, drawText } from "./tools/index";
     import { makeMaskTransparent } from "./tools/lib";
     import { enableSelectable, stopDraw } from "./tools/lib";
@@ -324,120 +324,122 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     </div>
 </div>
 
-<Shortcut
-    keyCombination="S"
-    on:action={() => {
-        disableFunctions();
-        enableSelectable(canvas, true);
-        activeTool = "cursor";
-    }}
-/>
-<Shortcut
-    keyCombination="R"
-    on:action={() => {
-        drawRectangle(canvas);
-        activeTool = "draw-rectangle";
-    }}
-/>
-<Shortcut
-    keyCombination="E"
-    on:action={() => {
-        drawEllipse(canvas);
-        activeTool = "draw-ellipse";
-    }}
-/>
-<Shortcut
-    keyCombination="P"
-    on:action={() => {
-        drawPolygon(canvas, instance);
-        activeTool = "draw-polygon";
-    }}
-/>
-<Shortcut
-    keyCombination="T"
-    on:action={() => {
-        drawText(canvas);
-        activeTool = "draw-text";
-    }}
-/>
-<Shortcut
-    keyCombination="M"
-    on:action={() => {
-        enableSelectable(canvas, false);
-        instance.resume();
-        activeTool = "magnify";
-    }}
-/>
-<Shortcut
-    keyCombination="Ctrl+Z"
-    on:action={() => {
-        undoStack.undo();
-        emitChangeSignal();
-    }}
-/>
-<Shortcut
-    keyCombination="Ctrl+Y"
-    on:action={() => {
-        undoStack.redo();
-        emitChangeSignal();
-    }}
-/>
-<Shortcut
-    keyCombination="Ctrl+-"
-    on:action={() => {
-        enableMagnify();
-        zoomTools[0].action(instance);
-    }}
-/>
-<Shortcut
-    keyCombination="Ctrl++"
-    on:action={() => {
-        enableMagnify();
-        zoomTools[1].action(instance);
-    }}
-/>
-<Shortcut
-    keyCombination="Ctrl+F"
-    on:action={() => {
-        enableMagnify();
-        zoomTools[2].action(instance);
-    }}
-/>
-<Shortcut
-    keyCombination="L"
-    on:action={() => {
-        maksOpacity = !maksOpacity;
-        makeMaskTransparent(canvas, maksOpacity);
-    }}
-/>
-<Shortcut
-    keyCombination="Delete"
-    on:action={() => {
-        deleteDuplicateTools[0].action(canvas);
-        emitChangeSignal();
-    }}
-/>
-<Shortcut
-    keyCombination="D"
-    on:action={() => {
-        deleteDuplicateTools[1].action(canvas);
-        emitChangeSignal();
-    }}
-/>
-<Shortcut
-    keyCombination="G"
-    on:action={() => {
-        groupUngroupTools[0].action(canvas);
-        emitChangeSignal();
-    }}
-/>
-<Shortcut
-    keyCombination="U"
-    on:action={() => {
-        groupUngroupTools[1].action(canvas);
-        emitChangeSignal();
-    }}
-/>
+{#if $ioMaskEditorVisible}
+    <Shortcut
+        keyCombination="S"
+        on:action={() => {
+            disableFunctions();
+            enableSelectable(canvas, true);
+            activeTool = "cursor";
+        }}
+    />
+    <Shortcut
+        keyCombination="R"
+        on:action={() => {
+            drawRectangle(canvas);
+            activeTool = "draw-rectangle";
+        }}
+    />
+    <Shortcut
+        keyCombination="E"
+        on:action={() => {
+            drawEllipse(canvas);
+            activeTool = "draw-ellipse";
+        }}
+    />
+    <Shortcut
+        keyCombination="P"
+        on:action={() => {
+            drawPolygon(canvas, instance);
+            activeTool = "draw-polygon";
+        }}
+    />
+    <Shortcut
+        keyCombination="T"
+        on:action={() => {
+            drawText(canvas);
+            activeTool = "draw-text";
+        }}
+    />
+    <Shortcut
+        keyCombination="M"
+        on:action={() => {
+            enableSelectable(canvas, false);
+            instance.resume();
+            activeTool = "magnify";
+        }}
+    />
+    <Shortcut
+        keyCombination="Ctrl+Z"
+        on:action={() => {
+            undoStack.undo();
+            emitChangeSignal();
+        }}
+    />
+    <Shortcut
+        keyCombination="Ctrl+Y"
+        on:action={() => {
+            undoStack.redo();
+            emitChangeSignal();
+        }}
+    />
+    <Shortcut
+        keyCombination="Ctrl+-"
+        on:action={() => {
+            enableMagnify();
+            zoomTools[0].action(instance);
+        }}
+    />
+    <Shortcut
+        keyCombination="Ctrl++"
+        on:action={() => {
+            enableMagnify();
+            zoomTools[1].action(instance);
+        }}
+    />
+    <Shortcut
+        keyCombination="Ctrl+F"
+        on:action={() => {
+            enableMagnify();
+            zoomTools[2].action(instance);
+        }}
+    />
+    <Shortcut
+        keyCombination="L"
+        on:action={() => {
+            maksOpacity = !maksOpacity;
+            makeMaskTransparent(canvas, maksOpacity);
+        }}
+    />
+    <Shortcut
+        keyCombination="Delete"
+        on:action={() => {
+            deleteDuplicateTools[0].action(canvas);
+            emitChangeSignal();
+        }}
+    />
+    <Shortcut
+        keyCombination="D"
+        on:action={() => {
+            deleteDuplicateTools[1].action(canvas);
+            emitChangeSignal();
+        }}
+    />
+    <Shortcut
+        keyCombination="G"
+        on:action={() => {
+            groupUngroupTools[0].action(canvas);
+            emitChangeSignal();
+        }}
+    />
+    <Shortcut
+        keyCombination="U"
+        on:action={() => {
+            groupUngroupTools[1].action(canvas);
+            emitChangeSignal();
+        }}
+    />
+{/if}
 
 <style>
     .top-tool-bar-container {
