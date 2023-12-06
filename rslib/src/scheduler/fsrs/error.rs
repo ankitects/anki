75681810b0ca8ec8f455aacd176fe3f858ccc341
Damien Elmes <gpg@ankiweb.net>
2023-12-06 13:15:45 +1000

    Update to latest fsrs-rs
    The extra entries in Cargo.lock and licenses.json are mostly spurious,
    and caused by https://github.com/tracel-ai/burn/pull/1025#issuecomment-1842006690

--- rslib/src/scheduler/fsrs/error.rs
+++ rslib/src/scheduler/fsrs/error.rs
@@ -4,6 +4,7 @@
 use fsrs::FSRSError;
 
 use crate::error::AnkiError;
+use crate::error::InvalidInputError;
 
 impl From<FSRSError> for AnkiError {
     fn from(err: FSRSError) -> Self {
@@ -12,6 +13,13 @@ impl From<FSRSError> for AnkiError {
             FSRSError::OptimalNotFound => AnkiError::FsrsUnableToDetermineDesiredRetention,
             FSRSError::Interrupted => AnkiError::Interrupted,
             FSRSError::InvalidWeights => AnkiError::FsrsWeightsInvalid,
+            FSRSError::InvalidInput => AnkiError::InvalidInput {
+                source: InvalidInputError {
+                    message: "invalid weights provided".to_string(),
+                    source: None,
+                    backtrace: None,
+                },
+            },
         }
     }
 }
