d87e705e29e8816eaa8da3fa08362136f10542bd
Kai Knoblich <kai@FreeBSD.org>
2024-01-27 12:32:15 +0100

    Add support for offline builds
    Downloading files during build time is a non-starter for FreeBSD ports
    (and presumably for other *BSD ports and some Linux distros as well).
    
    In order to still be able to build Anki successfully, two new
    environment variables have been added that can be set accordingly:
    
    * NO_VENV: If set, the Python system environment is used instead of
      a venv. This is necessary if there are no usable Python wheels for a
      platform, e.g. PyQt6.
    
    * OFFLINE_BUILD: If set, the git repository synchronization (translation
      files, build hash, etc.) is skipped.
    
    To successfully build Anki offline, following conditions must be met:
    
    1. All required dependencies (node, Python, rust, yarn, etc.) must be
       present in the build environment.
    
    2. The offline repositories for the translation files must be
       copied/linked to ftl/qt-repo and ftl/core-repo.
    
    3. The Python pseudo venv needs to be setup:
    
       $ mkdir out/pyenv/bin
       $ ln -s /path/to/python out/pyenv/bin/python
       $ ln -s /path/to/protoc-gen-mypy out/pyenv/bin/protoc-gen-mypy
    
    4. Create the offline cache for yarn and use its own environment
       variable YARN_CACHE_FOLDER to it:
    
       YARN_CACHE_FOLDER=/path/to/the/yarn/cache
       $ /path/to/yarn install --ignore-scripts
    
    5. Build Anki:
    
       $ /path/to/cargo build --package runner --release --verbose --verbose
       $ OFFLINE_BUILD=1 \
         NO_VENV=1 \
         ${WRKSRC}/out/rust/release/runner build wheels

--- build/configure/src/python.rs
+++ build/configure/src/python.rs
@@ -13,6 +13,7 @@ use ninja_gen::input::BuildInput;
 use ninja_gen::inputs;
 use ninja_gen::python::python_format;
 use ninja_gen::python::PythonEnvironment;
+use ninja_gen::python::PythonEnvironmentStub;
 use ninja_gen::python::PythonLint;
 use ninja_gen::python::PythonTypecheck;
 use ninja_gen::rsync::RsyncFiles;
@@ -75,6 +76,25 @@ pub fn setup_venv(build: &mut Build) -> Result<()> {
             base_requirements_txt: inputs!["python/requirements.base.txt"],
             requirements_txt: inputs![reqs_qt5, "python/requirements.qt5_14.txt"],
             extra_binary_exports: &[],
+        },
+    )?;
+
+    Ok(())
+}
+
+pub fn setup_venv_stub(build: &mut Build) -> Result<()> {
+    build.add_action(
+        "pyenv",
+        PythonEnvironmentStub {
+            folder: "pyenv",
+            extra_binary_exports: &[
+                "mypy",
+                "black",  // Required in some parts of the code, but not for build
+                "isort",  // dito
+                "pylint", // dito
+                "pytest", // dito
+                "protoc-gen-mypy",
+            ],
         },
     )?;
 
