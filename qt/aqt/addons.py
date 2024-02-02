312d396505a4806b76b74dfda6d276ec595e17ed
Abdo <abdo@abdnh.net>
2024-02-02 10:03:29 +0300

    Change default font in code editors (#2985)
    * Change default font in code editors
    
    * Fall back to system font

--- qt/aqt/addons.py
+++ qt/aqt/addons.py
@@ -1543,7 +1543,9 @@ class ConfigEditor(QDialog):
         tooltip(tr.addons_restored_defaults(), parent=self)
 
     def setupFonts(self) -> None:
-        font_mono = QFontDatabase.systemFont(QFontDatabase.SystemFont.FixedFont)
+        font_mono = QFont("Consolas")
+        if not font_mono.exactMatch():
+            font_mono = QFontDatabase.systemFont(QFontDatabase.SystemFont.FixedFont)
         font_mono.setPointSize(font_mono.pointSize() + 1)
         self.form.editor.setFont(font_mono)
 
