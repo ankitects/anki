97dcb776de721ce055013291b1de8a3d3800af41
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-02-18 00:09:05 -0600

    Refactor text attribute button components (#3012)
    * Factor out common code in bold/italic/underline/sub/sup buttons
    
    * Update exclusiveNames' states on click
    
    * Update exclusiveNames' states on keyboard shortcut

--- ts/editor/editor-toolbar/ItalicButton.svelte
+++ ts/editor/editor-toolbar/ItalicButton.svelte
@@ -4,20 +4,11 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
     import * as tr from "@tslib/ftl";
-    import { getPlatformString } from "@tslib/shortcuts";
     import { removeStyleProperties } from "@tslib/styling";
-    import { singleCallback } from "@tslib/typing";
-    import { onMount } from "svelte";
 
-    import IconButton from "../../components/IconButton.svelte";
-    import Shortcut from "../../components/Shortcut.svelte";
-    import WithState from "../../components/WithState.svelte";
     import type { MatchType } from "../../domlib/surround";
-    import { surrounder } from "../rich-text-input";
-    import { context as editorToolbarContext } from "./EditorToolbar.svelte";
     import { italicIcon } from "./icons";
-
-    const surroundElement = document.createElement("i");
+    import TextAttributeButton from "./TextAttributeButton.svelte";
 
     function matcher(element: HTMLElement | SVGElement, match: MatchType): void {
         if (element.tagName === "I" || element.tagName === "EM") {
@@ -35,62 +26,14 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             });
         }
     }
-
-    const key = "italic";
-
-    const format = {
-        surroundElement,
-        matcher,
-    };
-
-    const namedFormat = {
-        key,
-        name: tr.editingItalicText(),
-        show: true,
-        active: true,
-    };
-
-    const { removeFormats } = editorToolbarContext.get();
-    removeFormats.update((formats) => [...formats, namedFormat]);
-
-    async function updateStateFromActiveInput(): Promise<boolean> {
-        return disabled ? false : surrounder.isSurrounded(key);
-    }
-
-    function makeItalic(): void {
-        surrounder.surround(key);
-    }
-
-    const keyCombination = "Control+I";
-
-    let disabled: boolean;
-
-    onMount(() =>
-        singleCallback(
-            surrounder.active.subscribe((value) => (disabled = !value)),
-            surrounder.registerFormat(key, format),
-        ),
-    );
 </script>
 
-<WithState {key} update={updateStateFromActiveInput} let:state={active} let:updateState>
-    <IconButton
-        tooltip="{tr.editingItalicText()} ({getPlatformString(keyCombination)})"
-        {active}
-        {disabled}
-        on:click={(event) => {
-            makeItalic();
-            updateState(event);
-        }}
-    >
-        {@html italicIcon}
-    </IconButton>
-
-    <Shortcut
-        {keyCombination}
-        on:action={(event) => {
-            makeItalic();
-            updateState(event);
-        }}
-    />
-</WithState>
+<TextAttributeButton
+    tagName="i"
+    {matcher}
+    key="italic"
+    tooltip={tr.editingItalicText()}
+    keyCombination="Control+I"
+>
+    {@html italicIcon}
+</TextAttributeButton>
