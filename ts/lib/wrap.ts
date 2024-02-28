0b3d775de20a807c01f0c19ce1843c6649ac1949
Abdo <abdo@abdnh.net>
2024-02-28 16:58:35 +0300

    Avoid .extractContents() and handle partially covered tags

--- ts/lib/wrap.ts
+++ ts/lib/wrap.ts
@@ -61,13 +61,62 @@ export function wrapClozeInternal(base: Element, n: number): void {
     if (!range) {
         return;
     }
-    const fragment = range.extractContents()!;
+
+    // Expand the range to include parent nodes whose children are already included.
+    // This is intended to prevent the cloze from partially covering elements, which can cause display issues.
+    let startParent: Node | null = range.startContainer.parentNode;
+    if (
+        startParent !== base
+        && startParent?.firstChild === range.startContainer && range.startOffset === 0
+    ) {
+        range.setStartBefore(startParent);
+    }
+    let endParent: Node | null = range.endContainer.parentNode;
+    if (
+        endParent !== base
+        && endParent?.lastChild === range.endContainer && (
+            (range.endContainer.nodeType != Node.ELEMENT_NODE
+                && range.endOffset === range.endContainer.textContent?.length)
+            || (range.endContainer.nodeType === Node.ELEMENT_NODE
+                && range.endOffset === range.endContainer.childNodes.length)
+        )
+    ) {
+        range.setEndAfter(endParent);
+    }
+    let expand = false;
+    do {
+        if (
+            startParent
+            && startParent.parentNode !== base && startParent.parentNode?.firstChild === startParent
+            && range.isPointInRange(startParent.parentNode, startParent.parentNode?.childNodes.length)
+        ) {
+            startParent = startParent.parentNode;
+            range.setStartBefore(startParent);
+            expand = true;
+        } else {
+            expand = false;
+        }
+        if (
+            endParent && endParent.parentNode !== base && endParent.parentNode?.lastChild === endParent
+            && range.isPointInRange(endParent.parentNode, 0)
+        ) {
+            endParent = endParent.parentNode;
+            range.setEndAfter(endParent);
+            expand = true;
+        } else if (!expand) {
+            expand = false;
+        }
+    } while (expand);
+
+    const fragment = range.cloneContents()!;
     const isEmpty = fragment.childNodes.length === 0;
     const startNode = document.createTextNode(`{{c${n}::`);
     const endNode = document.createTextNode("}}");
-    range.insertNode(endNode);
-    range.insertNode(fragment);
     range.insertNode(startNode);
+    const range2 = range.cloneRange();
+    range2.collapse(false);
+    range2.insertNode(endNode);
+
     let referenceNode: Node;
     if (isEmpty) {
         referenceNode = startNode;
