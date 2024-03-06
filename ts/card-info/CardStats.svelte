a1c9b7cab6edc2da0c0d501e5c88e81f16967dac
Abdo <abdo@abdnh.net>
2024-03-06 04:41:31 +0300

    Show preset and original deck in card info

--- ts/card-info/CardStats.svelte
+++ ts/card-info/CardStats.svelte
@@ -105,7 +105,14 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
         statsRows.push({ label: tr2.cardStatsCardTemplate(), value: stats.cardType });
         statsRows.push({ label: tr2.cardStatsNoteType(), value: stats.notetype });
-        statsRows.push({ label: tr2.cardStatsDeckName(), value: stats.deck });
+        let deck: string;
+        if (stats.deck !== stats.originalDeck) {
+            deck = `${stats.deck} (${stats.originalDeck})`;
+        } else {
+            deck = stats.deck;
+        }
+        statsRows.push({ label: tr2.cardStatsDeckName(), value: deck });
+        statsRows.push({ label: tr2.cardStatsPreset(), value: stats.preset });
 
         statsRows.push({ label: tr2.cardStatsCardId(), value: stats.cardId });
         statsRows.push({ label: tr2.cardStatsNoteId(), value: stats.noteId });
