dc209c1c88d5c1d8990722eed64b388889c4dc5e
user1823 <92206575+user1823@users.noreply.github.com>
2024-03-09 23:37:19 +0530

    Fix font in HTML editor
    https://forums.ankiweb.net/t/anki-24-04-beta-rc/41792/47

--- ts/editor/CodeMirror.svelte
+++ ts/editor/CodeMirror.svelte
@@ -103,7 +103,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
         :global(.CodeMirror) {
             height: auto;
-            font-family: Consolas monospace;
+            font-family: Consolas, monospace;
         }
 
         :global(.CodeMirror-wrap pre) {
