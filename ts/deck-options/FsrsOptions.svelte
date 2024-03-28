8362ad53453e080030536e278cac34bb52d44890
Damien Elmes <gpg@ankiweb.net>
2024-03-28 16:02:05 +0700

    Update optimal retention tooltip, and add a few extra translations

--- ts/deck-options/FsrsOptions.svelte
+++ ts/deck-options/FsrsOptions.svelte
@@ -340,7 +340,9 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             min={1}
             max={3650}
         >
-            <SettingTitle>Days to simulate</SettingTitle>
+            <SettingTitle on:click={() => openHelpModal("computeOptimalRetention")}>
+                Days to simulate
+            </SettingTitle>
         </SpinBoxRow>
 
         <button
