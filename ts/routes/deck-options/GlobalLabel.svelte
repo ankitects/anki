0840afcb9706946891ca99209cda73f91340ca85
RumovZ <gp5glkw78@relay.firefox.com>
2024-04-11 21:57:48 +0200

    Add tooltip to global label icon

--- ts/routes/deck-options/GlobalLabel.svelte
+++ ts/routes/deck-options/GlobalLabel.svelte
@@ -3,13 +3,15 @@ Copyright: Ankitects Pty Ltd and contributors
 License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
+    import * as tr from "@generated/ftl";
     import { mdiEarth } from "$lib/components/icons";
     import Icon from "$lib/components/Icon.svelte";
+
     export let title: string;
 </script>
 
 {title}
-<div class="inline-icon">
+<div class="inline-icon" title={tr.deckConfigAffectsEntireCollection()}>
     <Icon icon={mdiEarth} />
 </div>
 
