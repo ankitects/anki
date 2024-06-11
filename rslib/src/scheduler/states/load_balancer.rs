63fa18ba9f53cbe3c3fc2bcc14e9068955724c23
Jake Probst <jake.probst@gmail.com>
2024-06-11 00:02:10 -0600

    copyright header on load_balancer.rs

--- rslib/src/scheduler/states/load_balancer.rs
+++ rslib/src/scheduler/states/load_balancer.rs
@@ -1,3 +1,6 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 use crate::decks::DeckId;
 use crate::notes::NoteId;
 use crate::storage::SqliteStorage;
