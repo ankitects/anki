09f064af4097eb64ed6cac547e25d43718783a6a
Abdo <abdo@abdnh.net>
2024-06-19 15:25:17 +0300

    Remove unused build var

--- build/ninja_gen/src/node.rs
+++ build/ninja_gen/src/node.rs
@@ -226,18 +226,6 @@ impl BuildAction for SvelteCheck {
         build.add_inputs("tsconfig", &self.tsconfig);
         build.add_inputs("", &self.inputs);
         build.add_inputs("", inputs!["yarn.lock"]);
-        build.add_variable(
-            "compiler_warnings",
-            [
-                "a11y-click-events-have-key-events",
-                "a11y-no-noninteractive-tabindex",
-                "a11y-no-static-element-interactions",
-            ]
-            .iter()
-            .map(|warning| format!("{}$:ignore", warning))
-            .collect::<Vec<_>>()
-            .join(","),
-        );
         let hash = simple_hash(&self.tsconfig);
         build.add_output_stamp(format!("tests/svelte-check.{hash}"));
     }
