0d8d816a0c424b1e033af7d4d1331816bbb61426
Damien Elmes <gpg@ankiweb.net>
2024-06-24 15:24:23 +0700

    Include error message text on page
    Also remove the '500' h1 from the error
    
    Closes #3248

---
+++ ts/routes/+error.svelte
@@ -0,0 +1,11 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    $: message = $page.error!.message;
+</script>
+
+{message}
