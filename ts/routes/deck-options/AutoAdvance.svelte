b076286b8a9e184249f227c942249e620f803710
user1823 <92206575+user1823@users.noreply.github.com>
2024-07-21 15:54:41 +0530

    Update references to tooltip strings (#3312)
    * Update string reference
    
    * Update FsrsOptionsOuter.svelte

--- ts/routes/deck-options/AutoAdvance.svelte
+++ ts/routes/deck-options/AutoAdvance.svelte
@@ -42,7 +42,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         },
         answerAction: {
             title: tr.deckConfigAnswerAction(),
-            help: tr.deckConfigAnswerActionTooltip(),
+            help: tr.deckConfigAnswerActionTooltip2(),
         },
         questionAction: {
             title: tr.deckConfigQuestionAction(),
