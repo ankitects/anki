1e5e7c2dac5ea1a4c2b81668a23ebb9e43c99777
David Culley <6276049+davidculley@users.noreply.github.com>
2024-07-27 16:13:19 +0200

    refactor: use BaseException for fallback
    Co-authored-by: Damien Elmes <dae@users.noreply.github.com>

--- qt/aqt/profiles.py
+++ qt/aqt/profiles.py
@@ -285,7 +285,7 @@ class ProfileManager:
                 showWarning(tr.profiles_anki_could_not_rename_your_profile())
             else:
                 raise
-        except (SystemExit, KeyboardInterrupt, GeneratorExit):
+        except BaseException:
             self.db.rollback()
             raise
         else:
