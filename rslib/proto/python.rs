425bf07f881f5e06e73400878305a73f45059100
Jarrett Ye <jarrett.ye@outlook.com>
2024-07-28 22:05:54 +0800

    __CARGO_FIX_YOLO=1 cargo clippy --fix

--- rslib/proto/python.rs
+++ rslib/proto/python.rs
@@ -97,8 +97,8 @@ fn format_comments(comments: &Option<String>) -> String {
 /// - it has a single field
 /// - its name ends in Request
 /// - it has any optional fields
-/// ...then destructuring will be skipped, and the method will take the input
-/// message directly. Returns (params_line, assignment_lines)
+///   ...then destructuring will be skipped, and the method will take the input
+///   message directly. Returns (params_line, assignment_lines)
 fn maybe_destructured_input(input: &MessageDescriptor) -> (String, String) {
     if (input.name().ends_with("Request") || input.fields().len() < 2)
         && input.oneofs().next().is_none()
