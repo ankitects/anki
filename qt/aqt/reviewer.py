d891c0cacca254f9fe891b9ddc1a936f43f7010b
David Culley <6276049+davidculley@users.noreply.github.com>
2024-08-22 17:15:29 +0200

    refactor: invert logic of if-construct
    to avoid nesting.

--- qt/aqt/reviewer.py
+++ qt/aqt/reviewer.py
@@ -597,12 +597,11 @@ class Reviewer:
         ):
             for ease in aqt.mw.pm.default_answer_keys:
                 key = aqt.mw.pm.get_answer_key(ease)
-                if key:
-                    ease = cast(Literal[1, 2, 3, 4], ease)
-                    answer_card_according_to_pressed_key = partial(
-                        self._answerCard, ease
-                    )
-                    yield (key, answer_card_according_to_pressed_key)
+                if not key:
+                    continue
+                ease = cast(Literal[1, 2, 3, 4], ease)
+                answer_card_according_to_pressed_key = partial(self._answerCard, ease)
+                yield (key, answer_card_according_to_pressed_key)
 
         return [
             ("e", self.mw.onEditCurrent),
