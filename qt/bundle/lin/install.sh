62e0f9fed34af49b9c046b90b7cef776ad5ccf51
Ingemar Berg <73386284+ingemarberg@users.noreply.github.com>
2024-09-01 10:32:38 +0200

    Change Wayland app_id to net.ankiweb.Anki

--- qt/bundle/lin/install.sh
+++ qt/bundle/lin/install.sh
@@ -23,13 +23,13 @@ mkdir -p "$PREFIX"/share/applications
 mkdir -p "$PREFIX"/share/man/man1
 cd "$PREFIX"/share/anki && (\
 mv -Z anki.xpm anki.png "$PREFIX"/share/pixmaps/;\
-mv -Z anki.desktop "$PREFIX"/share/applications/;\
+mv -Z net.ankiweb.Anki.desktop "$PREFIX"/share/applications/;\
 mv -Z anki.1 "$PREFIX"/share/man/man1/)
 
 xdg-mime install anki.xml --novendor
-xdg-mime default anki.desktop application/x-colpkg
-xdg-mime default anki.desktop application/x-apkg
-xdg-mime default anki.desktop application/x-ankiaddon
+xdg-mime default net.ankiweb.Anki.desktop application/x-colpkg
+xdg-mime default net.ankiweb.Anki.desktop application/x-apkg
+xdg-mime default net.ankiweb.Anki.desktop application/x-ankiaddon
 
 rm install.sh
 
--- qt/bundle/lin/uninstall.sh
+++ qt/bundle/lin/uninstall.sh
@@ -12,7 +12,9 @@ rm -rf "$PREFIX"/share/anki
 rm -rf "$PREFIX"/bin/anki
 rm -rf "$PREFIX"/share/pixmaps/anki.xpm
 rm -rf "$PREFIX"/share/pixmaps/anki.png
+# anki.desktop has been renamed net.ankiweb.Anki.desktop for better Wayland compatibility
 rm -rf "$PREFIX"/share/applications/anki.desktop
+rm -rf "$PREFIX"/share/applications/net.ankiweb.Anki.desktop
 rm -rf "$PREFIX"/share/man/man1/anki.1
 
 echo "Uninstall complete."
