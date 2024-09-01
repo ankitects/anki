15b8bacb26e52d6cda57121a68fa5d35a9c5dfc0
Ingemar Berg <73386284+ingemarberg@users.noreply.github.com>
2024-09-01 13:04:02 +0200

    Update about.py

--- qt/aqt/about.py
+++ qt/aqt/about.py
@@ -199,6 +199,7 @@ def show(mw: aqt.AnkiQt) -> QDialog:
             "Ian Samir Yep Manzano",
             "Asuka Minato",
             "Eros Cardoso",
+            "Ingemar Berg",
         )
     )
 
