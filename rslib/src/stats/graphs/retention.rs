71bd331c552e8293b3a840d0be17354554b3b2c9
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-18 15:01:55 +0800

    ./ninja fix:minilints

--- rslib/src/stats/graphs/retention.rs
+++ rslib/src/stats/graphs/retention.rs
@@ -1,3 +1,5 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 use std::collections::HashMap;
 
 use anki_proto::stats::graphs_response::true_retention_stats::TrueRetention;
--- ts/routes/graphs/true-retention.ts
+++ ts/routes/graphs/true-retention.ts
@@ -1,3 +1,5 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import type { GraphsResponse } from "@generated/anki/stats_pb";
 import { localizedNumber } from "@tslib/i18n";
 
