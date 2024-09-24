34236b61efc9cd308febe298deada65449add514
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-24 15:05:13 +0800

    fix eslint

--- ts/routes/card-info/ForgettingCurve.svelte
+++ ts/routes/card-info/ForgettingCurve.svelte
@@ -28,7 +28,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     }
 
     function prepareData() {
-        let data: DataPoint[] = [];
+        const data: DataPoint[] = [];
         let lastReviewTime = 0;
         let lastStability = 0;
 
