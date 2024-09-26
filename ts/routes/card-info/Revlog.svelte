3247df6792149ae8360bd08ff259c54cb739bbed
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-26 14:39:48 +0800

    fix eslint

--- ts/routes/card-info/Revlog.svelte
+++ ts/routes/card-info/Revlog.svelte
@@ -4,10 +4,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 -->
 <script lang="ts">
     import type { CardStatsResponse_StatsRevlogEntry as RevlogEntry } from "@generated/anki/stats_pb";
-    import {
-        RevlogEntry_ReviewKind as ReviewKind,
-        RevlogEntry_ReviewKind,
-    } from "@generated/anki/stats_pb";
+    import { RevlogEntry_ReviewKind as ReviewKind } from "@generated/anki/stats_pb";
     import * as tr2 from "@generated/ftl";
     import { timeSpan, Timestamp } from "@tslib/time";
     import { filterRevlogByReviewKind } from "./forgetting-curve";
