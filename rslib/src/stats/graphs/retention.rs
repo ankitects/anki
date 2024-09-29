a37978097329a55e8c6f8bc0a71e8f4ecff34c82
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-29 17:29:11 +0800

    improve code-style

--- rslib/src/stats/graphs/retention.rs
+++ rslib/src/stats/graphs/retention.rs
@@ -53,43 +53,25 @@ impl GraphsContext {
         self.revlog
             .iter()
             .filter(|review| {
-                review.button_chosen > 0 // not manually rescheduled
+                // not manually rescheduled
+                review.button_chosen > 0
+                    // not cramming
                     && (review.review_kind != RevlogReviewKind::Filtered || review.ease_factor != 0)
-                // not cramming
+                    // cards with an interval ≥ 1 day
+                    && (review.review_kind == RevlogReviewKind::Review
+                        || review.last_interval <= -86400
+                        || review.last_interval >= 1)
             })
             .for_each(|review| {
                 for (period_name, start, end) in &periods {
                     if review.id.as_secs() >= *start && review.id.as_secs() < *end {
                         let period_stat = period_stats.get_mut(period_name).unwrap();
                         const MATURE_IVL: i32 = 21; // mature interval is 21 days
-                        if review.last_interval < MATURE_IVL
-                            && review.button_chosen == 1
-                            && (review.review_kind == RevlogReviewKind::Review
-                                || review.last_interval <= -86400
-                                || review.last_interval >= 1)
-                        {
-                            period_stat.young_failed += 1;
-                        } else if review.last_interval < MATURE_IVL
-                            && review.button_chosen > 1
-                            && (review.review_kind == RevlogReviewKind::Review
-                                || review.last_interval <= -86400
-                                || review.last_interval >= 1)
-                        {
-                            period_stat.young_passed += 1;
-                        } else if review.last_interval >= MATURE_IVL
-                            && review.button_chosen == 1
-                            && (review.review_kind == RevlogReviewKind::Review
-                                || review.last_interval <= -86400
-                                || review.last_interval >= 1)
-                        {
-                            period_stat.mature_failed += 1;
-                        } else if review.last_interval >= MATURE_IVL
-                            && review.button_chosen > 1
-                            && (review.review_kind == RevlogReviewKind::Review
-                                || review.last_interval <= -86400
-                                || review.last_interval >= 1)
-                        {
-                            period_stat.mature_passed += 1;
+                        match (review.last_interval < MATURE_IVL, review.button_chosen) {
+                            (true, 1) => period_stat.young_failed += 1,
+                            (true, _) => period_stat.young_passed += 1,
+                            (false, 1) => period_stat.mature_failed += 1,
+                            (false, _) => period_stat.mature_passed += 1,
                         }
                     }
                 }
