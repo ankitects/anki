9ced6be03ec25afef66452a655c88a16bb5eb607
mmjang <752515918@qq.com>
2024-10-02 17:37:40 +0800

    Decide if element is bold by getComputedStyle (#2453) (#2579)
    * Decide if element is bold by getComputedStyle (#2453)
    
    * Use getComputedStyle() for italics too (dae)

--- ts/editor/editor-toolbar/ItalicButton.svelte
+++ ts/editor/editor-toolbar/ItalicButton.svelte
@@ -17,7 +17,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             return match.remove();
         }
 
-        if (["italic", "oblique"].includes(element.style.fontStyle)) {
+        const fontStyle = getComputedStyle(element).fontStyle;
+        if (["italic", "oblique"].includes(fontStyle)) {
             return match.clear((): void => {
                 if (
                     removeStyleProperties(element, "font-style") &&
