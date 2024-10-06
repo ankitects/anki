0069463a2fb92023e37f16ec48fbf6a8336a94da
voczi <dev@voczi.com>
2024-10-06 13:40:29 +0200

    Add 127.0.0.1 to allowed origins

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ set PYTHONWARNINGS=default
 set PYTHONPYCACHEPREFIX=out\pycache
 set ANKIDEV=1
 set QTWEBENGINE_REMOTE_DEBUGGING=8080
-set QTWEBENGINE_CHROMIUM_FLAGS=--remote-allow-origins=http://localhost:8080
+set QTWEBENGINE_CHROMIUM_FLAGS=--remote-allow-origins=http://localhost:8080,http://127.0.0.1:8080
 set ANKI_API_PORT=40000
 set ANKI_API_HOST=127.0.0.1
 
