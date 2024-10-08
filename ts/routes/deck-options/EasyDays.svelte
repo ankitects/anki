658d9853c9e25b1fc5996d457d3ebe3e2245eb39
Jarrett Ye <jarrett.ye@outlook.com>
2024-10-08 14:16:20 +0800

    configure Easy Days via table

--- ts/routes/deck-options/EasyDays.svelte
+++ ts/routes/deck-options/EasyDays.svelte
@@ -11,17 +11,14 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import DynamicallySlottable from "$lib/components/DynamicallySlottable.svelte";
     import HelpModal from "$lib/components/HelpModal.svelte";
     import Item from "$lib/components/Item.svelte";
-    import SettingTitle from "$lib/components/SettingTitle.svelte";
     import TitledContainer from "$lib/components/TitledContainer.svelte";
     import { type HelpItem } from "$lib/components/types";
     import type { DeckOptionsState } from "./lib";
-    import WeightsInputRow from "./WeightsInputRow.svelte";
 
     export let state: DeckOptionsState;
     export let api: Record<string, never>;
 
     const config = state.currentConfig;
-    const defaults = state.defaults;
     const fsrs = state.fsrs;
 
     const settings = {
@@ -42,7 +39,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     }
 </script>
 
-<TitledContainer title={tr.deckConfigAdvancedTitle()}>
+<TitledContainer title={"Easy Days"}>
     <HelpModal
         title={tr.deckConfigAdvancedTitle()}
         url={HelpPage.DeckOptions.advanced}
@@ -57,21 +54,75 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     <DynamicallySlottable slotHost={Item} {api}>
         {#if $fsrs}
             <Item>
-                <WeightsInputRow
-                    bind:value={$config.easyDaysPercentages}
-                    defaultValue={[]}
-                    defaults={defaults.easyDaysPercentages}
-                >
-                    <SettingTitle
-                        on:click={() =>
-                            openHelpModal(
-                                Object.keys(settings).indexOf("easyDaysPercentages"),
-                            )}
-                    >
-                        {settings.easyDays.title}
-                    </SettingTitle>
-                </WeightsInputRow>
+                <div class="easy-days-settings">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Normal</th>
+                                <th>Reduced</th>
+                                <th>Minimum</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {#each ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] as day, index}
+                                <tr>
+                                    <td>{day}</td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            bind:group={$config.easyDaysPercentages[
+                                                index
+                                            ]}
+                                            value={1.0}
+                                            checked={$config.easyDaysPercentages[
+                                                index
+                                            ] === 1.0}
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            bind:group={$config.easyDaysPercentages[
+                                                index
+                                            ]}
+                                            value={0.5}
+                                            checked={$config.easyDaysPercentages[
+                                                index
+                                            ] === 0.5}
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            bind:group={$config.easyDaysPercentages[
+                                                index
+                                            ]}
+                                            value={0.0}
+                                            checked={$config.easyDaysPercentages[
+                                                index
+                                            ] === 0.0}
+                                        />
+                                    </td>
+                                </tr>
+                            {/each}
+                        </tbody>
+                    </table>
+                </div>
             </Item>
         {/if}
     </DynamicallySlottable>
 </TitledContainer>
+
+<style>
+    .easy-days-settings table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    .easy-days-settings th,
+    .easy-days-settings td {
+        padding: 8px;
+        text-align: center;
+        border: 1px solid #ddd;
+    }
+</style>
