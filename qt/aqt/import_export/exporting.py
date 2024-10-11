c559a69b814467370fce58f8f7a095f440275135
bpnguyen107 <bpnguyen107@g.ucla.edu>
2024-10-11 16:17:54 -0700

    Better variable name

--- qt/aqt/import_export/exporting.py
+++ qt/aqt/import_export/exporting.py
@@ -88,9 +88,9 @@ class ExportDialog(QDialog):
         self.frm.includeHTML.setChecked(True)
         # set default option if accessed through deck button
         if did:
-            deck_dict = self.mw.col.decks.get(did)
-            assert deck_dict
-            name = deck_dict["name"]
+            deck = self.mw.col.decks.get(did)
+            assert deck
+            name = deck["name"]
             index = self.frm.deck.findText(name)
             self.frm.deck.setCurrentIndex(index)
             self.frm.includeSched.setChecked(False)
