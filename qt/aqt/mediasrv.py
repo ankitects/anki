76eb24276c0f5acd30eb493c6db6dcc8a6eeb599
Jarrett Ye <jarrett.ye@outlook.com>
2024-10-12 12:49:14 +0800

    add get_revlogs API && fix the style of tooltipText of ReviewsGraph (#3490)
    * add get_revlogs API
    
    * fix tooltipText of ReviewsGraph
    
    the style of true-retention shouldn't affect the style of tooltipText of ReviewsGraph
    
    * More verbose wording (dae)

--- qt/aqt/mediasrv.py
+++ qt/aqt/mediasrv.py
@@ -608,6 +608,7 @@ exposed_backend_list = [
     "get_change_notetype_info",
     # StatsService
     "card_stats",
+    "get_review_logs",
     "graphs",
     "get_graph_preferences",
     "set_graph_preferences",
