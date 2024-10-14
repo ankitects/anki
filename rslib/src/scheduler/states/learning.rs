c4c4c29dfc22dedd5e258704084862bd345847d4
Jarrett Ye <jarrett.ye@outlook.com>
2024-10-14 17:11:59 +0800

    Fix/only let FSRS take over short-term schedule when steps are empty

--- rslib/src/scheduler/states/learning.rs
+++ rslib/src/scheduler/states/learning.rs
@@ -49,7 +49,10 @@ impl LearnState {
         } else {
             let (minimum, maximum) = ctx.min_and_max_review_intervals(1);
             let (interval, short_term) = if let Some(states) = &ctx.fsrs_next_states {
-                (states.again.interval, states.again.interval < 0.5)
+                (
+                    states.again.interval,
+                    ctx.steps.is_empty() && states.again.interval < 0.5,
+                )
             } else {
                 (ctx.graduating_interval_good as f32, false)
             };
@@ -91,7 +94,10 @@ impl LearnState {
         } else {
             let (minimum, maximum) = ctx.min_and_max_review_intervals(1);
             let (interval, short_term) = if let Some(states) = &ctx.fsrs_next_states {
-                (states.hard.interval, states.hard.interval < 0.5)
+                (
+                    states.hard.interval,
+                    ctx.steps.is_empty() && states.hard.interval < 0.5,
+                )
             } else {
                 (ctx.graduating_interval_good as f32, false)
             };
@@ -133,7 +139,10 @@ impl LearnState {
         } else {
             let (minimum, maximum) = ctx.min_and_max_review_intervals(1);
             let (interval, short_term) = if let Some(states) = &ctx.fsrs_next_states {
-                (states.good.interval, states.good.interval < 0.5)
+                (
+                    states.good.interval,
+                    ctx.steps.is_empty() && states.good.interval < 0.5,
+                )
             } else {
                 (ctx.graduating_interval_good as f32, false)
             };
