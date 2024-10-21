30e734b9253a1a1e073be9cf65ca082f35bbf181
Damien Elmes <gpg@ankiweb.net>
2024-10-21 13:38:29 +1000

    Bump Rust deps
    Primarily for prost, which had incompatible changes

--- rslib/src/stats/graphs/retention.rs
+++ rslib/src/stats/graphs/retention.rs
@@ -77,12 +77,12 @@ impl GraphsContext {
                 }
             });
 
-        stats.today = Some(period_stats["today"].clone());
-        stats.yesterday = Some(period_stats["yesterday"].clone());
-        stats.week = Some(period_stats["week"].clone());
-        stats.month = Some(period_stats["month"].clone());
-        stats.year = Some(period_stats["year"].clone());
-        stats.all_time = Some(period_stats["all_time"].clone());
+        stats.today = Some(period_stats["today"]);
+        stats.yesterday = Some(period_stats["yesterday"]);
+        stats.week = Some(period_stats["week"]);
+        stats.month = Some(period_stats["month"]);
+        stats.year = Some(period_stats["year"]);
+        stats.all_time = Some(period_stats["all_time"]);
 
         stats
     }
