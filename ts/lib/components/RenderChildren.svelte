b210c62d75b05cf97d06d9be7a657cfe8d029bda
Abdo <abdo@abdnh.net>
2024-11-12 12:48:57 +0300

    Fix editor width changing as tag completions shown

---
+++ ts/lib/components/RenderChildren.svelte
@@ -0,0 +1,12 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+{@render children?.()}
