d9690bd72ecb6a626d5a6b2c9429782276947c22
iamllama <100429699+iamllama@users.noreply.github.com>
2024-12-12 17:28:20 +0800

    Add global _updateCardId fn to card-info

--- ts/routes/card-info/[cardId]/+page.ts
+++ ts/routes/card-info/[cardId]/+page.ts
@@ -1,6 +1,8 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import { cardStats } from "@generated/backend";
+import { goto } from "$app/navigation";
+import { browser } from "$app/environment";
 
 import type { PageLoad } from "./$types";
 
@@ -17,3 +19,12 @@ export const load = (async ({ params }) => {
     const info = cid !== null ? await cardStats({ cid }) : null;
     return { info };
 }) satisfies PageLoad;
+
+function _updateCardId(cardId: BigInt) {
+    goto(`/card-info/${cardId}`);
+}
+
+if(browser) {
+    // called by CardInfoDialog.update_card in card_info.py
+    window["_updateCardId"] = _updateCardId;
+}
