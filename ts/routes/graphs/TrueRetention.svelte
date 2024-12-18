5637390b501e92a46160557e581f8e1ccd79b1ce
Ross Brown <rbrownwsws@googlemail.com>
2024-12-18 13:41:57 +0000

    Make the "True Retention" table pretty (#3640)
    * Make the True Retention table pretty
    
    * Hide absolute pass/fail table for 'all'
    
    * Run './ninja format'
    
    * Manually run prettier on Svelte 5 components
    
    * Refactor to not use {#snippet}
    
    * Fix lint to pass check:eslint
    
    * Fix lint to pass check:svelte
    
    * Rename t9n -> tr to follow code style
    
    * Replace hard-coded string with a translation string
    
    * Use assertUnreachable(...) for exhaustively matching enum

--- ts/routes/graphs/TrueRetention.svelte
+++ ts/routes/graphs/TrueRetention.svelte
@@ -6,34 +6,87 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     import type { GraphsResponse } from "@generated/anki/stats_pb";
     import * as tr from "@generated/ftl";
 
-    import { renderTrueRetention } from "./true-retention";
+    import { type RevlogRange } from "./graph-helpers";
+    import { DisplayMode, type PeriodTrueRetentionData, Scope } from "./true-retention";
+
     import Graph from "./Graph.svelte";
-    import type { RevlogRange } from "./graph-helpers";
+    import InputBox from "./InputBox.svelte";
+    import TrueRetentionCombined from "./TrueRetentionCombined.svelte";
+    import TrueRetentionSingle from "./TrueRetentionSingle.svelte";
+    import { assertUnreachable } from "@tslib/typing";
 
-    export let revlogRange: RevlogRange;
-    export let sourceData: GraphsResponse | null = null;
-
-    let trueRetentionHtml: string;
-
-    $: if (sourceData) {
-        trueRetentionHtml = renderTrueRetention(sourceData, revlogRange);
+    interface Props {
+        revlogRange: RevlogRange;
+        sourceData: GraphsResponse | null;
     }
+
+    const { revlogRange, sourceData = null }: Props = $props();
+
+    const retentionData: PeriodTrueRetentionData | null = $derived.by(() => {
+        if (sourceData === null) {
+            return null;
+        } else {
+            // Assert that all the True Retention data will be defined
+            return sourceData.trueRetention as PeriodTrueRetentionData;
+        }
+    });
+
+    let mode: DisplayMode = $state(DisplayMode.Summary);
 
     const title = tr.statisticsTrueRetentionTitle();
     const subtitle = tr.statisticsTrueRetentionSubtitle();
 </script>
 
 <Graph {title} {subtitle}>
-    {#if trueRetentionHtml}
-        <div class="true-retention-table">
-            {@html trueRetentionHtml}
-        </div>
-    {/if}
+    <InputBox>
+        <label>
+            <input type="radio" bind:group={mode} value={DisplayMode.Young} />
+            {tr.statisticsCountsYoungCards()}
+        </label>
+
+        <label>
+            <input type="radio" bind:group={mode} value={DisplayMode.Mature} />
+            {tr.statisticsCountsMatureCards()}
+        </label>
+
+        <label>
+            <input type="radio" bind:group={mode} value={DisplayMode.Summary} />
+            {tr.statisticsTrueRetentionAll()}
+        </label>
+    </InputBox>
+
+    <div class="table-container">
+        {#if retentionData === null}
+            <div>{tr.statisticsNoData()}</div>
+        {:else if mode === DisplayMode.Young}
+            <TrueRetentionSingle
+                {revlogRange}
+                data={retentionData}
+                scope={Scope.Young}
+            />
+        {:else if mode === DisplayMode.Mature}
+            <TrueRetentionSingle
+                {revlogRange}
+                data={retentionData}
+                scope={Scope.Mature}
+            />
+        {:else if mode === DisplayMode.All}
+            <TrueRetentionSingle {revlogRange} data={retentionData} scope={Scope.All} />
+        {:else if mode === DisplayMode.Summary}
+            <TrueRetentionCombined {revlogRange} data={retentionData} />
+        {:else}
+            {assertUnreachable(mode)}
+        {/if}
+    </div>
 </Graph>
 
 <style>
-    .true-retention-table {
-        overflow-x: auto;
+    .table-container {
         margin-top: 1rem;
+        overflow-x: auto;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 </style>
---
+++ ts/routes/graphs/TrueRetentionCombined.svelte
@@ -0,0 +1,101 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="ts">
+    import * as tr from "@generated/ftl";
+    import { localizedNumber } from "@tslib/i18n";
+
+    import { type RevlogRange } from "./graph-helpers";
+    import {
+        calculateRetentionPercentageString,
+        getRowData,
+        type PeriodTrueRetentionData,
+        type RowData,
+    } from "./true-retention";
+
+    interface Props {
+        revlogRange: RevlogRange;
+        data: PeriodTrueRetentionData;
+    }
+
+    const { revlogRange, data }: Props = $props();
+
+    const rowData: RowData[] = $derived(getRowData(data, revlogRange));
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <td></td>
+            <th scope="col" class="col-header young">
+                {tr.statisticsCountsYoungCards()}
+            </th>
+            <th scope="col" class="col-header mature">
+                {tr.statisticsCountsMatureCards()}
+            </th>
+            <th scope="col" class="col-header total">
+                {tr.statisticsCountsTotalCards()}
+            </th>
+            <th scope="col" class="col-header count">
+                {tr.statisticsTrueRetentionCount()}
+            </th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {#each rowData as row}
+            {@const totalPassed = row.data.youngPassed + row.data.maturePassed}
+            {@const totalFailed = row.data.youngFailed + row.data.matureFailed}
+
+            <tr>
+                <th scope="row" class="row-header">{row.title}</th>
+
+                <td class="young">
+                    {calculateRetentionPercentageString(
+                        row.data.youngPassed,
+                        row.data.youngFailed,
+                    )}
+                </td>
+                <td class="mature">
+                    {calculateRetentionPercentageString(
+                        row.data.maturePassed,
+                        row.data.matureFailed,
+                    )}
+                </td>
+                <td class="total">
+                    {calculateRetentionPercentageString(totalPassed, totalFailed)}
+                </td>
+
+                <td class="count">{localizedNumber(totalPassed + totalFailed)}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style lang="scss">
+    @use "true-retention-base";
+
+    .young,
+    .mature,
+    .total,
+    .count {
+        text-align: right;
+    }
+
+    .young {
+        color: #64c476;
+    }
+
+    .mature {
+        color: #31a354;
+    }
+
+    .total {
+        color: var(--fg);
+    }
+
+    .count {
+        color: var(--fg-subtle);
+    }
+</style>
