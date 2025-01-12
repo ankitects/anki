a2341bba30a6bdc411b0100b2f94df79549ce6f6
llama <100429699+iamllama@users.noreply.github.com>
2025-01-13 07:41:35 +0800

    fix lint

--- qt/aqt/stylesheets.py
+++ qt/aqt/stylesheets.py
@@ -1,7 +1,7 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from anki.utils import is_win, is_mac
+from anki.utils import is_mac, is_win
 from aqt import colors, props
 from aqt.theme import ThemeManager
 
