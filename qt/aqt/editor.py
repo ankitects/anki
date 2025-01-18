c09e5f8c4f2d07375effa75de55a0d7ed4d6666f
llama <100429699+iamllama@users.noreply.github.com>
2025-01-18 12:46:14 +0800

    remove orig_note_id param

--- qt/aqt/editor.py
+++ qt/aqt/editor.py
@@ -528,22 +528,19 @@ require("anki/ui").loaded.then(() => require("anki/NoteEditor").instances[0].too
         note: Note | None,
         hide: bool = True,
         focusTo: int | None = None,
-        orig_note_id: NoteId | None = None,
     ) -> None:
         "Make NOTE the current note."
         self.note = note
         self.currentField = None
         if self.note:
-            self.loadNote(focusTo=focusTo, orig_note_id=orig_note_id)
+            self.loadNote(focusTo=focusTo)
         elif hide:
             self.widget.hide()
 
     def loadNoteKeepingFocus(self) -> None:
         self.loadNote(self.currentField)
 
-    def loadNote(
-        self, focusTo: int | None = None, orig_note_id: NoteId | None = None
-    ) -> None:
+    def loadNote(self, focusTo: int | None = None) -> None:
         if not self.note:
             return
 
