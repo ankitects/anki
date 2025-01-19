e1f7e2a8fffd7ed45275095492b1b7f5c6d3e8b1
user1823 <92206575+user1823@users.noreply.github.com>
2025-01-19 13:57:56 +0530

    Match calc of relative overdueness in SM2 and FSRS

--- rslib/src/storage/card/mod.rs
+++ rslib/src/storage/card/mod.rs
@@ -774,8 +774,9 @@ impl fmt::Display for ReviewOrderSubclause {
             ReviewOrderSubclause::DifficultyAscending => "extract_fsrs_variable(data, 'd') asc",
             ReviewOrderSubclause::DifficultyDescending => "extract_fsrs_variable(data, 'd') desc",
             ReviewOrderSubclause::RetrievabilitySm2 { today, order } => {
+                // - (elapsed days+0.001)/(scheduled interval)
                 temp_string = format!(
-                    "ivl / cast({today}-due+0.001 as real) {order}",
+                    "(-1 - cast({today}-due+0.001 as real)/ivl) {order}",
                     today = today
                 );
                 &temp_string
