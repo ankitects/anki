ca5b9c69157d2148814534e2af9d81db105806f5
llama <100429699+iamllama@users.noreply.github.com>
2025-01-29 02:29:24 +0800

    use QApplication.setActiveWindow instead

--- qt/aqt/utils.py
+++ qt/aqt/utils.py
@@ -432,8 +432,10 @@ def askUser(
     # https://doc.qt.io/qt-6/qapplication.html#activeWindow
     # on some systems, qt unsets the active window when this messagebox causes the previous window to lose focus
     # to handle that case, this hack sets the previously active window (parent) as the current active window anew
+    # QApplication.setActiveWindow was deprecated in 6.5, but its recommended alternative (QWidget.activateWindow)
+    # doesn't work, for some reason
     if parent and not parent.isActiveWindow():
-        parent.activateWindow()
+        aqt.mw.app.setActiveWindow(parent)
     return r == QMessageBox.StandardButton.Yes
 
 
