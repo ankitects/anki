a97c8743eddb8507ebd5582c3826dbe305b94af4
Jarrett Ye <jarrett.ye@outlook.com>
2025-02-13 13:42:05 +0800

    ./ninja fix:minilints

--- rslib/src/scheduler/fsrs/rescheduler.rs
+++ rslib/src/scheduler/fsrs/rescheduler.rs
@@ -1,3 +1,5 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 use std::collections::HashMap;
 
 use chrono::Datelike;
