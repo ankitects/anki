8ec139f62a5b236cfb4993edbcd537466d182985
llama <100429699+iamllama@users.noreply.github.com>
2025-02-21 17:39:11 +0800

    Debounce mathjax rendering to avoid stalling (#3827)
    * move change-timer to editable
    
    * debounce mathjax rendering

--- ts/editor/NoteEditor.svelte
+++ ts/editor/NoteEditor.svelte
@@ -57,7 +57,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         type ImageLoadedEvent,
         resetIOImage,
     } from "../routes/image-occlusion/mask-editor";
-    import { ChangeTimer } from "./change-timer";
+    import { ChangeTimer } from "../editable/change-timer";
     import { clearableArray } from "./destroyable";
     import DuplicateLink from "./DuplicateLink.svelte";
     import EditorToolbar from "./editor-toolbar";
