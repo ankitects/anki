5cb420c2ad9379f198462c1d69deff9eddeb5983
RisingOrange <jakub.fidler@protonmail.com>
2025-03-03 10:14:30 +0100

    Undo taskman changes

--- qt/aqt/taskman.py
+++ qt/aqt/taskman.py
@@ -68,14 +68,7 @@ class TaskManager(QObject):
         # background task starts, and it takes out a long-running lock on the database,
         # the UI thread will hang until the end of the op.
         if current_thread() is main_thread():
-            try:
-                self._on_closures_pending()
-            except Exception as e:
-                # We need to raise the exception from the closure, but we don't want it to disrupt the current call stack.
-                def raise_exception() -> None:
-                    raise e
-
-                self.run_on_main(raise_exception)
+            self._on_closures_pending()
         else:
             print("bug: run_in_background not called from main thread")
             traceback.print_stack()
