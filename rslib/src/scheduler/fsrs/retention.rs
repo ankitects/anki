79b6f658c38db053b110eb989eec1c6065272139
Luc Mcgrady <lucmcgrady@gmail.com>
2025-03-15 10:28:15 +0000

    Feat: Simulator suspend after lapse count (#3837)
    * Added: Leech suspend to simulator
    
    * Added: leech threshold spin box
    
    * Update git rev
    
    * Added: Save to preset options
    
    * ./check
    
    * Added: "Advanced settings" dropdown
    
    * Removed: Indent
    
    * Added: Easy days
    
    * Added: Sticky header
    
    * Removed: Easy Day updating without saving
    
    * un-nest disclosure
    
    * bump fsrs
    
    * Update a VSCode setting to match recent releases
    
    * Move Easy Days above the Advanced settings
    
    I think it's a bit more logical to have Advanced come last.
    
    * Ensure graph fits inside screen height
    
    * Bump fsrs version

--- rslib/src/scheduler/fsrs/retention.rs
+++ rslib/src/scheduler/fsrs/retention.rs
@@ -44,9 +44,9 @@ impl Collection {
         let post_scheduling_fn: Option<PostSchedulingFn> =
             if self.get_config_bool(BoolKey::LoadBalancerEnabled) {
                 Some(PostSchedulingFn(Arc::new(
-                    move |interval, max_interval, today, due_cnt_per_day, rng| {
+                    move |card, max_interval, today, due_cnt_per_day, rng| {
                         apply_load_balance_and_easy_days(
-                            interval,
+                            card.interval,
                             max_interval,
                             today,
                             due_cnt_per_day,
@@ -78,6 +78,7 @@ impl Collection {
                     learn_limit,
                     review_limit: usize::MAX,
                     new_cards_ignore_review_limit: true,
+                    suspend_after_lapses: None,
                     post_scheduling_fn,
                     review_priority_fn: None,
                 },
