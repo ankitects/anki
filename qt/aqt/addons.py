cc83ec4b2b80c9ee4f6092dfea05840985c70839
beyondcompute <beyondcompute@users.noreply.github.com>
2025-03-17 00:59:01 +0200

    Enable View Page/Config buttons only when 1 add-on selected

--- qt/aqt/addons.py
+++ qt/aqt/addons.py
@@ -820,12 +820,16 @@ class AddonsDialog(QDialog):
         qconnect(f.config.clicked, self.onConfig)
         qconnect(self.form.addonList.itemDoubleClicked, self.onConfig)
         qconnect(self.form.addonList.currentRowChanged, self._onAddonItemSelected)
+        qconnect(
+            self.form.addonList.itemSelectionChanged, self._onAddonSelectionChanged
+        )
         self.setWindowTitle(tr.addons_window_title())
         disable_help_button(self)
         self.setAcceptDrops(True)
         self.redrawAddons()
         restoreGeom(self, "addons")
         gui_hooks.addons_dialog_will_show(self)
+        self._onAddonSelectionChanged()
         self.show()
 
     def dragEnterEvent(self, event: QDragEnterEvent) -> None:
@@ -901,18 +905,33 @@ class AddonsDialog(QDialog):
 
         addonList.reset()
 
+    def _onAddonSelectionChanged(self) -> None:
+        self.form.viewFiles.setEnabled(False)
+        self.form.viewPage.setEnabled(False)
+        self.form.config.setEnabled(False)
+
+        selected_count = len(self.selectedAddons())
+        if selected_count == 0:
+            # View Files button shows top-level add-ons directory when nothing is selected
+            self.form.viewFiles.setEnabled(True)
+        elif selected_count == 1:
+            addon = self.addons[self.form.addonList.currentRow()]
+
+            self.form.viewFiles.setEnabled(True)
+            self.form.viewPage.setEnabled(addon.page() is not None)
+            self.form.config.setEnabled(
+                bool(
+                    self.mgr.getConfig(addon.dir_name)
+                    or self.mgr.configAction(addon.dir_name)
+                )
+            )
+        return
+
     def _onAddonItemSelected(self, row_int: int) -> None:
         try:
             addon = self.addons[row_int]
         except IndexError:
             return
-        self.form.viewPage.setEnabled(addon.page() is not None)
-        self.form.config.setEnabled(
-            bool(
-                self.mgr.getConfig(addon.dir_name)
-                or self.mgr.configAction(addon.dir_name)
-            )
-        )
         gui_hooks.addons_dialog_did_change_selected_addon(self, addon)
         return
 
