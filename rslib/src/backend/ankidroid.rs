76978135a4bdcb7b6c8499b8f89d92e064e2a93c
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-04-06 01:14:07 +0530

    correct timestamp handling in AnkiDroid backend functions

--- rslib/src/backend/ankidroid.rs
+++ rslib/src/backend/ankidroid.rs
@@ -1,8 +1,6 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
 use anki_proto::generic;
-
 use super::Backend;
 use crate::ankidroid::db;
 use crate::ankidroid::error::debug_produce_error;
@@ -16,21 +14,31 @@ impl BackendAnkidroidService for Backend {
         &self,
         input: anki_proto::ankidroid::SchedTimingTodayLegacyRequest,
     ) -> Result<anki_proto::scheduler::SchedTimingTodayResponse> {
+        // Convert timestamps properly
+        let created_secs = TimestampSecs(input.created_secs);
+        let now_secs = TimestampSecs(input.now_secs);
+        
+        // Handle timezone offsets
+        let created_offset = input.created_mins_west.map(fixed_offset_from_minutes);
+        let now_offset = fixed_offset_from_minutes(input.now_mins_west);
+        
         let result = timing::sched_timing_today(
-            TimestampSecs::from(input.created_secs),
-            TimestampSecs::from(input.now_secs),
-            input.created_mins_west.map(fixed_offset_from_minutes),
-            fixed_offset_from_minutes(input.now_mins_west),
+            created_secs,
+            now_secs,
+            created_offset,
+            now_offset,
             Some(input.rollover_hour as u8),
         )?;
+        
         Ok(anki_proto::scheduler::SchedTimingTodayResponse::from(
             result,
         ))
     }
 
     fn local_minutes_west_legacy(&self, input: generic::Int64) -> Result<generic::Int32> {
+        let timestamp = TimestampSecs(input.val);
         Ok(generic::Int32 {
-            val: timing::local_minutes_west_for_stamp(input.val.into())?,
+            val: timing::local_minutes_west_for_stamp(timestamp)?,
         })
     }
 
