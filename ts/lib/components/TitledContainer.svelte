fa060e023e2578eaf4e79393a2497962e72d62e7
GithubAnon0000 <160563432+GithubAnon0000@users.noreply.github.com>
2025-04-20 21:38:34 +0000

    Update ts/lib/components/TitledContainer.svelte
    Co-authored-by: Luc Mcgrady <lucmcgrady@gmail.com>

--- ts/lib/components/TitledContainer.svelte
+++ ts/lib/components/TitledContainer.svelte
@@ -24,9 +24,22 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     style:--container-margin="0"
 >
     <div class="position-relative">
-        <h1 on:click>
-            {title}
-        </h1>
+        {#if onTitleClick}
+            <span
+                on:click={onTitleClick}
+                on:keydown={onTitleClick}
+                role="button"
+                tabindex="0"
+            >
+                <h1>
+                    {title}
+                </h1>
+            </span>
+        {:else}
+            <h1>
+                {title}
+            </h1>
+        {/if}
         <div class="help-badge position-absolute" class:rtl>
             <slot name="tooltip" />
         </div>
