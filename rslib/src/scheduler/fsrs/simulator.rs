c796930251e660722b351a606edca701434fc2c4
Luc Mcgrady <lucmcgrady@gmail.com>
2025-04-23 11:15:02 +0100

    Save states to cards

--- rslib/src/scheduler/fsrs/simulator.rs
+++ rslib/src/scheduler/fsrs/simulator.rs
@@ -130,8 +130,10 @@ impl Collection {
         } 
         for c in &mut cards {
             if is_included_card(c) && c.memory_state.is_none() {
+                let original = c.clone();
                 let new_state = self.compute_memory_state(c.id)?.state;
                 c.memory_state = new_state.map(Into::into);
+                self.update_card_inner(c, original, self.usn()?)?;
             }
         }
         let days_elapsed = self.timing_today().unwrap().days_elapsed as i32;
