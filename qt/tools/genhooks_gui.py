9a1feaf7a4481802049363dae1d98412b72b3edf
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2025-04-25 10:40:24 +0200

    Add a way to pass information from browser_will_search to browser_did_search without having it going to the backend (#3945)
    * Add a way to pass information from browser_will_search to browser_did_search without having it going to the backend
    
    * Allow None for SearchContext.properties
    
    * Adding myself in CONTRIBUTORS
    
    * Rename SearchContext.properties to SearchContext.addon_metadata
    
    * Revert "Adding myself in CONTRIBUTORS"
    
    This reverts commit a9935772790f78378fff17e5c0f90bb94b5e783f.
    
    * Reapply "Adding myself in CONTRIBUTORS"
    
    This reverts commit f3ce51c83ddd953db88f6f86f7db7da791bd4a29.

--- qt/tools/genhooks_gui.py
+++ qt/tools/genhooks_gui.py
@@ -549,6 +549,10 @@ hooks = [
          You can modify context.search to change the text that is sent to the
          searching backend.
 
+         If you need to pass metadata to the browser_did_search hook, you can
+         do it with context.addon_metadata. For example, to trigger filtering
+         based on a new custom filter.
+
          If you set context.ids to a list of ids, the regular search will
          not be performed, and the provided ids will be used instead.
 
@@ -577,6 +581,9 @@ hooks = [
         You can mutate the row object to change what is displayed. Any columns the
         backend did not recognize will be returned as an empty string, and can be
         replaced with custom content.
+
+        You can retrieve metadata passed from browser_will_search with
+        context.addon_metadata (for example to trigger post-processing filtering).
 
         Columns is a list of string values identifying what each column in the row
         represents.
