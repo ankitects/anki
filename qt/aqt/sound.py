6213c9b6f99ebda181d004f8915b92fe3618b939
Damien Elmes <gpg@ankiweb.net>
2025-05-10 18:38:58 +1000

    Disable YouTube DL in mpv
    mpv looks for ytdl on the path, which includes the CWD on Windows.
    A malicious shared deck could place an executable called yt-dlp.exe in the
    media folder, which mpv would then helpfully invoke the first time
    a YouTube link was encountered.
    
    A big thank you to Michael Lappas for the report.
    
    (cherry picked from commit 5080451829505842b16d4a50f398ad44560a3e48)

--- qt/aqt/sound.py
+++ qt/aqt/sound.py
@@ -388,6 +388,7 @@ class SimpleMpvPlayer(SimpleProcessPlayer, VideoPlayer):
             "--keep-open=no",
             "--input-media-keys=no",
             "--autoload-files=no",
+            "--no-ytdl",
         ]
     )
 
