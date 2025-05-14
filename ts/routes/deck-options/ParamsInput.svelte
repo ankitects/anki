537aeed82fc40a937f8306be5e11165752fb0704
llama <gh@siid.sh>
2025-05-14 22:05:24 +0800

    resize on window resize

--- ts/routes/deck-options/ParamsInput.svelte
+++ ts/routes/deck-options/ParamsInput.svelte
@@ -36,6 +36,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     }
 </script>
 
+<svelte:window onresize={updateHeight} />
+
 <textarea
     bind:this={taRef}
     value={stringValue}
