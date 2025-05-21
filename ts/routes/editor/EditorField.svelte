f453e3790c90b7b531cd9207fa0ec76d369d248e
Abdo <abdo@abdnh.net>
2025-05-22 02:56:30 +0300

    Fix scss paths

--- ts/routes/editor/EditorField.svelte
+++ ts/routes/editor/EditorField.svelte
@@ -130,7 +130,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 </div>
 
 <style lang="scss">
-    @use "../lib/sass/elevation" as *;
+    @use "../../lib/sass/elevation" as *;
 
     /* Make sure labels are readable on custom Qt backgrounds */
     .field-container {
