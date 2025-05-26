bb59156b17b85ce19f738c5cde045343a390f0a0
Luc Mcgrady <lucmcgrady@gmail.com>
2025-05-26 16:07:58 +0100

    Fix: Doesn't update on arrow keys change

--- ts/lib/components/SpinBox.svelte
+++ ts/lib/components/SpinBox.svelte
@@ -129,6 +129,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         value={stringValue}
         bind:this={input}
         on:blur={update}
+        on:change={update}
         on:input={onInput}
         on:focusin={() => (focused = true)}
         on:focusout={() => (focused = false)}
