1ca31413f7d3a1897edf50955954702ce6612ca5
GithubAnon0000 <160563432+GithubAnon0000@users.noreply.github.com>
2025-07-07 08:23:39 +0000

    FIX revert button is visible for screenreaders (#4174)

--- ts/lib/components/RevertButton.svelte
+++ ts/lib/components/RevertButton.svelte
@@ -76,7 +76,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     }
 
     .hide :global(.badge) {
-        opacity: 0;
+        display: none;
         cursor: initial;
     }
 </style>
