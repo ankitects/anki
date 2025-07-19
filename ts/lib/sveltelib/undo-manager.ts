84de8aec3e46be42eee25739699b32c594933297
fcolona <felipedev202@gmail.com>
2025-07-19 14:20:04 -0300

    add copyright header

--- ts/lib/sveltelib/undo-manager.ts
+++ ts/lib/sveltelib/undo-manager.ts
@@ -1,3 +1,6 @@
+// Copyright: Ankitects Pty Ltd and contributors
+// License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
 import { getRange, getSelection } from "@tslib/cross-browser";
 import { getMaxOffset } from "./input-handler";
 
