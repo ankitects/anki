f57c41bf3c09f351b82ee9867acd6ed63df54f8c
Abdo <abdo@abdnh.net>
2025-08-27 02:36:39 +0300

    Clear lastAddedNote on notetype change

--- ts/routes/editor/NoteEditor.svelte
+++ ts/routes/editor/NoteEditor.svelte
@@ -314,6 +314,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
             const deckId = await defaultDeckForNotetype({ ntid: notetype.id });
             deckChooser.select(deckId.did);
         }
+        lastAddedNote = null;
     }
 
     let notetypeMeta: NotetypeIdAndModTime;
