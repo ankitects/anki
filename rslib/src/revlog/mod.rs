193d6bdf11800479bbe3efdb34b31025928c2dbe
user1823 <92206575+user1823@users.noreply.github.com>
2025-09-26 20:58:12 +0530

    Implement last_interval_secs function

--- rslib/src/revlog/mod.rs
+++ rslib/src/revlog/mod.rs
@@ -85,6 +85,15 @@ impl RevlogEntry {
         .unwrap()
     }
 
+    pub(crate) fn last_interval_secs(&self) -> u32 {
+        u32::try_from(if self.last_interval > 0 {
+            self.last_interval.saturating_mul(86_400)
+        } else {
+            self.last_interval.saturating_mul(-1)
+        })
+        .unwrap()
+    }
+
     /// Returns true if this entry represents a reset operation.
     /// These entries are created when a card is reset using
     /// [`Collection::reschedule_cards_as_new`].
