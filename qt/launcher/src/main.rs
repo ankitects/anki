4fcafe69839c63a102e163c167068a525d63da3e
user1823 <92206575+user1823@users.noreply.github.com>
2025-10-02 19:06:23 +0530

    Add prompt to press enter for installation

--- qt/launcher/src/main.rs
+++ qt/launcher/src/main.rs
@@ -170,6 +170,7 @@ fn run() -> Result<()> {
 
     print!("\x1B[2J\x1B[H"); // Clear screen and move cursor to top
     println!("\x1B[1m{}\x1B[0m\n", state.tr.launcher_title());
+    println!("{}\n", state.tr.launcher_press_enter_to_install());
 
     ensure_os_supported()?;
 
