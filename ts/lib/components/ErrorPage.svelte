63803e0ff3ec7f8657ec1509011b7041a4e11d8c
medProgAyat <hosseinmehriyari1383@gmail.com>
2025-10-06 19:05:34 +0330

    format using ninja format

--- ts/lib/components/ErrorPage.svelte
+++ ts/lib/components/ErrorPage.svelte
@@ -25,19 +25,19 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 <style lang="scss">
     .error-box {
-    background-color: var(--window-bg);
-    border: 1px solid var(--error-fg);
-    color: var(--fg);
-    padding: 1rem;
-    border-radius: 0.5rem;
-    max-width: 400px;
-    margin: 1rem auto;
-    text-align: center;
-    box-shadow: 0 0 6px rgba(0, 0, 0, 0.15);
-  }
-  .error-text {
-    color: var(--error-fg);
-    font-weight: 600;
-    margin-bottom: 0.75rem;
-  }
+        background-color: var(--window-bg);
+        border: 1px solid var(--error-fg);
+        color: var(--fg);
+        padding: 1rem;
+        border-radius: 0.5rem;
+        max-width: 400px;
+        margin: 1rem auto;
+        text-align: center;
+        box-shadow: 0 0 6px rgba(0, 0, 0, 0.15);
+    }
+    .error-text {
+        color: var(--error-fg);
+        font-weight: 600;
+        margin-bottom: 0.75rem;
+    }
 </style>
