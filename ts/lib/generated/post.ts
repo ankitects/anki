021d0cd968871dfed15840758cb4700483203a96
medProgAyat <hosseinmehriyari1383@gmail.com>
2025-10-07 19:47:27 +0330

    format post.ts to meet ninja check

--- ts/lib/generated/post.ts
+++ ts/lib/generated/post.ts
@@ -19,7 +19,10 @@ export async function postProto<T>(
         return outputType.fromBinary(outputBytes);
     } catch (err) {
         const { alertOnError = true } = options;
-        if (alertOnError && !(err instanceof Error && err.message === "500: Interrupted")) {
+        if (
+            alertOnError &&
+            !(err instanceof Error && err.message === "500: Interrupted")
+        ) {
             alert(err);
         }
         throw err;
@@ -42,7 +45,11 @@ async function postProtoInner(url: string, body: Uint8Array): Promise<Uint8Array
             // ignore
         }
         // it is ugly to show internall server client status to user, we should just show error's to user
-        throw new Error(process.env.NODE_ENV === "production"?`${msg}`:`${result.status}: ${msg}`);
+        throw new Error(
+            process.env.NODE_ENV === "production"
+                ? `${msg}`
+                : `${result.status}: ${msg}`,
+        );
     }
     const blob = await result.blob();
     const respBuf = await new Response(blob).arrayBuffer();
