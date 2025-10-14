2c731f5610de0aa8816389c280e51fc850d43eca
llama <gh@siid.sh>
2025-10-14 09:23:51 +0800

    add to workspace, add helper scripts

---
+++ launcher.bat
@@ -0,0 +1,16 @@
+@echo off
+
+.\tools\ninja.bat node_modules extract:uv ts:generated
+
+mkdir ,\target\debug
+mkdir .\target\release
+
+copy .\.python-version .\target\debug\
+copy .\qt\launcher\versions.py .\target\debug\
+copy .\qt\launcher\pyproject.toml .\target\debug\
+copy .\out\extracted\uv\uv.exe .\target\debug\
+
+copy .\.python-version .\target\release\
+copy .\qt\launcher\versions.py .\target\release\
+copy .\qt\launcher\pyproject.toml .\target\debug\
+copy .\out\extracted\uv\uv.exe .\target\release\
