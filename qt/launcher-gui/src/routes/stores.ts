167551839d1606216a06c8684cc72bff6067e679
llama <gh@siid.sh>
2025-10-18 10:02:28 +0800

    provide tr as store to avoid using {#key ...}

--- qt/launcher-gui/src/routes/stores.ts
+++ qt/launcher-gui/src/routes/stores.ts
@@ -1,6 +1,7 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import * as _tr from "@generated/ftl-launcher";
 import type { GetLangsResponse_Pair, GetMirrorsResponse_Pair, GetVersionsResponse } from "@generated/anki/launcher_pb";
 import { writable } from "svelte/store";
 
@@ -10,3 +11,6 @@ export const mirrorsStore = writable<GetMirrorsResponse_Pair[]>([]);
 export const currentLang = writable("");
 export const initialLang = writable("");
 export const versionsStore = writable<GetVersionsResponse | undefined>(undefined);
+
+export const tr = writable(_tr);
+currentLang.subscribe(() => tr.set(_tr));
