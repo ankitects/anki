b97b8462d9dc1e3e7d80276a480de02bb116d423
llama <gh@siid.sh>
2025-10-18 10:01:38 +0800

    remove getVersions call

--- qt/launcher-gui/src/routes/+page.ts
+++ qt/launcher-gui/src/routes/+page.ts
@@ -1,20 +1,15 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import { getLangs, getMirrors, getOptions, getVersions } from "@generated/backend-launcher";
+import { getLangs, getMirrors, getOptions } from "@generated/backend-launcher";
 import { ModuleName, setupI18n } from "@tslib/i18n";
 import type { PageLoad } from "./$types";
-import { versionsStore } from "./stores";
 
 export const load = (async () => {
     const i18nPromise = setupI18n({ modules: [ModuleName.LAUNCHER] }, true);
     const langsPromise = getLangs({});
     const optionsPromise = getOptions({});
     const mirrorsPromise = getMirrors({});
-
-    getVersions({}).then((res) => {
-        versionsStore.set(res);
-    });
 
     const [_, { userLocale, langs }, options, { mirrors }] = await Promise.all([
         i18nPromise,
