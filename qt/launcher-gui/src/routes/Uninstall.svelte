54d9a19a1cf721cec3a49db9e63db095e68462e6
llama <gh@siid.sh>
2025-10-20 10:03:45 +0800

    add components

---
+++ qt/launcher-gui/src/routes/Uninstall.svelte
@@ -0,0 +1,28 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+
+<script lang="ts">
+    let { footer = $bindable() }: { footer: any } = $props();
+
+    footer = null;
+</script>
+
+<div class="outer">
+    <div id="label">TODO: Uninstall</div>
+</div>
+
+<style lang="scss">
+    .outer {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    #label {
+        font-size: x-large;
+        text-align: center;
+    }
+</style>
