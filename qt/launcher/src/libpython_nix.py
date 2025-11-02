a64aad5e485f7c3a90079e86ce452795a87162ed
llama <gh@siid.sh>
2025-11-02 17:53:09 +0800

    refactor get_libpython_path into get_python_env_info
    instead of just libpython's path we now get and cache the nodot version and venv bin path

--- qt/launcher/src/libpython_nix.py
+++ qt/launcher/src/libpython_nix.py
@@ -1,10 +1,13 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import json
 import os
+import sys
 import sysconfig
 
 cfg = sysconfig.get_config_var
 base = cfg("installed_base") or cfg("installed_platbase")
 lib = cfg("LDLIBRARY") or cfg("INSTSONAME")
-print(os.path.join(base, "lib", lib))
+version = cfg("py_version_nodot")
+print(json.dumps([version, os.path.join(base, "lib", lib), sys.executable]))
--- qt/launcher/src/libpython_win.py
+++ qt/launcher/src/libpython_win.py
@@ -1,10 +1,13 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import json
 import os
+import sys
 import sysconfig
 
 cfg = sysconfig.get_config_var
 base = cfg("installed_base") or cfg("installed_platbase")
-lib = "python" + cfg("py_version_nodot") + ".dll"
-print(os.path.join(base, lib))
+version = cfg("py_version_nodot")
+lib = "python" + version + ".dll"
+print(json.dumps([version, os.path.join(base, lib), sys.executable]))
