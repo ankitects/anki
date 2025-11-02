09766c1b4a8be9d5b5a614f95a80d1bdf9071c83
llama <gh@siid.sh>
2025-11-02 19:24:02 +0800

    sys.executable is the bin we're getting sys.executable from 🤦‍♂️

--- qt/launcher/src/libpython_win.py
+++ qt/launcher/src/libpython_win.py
@@ -1,13 +1,12 @@
 # Copyright: Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import json
 import os
-import sys
 import sysconfig
 
 cfg = sysconfig.get_config_var
 base = cfg("installed_base") or cfg("installed_platbase")
 version = cfg("py_version_nodot")
 lib = "python" + version + ".dll"
-print(json.dumps([version, os.path.join(base, lib), sys.executable]))
+print(version)
+print(os.path.join(base, lib))
