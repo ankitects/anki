a4a1783bc234bdf66ec166edab20b0c70e3b4db4
Luc Mcgrady <lucmcgrady@gmail.com>
2025-11-04 21:13:29 +0000

    Fix: Use small arrow instead of placeholder I tested this on my english windows 10 and seemed to cause no problems.

--- ts/routes/reviewer/reviewer-bottom/More.svelte
+++ ts/routes/reviewer/reviewer-bottom/More.svelte
@@ -37,7 +37,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         }}
         title={tr.actionsShortcutKey({ val: "M" })}
     >
-        {tr.studyingMore()}&#8615
+        {tr.studyingMore()}{"▾"}
     </button>
 
     <div slot="items">
