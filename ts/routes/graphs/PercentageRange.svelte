372d15578fe6e4c547f6207ab45fda76990dc830
Luc Mcgrady <lucmcgrady@gmail.com>
2025-11-08 15:52:30 +0000

    Remove 50% option

--- ts/routes/graphs/PercentageRange.svelte
+++ ts/routes/graphs/PercentageRange.svelte
@@ -15,14 +15,6 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         <input
             type="radio"
             bind:group={range}
-            value={PercentageRangeEnum.Percentile50}
-        />
-        50%
-    </label>
-    <label>
-        <input
-            type="radio"
-            bind:group={range}
             value={PercentageRangeEnum.Percentile95}
         />
         95%
