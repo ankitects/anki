2d790920d389b33dd3f5e92bf2e01700d2cad8f7
Luc Mcgrady <lucmcgrady@gmail.com>
2025-11-08 15:46:22 +0000

    Fix: Typo

--- ts/routes/graphs/PercentageRange.svelte
+++ ts/routes/graphs/PercentageRange.svelte
@@ -15,7 +15,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         <input
             type="radio"
             bind:group={range}
-            value={PercentageRangeEnum.Percentile95}
+            value={PercentageRangeEnum.Percentile50}
         />
         50%
     </label>
