46e3d7c8807a1df1c925266a149bdad8eff4d75d
Elie Carrot <eyh.carrot@proton.me>
2026-01-29 14:18:27 +0100

    Update strikethrough button key combination to Control+Alt+X

--- ts/editor/editor-toolbar/StrikethroughButton.svelte
+++ ts/editor/editor-toolbar/StrikethroughButton.svelte
@@ -23,7 +23,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     {matcher}
     key="strikethrough"
     tooltip={tr.editingStrikethroughText()}
-    keyCombination="Control+Shift+S"
+    keyCombination="Control+Alt+X"
 >
     <Icon icon={strikethroughIcon} />
 </TextAttributeButton>
