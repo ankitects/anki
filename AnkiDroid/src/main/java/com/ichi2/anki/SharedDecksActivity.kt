b09cbd1893ebcd6c091facca724ecae20749f14f
largeblueberry <onlym3480@naver.com>
2026-02-25 20:46:30 +0900

    test: verify login redirection on HTTP 429 using Robolectric

--- AnkiDroid/src/main/java/com/ichi2/anki/SharedDecksActivity.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/SharedDecksActivity.kt
@@ -29,10 +29,10 @@ import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
+import androidx.annotation.VisibleForTesting
 import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
-import com.ichi2.anki.common.annotations.NeedsTest
 import com.ichi2.anki.databinding.ActivitySharedDecksBinding
 import com.ichi2.anki.snackbar.showSnackbar
 import com.ichi2.anki.workarounds.SafeWebViewLayout
@@ -68,143 +68,148 @@ class SharedDecksActivity : AnkiActivity(R.layout.activity_shared_decks) {
      * History should not be cleared before the page finishes loading otherwise there would be
      * an extra entry in the history since the previous page would not get cleared.
      */
-    private val webViewClient =
-        object : WebViewClient() {
-            private var redirectTimes = 0
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    internal inner class SharedDeckWebViewClient : WebViewClient() {
+        private var redirectTimes = 0
 
-            override fun doUpdateVisitedHistory(
-                view: WebView?,
-                url: String?,
-                isReload: Boolean,
-            ) {
-                super.doUpdateVisitedHistory(view, url, isReload)
-                onBackPressedCallback.isEnabled = binding.webView.canGoBack()
-            }
+        override fun doUpdateVisitedHistory(
+            view: WebView?,
+            url: String?,
+            isReload: Boolean,
+        ) {
+            super.doUpdateVisitedHistory(view, url, isReload)
+            onBackPressedCallback.isEnabled = binding.webView.canGoBack()
+        }
 
-            override fun onPageFinished(
-                view: WebView?,
-                url: String?,
-            ) {
-                // Clear history if mShouldHistoryBeCleared is true and set it to false
-                if (shouldHistoryBeCleared) {
-                    binding.webView.clearHistory()
-                    shouldHistoryBeCleared = false
-                }
-                super.onPageFinished(view, url)
-            }
-
-            /**
-             * Prevent the WebView from loading urls which arent needed for importing shared decks.
-             * This is to prevent potential misuse, such as bypassing content restrictions or
-             * using the AnkiDroid WebView as a regular browser to bypass browser blocks,
-             * which could lead to procrastination.
-             */
-            override fun shouldOverrideUrlLoading(
-                view: WebView?,
-                request: WebResourceRequest?,
-            ): Boolean {
-                val host = request?.url?.host
-                if (host != null) {
-                    if (allowedHosts.any { regex -> regex.matches(host) }) {
-                        return super.shouldOverrideUrlLoading(view, request)
-                    }
-                }
-
-                request?.url?.let { super@SharedDecksActivity.openUrl(it) }
-
-                return true
-            }
-
-            private val cookieManager: CookieManager by lazy {
-                CookieManager.getInstance()
-            }
-
-            private val isLoggedInToAnkiWeb: Boolean
-                get() {
-                    try {
-                        // cookies are null after the user logs out, or if the site is first visited
-                        val cookies = cookieManager.getCookie("https://ankiweb.net") ?: return false
-                        // ankiweb currently (2024-09-25) sets two cookies:
-                        // * `ankiweb`, which is base64-encoded JSON
-                        // * `has_auth`, which is 1
-                        return cookies.contains("has_auth=1")
-                    } catch (e: Exception) {
-                        Timber.w(e, "Could not determine login status")
-                        return false
-                    }
-                }
-
-            @NeedsTest("A user is not redirected to login/signup if they are logged in to AnkiWeb")
-            override fun onReceivedHttpError(
-                view: WebView?,
-                request: WebResourceRequest?,
-                errorResponse: WebResourceResponse?,
-            ) {
-                super.onReceivedHttpError(view, request, errorResponse)
-
-                if (errorResponse?.statusCode != HTTP_STATUS_TOO_MANY_REQUESTS) return
-
-                // If a user is logged in, they see: "Daily limit exceeded; please try again tomorrow."
-                // We have nothing we can do here
-                if (isLoggedInToAnkiWeb) return
-
-                // The following cases are handled below:
-                // "Please log in to download more decks." - on clicking "Download"
-                // "Please log in to perform more searches" - on searching
-                redirectUserToSignUpOrLogin()
-            }
-
-            override fun onReceivedError(
-                view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?,
-            ) {
-                // Set mShouldHistoryBeCleared to false if error occurs since it might have been true
+        override fun onPageFinished(
+            view: WebView?,
+            url: String?,
+        ) {
+            // Clear history if mShouldHistoryBeCleared is true and set it to false
+            if (shouldHistoryBeCleared) {
+                binding.webView.clearHistory()
                 shouldHistoryBeCleared = false
-                super.onReceivedError(view, request, error)
+            }
+            super.onPageFinished(view, url)
+        }
+
+        /**
+         * Prevent the WebView from loading urls which arent needed for importing shared decks.
+         * This is to prevent potential misuse, such as bypassing content restrictions or
+         * using the AnkiDroid WebView as a regular browser to bypass browser blocks,
+         * which could lead to procrastination.
+         */
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?,
+        ): Boolean {
+            val host = request?.url?.host
+            if (host != null) {
+                if (allowedHosts.any { regex -> regex.matches(host) }) {
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
             }
 
-            /**
-             * Redirects the user to a login page
-             *
-             * A message is shown informing the user they need to log in to download more decks
-             *
-             * If the user has not logged in **inside AnkiDroid** then the message provides
-             * the user with an action to sign up
-             *
-             * The redirect is not performed if [redirectTimes] is 3 or more
-             */
-            private fun redirectUserToSignUpOrLogin() {
-                // inform the user they need to log in as they've hit a rate limit
-                showSnackbar(R.string.shared_decks_login_required, LENGTH_INDEFINITE) {
-                    if (isLoggedIn()) return@showSnackbar
+            request?.url?.let { super@SharedDecksActivity.openUrl(it) }
 
-                    // If a user is not logged in inside AnkiDroid, assume they have no AnkiWeb account
-                    // and give them the option to sign up
-                    setAction(R.string.sign_up) {
-                        binding.webView.loadUrl(getString(R.string.shared_decks_sign_up_url))
-                    }
+            return true
+        }
+
+        private val cookieManager: CookieManager by lazy {
+            CookieManager.getInstance()
+        }
+
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        internal val isLoggedInToAnkiWeb: Boolean
+            get() {
+                try {
+                    // cookies are null after the user logs out, or if the site is first visited
+                    val cookies = cookieManager.getCookie("https://ankiweb.net") ?: return false
+                    // ankiweb currently (2024-09-25) sets two cookies:
+                    // * `ankiweb`, which is base64-encoded JSON
+                    // * `has_auth`, which is 1
+                    return cookies.contains("has_auth=1")
+                } catch (e: Exception) {
+                    Timber.w(e, "Could not determine login status")
+                    return false
                 }
+            }
 
-                // redirect user to /account/login
-                // TODO: the result of login is typically redirecting the user to their decks
-                // this should be improved
+        override fun onReceivedHttpError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            errorResponse: WebResourceResponse?,
+        ) {
+            super.onReceivedHttpError(view, request, errorResponse)
 
-                if (redirectTimes++ < 3) {
-                    val url = getString(R.string.shared_decks_login_url)
-                    Timber.i("HTTP 429, redirecting to login: '$url'")
-                    binding.webView.loadUrl(url)
-                } else {
-                    // Ensure that we do not have an infinite redirect
-                    Timber.w("HTTP 429 redirect limit exceeded, only displaying message")
+            if (errorResponse?.statusCode != HTTP_STATUS_TOO_MANY_REQUESTS) return
+
+            // If a user is logged in, they see: "Daily limit exceeded; please try again tomorrow."
+            // We have nothing we can do here
+            if (isLoggedInToAnkiWeb) return
+
+            // The following cases are handled below:
+            // "Please log in to download more decks." - on clicking "Download"
+            // "Please log in to perform more searches" - on searching
+            redirectUserToSignUpOrLogin()
+        }
+
+        override fun onReceivedError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            error: WebResourceError?,
+        ) {
+            // Set mShouldHistoryBeCleared to false if error occurs since it might have been true
+            shouldHistoryBeCleared = false
+            super.onReceivedError(view, request, error)
+        }
+
+        /**
+         * Redirects the user to a login page
+         *
+         * A message is shown informing the user they need to log in to download more decks
+         *
+         * If the user has not logged in **inside AnkiDroid** then the message provides
+         * the user with an action to sign up
+         *
+         * The redirect is not performed if [redirectTimes] is 3 or more
+         */
+        private fun redirectUserToSignUpOrLogin() {
+            // inform the user they need to log in as they've hit a rate limit
+            showSnackbar(R.string.shared_decks_login_required, LENGTH_INDEFINITE) {
+                if (isLoggedIn()) return@showSnackbar
+
+                // If a user is not logged in inside AnkiDroid, assume they have no AnkiWeb account
+                // and give them the option to sign up
+                setAction(R.string.sign_up) {
+                    binding.webView.loadUrl(getString(R.string.shared_decks_sign_up_url))
                 }
+            }
+
+            // redirect user to /account/login
+            // TODO: the result of login is typically redirecting the user to their decks
+            // this should be improved
+
+            if (redirectTimes++ < 3) {
+                val url = getString(R.string.shared_decks_login_url)
+                Timber.i("HTTP 429, redirecting to login: '$url'")
+                binding.webView.loadUrl(url)
+            } else {
+                // Ensure that we do not have an infinite redirect
+                Timber.w("HTTP 429 redirect limit exceeded, only displaying message")
             }
         }
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    internal val webViewClient = SharedDeckWebViewClient()
 
     companion object {
         const val SHARED_DECKS_DOWNLOAD_FRAGMENT = "SharedDecksDownloadFragment"
         const val DOWNLOAD_FILE = "DownloadFile"
-        private const val HTTP_STATUS_TOO_MANY_REQUESTS = 429
+
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        const val HTTP_STATUS_TOO_MANY_REQUESTS = 429
     }
 
     // Show WebView with AnkiWeb shared decks with the functionality to capture downloads and import decks.
