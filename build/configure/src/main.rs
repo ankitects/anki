01d318d8508f1d34e6ef18f0c230f225eb0c7609
Abdo <abdo@abdnh.net>
2026-03-19 17:20:26 +0300

    Use a native Python environment on Windows ARM

--- build/configure/src/main.rs
+++ build/configure/src/main.rs
@@ -22,7 +22,6 @@ use ninja_gen::protobuf::check_proto;
 use ninja_gen::protobuf::setup_protoc;
 use ninja_gen::python::setup_uv;
 use ninja_gen::Build;
-use platform::overriden_python_venv_platform;
 use pylib::build_pylib;
 use pylib::check_pylib;
 use python::check_python;
@@ -50,7 +49,7 @@ fn main() -> Result<()> {
     if env::var("OFFLINE_BUILD").is_err() {
         setup_uv(
             build,
-            overriden_python_venv_platform().unwrap_or(build.host_platform),
+            build.host_platform
         )?;
     }
     setup_venv(build)?;
