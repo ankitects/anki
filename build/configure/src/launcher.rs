c5c9e0b87af6c61e95853e0c5004898d57294fde
Abdo <abdo@abdnh.net>
2026-04-01 16:39:49 +0300

    Skip uv_universal action if not macOS
    Fixes a CI failure on Linux ARM where UV_BINARY is defined

--- build/configure/src/launcher.rs
+++ build/configure/src/launcher.rs
@@ -11,7 +11,7 @@ use ninja_gen::inputs;
 use ninja_gen::Build;
 
 pub fn setup_uv_universal(build: &mut Build) -> Result<()> {
-    if !cfg!(target_arch = "aarch64") {
+    if !(cfg!(target_os = "macos") && cfg!(target_arch = "aarch64")) {
         return Ok(());
     }
 
