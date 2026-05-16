bd8508affec34f4133b36522f9e341211d30ab8f
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2026-05-16 09:33:59 -0300

    test: card side selection screenshot test

---
+++ AnkiDroid/src/test/java/com/ichi2/anki/dialogs/CardSideSelectionDialogScreenshotTest.kt
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2026 Brayan Oliveira <brayandso.dev@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+package com.ichi2.anki.dialogs
+
+import androidx.fragment.app.FragmentActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.ScreenshotTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric.buildActivity
+
+@RunWith(AndroidJUnit4::class)
+class CardSideSelectionDialogScreenshotTest : ScreenshotTest() {
+    @Test
+    fun testCardSideSelectionDialogAppearance() {
+        val activity = buildActivity(FragmentActivity::class.java).setup().get()
+        CardSideSelectionDialog.displayInstance(activity) {}
+        captureScreen("dialog")
+    }
+}
