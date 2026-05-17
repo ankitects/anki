ffa8e193ee0bd12d39cc731421761f2df06041bf
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2026-05-17 09:46:35 -0300

    fix(test): avoid diff on collection path pref
    Robolectric generates a different temporary path every time, so avoid creating an unnecessary diff in the collection path pref summary

--- AnkiDroid/src/test/java/com/ichi2/anki/preferences/PreferencesScreenshotTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/preferences/PreferencesScreenshotTest.kt
@@ -2,7 +2,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 package com.ichi2.anki.preferences
 
+import androidx.preference.Preference
 import androidx.test.core.app.ActivityScenario
+import com.ichi2.anki.CollectionHelper
+import com.ichi2.anki.R
 import com.ichi2.anki.ScreenshotTest
 import com.ichi2.anki.settings.Prefs
 import org.junit.Test
@@ -22,7 +25,16 @@ class PreferencesScreenshotTest : ScreenshotTest() {
                 .launch<PreferencesActivity>(
                     PreferencesActivity.getIntent(targetContext, fragmentClass),
                 ).use { scenario ->
-                    scenario.onActivity {
+                    scenario.onActivity { activity ->
+                        val mainFragment = activity!!.fragment as PreferencesFragment
+                        val settingsFragment = mainFragment.childFragmentManager.findFragmentById(R.id.settings_container)
+                        // Robolectric generates a different temporary path every time,
+                        // so avoid creating an unnecessary diff in the collection path pref summary
+                        (settingsFragment as? AdvancedSettingsFragment)?.apply {
+                            requirePreference<Preference>(CollectionHelper.PREF_COLLECTION_PATH).summaryProvider = {
+                                "/storage/emulated/0/AnkiDroid"
+                            }
+                        }
                         captureScreen(fragmentClass.simpleName!!)
                     }
                 }
