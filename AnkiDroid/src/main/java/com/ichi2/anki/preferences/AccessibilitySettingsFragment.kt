f068cecbc58d1af6c7e12c1a0c512faa48a76081
Brayan Oliveira <69634269+brayandso@users.noreply.github.com>
2026-05-18 12:29:26 -0300

    feat: move and rename "Safe display" pref
    to the accessibility settings
    
    note: the "use safer method for drawing cards" part apparently does nothing, based on the current uses of the preference

--- AnkiDroid/src/main/java/com/ichi2/anki/preferences/AccessibilitySettingsFragment.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/preferences/AccessibilitySettingsFragment.kt
@@ -16,8 +16,10 @@
 package com.ichi2.anki.preferences
 
 import androidx.preference.Preference
+import androidx.preference.SwitchPreferenceCompat
 import com.ichi2.anki.R
 import com.ichi2.anki.settings.Prefs
+import com.ichi2.anki.utils.AnimationUtils
 
 /**
  * Fragment with preferences related to notifications
@@ -41,6 +43,12 @@ class AccessibilitySettingsFragment : SettingsFragment() {
             val keyString = getString(key)
             findPreference<Preference>(keyString)?.isVisible = false
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requirePreference<SwitchPreferenceCompat>(R.string.safe_display_key).isEnabled =
+            AnimationUtils.areSystemAnimationsEnabled(requireContext())
     }
 
     companion object {
