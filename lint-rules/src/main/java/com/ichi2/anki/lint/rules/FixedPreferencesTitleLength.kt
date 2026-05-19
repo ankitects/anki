be12b1665a333ca1384476304d8ba8c4b1d93ee0
David Allison <62114487+david-allison@users.noreply.github.com>
2026-05-20 00:35:39 +0100

    docs(lint-rules): convert licenses to SPDX format
    AnkiDroid is standardizing licenses to SPDX.
    
    I removed my copyright lines from the files as-per
    docs/contributing/copyright-headers.md
    
    Issue 20954
    
    This change was not performed using LLMs and I have verified that it is
    correct

--- lint-rules/src/main/java/com/ichi2/anki/lint/rules/FixedPreferencesTitleLength.kt
+++ lint-rules/src/main/java/com/ichi2/anki/lint/rules/FixedPreferencesTitleLength.kt
@@ -1,18 +1,5 @@
-/*
- *  Copyright (c) 2021 Prateek Singh <prateeksingh3212@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free Software
- *  Foundation; either version 3 of the License, or (at your option) any later
- *  version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with
- *  this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: Copyright (c) 2021 Prateek Singh <prateeksingh3212@gmail.com>
 
 @file:Suppress("UnstableApiUsage")
 
@@ -29,7 +16,6 @@ import com.android.tools.lint.detector.api.XmlContext
 import com.android.tools.lint.detector.api.XmlScanner
 import com.ichi2.anki.lint.utils.Constants
 import org.w3c.dom.Element
-import java.lang.IllegalArgumentException
 import java.util.Locale
 
 /**
--- lint-rules/src/main/java/com/ichi2/anki/lint/rules/NonPositionalFormatSubstitutions.kt
+++ lint-rules/src/main/java/com/ichi2/anki/lint/rules/NonPositionalFormatSubstitutions.kt
@@ -1,18 +1,4 @@
-/*
- *  Copyright (c) 2022 David Allison <davidallisongithub@gmail.com>
- *
- *  This program is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free Software
- *  Foundation; either version 3 of the License, or (at your option) any later
- *  version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- *  PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with
- *  this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 @file:Suppress("UnstableApiUsage")
 
@@ -32,7 +18,6 @@ import com.ichi2.anki.lint.utils.StringFormatDetector
 import com.ichi2.anki.lint.utils.childrenSequence
 import org.w3c.dom.Element
 import org.w3c.dom.Node
-import java.lang.IllegalStateException
 
 /**
  * Fix for "Multiple substitutions specified in non-positional format"
