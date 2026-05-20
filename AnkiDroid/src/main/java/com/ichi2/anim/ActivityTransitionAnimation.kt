d49d9b4f77bf474161642acbe2fe35bf19ed58be
cranberry-platypus <cranberryplatypus968@gmail.com>
2026-05-20 15:37:28 +0100

    refactor: remove unused 'getAnimationOptions'
    Prep for issue 14558
    
    Co-authored-by: David Allison <62114487+david-allison@users.noreply.github.com>

--- AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.kt
+++ AnkiDroid/src/main/java/com/ichi2/anim/ActivityTransitionAnimation.kt
@@ -6,7 +6,7 @@ import android.app.Activity
 import android.content.Context
 import android.os.Parcelable
 import android.util.LayoutDirection
-import androidx.core.app.ActivityOptionsCompat
+import com.ichi2.anim.ActivityTransitionAnimation.getInverseTransition
 import com.ichi2.anki.R
 import kotlinx.parcelize.Parcelize
 
@@ -39,48 +39,6 @@ object ActivityTransitionAnimation {
             }
         }
     }
-
-    fun getAnimationOptions(
-        activity: Activity,
-        direction: Direction?,
-    ): ActivityOptionsCompat =
-        when (direction) {
-            Direction.START ->
-                if (isRightToLeft(
-                        activity,
-                    )
-                ) {
-                    ActivityOptionsCompat.makeCustomAnimation(
-                        activity,
-                        R.anim.slide_right_in,
-                        R.anim.slide_right_out,
-                    )
-                } else {
-                    ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_left_in, R.anim.slide_left_out)
-                }
-            Direction.END ->
-                if (isRightToLeft(
-                        activity,
-                    )
-                ) {
-                    ActivityOptionsCompat.makeCustomAnimation(
-                        activity,
-                        R.anim.slide_left_in,
-                        R.anim.slide_left_out,
-                    )
-                } else {
-                    ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_right_in, R.anim.slide_right_out)
-                }
-            Direction.RIGHT -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_right_in, R.anim.slide_right_out)
-            Direction.LEFT -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_left_in, R.anim.slide_left_out)
-            Direction.FADE -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.fade_in, R.anim.fade_out)
-            Direction.UP -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_up_in, R.anim.slide_up_out)
-            Direction.DOWN -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.slide_down_in, R.anim.slide_down_out)
-            Direction.NONE -> ActivityOptionsCompat.makeCustomAnimation(activity, R.anim.none, R.anim.none)
-            Direction.DEFAULT -> // this is the default animation, we shouldn't try to override it
-                ActivityOptionsCompat.makeBasic()
-            else -> ActivityOptionsCompat.makeBasic()
-        }
 
     private fun isRightToLeft(c: Context): Boolean = c.resources.configuration.layoutDirection == LayoutDirection.RTL
 
