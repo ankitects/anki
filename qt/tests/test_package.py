d0307223f5c95b21b1a3bcc32a196ac35658d8da
Abdo <abdo@abdnh.net>
2026-05-26 13:15:23 +0300

    Call is_launcher in is_launcher=False branch

--- qt/tests/test_package.py
+++ qt/tests/test_package.py
@@ -13,14 +13,15 @@ def test_app_user_model_id_set(is_launcher: bool) -> None:
     from pywintypes import com_error
     from win32com.shell import shell
 
+    from aqt.package import _fix_win_taskbar_pinning
+
     if is_launcher:
         os.environ["ANKI_LAUNCHER"] = "testpath"
-
-        from aqt.package import _fix_win_taskbar_pinning
-
         _fix_win_taskbar_pinning()
 
         assert shell.GetCurrentProcessExplicitAppUserModelID() == "Ankitects.Anki"
     else:
+        _fix_win_taskbar_pinning()
+
         with pytest.raises(com_error):
             shell.GetCurrentProcessExplicitAppUserModelID()
