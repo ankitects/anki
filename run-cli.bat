3030e6a26fecbd5354eca700f9699b51c2e850cf
Isaac Flath <Isaac.flath@gmail.com>
2026-06-04 13:20:12 -0400

    Quiet CLI dev runner on success

--- run-cli.bat
+++ run-cli.bat
@@ -5,7 +5,18 @@ set PYTHONPYCACHEPREFIX=out\pycache
 
 @if not defined PYENV set PYENV=out\pyenv
 
-call tools\ninja pylib 1>&2 || exit /b 1
+set "BUILD_LOG=%TEMP%\anki-cli-build-%RANDOM%%RANDOM%.log"
+call tools\ninja pylib > "%BUILD_LOG%" 2>&1
+if errorlevel 1 (
+    type "%BUILD_LOG%" 1>&2
+    del "%BUILD_LOG%" > nul 2>&1
+    popd
+    exit /b 1
+)
+del "%BUILD_LOG%" > nul 2>&1
+
 set "PYTHONPATH=pylib;out\pylib;%PYTHONPATH%"
-%PYENV%\Scripts\python -m anki.cli %* || exit /b 1
+%PYENV%\Scripts\python -m anki.cli %*
+set "EXIT_CODE=%ERRORLEVEL%"
 popd
+exit /b %EXIT_CODE%
