3552bc021040a1d145f7901332b4f1f1ae20fe12
Luc Mcgrady <lucmcgrady@gmail.com>
2026-06-09 16:06:18 +0100

    fix: use double quotes for cog command

--- build/ninja_gen/src/cog.rs
+++ build/ninja_gen/src/cog.rs
@@ -17,7 +17,7 @@ struct CogRender<'a> {
 
 impl BuildAction for CogRender<'_> {
     fn command(&self) -> &str {
-        "$uv run --with cogapp python -m cogapp --markers '<<<cog >>> <<<end>>>' $mode $in"
+        "$uv run --with cogapp python -m cogapp --markers \"<<<cog >>> <<<end>>>\" $mode $in"
     }
 
     fn files(&mut self, build: &mut impl FilesHandle) {
