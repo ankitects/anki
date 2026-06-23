93515be1157e68e89c419e03b534c7eeaf621821
Luc Mcgrady <lucmcgrady@gmail.com>
2026-06-23 01:24:25 +0100

    Front-end mockup

--- ts/routes/preferences/+page.svelte
+++ ts/routes/preferences/+page.svelte
@@ -1,5 +1,44 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
 <script>
-    import "../deck-options/deck-options-base.scss"
+    import "../deck-options/deck-options-base.scss";
+    import Warning from "../deck-options/Warning.svelte";
+    import LabItem from "./LabItem.svelte";
 </script>
 
-Hello Labs
+<div class="container">
+    <Warning
+        warning="These features may change, break, or be removed without notice, use at your own risk."
+    ></Warning>
+
+    <div class="lab-grid">
+        <LabItem
+            title="Svelte note editor"
+            description="Replaces the legacy editor with a new Svelte-based implementation. May affect addon compatibility."
+        ></LabItem>
+        <LabItem title="Example 2"></LabItem>
+        <LabItem
+            title="Example 3"
+            description="This is a long winded description to help demonstrate the tiling of these elements. They should remain in line because I have used the css grid rather than flex boxes for easy use."
+        ></LabItem>
+        <LabItem title="Example 4"></LabItem>
+    </div>
+</div>
+
+<style>
+    .lab-grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 1em;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        margin: 1em;
+        align-items: center;
+    }
+</style>
---
+++ ts/routes/preferences/LabItem.svelte
@@ -0,0 +1,31 @@
+<!--
+Copyright: Ankitects Pty Ltd and contributors
+License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+-->
+<script lang="ts">
+    import Switch from "$lib/components/Switch.svelte";
+
+    export let title: string;
+    export let description = "";
+</script>
+
+<label>
+    <div class="header">
+        <b>{title}</b>
+        <Switch id={title} value={false}></Switch>
+    </div>
+    {description}
+</label>
+
+<style>
+    label {
+        padding: 1em;
+        background-color: var(--bs-secondary-bg);
+        border-radius: 1em;
+    }
+    div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
