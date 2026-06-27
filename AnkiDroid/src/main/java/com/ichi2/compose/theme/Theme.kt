ankidroid/Anki-Android

feat(compose): add Theme bridge and preview infrastructure

AnkiDroid/src/main/java/com/ichi2/compose/theme/Theme.kt
@@ -1,0 +1,90 @@
+// SPDX-FileCopyrightText: 2026 Ashish Yadav <mailtoashish693@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package com.ichi2.compose.theme
+
+import android.content.Context
+import android.content.res.TypedArray
+import androidx.annotation.StyleableRes
+import androidx.annotation.VisibleForTesting
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.withStyledAttributes
+import com.ichi2.anki.R
+
+/**
+ * Wraps [content] in a Material3 [MaterialTheme] populated from the currently
+ * applied AnkiDroid XML theme (light, plain, dark or black).
+ *
+ * Color attributes are read from the host Activity's theme via the
+ * [R.styleable.ComposeTheme] styleable, so a theme change in app settings
+ * (which already triggers Activity.recreate()) flows through automatically.
+ *
+ * For any color slot not defined by the active theme, the Material3 default is used.
+ *
+ * Wrap all top-level Compose content in this composable so screens pick up the
+ * same colors as the rest of the app.
+ */
+@Composable
+fun Theme(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    val colorScheme = remember(context) { context.toMaterial3ColorScheme() }
+    MaterialTheme(colorScheme = colorScheme, content = content)
+}
+
+@VisibleForTesting
+internal fun Context.toMaterial3ColorScheme(): ColorScheme {
+    var scheme: ColorScheme = lightColorScheme()
+    withStyledAttributes(attrs = R.styleable.ComposeTheme) {
+        val isLight = getBoolean(R.styleable.ComposeTheme_isLightTheme, true)
+        val base = if (isLight) lightColorScheme() else darkColorScheme()
+        scheme =
+            base.copy(
+                primary = color(R.styleable.ComposeTheme_colorPrimary, base.primary),
+                onPrimary = color(R.styleable.ComposeTheme_colorOnPrimary, base.onPrimary),
+                // primaryContainer / onPrimaryContainer are intentionally NOT read from
+                // the AnkiDroid XML themes. Those XML values were chosen for legacy
+                // Material Components purposes (switch thumb, old-style FAB icon tint)
+                // and conflict with Material3's semantic (a real container background
+                // for FABs and similar). Falling back to the Material3 base colorScheme
+                // gives the right behavior for Compose components.
+                secondary = color(R.styleable.ComposeTheme_colorSecondary, base.secondary),
+                onSecondary = color(R.styleable.ComposeTheme_colorOnSecondary, base.onSecondary),
+                secondaryContainer = color(R.styleable.ComposeTheme_colorSecondaryContainer, base.secondaryContainer),
+                onSecondaryContainer = color(R.styleable.ComposeTheme_colorOnSecondaryContainer, base.onSecondaryContainer),
+                tertiary = color(R.styleable.ComposeTheme_colorTertiary, base.tertiary),
+                onTertiary = color(R.styleable.ComposeTheme_colorOnTertiary, base.onTertiary),
+                tertiaryContainer = color(R.styleable.ComposeTheme_colorTertiaryContainer, base.tertiaryContainer),
+                onTertiaryContainer = color(R.styleable.ComposeTheme_colorOnTertiaryContainer, base.onTertiaryContainer),
+                error = color(R.styleable.ComposeTheme_colorError, base.error),
+                onError = color(R.styleable.ComposeTheme_colorOnError, base.onError),
+                errorContainer = color(R.styleable.ComposeTheme_colorErrorContainer, base.errorContainer),
+                onErrorContainer = color(R.styleable.ComposeTheme_colorOnErrorContainer, base.onErrorContainer),
+                surface = color(R.styleable.ComposeTheme_colorSurface, base.surface),
+                onSurface = color(R.styleable.ComposeTheme_colorOnSurface, base.onSurface),
+                surfaceVariant = color(R.styleable.ComposeTheme_colorSurfaceVariant, base.surfaceVariant),
+                onSurfaceVariant = color(R.styleable.ComposeTheme_colorOnSurfaceVariant, base.onSurfaceVariant),
+                outline = color(R.styleable.ComposeTheme_colorOutline, base.outline),
+                outlineVariant = color(R.styleable.ComposeTheme_colorOutlineVariant, base.outlineVariant),
+                surfaceContainer = color(R.styleable.ComposeTheme_colorSurfaceContainer, base.surfaceContainer),
+                surfaceContainerHigh = color(R.styleable.ComposeTheme_colorSurfaceContainerHigh, base.surfaceContainerHigh),
+                surfaceContainerHighest = color(R.styleable.ComposeTheme_colorSurfaceContainerHighest, base.surfaceContainerHighest),
+                surfaceContainerLow = color(R.styleable.ComposeTheme_colorSurfaceContainerLow, base.surfaceContainerLow),
+                surfaceContainerLowest = color(R.styleable.ComposeTheme_colorSurfaceContainerLowest, base.surfaceContainerLowest),
+                background = color(R.styleable.ComposeTheme_android_colorBackground, base.background),
+            )
+    }
+    return scheme
+}
+
+private fun TypedArray.color(
+    @StyleableRes index: Int,
+    fallback: Color,
+): Color = if (hasValue(index)) Color(getColor(index, fallback.toArgb())) else fallback

AnkiDroid/src/test/java/com/ichi2/compose/theme/ThemeTest.kt
@@ -1,0 +1,86 @@
+// SPDX-FileCopyrightText: 2026 Ashish Yadav <mailtoashish693@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package com.ichi2.compose.theme
+
+import android.content.Context
+import android.view.ContextThemeWrapper
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
+import com.ichi2.anki.R
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+/**
+ * Verifies that the four AnkiDroid XML themes (light, plain, dark, black) are
+ * correctly bridged into a Material3 [androidx.compose.material3.ColorScheme]
+ * by [toMaterial3ColorScheme].
+ *
+ * The colors expected here are pinned to the values declared in the matching
+ * theme XML under res/values/theme_*.xml. If a theme's primary color is
+ * intentionally changed, the matching assertion here should be updated.
+ */
+@RunWith(RobolectricTestRunner::class)
+class ThemeTest {
+    private val appContext: Context get() = ApplicationProvider.getApplicationContext()
+
+    private fun themed(themeRes: Int) = ContextThemeWrapper(appContext, themeRes)
+
+    @Test
+    fun `light theme bridges colorPrimary to scheme primary`() {
+        val scheme = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val expected = ContextCompat.getColor(appContext, R.color.material_light_blue_500)
+        assertEquals(expected, scheme.primary.toArgb())
+    }
+
+    @Test
+    fun `dark theme bridges colorPrimary to scheme primary`() {
+        val scheme = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val expected = ContextCompat.getColor(appContext, R.color.material_blue_400)
+        assertEquals(expected, scheme.primary.toArgb())
+    }
+
+    @Test
+    fun `plain theme produces a different primary than light theme`() {
+        val light = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val plain = themed(R.style.Theme_Light_Plain).toMaterial3ColorScheme()
+        assertNotEquals(
+            light.primary.toArgb(),
+            plain.primary.toArgb(),
+            "Plain theme should have its own brand color distinct from Light theme",
+        )
+    }
+
+    @Test
+    fun `black theme produces a different surface than dark theme`() {
+        val dark = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val black = themed(R.style.Theme_Dark_Black).toMaterial3ColorScheme()
+        assertNotEquals(
+            dark.surface.toArgb(),
+            black.surface.toArgb(),
+            "Black theme should be visually distinct from Dark theme on surface color",
+        )
+    }
+
+    @Test
+    fun `light theme uses lightColorScheme as base for unspecified slots`() {
+        val scheme = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val lightDefaults = lightColorScheme()
+        // inverseSurface is not overridden by Theme_Light, so it should come from the base
+        assertEquals(lightDefaults.inverseSurface.toArgb(), scheme.inverseSurface.toArgb())
+    }
+
+    @Test
+    fun `dark theme uses darkColorScheme as base for unspecified slots`() {
+        val scheme = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val darkDefaults = darkColorScheme()
+        // inverseSurface is not overridden by Theme_Dark, so it should come from the base
+        assertEquals(darkDefaults.inverseSurface.toArgb(), scheme.inverseSurface.toArgb())
+    }
+}

ankitects/anki

AnkiDroid/src/main/java/com/ichi2/compose/theme/Theme.kt
@@ -1,0 +1,90 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 Ashish Yadav <mailtoashish693@gmail.com>
+
+package com.ichi2.compose.theme
+
+import android.content.Context
+import android.content.res.TypedArray
+import androidx.annotation.StyleableRes
+import androidx.annotation.VisibleForTesting
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.withStyledAttributes
+import com.ichi2.anki.R
+
+/**
+ * Wraps [content] in a Material3 [MaterialTheme] populated from the currently
+ * applied AnkiDroid XML theme (light, plain, dark or black).
+ *
+ * Color attributes are read from the host Activity's theme via the
+ * [R.styleable.ComposeTheme] styleable, so a theme change in app settings
+ * (which already triggers Activity.recreate()) flows through automatically.
+ *
+ * For any color slot not defined by the active theme, the Material3 default is used.
+ *
+ * Wrap all top-level Compose content in this composable so screens pick up the
+ * same colors as the rest of the app.
+ */
+@Composable
+fun AnkiDroidTheme(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    val colorScheme = remember(context) { context.toMaterial3ColorScheme() }
+    MaterialTheme(colorScheme = colorScheme, content = content)
+}
+
+@VisibleForTesting
+internal fun Context.toMaterial3ColorScheme(): ColorScheme {
+    var scheme: ColorScheme = lightColorScheme()
+    withStyledAttributes(attrs = R.styleable.ComposeTheme) {
+        val isLight = getBoolean(R.styleable.ComposeTheme_isLightTheme, true)
+        val base = if (isLight) lightColorScheme() else darkColorScheme()
+        scheme =
+            base.copy(
+                primary = color(R.styleable.ComposeTheme_colorPrimary, base.primary),
+                onPrimary = color(R.styleable.ComposeTheme_colorOnPrimary, base.onPrimary),
+                // primaryContainer / onPrimaryContainer are intentionally NOT read from
+                // the AnkiDroid XML themes. Those XML values were chosen for legacy
+                // Material Components purposes (switch thumb, old-style FAB icon tint)
+                // and conflict with Material3's semantic (a real container background
+                // for FABs and similar). Falling back to the Material3 base colorScheme
+                // gives the right behavior for Compose components.
+                secondary = color(R.styleable.ComposeTheme_colorSecondary, base.secondary),
+                onSecondary = color(R.styleable.ComposeTheme_colorOnSecondary, base.onSecondary),
+                secondaryContainer = color(R.styleable.ComposeTheme_colorSecondaryContainer, base.secondaryContainer),
+                onSecondaryContainer = color(R.styleable.ComposeTheme_colorOnSecondaryContainer, base.onSecondaryContainer),
+                tertiary = color(R.styleable.ComposeTheme_colorTertiary, base.tertiary),
+                onTertiary = color(R.styleable.ComposeTheme_colorOnTertiary, base.onTertiary),
+                tertiaryContainer = color(R.styleable.ComposeTheme_colorTertiaryContainer, base.tertiaryContainer),
+                onTertiaryContainer = color(R.styleable.ComposeTheme_colorOnTertiaryContainer, base.onTertiaryContainer),
+                error = color(R.styleable.ComposeTheme_colorError, base.error),
+                onError = color(R.styleable.ComposeTheme_colorOnError, base.onError),
+                errorContainer = color(R.styleable.ComposeTheme_colorErrorContainer, base.errorContainer),
+                onErrorContainer = color(R.styleable.ComposeTheme_colorOnErrorContainer, base.onErrorContainer),
+                surface = color(R.styleable.ComposeTheme_colorSurface, base.surface),
+                onSurface = color(R.styleable.ComposeTheme_colorOnSurface, base.onSurface),
+                surfaceVariant = color(R.styleable.ComposeTheme_colorSurfaceVariant, base.surfaceVariant),
+                onSurfaceVariant = color(R.styleable.ComposeTheme_colorOnSurfaceVariant, base.onSurfaceVariant),
+                outline = color(R.styleable.ComposeTheme_colorOutline, base.outline),
+                outlineVariant = color(R.styleable.ComposeTheme_colorOutlineVariant, base.outlineVariant),
+                surfaceContainer = color(R.styleable.ComposeTheme_colorSurfaceContainer, base.surfaceContainer),
+                surfaceContainerHigh = color(R.styleable.ComposeTheme_colorSurfaceContainerHigh, base.surfaceContainerHigh),
+                surfaceContainerHighest = color(R.styleable.ComposeTheme_colorSurfaceContainerHighest, base.surfaceContainerHighest),
+                surfaceContainerLow = color(R.styleable.ComposeTheme_colorSurfaceContainerLow, base.surfaceContainerLow),
+                surfaceContainerLowest = color(R.styleable.ComposeTheme_colorSurfaceContainerLowest, base.surfaceContainerLowest),
+                background = color(R.styleable.ComposeTheme_android_colorBackground, base.background),
+            )
+    }
+    return scheme
+}
+
+private fun TypedArray.color(
+    @StyleableRes index: Int,
+    fallback: Color,
+): Color = if (hasValue(index)) Color(getColor(index, fallback.toArgb())) else fallback

AnkiDroid/src/test/java/com/ichi2/compose/theme/ThemeTest.kt
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 Ashish Yadav <mailtoashish693@gmail.com>
+
+package com.ichi2.compose.theme
+
+import android.content.Context
+import android.view.ContextThemeWrapper
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
+import com.ichi2.anki.R
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import com.ichi2.anki.common.android.R as CommonR
+
+/**
+ * Verifies that the four AnkiDroid XML themes (light, plain, dark, black) are
+ * correctly bridged into a Material3 [androidx.compose.material3.ColorScheme]
+ * by [toMaterial3ColorScheme].
+ *
+ * The colors expected here are pinned to the values declared in the matching
+ * theme XML under res/values/theme_*.xml. If a theme's primary color is
+ * intentionally changed, the matching assertion here should be updated.
+ */
+@RunWith(RobolectricTestRunner::class)
+class ThemeTest {
+    private val appContext: Context get() = ApplicationProvider.getApplicationContext()
+
+    private fun themed(themeRes: Int) = ContextThemeWrapper(appContext, themeRes)
+
+    @Test
+    fun `light theme bridges colorPrimary to scheme primary`() {
+        val scheme = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val expected = ContextCompat.getColor(appContext, CommonR.color.material_light_blue_500)
+        assertEquals(expected, scheme.primary.toArgb())
+    }
+
+    @Test
+    fun `dark theme bridges colorPrimary to scheme primary`() {
+        val scheme = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val expected = ContextCompat.getColor(appContext, CommonR.color.material_blue_400)
+        assertEquals(expected, scheme.primary.toArgb())
+    }
+
+    @Test
+    fun `plain theme produces a different primary than light theme`() {
+        val light = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val plain = themed(R.style.Theme_Light_Plain).toMaterial3ColorScheme()
+        assertNotEquals(
+            light.primary.toArgb(),
+            plain.primary.toArgb(),
+            "Plain theme should have its own brand color distinct from Light theme",
+        )
+    }
+
+    @Test
+    fun `black theme produces a different surface than dark theme`() {
+        val dark = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val black = themed(R.style.Theme_Dark_Black).toMaterial3ColorScheme()
+        assertNotEquals(
+            dark.surface.toArgb(),
+            black.surface.toArgb(),
+            "Black theme should be visually distinct from Dark theme on surface color",
+        )
+    }
+
+    @Test
+    fun `light theme uses lightColorScheme as base for unspecified slots`() {
+        val scheme = themed(R.style.Theme_Light).toMaterial3ColorScheme()
+        val lightDefaults = lightColorScheme()
+        // inverseSurface is not overridden by Theme_Light, so it should come from the base
+        assertEquals(lightDefaults.inverseSurface.toArgb(), scheme.inverseSurface.toArgb())
+    }
+
+    @Test
+    fun `dark theme uses darkColorScheme as base for unspecified slots`() {
+        val scheme = themed(R.style.Theme_Dark).toMaterial3ColorScheme()
+        val darkDefaults = darkColorScheme()
+        // inverseSurface is not overridden by Theme_Dark, so it should come from the base
+        assertEquals(darkDefaults.inverseSurface.toArgb(), scheme.inverseSurface.toArgb())
+    }
+}