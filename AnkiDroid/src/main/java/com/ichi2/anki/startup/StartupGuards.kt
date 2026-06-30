david-allison/Anki-Android

fix: handle storage decision undecided
...by redirecting to IntentHandler

This fixes a bug in Reviewer and other entry points (see fixed test),
and hardens/standardizes other collection validation in the app

Assisted-by: Claude Fable 5 - implemented after a long discussion,
comments and naming are mine

AnkiDroid/src/main/java/com/ichi2/anki/startup/StartupGuards.kt
@@ -3,25 +3,83 @@
 package com.ichi2.anki.startup
 
 import android.app.Activity
+import android.content.Intent
+import com.ichi2.anki.CollectionHelper
 import com.ichi2.anki.IntentHandler
 import com.ichi2.anki.exception.SystemStorageException
+import com.ichi2.anki.storage.StorageDecision
 import timber.log.Timber
 
 /**
- * If storage permissions are not granted, shows a toast message and finishes the activity.
+ * Ensures storage is ready for use, finishes the activity is otherwise.
+ *
+ * - the user has decided where the collection is stored
+ * - the app has permission to access
  *
  * This should be called AFTER a call to `super.`[onCreate][Activity.onCreate]
+ *
+ * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
+ * should stop executing
+ *
+ * @throws SystemStorageException if `getExternalFilesDir` returns null
+ */
+fun Activity.ensureStorageReady(): Boolean {
+    // The storage decision is checked first
+    // as the permissions required depend on the chosen folder
+    return ensureStorageConfigured() && ensureStoragePermissions()
+}
+
+/**
+ * If the user has not yet decided where the collection is stored, finishes the
+ * activity and redirects to the app start.
+ *
+ * Catches launches which bypass [IntentHandler]: pinned shortcuts, task restoration
+ * and internal navigation.
+ *
+ * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
+ * should stop executing as storage is not configured
+ *
+ * @see redirectToMainEntryPoint
+ */
+private fun Activity.ensureStorageConfigured(): Boolean {
+    if (CollectionHelper.storageDecision() == StorageDecision.Decided) {
+        return true
+    }
+    Timber.w("finishing activity. Storage is not configured")
+    redirectToMainEntryPoint()
+    return false
+}
+
+/**
+ * If storage permissions are not granted, shows a toast message and finishes the activity.
  *
  * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
  * should stop executing as storage permissions are mot granted
  *
  * @throws SystemStorageException if `getExternalFilesDir` returns null
  */
-fun Activity.ensureStoragePermissions(): Boolean {
+private fun Activity.ensureStoragePermissions(): Boolean {
     if (IntentHandler.grantedStoragePermissions(this, showToast = true)) {
         return true
     }
     Timber.w("finishing activity. No storage permission")
     finish()
     return false
+}
+
+/**
+ * Finishes this activity and opens the primary entry point.
+ *
+ * Use when an entry point cannot run (e.g. storage is not configured), the target screen handles
+ * the failures.
+ */
+fun Activity.redirectToMainEntryPoint() {
+    Timber.i("redirecting to app start")
+    startActivity(
+        Intent(this, IntentHandler::class.java).apply {
+            action = Intent.ACTION_MAIN
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        },
+    )
+    finish()
 }

AnkiDroid/src/test/java/com/ichi2/anki/startup/StartupGuardsTest.kt
@@ -1,0 +1,73 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package com.ichi2.anki.startup
+
+import android.app.Activity
+import android.content.Intent
+import androidx.core.content.edit
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.CollectionHelper
+import com.ichi2.anki.IntentHandler
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.common.preferences.sharedPrefs
+import com.ichi2.anki.storage.StorageDecision
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.Shadows.shadowOf
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@RunWith(AndroidJUnit4::class)
+class StartupGuardsTest : RobolectricTest() {
+    @After
+    fun resetStorageDecision() {
+        CollectionHelper.storageDecisionTestOverride = null
+    }
+
+    private fun buildActivity(): Activity = Robolectric.buildActivity(Activity::class.java).create().get()
+
+    @Test
+    fun `passes when storage is decided and accessible`() {
+        val activity = buildActivity()
+
+        assertTrue(activity.ensureStorageReady())
+
+        assertFalse(activity.isFinishing, "activity should continue to start")
+        assertNull(shadowOf(activity).nextStartedActivity, "no redirect expected")
+    }
+
+    @Test
+    fun `redirects to the main entry point when storage is undecided`() {
+        CollectionHelper.storageDecisionTestOverride = StorageDecision.Undecided
+        val activity = buildActivity()
+
+        assertFalse(activity.ensureStorageReady())
+
+        assertTrue(activity.isFinishing, "activity should finish")
+        val redirect = shadowOf(activity).nextStartedActivity
+        assertNotNull(redirect, "the main entry point should be opened")
+        assertEquals(IntentHandler::class.qualifiedName, redirect.component?.className)
+        assertEquals(Intent.ACTION_MAIN, redirect.action)
+        val expectedFlags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        assertEquals(expectedFlags, redirect.flags and expectedFlags, "task should be cleared")
+    }
+
+    @Test
+    fun `finishes without redirect when a legacy folder is inaccessible`() {
+        // a legacy collection folder requires storage permissions, which tests do not hold
+        targetContext.sharedPrefs().edit {
+            putString(CollectionHelper.PREF_COLLECTION_PATH, "/storage/emulated/0/AnkiDroid")
+        }
+        val activity = buildActivity()
+
+        assertFalse(activity.ensureStorageReady())
+
+        assertTrue(activity.isFinishing, "activity should finish")
+        assertNull(shadowOf(activity).nextStartedActivity, "no redirect expected")
+    }
+}

ankitects/anki

AnkiDroid/src/main/java/com/ichi2/anki/startup/StartupGuards.kt
@@ -3,25 +3,83 @@
 package com.ichi2.anki.startup
 
 import android.app.Activity
+import android.content.Intent
+import com.ichi2.anki.CollectionHelper
 import com.ichi2.anki.IntentHandler
 import com.ichi2.anki.exception.SystemStorageException
+import com.ichi2.anki.storage.StorageDecision
 import timber.log.Timber
 
 /**
- * If storage permissions are not granted, shows a toast message and finishes the activity.
+ * Ensures storage is ready for use, finishes the activity is otherwise.
+ *
+ * - the user has decided where the collection is stored
+ * - the app has permission to access
  *
  * This should be called AFTER a call to `super.`[onCreate][Activity.onCreate]
+ *
+ * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
+ * should stop executing
+ *
+ * @throws SystemStorageException if `getExternalFilesDir` returns null
+ */
+fun Activity.ensureStorageIsReady(): Boolean {
+    // The storage decision is checked first
+    // as the permissions required depend on the chosen folder
+    return ensureStorageIsConfigured() && ensureStoragePermissions()
+}
+
+/**
+ * If the user has not yet decided where the collection is stored, finishes the
+ * activity and redirects to the app start.
+ *
+ * Catches launches which bypass [IntentHandler]: pinned shortcuts, task restoration
+ * and internal navigation.
+ *
+ * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
+ * should stop executing as storage is not configured
+ *
+ * @see redirectToMainEntryPoint
+ */
+private fun Activity.ensureStorageIsConfigured(): Boolean {
+    if (CollectionHelper.storageDecision() == StorageDecision.Decided) {
+        return true
+    }
+    Timber.w("finishing activity. Storage is not configured")
+    redirectToMainEntryPoint()
+    return false
+}
+
+/**
+ * If storage permissions are not granted, shows a toast message and finishes the activity.
  *
  * @return `true`: activity may continue to start, `false`: [onCreate][Activity.onCreate]
  * should stop executing as storage permissions are mot granted
  *
  * @throws SystemStorageException if `getExternalFilesDir` returns null
  */
-fun Activity.ensureStoragePermissions(): Boolean {
+private fun Activity.ensureStoragePermissions(): Boolean {
     if (IntentHandler.grantedStoragePermissions(this, showToast = true)) {
         return true
     }
     Timber.w("finishing activity. No storage permission")
     finish()
     return false
+}
+
+/**
+ * Finishes this activity and opens the primary entry point.
+ *
+ * Use when an entry point cannot run (e.g. storage is not configured), the target screen handles
+ * the failures.
+ */
+fun Activity.redirectToMainEntryPoint() {
+    Timber.i("redirecting to app start")
+    startActivity(
+        Intent(this, IntentHandler::class.java).apply {
+            action = Intent.ACTION_MAIN
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        },
+    )
+    finish()
 }

AnkiDroid/src/test/java/com/ichi2/anki/startup/StartupGuardsTest.kt
@@ -1,0 +1,73 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package com.ichi2.anki.startup
+
+import android.app.Activity
+import android.content.Intent
+import androidx.core.content.edit
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ichi2.anki.CollectionHelper
+import com.ichi2.anki.IntentHandler
+import com.ichi2.anki.RobolectricTest
+import com.ichi2.anki.common.preferences.sharedPrefs
+import com.ichi2.anki.storage.StorageDecision
+import org.junit.After
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.Shadows.shadowOf
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@RunWith(AndroidJUnit4::class)
+class StartupGuardsTest : RobolectricTest() {
+    @After
+    fun resetStorageDecision() {
+        CollectionHelper.storageDecisionTestOverride = null
+    }
+
+    private fun buildActivity(): Activity = Robolectric.buildActivity(Activity::class.java).create().get()
+
+    @Test
+    fun `passes when storage is decided and accessible`() {
+        val activity = buildActivity()
+
+        assertTrue(activity.ensureStorageIsReady())
+
+        assertFalse(activity.isFinishing, "activity should continue to start")
+        assertNull(shadowOf(activity).nextStartedActivity, "no redirect expected")
+    }
+
+    @Test
+    fun `redirects to the main entry point when storage is undecided`() {
+        CollectionHelper.storageDecisionTestOverride = StorageDecision.Undecided
+        val activity = buildActivity()
+
+        assertFalse(activity.ensureStorageIsReady())
+
+        assertTrue(activity.isFinishing, "activity should finish")
+        val redirect = shadowOf(activity).nextStartedActivity
+        assertNotNull(redirect, "the main entry point should be opened")
+        assertEquals(IntentHandler::class.qualifiedName, redirect.component?.className)
+        assertEquals(Intent.ACTION_MAIN, redirect.action)
+        val expectedFlags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        assertEquals(expectedFlags, redirect.flags and expectedFlags, "task should be cleared")
+    }
+
+    @Test
+    fun `finishes without redirect when a legacy folder is inaccessible`() {
+        // a legacy collection folder requires storage permissions, which tests do not hold
+        targetContext.sharedPrefs().edit {
+            putString(CollectionHelper.PREF_COLLECTION_PATH, "/storage/emulated/0/AnkiDroid")
+        }
+        val activity = buildActivity()
+
+        assertFalse(activity.ensureStorageIsReady())
+
+        assertTrue(activity.isFinishing, "activity should finish")
+        assertNull(shadowOf(activity).nextStartedActivity, "no redirect expected")
+    }
+}