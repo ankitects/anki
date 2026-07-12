b046a82e12db78494cbe1110714cf37df2808206
DespicableGoose <68354378+DespicableGoose@users.noreply.github.com>
2026-07-12 20:11:44 +0200

    Implement requested changes
    - Added depends
    - Replaced invalidateAll with invalidate so that only the card-info reloads

--- ts/routes/card-info/[cardId]/[previousId]/+page.svelte
+++ ts/routes/card-info/[cardId]/[previousId]/+page.svelte
@@ -7,7 +7,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
     import CardInfo from "../../CardInfo.svelte";
     import type { PageData } from "./$types";
-    import { goto, invalidateAll } from "$app/navigation";
+    import { goto, invalidate } from "$app/navigation";
 
     export let data: PageData;
 
@@ -18,7 +18,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
     globalThis.anki.updateCardInfos = async (card_id: string): Promise<void> => {
         const path = `/card-info/${card_id}`;
         if (page.params.cardId === card_id && !page.params.previousId) {
-            await invalidateAll();
+            await invalidate("anki:card-info");
         } else {
             await goto(path).catch(() => {
                 window.location.href = path;
