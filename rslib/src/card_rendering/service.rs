57d77574ba291fd877497b9de897868fd31ff3dc
Ashish Yadav <48384865+criticalAY@users.noreply.github.com>
2026-08-04 11:16:45 +0530

    feat: Add {{type:ci:}} modifier for case-insensitive answer checking

--- rslib/src/card_rendering/service.rs
+++ rslib/src/card_rendering/service.rs
@@ -167,7 +167,13 @@ impl crate::services::CardRenderingService for Collection {
         &mut self,
         input: anki_proto::card_rendering::CompareAnswerRequest,
     ) -> Result<generic::String> {
-        Ok(compare_answer(&input.expected, &input.provided, input.combining).into())
+        Ok(compare_answer(
+            &input.expected,
+            &input.provided,
+            input.combining,
+            input.ignore_case,
+        )
+        .into())
     }
 
     fn extract_cloze_for_typing(
