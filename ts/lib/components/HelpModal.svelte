b31c441b3ba4845eb025218aa8635ec2e65b3c38
Abdo <abdo@abdnh.net>
2026-08-05 16:45:32 +0300

    fix: broken OK button in help modals (#5279)
    ## Linked issue
    
    Closes #5276
    
    ## Summary
    
    Fix broken OK button in help modals (e.g. in the Deck Options screen).
    
    ## Steps to reproduce (before)
    
    1. Open the Deck Options screen.
    2. Click on any of the "?" icons.
    3. Click "OK" and notice the modal is not closed.
    
    ## How to test (after)
    
    Confirm the modal is closed.

--- ts/lib/components/HelpModal.svelte
+++ ts/lib/components/HelpModal.svelte
@@ -123,7 +123,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         </Row>
     </div>
     <div slot="footer" class="modal-footer">
-        <button type="button" class="btn btn-primary" on:click={modal.onOkClicked}>
+        <button type="button" class="btn btn-primary" on:click={modal.acceptHandler}>
             {tr.helpOk()}
         </button>
     </div>
