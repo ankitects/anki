5609fb6ca2d134f85977198a9db5153c74fc2e45
Hetan Thakkar <38756320+hetanthakkar@users.noreply.github.com>
2026-09-05 20:57:42 -0700

    Configure browser isolation for SQLite OPFS

---
+++ pwa/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" }
+        ]
+      }
+    ];
+  }
+};
+
+export default nextConfig;
