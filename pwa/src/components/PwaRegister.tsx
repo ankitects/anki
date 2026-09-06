38049705cddd9b75a13b538b078618dd216c2c05
Hetan Thakkar <38756320+hetanthakkar@users.noreply.github.com>
2026-09-05 21:00:41 -0700

    Avoid service worker caching during development

--- pwa/src/components/PwaRegister.tsx
+++ pwa/src/components/PwaRegister.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 export function PwaRegister() {
   useEffect(() => {
+    if (process.env.NODE_ENV !== "production") return;
     if (!("serviceWorker" in navigator)) return;
 
     const register = async () => {
