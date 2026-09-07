a69a68d1c6b97980812c376cc75fab38d6e25315
llama <gh@siid.sh>
2026-09-08 04:53:57 +0800

    fix: fallback to target deck if invalid override

--- rslib/src/notetype/cardgen.rs
+++ rslib/src/notetype/cardgen.rs
@@ -297,7 +297,7 @@ impl Collection {
         cache: &mut CardGenCache,
     ) -> Result<()> {
         for c in cards {
-            let (did, dcid) = self.deck_for_adding(c.did.or(target_deck_id))?;
+            let (did, dcid) = self.deck_for_adding(target_deck_id, c.did)?;
             let due = if let Some(due) = c.due {
                 // use existing due number if provided
                 due
@@ -341,10 +341,14 @@ impl Collection {
         }
     }
 
-    /// If deck ID does not exist or points to a filtered deck, fall back on
-    /// default.
-    fn deck_for_adding(&mut self, did: Option<DeckId>) -> Result<(DeckId, DeckConfigId)> {
-        if let Some(did) = did {
+    /// If deck ID or overriding deck id both don't exist or point to filtered
+    /// decks, fall back on default.
+    fn deck_for_adding(
+        &mut self,
+        did: Option<DeckId>,
+        did_override: Option<DeckId>,
+    ) -> Result<(DeckId, DeckConfigId)> {
+        for did in did_override.into_iter().chain(did) {
             if let Some(deck) = self.deck_conf_if_normal(did)? {
                 return Ok(deck);
             }
