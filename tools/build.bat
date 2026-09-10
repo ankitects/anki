a287d18ff2df2ec950e2eac3c8df0cd9f21c25e5
Michael McDonald <mcdmp@outlook.com>
2026-09-10 11:00:46 -0700

    fix: route legacy wheel builders through the current recipe

--- tools/build.bat
+++ tools/build.bat
@@ -1,7 +1,6 @@
 @echo off
 pushd "%~dp0"\..
-if exist out\wheels rmdir /s /q out\wheels
 set RELEASE=2
-tools\ninja wheels || exit /b 1
+just wheels || exit /b 1
 echo wheels are in out/wheels
 popd
