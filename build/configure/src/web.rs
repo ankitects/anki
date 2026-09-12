07586ccdf787f7b49a1ee84321aa56e0f3ff1233
Abdo <abdo@abdnh.net>
2026-09-12 11:36:01 +0300

    Remove moved folders from check:vitest's dependencies

--- build/configure/src/web.rs
+++ build/configure/src/web.rs
@@ -346,9 +346,7 @@ fn check_web(build: &mut Build) -> Result<()> {
                 ":node_modules",
                 ":ts:generated",
                 glob!["ts/{svelte.config.js,vite.config.ts,tsconfig.json}"],
-                glob![
-                    "ts/{lib,routes,deck-options,html-filter,domlib,reviewer,change-notetype}/**/*"
-                ],
+                glob!["ts/{lib,routes,reviewer}/**/*"],
             ],
         },
     )?;
