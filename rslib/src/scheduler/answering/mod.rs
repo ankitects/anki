2f26bfe15fbcc6906fa9604bc5a209b5be79bb06
Luc Mcgrady <lucmcgrady@gmail.com>
2026-09-14 16:13:37 +0100

    ./check

--- rslib/src/scheduler/answering/mod.rs
+++ rslib/src/scheduler/answering/mod.rs
@@ -483,9 +483,7 @@ impl Collection {
 
         let desired_retention = home_deck.effective_desired_retention(&config);
         let fsrs_enabled = self.fsrs_enabled();
-        dbg!("Here2");
         let fsrs_next_states = if fsrs_enabled {
-            dbg!("Here1");
             let params = config.fsrs_params();
             let fsrs = FSRS::new(params)?;
             card.decay = Some(get_decay_from_params(params));
@@ -745,7 +743,7 @@ pub(crate) mod test {
 
         Ok(())
     }
-    
+
     #[test]
     fn get_scheduling_states_inner_fills_missing_fsrs_fields_on_returned_card() -> Result<()> {
         let mut col = Collection::new();
@@ -770,7 +768,6 @@ pub(crate) mod test {
         let (_, returned) = col.get_scheduling_states_inner(input)?;
         assert!(returned.memory_state.is_some());
 
-        // No need to update the memory state in the database, since it will be recalculated on the next answer.
         let stored = col.storage.get_card(card_id)?.unwrap();
         assert!(stored.memory_state.is_none());
 
