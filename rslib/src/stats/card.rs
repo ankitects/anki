029c80aed18bee765abc93b0194bc2e747fe9fe6
Luc Mcgrady <lucmcgrady@gmail.com>
2026-09-21 15:17:09 +0100

    refactor: use col.fsrs_enabled in more places (#5651)
    closes #5652
    
    `col.fsrs_enabled` should be usable in any place where we have access to
    the collection (And that doesn't involve building the queue or modifying
    the config).
    
    c.c. @user1823

--- rslib/src/stats/card.rs
+++ rslib/src/stats/card.rs
@@ -30,7 +30,7 @@ impl Collection {
 
         let (average_secs, total_secs) = average_and_total_secs_strings(&revlog);
         let timing = self.timing_today()?;
-        let fsrs_enabled = card.desired_retention.is_some();
+        let fsrs_enabled = self.fsrs_enabled();
 
         let last_review_time = if let Some(last_review_time) = card.last_review_time {
             last_review_time
@@ -272,7 +272,7 @@ mod test {
         let mut card = col.storage.get_card(cid)?.unwrap();
         assert!(card.memory_state.is_some());
 
-        card.memory_state = None;
+        card.clear_fsrs_data();
         col.storage.update_card(&card)?;
 
         let card = col.storage.get_card(cid)?.unwrap();
